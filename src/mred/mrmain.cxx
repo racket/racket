487326671499addf13a116aac997694207e4ce5a
Matthew Flatt <mflatt@racket-lang.org>
2006-06-08 10:01:50 +0000

    attempt to fix manifest problem with MSVC 8.0
    svn: r3275

--- src/mred/mrmain.cxx
+++ src/mred/mrmain.cxx
@@ -646,4 +646,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR ignored
 END_XFORM_SKIP;
 # endif
 
+#if _MSC_VER >= 1400
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
 #endif
