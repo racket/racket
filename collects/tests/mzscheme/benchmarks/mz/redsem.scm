204bc48a50c8ce354aac2a92dd03ad3034a9da15
Robby Findler <robby@racket-lang.org>
2006-09-04 01:55:59 +0000

    set things up to remove the cache
    svn: r4234

--- collects/tests/mzscheme/benchmarks/mz/redsem.scm
+++ collects/tests/mzscheme/benchmarks/mz/redsem.scm
@@ -1,7 +1,20 @@
-(require (planet "beginner.ss" ("robby" "redex.plt") "examples"))
+#|
+
+Time the cache-less version of the pattern matcher
+under the theory that if this speeds up, the cache
+can be used less, leading to less memory use, and
+better performance.
+
+(and I'm pretty sure that measuing the cache isn't
+super useful.)
+
+Robby
+
+|#
+
+(require (planet "reduction-semantics.ss" ("robby" "redex.plt" 2 4)))
+(set-cache-size! #f)
+(require (planet "beginner.ss" ("robby" "redex.plt" 2 4) "examples"))
 (collect-garbage)
 (printf "Now\n")
-(time (begin 
-	(run-tests) (run-tests) (run-tests) (run-tests) (run-tests)
-	(run-tests) (run-tests) (run-tests) (run-tests) (run-tests)
-	))
+(time (begin (run-tests) (run-tests) (run-tests)))
