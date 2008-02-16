e7a35198a47a2a3f68a349fe641e967d26feba72
Eli Barzilay <eli@racket-lang.org>
2008-02-16 10:58:25 +0000

    version conventions
    svn: r8688

--- src/mzscheme/src/schvers.h
+++ src/mzscheme/src/schvers.h
@@ -3,12 +3,15 @@
       X.Y
       X.Y.Z     Z != 0
       X.Y.Z.W   W != 0
-   where each X, Y, Z, W is a non-negative exact
-   integer, Y must not exceed 99, and Z must not
-   exceed 999. 
+   where each X, Y, Z, W is a non-negative exact integer, Y must not
+   exceed 99, and Z must not exceed 999.  Y>=90 means that this is
+   working towards {X+1}.0, and X.Y (Z=0, W=0) is an alpha version for
+   {X+1}.0; Z>=900 means working towards X.{Y+1}, and X.Y.Z as an
+   alpha release.
 
-   The string and the separate X/Y/Z/W numbers must
-   be updated consistently. */
+   (The string and the separate X/Y/Z/W numbers must be updated
+   consistently.)
+*/
 
 #define MZSCHEME_VERSION "3.99.0.12"
 
