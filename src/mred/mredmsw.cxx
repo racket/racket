a1cc8d30a1e4e0f330002eacdd087c4114b56325
Matthew Flatt <mflatt@racket-lang.org>
2009-04-08 15:40:21 +0000

    fix MrEd Windows handle-block problem (PR 10183)
    svn: r14467

--- src/mred/mredmsw.cxx
+++ src/mred/mredmsw.cxx
@@ -931,6 +931,8 @@ void MrEdMSWSleep(float secs, void *fds)
       if (rps[result])
         ReleaseSemaphore(handles[result], 1, NULL);
     }
+
+    scheme_collapse_win_fd(fds); /* cleans up */
   } else if (wxTheApp->keep_going) {
     MsgWaitForMultipleObjects(0, NULL, FALSE,
 			      secs ? msecs : INFINITE,
