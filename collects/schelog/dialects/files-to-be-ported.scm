9f87f58a559caebf0ac009a47f8fb887c5bcb0cd
John Clements <clements@racket-lang.org>
2010-04-22 10:20:02 -0700

    Looks clean.  Ready to merge.

--- collects/schelog/dialects/files-to-be-ported.scm
+++
@@ -1,4 +0,0 @@
-#lang racket
-
-#|schelog.scm
-|#
--- collects/schelog/dialects/gambit-schelog.scm
+++
@@ -1,6 +0,0 @@
-(declare (standard-bindings) (extended-bindings) (block) (not safe))
-
-;if all your arithmetic is going to be fixnum-only,
-;you might want to
-
-;(declare (fixnum))
