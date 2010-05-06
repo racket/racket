2435f6fe061d0b539d98ba6e3f5bb02a1dae162b
Vincent St-Amour <stamourv@ccs.neu.edu>
2010-05-06 11:59:45 -0400

    Changed the common benchmark wrapper to use the racket language and to print return values.

--- collects/tests/racket/benchmarks/common/wrap.rkt
+++ collects/tests/racket/benchmarks/common/wrap.rkt
@@ -1,8 +1,8 @@
 
-(module wrap mzscheme
-  (provide (rename module-begin #%module-begin))
+(module wrap racket
+  (provide (rename-out (module-begin #%module-begin)))
   (require (lib "include.ss"))
   (define-syntax (module-begin stx)
     (let ([name (syntax-property stx 'enclosing-module-name)])
-      #`(#%plain-module-begin 
+      #`(#%module-begin 
          (include #,(format "~a.sch" name))))))
