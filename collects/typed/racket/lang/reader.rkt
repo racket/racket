820040abc14381dc4b2ad7e4f11bcadbd821e089
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-05-09 11:44:38 -0400

    add typed/racket and typed/racket/base

---
+++ collects/typed/racket/lang/reader.rkt
@@ -0,0 +1,18 @@
+#lang s-exp syntax/module-reader
+
+typed/racket
+
+#:read r:read
+#:read-syntax r:read-syntax
+#:info make-info
+#:language-info make-language-info
+
+(define (make-info key default use-default)
+  (case key
+    [else (use-default key default)]))
+
+(define make-language-info
+  `#(typed-scheme/language-info get-info ()))
+
+
+(require (prefix-in r: typed-scheme/typed-reader))
---
+++ collects/typed/racket/system.rkt
@@ -0,0 +1,1 @@
+#lang s-exp typed/private/wrap racket/system
