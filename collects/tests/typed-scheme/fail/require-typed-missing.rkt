27e971dce20634fa6be551fecd64e639fbd909aa
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-05-24 18:45:16 -0700

    Check that at least one require spec is provided.  Closes PR 10617.  Merge to v5.0.

---
+++ collects/tests/typed-scheme/fail/require-typed-missing.rkt
@@ -0,0 +1,16 @@
+#;
+(exn-pred "at least one")
+#lang typed/racket
+
+
+(require/typed  (make-main (([Listof Node] [Listof Edge] -> Graph)
+                           (State Number Number MouseEvent -> State)
+                           (State KeyEvent -> State)
+                           (State -> Scene)
+                           (Any -> Boolean)
+                           (State -> Boolean)
+                           (Stop -> Graph)
+                           (Any -> Edge)
+                           (Edge -> Graph)
+                           ->
+                           (Boolean -> Graph))))
