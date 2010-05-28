e9f8cc9aca6a67a9b56f0ba9dd202f0fa2669ab6
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-05-28 15:27:49 -0400

    Refactor test harness.

--- collects/tests/typed-scheme/run.rkt
+++ collects/tests/typed-scheme/run.rkt
@@ -1,7 +1,15 @@
-#lang racket/base
-(require racket/vector)
+#lang racket
+(require racket/vector racket/gui/dynamic)
 
 (require "main.ss")
 (current-namespace (make-base-namespace))
-(unless (= 0 (go/text (vector-member "unit" (current-command-line-arguments))))
+(define exec (make-parameter go/text))
+(define unit-only? (make-parameter #f))
+(command-line
+ #:once-each
+ ["--unit" "run just the unit tests" (unit-only? #t)]
+ ["--gui" "run using the gui" 
+  (current-namespace ((gui-dynamic-require 'make-gui-namespace)))
+  (exec go)])
+(unless (= 0 ((exec) (unit-only?)))
   (error "Typed Scheme Tests did not pass."))
