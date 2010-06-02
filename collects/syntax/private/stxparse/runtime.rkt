7fc786256a256f6d8cb2eee49cc62ef11706f5c9
Ryan Culpepper <ryanc@racket-lang.org>
2010-06-02 17:19:12 -0600

    syntax/parse: fixed bug in litsets at higher phases (cherry picked from commit b2196cc5953a8d0a54252b321fbf4d2d5d110720)

--- collects/syntax/private/stxparse/runtime.rkt
+++ collects/syntax/private/stxparse/runtime.rkt
@@ -601,12 +601,8 @@ An Expectation is one of
 
 ;; 
 
-(provide phase+
-         check-literal
+(provide check-literal
          free-identifier=?/phases)
-
-(define (phase+ a b)
-  (and (number? a) (number? b) (+ a b)))
 
 ;; check-literal : id phase-level stx -> void
 ;; FIXME: change to normal 'error', if src gets stripped away
@@ -621,23 +617,22 @@ An Expectation is one of
 ;; that y has at phase-level y.
 ;; At least one of the identifiers MUST have a binding (module or lexical)
 (define (free-identifier=?/phases x phase-x y phase-y)
-  (let ([base-phase (syntax-local-phase-level)])
-    (let ([bx (identifier-binding x (phase+ base-phase phase-x))]
-          [by (identifier-binding y (phase+ base-phase phase-y))])
-      (cond [(and (list? bx) (list? by))
-             (let ([modx (module-path-index-resolve (first bx))]
-                   [namex (second bx)]
-                   [phasex (fifth bx)]
-                   [mody (module-path-index-resolve (first by))]
-                   [namey (second by)]
-                   [phasey (fifth by)])
-               (and (eq? modx mody) ;; resolved-module-paths are interned
-                    (eq? namex namey)
-                    (equal? phasex phasey)))]
-            [else
-             ;; One must be lexical (can't be #f, since one must be bound)
-             ;; lexically-bound names bound in only one phase; just compare
-             (free-identifier=? x y)]))))
+  (let ([bx (identifier-binding x phase-x)]
+        [by (identifier-binding y phase-y)])
+    (cond [(and (list? bx) (list? by))
+           (let ([modx (module-path-index-resolve (first bx))]
+                 [namex (second bx)]
+                 [phasex (fifth bx)]
+                 [mody (module-path-index-resolve (first by))]
+                 [namey (second by)]
+                 [phasey (fifth by)])
+             (and (eq? modx mody) ;; resolved-module-paths are interned
+                  (eq? namex namey)
+                  (equal? phasex phasey)))]
+          [else
+           ;; One must be lexical (can't be #f, since one must be bound)
+           ;; lexically-bound names bound in only one phase; just compare
+           (free-identifier=? x y)])))
 
 ;; ----
 
