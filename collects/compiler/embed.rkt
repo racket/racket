ccfa9c254fbc1df0affe47e0841d9c28cac507aa
Robby Findler <robby@racket-lang.org>
2010-06-05 11:06:31 -0500

    fixed the contract for create-embedding-executable (and changed the file to the racket/base language)

--- collects/compiler/embed.rkt
+++ collects/compiler/embed.rkt
@@ -1,55 +1,55 @@
+#lang scheme/base
+(require scheme/unit
+         scheme/contract)
 
-(module embed mzscheme
-  (require mzlib/unit
-           mzlib/contract)
-  
-  (require "sig.ss")
-  
-  (require "embed-unit.ss"
-	   "embed-sig.ss")
-  
-  (define-values/invoke-unit/infer compiler:embed@)
-  
-  (provide/contract [make-embedding-executable
-                     (opt-> (path-string?
-                             any/c
-                             any/c
-                             (listof (list/c (or/c boolean? symbol?) any/c))
-                             (listof path-string?)
-                             any/c
-                             (listof string?))
-                            ((listof (cons/c symbol? any/c))
-                             any/c
-                             symbol?)
-                            void?)]
-                    [create-embedding-executable
-                     (->* (path-string?)
-                          (#:modules 
-                           (lisof (list/c (or/c symbol? #f #t)
-                                          module-path?))
-                           #:configure-via-first-module? any/c	 
-                           #:literal-files (listof path-string)	 
-                           #:literal-expression any/c	 
-                           #:literal-expressions (listof any/c)	 
-                           #:cmdline (listof string?)
-                           #:gracket? any/c
-                           #:mred? any/c	 
-                           #:variant (or/c '3m 'cgc)	 
-                           #:aux (listof (cons/c symbol? any/c))
-                           #:collects-path (or/c #f
-                                                 path-string?
-                                                 (listof path-string?))
-                           #:launcher? any/c	 
-                           #:verbose? any/c	 
-                           #:compiler (-> any/c compiled-expression?)	 
-                           #:expand-namespace namespace?
-                           #:src-filter (-> path? any)
-                           #:on-extension (or/c #f (-> path-string? boolean? any))	 
-                           #:get-extra-imports (-> path? compiled-module? (listof module-path?)))
-                          void?)])
-  (provide write-module-bundle
-           embedding-executable-is-directory?
-           embedding-executable-is-actually-directory?
-           embedding-executable-put-file-extension+style+filters
-	   embedding-executable-add-suffix))
+(require "sig.ss")
+
+(require "embed-unit.ss"
+         "embed-sig.ss")
+
+(define-values/invoke-unit/infer compiler:embed@)
+
+(provide/contract [make-embedding-executable
+                   (->* (path-string?
+                         any/c
+                         any/c
+                         (listof (list/c (or/c boolean? symbol?) any/c))
+                         (listof path-string?)
+                         any/c
+                         (listof string?))
+                        ((listof (cons/c symbol? any/c))
+                         any/c
+                         symbol?)
+                        void?)]
+                  [create-embedding-executable
+                   (->* (path-string?)
+                        (#:modules 
+                         (listof (list/c (or/c symbol? #f #t)
+                                         (or/c path? module-path?)))
+                         #:configure-via-first-module? any/c	 
+                         #:literal-files (listof path-string?)	 
+                         #:literal-expression any/c	 
+                         #:literal-expressions (listof any/c)	 
+                         #:cmdline (listof string?)
+                         #:gracket? any/c
+                         #:mred? any/c	 
+                         #:variant (or/c '3m 'cgc)	 
+                         #:aux (listof (cons/c symbol? any/c))
+                         #:collects-path (or/c #f
+                                               path-string?
+                                               (listof path-string?))
+                         #:launcher? any/c	 
+                         #:verbose? any/c	 
+                         #:compiler (-> any/c compiled-expression?)	 
+                         #:expand-namespace namespace?
+                         #:src-filter (-> path? any)
+                         #:on-extension (or/c #f (-> path-string? boolean? any))	 
+                         #:get-extra-imports (-> path? compiled-module-expression? (listof module-path?)))
+                        void?)])
+
+(provide write-module-bundle
+         embedding-executable-is-directory?
+         embedding-executable-is-actually-directory?
+         embedding-executable-put-file-extension+style+filters
+         embedding-executable-add-suffix)
 
