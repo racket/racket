db0f73bc4f381fbd9e205f60a471b3db92e86c25
Robby Findler <robby@racket-lang.org>
2010-06-05 08:19:12 -0500

    added contract to create-embedding-executable

--- collects/compiler/embed.rkt
+++ collects/compiler/embed.rkt
@@ -21,9 +21,33 @@
                             ((listof (cons/c symbol? any/c))
                              any/c
                              symbol?)
-                            void?)])
+                            void?)]
+                    [create-embedding-executable
+                     (->* (path-string?)
+                          (#:modules 
+                           (lisof (list/c (or/c symbol? #f #t)
+                                          module-path?))
+                           #:configure-via-first-module? any/c	 
+                           #:literal-files (listof path-string)	 
+                           #:literal-expression any/c	 
+                           #:literal-expressions (listof any/c)	 
+                           #:cmdline (listof string?)
+                           #:gracket? any/c
+                           #:mred? any/c	 
+                           #:variant (or/c '3m 'cgc)	 
+                           #:aux (listof (cons/c symbol? any/c))
+                           #:collects-path (or/c #f
+                                                 path-string?
+                                                 (listof path-string?))
+                           #:launcher? any/c	 
+                           #:verbose? any/c	 
+                           #:compiler (-> any/c compiled-expression?)	 
+                           #:expand-namespace namespace?
+                           #:src-filter (-> path? any)
+                           #:on-extension (or/c #f (-> path-string? boolean? any))	 
+                           #:get-extra-imports (-> path? compiled-module? (listof module-path?)))
+                          void?)])
   (provide write-module-bundle
-	   create-embedding-executable
            embedding-executable-is-directory?
            embedding-executable-is-actually-directory?
            embedding-executable-put-file-extension+style+filters
