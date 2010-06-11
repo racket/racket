b866ea4de69729b85375e64769b98995d7c3a780
Eli Barzilay <eli@barzilay.org>
2010-06-11 15:55:00 -0400

    README for the planet stub.

--- collects/meta/web/stubs/planet.rkt
+++ collects/meta/web/stubs/planet.rkt
@@ -8,3 +8,10 @@
         #:extra-headers "{{{HEAD}}}"]{
     @; This is a stub page to get the header for planet
     {{{BODY}}}})
+
+(define readme
+  @plain[#:file "README"]{
+    This directory contains files that are placed here automatically by
+    the web build process.  The HTML file contains holes to be filled in
+    for the title, header, and body, and the other files are resources
+    that are used by the template.})
