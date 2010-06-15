8dfd7d87ecd5d167514f8c7c5468448c6ad86547
Vincent St-Amour <stamourv@racket-lang.org>
2010-06-15 12:27:28 -0400

    Reimplemented assert as a macro.

--- collects/typed-scheme/private/extra-procs.rkt
+++ collects/typed-scheme/private/extra-procs.rkt
@@ -1,8 +1,10 @@
 #lang scheme/base
 (provide assert)
 
-(define (assert v [pred values])
-  (unless (pred v)
-    (error "Assertion failed"))
-  v)
-
+(define-syntax assert
+  (syntax-rules ()
+    ((assert v)
+     (or v (error "Assertion failed")))
+    ((assert v pred)
+     (let ((val v))
+       (if (pred val) val (error "Assertion failed"))))))
