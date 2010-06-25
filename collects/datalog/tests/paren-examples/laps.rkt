8636c4ee362add3bad7437b5a062bbb5bd210cc1
Jay McCarthy <jay@racket-lang.org>
2010-06-25 15:29:48 -0600

    Paren examples

--- collects/datalog/tests/paren-examples/laps.rkt
+++ collects/datalog/tests/paren-examples/laps.rkt
@@ -1,12 +1,13 @@
 #lang datalog/sexp
-% Laps Test
-contains(ca, store, rams_couch, rams).
-contains(rams, fetch, rams_couch, will).
-contains(ca, fetch, Name, Watcher) :-
-    contains(ca, store, Name, Owner),
-    contains(Owner, fetch, Name, Watcher).
-trusted(ca).
-permit(User, Priv, Name) :-
-    contains(Auth, Priv, Name, User),
-    trusted(Auth).
-permit(User, Priv, Name)?
+; Laps Test
+(! (contains ca store rams_couch rams))
+(! (contains rams fetch rams_couch will))
+(:- (contains ca ,Fetch ,Name ,Watcher)
+    (contains ca store ,Name ,Owner)
+    (contains ,Owner fetch ,Name ,Watcher))
+(! (trusted ca))
+(:- (permit ,User ,Priv ,Name)
+    (contains ,Auth ,Priv ,Name ,User)
+    (trusted ,Auth))
+(? (permit ,User ,Priv ,Name))
+
