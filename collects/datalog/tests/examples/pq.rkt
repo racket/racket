30e3cd10719a7f2c694d30ec59acb4ba868fdd66
Jay McCarthy <jay@racket-lang.org>
2010-06-25 22:00:14 -0600

    Moving tests

--- collects/datalog/tests/examples/pq.rkt
+++
@@ -1,6 +0,0 @@
-#lang datalog
-% p q test from Chen & Warren
-q(X) :- p(X).
-q(a).
-p(X) :- q(X).
-q(X)?
--- collects/datalog/tests/paren-examples/pq.rkt
+++
@@ -1,8 +0,0 @@
-#lang datalog/sexp
-; p q test from Chen & Warren
-(! (:- (q ,X)
-       (p ,X)))
-(! (q a))
-(! (:- (p ,X)
-       (q ,X)))
-(? (q ,X))
