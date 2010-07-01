3d347f117b9ba648536094eb9e0bf1ab1653ea76
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-07-01 13:47:02 -0400

    Add --just flag for running tests.

--- collects/tests/typed-scheme/run.rkt
+++ collects/tests/typed-scheme/run.rkt
@@ -4,22 +4,23 @@
 (require "main.ss")
 
 (define exec (make-parameter go/text))
-(define unit-only? (make-parameter #f))
-(define int-only? (make-parameter #f))
+(define the-tests (make-parameter tests))
 (define skip-all? #f)
 (current-namespace (make-base-namespace))
 (command-line
  #:once-each
- ["--unit" "run just the unit tests" (unit-only? #t)]
- ["--int" "run just the integration tests" (int-only? #t)]
+ ["--unit" "run just the unit tests" (the-tests unit-tests)]
+ ["--int" "run just the integration tests" (the-tests int-tests)]
  ["--nightly" "for the nightly builds" (when (eq? 'cgc (system-type 'gc))
                                          (set! skip-all? #t))]
+ ["--just" path "run only this test" (the-tests (just-one path))]
  ["--gui" "run using the gui" 
           (if (gui-available?)
               (begin (exec go))
-              (error "GUI not available"))])
+              (error "GUI not available"))]
+ )
 
 (if skip-all?
     (printf "Skipping Typed Racket tests.\n")
-    (unless (= 0 ((exec) (unit-only?) (int-only?)))
+    (unless (= 0 ((exec) (the-tests)))
       (error "Typed Racket Tests did not pass.")))
