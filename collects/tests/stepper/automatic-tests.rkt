de94854243e2dea67d75c17eb57d2cf53311db1b
Mike Sperber <sperber@deinprogramm.de>
2010-07-14 14:07:13 +0200

    Unbreak stepper tests.
    ... via a spectacularly ugly hack.

--- collects/tests/stepper/automatic-tests.rkt
+++ collects/tests/stepper/automatic-tests.rkt
@@ -3,9 +3,12 @@
 (require "through-tests.ss" 
          "test-engine.ss")
 
-(parameterize ([display-only-errors #t]
-               [current-output-port (open-output-string)]
-               [current-namespace (make-base-namespace)])
-  (if (run-all-tests-except '(bad-and bad-cons check-error begin-let-bug prims qq-splice time set! local-set! lazy1 lazy2 lazy3))
-      (exit 0)
-      (exit 1)))
+(let ((outer-namespace (current-namespace)))
+  (parameterize ([display-only-errors #t]
+		 [current-output-port (open-output-string)]
+		 [current-namespace (make-base-namespace)])
+    ;; make sure the tests' print-convert sees the teaching languages' properties
+    (namespace-attach-module outer-namespace 'mzlib/pconvert-prop (current-namespace))
+    (if (run-all-tests-except '(bad-and bad-cons check-error begin-let-bug prims qq-splice time set! local-set! lazy1 lazy2 lazy3))
+	(exit 0)
+	(exit 1))))
