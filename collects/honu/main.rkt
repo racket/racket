9c30bda973ae96d434baec8b41fc955d456f1c96
Jon Rafkind <rafkind@racket-lang.org>
2010-07-16 11:23:15 -0600

    use honu/core. make a convenient macro for providing stuff

--- collects/honu/main.rkt
+++ collects/honu/main.rkt
@@ -2,7 +2,23 @@
 
 (require (prefix-in racket: racket/base))
 
+;; require's and provide's a module
+(define-syntax-rule (provide-module module ...)
+  (begin
+    (begin
+      (racket:require module)
+      (racket:provide [all-from-out module]))
+    ...))
+
+(provide-module "core/main.rkt"
+                "private/struct.honu"
+                "private/function.honu")
+
+#|
 (racket:require "core/main.rkt"
-                "private/struct.honu")
+                "private/struct.honu"
+                "private/function.honu")
 (racket:provide [all-from-out "core/main.rkt"])
-(racket:provide [all-from-out "private/struct.honu"])
+(racket:provide [all-from-out "private/struct.honu"
+                              "private/function.honu"])
+|#
