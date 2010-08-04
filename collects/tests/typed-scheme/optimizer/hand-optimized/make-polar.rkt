b1e744b1e12e3fb4256695415b7f61321152cdda
Vincent St-Amour <stamourv@racket-lang.org>
2010-08-04 11:48:27 -0400

    Fixed tests that used complex literals.

--- collects/tests/typed-scheme/optimizer/hand-optimized/make-polar.rkt
+++ collects/tests/typed-scheme/optimizer/hand-optimized/make-polar.rkt
@@ -12,17 +12,16 @@
   (unsafe-make-flrectangular unboxed-gensym-3 unboxed-gensym-4))
 
 ;; nested
-(let*-values (((unboxed-gensym-1) 1.0+2.0i)
-              ((unboxed-gensym-2) (unsafe-flreal-part unboxed-gensym-1))
-              ((unboxed-gensym-3) (unsafe-flimag-part unboxed-gensym-1))
-              ((unboxed-gensym-4) 2.0)
-              ((unboxed-gensym-5) 4.0)
-              ((unboxed-gensym-6) (unsafe-fl* unboxed-gensym-4
-                                              (unsafe-flcos unboxed-gensym-5)))
-              ((unboxed-gensym-7) (unsafe-fl* unboxed-gensym-4
-                                              (unsafe-flsin unboxed-gensym-5)))
-              ((unboxed-gensym-8) (unsafe-fl+ unboxed-gensym-2 unboxed-gensym-6))
-              ((unboxed-gensym-9) (unsafe-fl+ unboxed-gensym-3 unboxed-gensym-7)))
-  (unsafe-make-flrectangular unboxed-gensym-8 unboxed-gensym-9))
+(let*-values (((unboxed-gensym-1) 1.0)
+              ((unboxed-gensym-2) 2.0)
+              ((unboxed-gensym-3) 2.0)
+              ((unboxed-gensym-4) 4.0)
+              ((unboxed-gensym-5) (unsafe-fl* unboxed-gensym-3
+                                              (unsafe-flcos unboxed-gensym-4)))
+              ((unboxed-gensym-6) (unsafe-fl* unboxed-gensym-3
+                                              (unsafe-flsin unboxed-gensym-4)))
+              ((unboxed-gensym-7) (unsafe-fl+ unboxed-gensym-1 unboxed-gensym-5))
+              ((unboxed-gensym-8) (unsafe-fl+ unboxed-gensym-2 unboxed-gensym-6)))
+  (unsafe-make-flrectangular unboxed-gensym-7 unboxed-gensym-8))
 
 (void)
--- collects/tests/typed-scheme/optimizer/hand-optimized/unboxed-let.rkt
+++ collects/tests/typed-scheme/optimizer/hand-optimized/unboxed-let.rkt
@@ -2,23 +2,19 @@
 
 (require racket/unsafe/ops)
 
-(let* ((unboxed-gensym-1 1.0+2.0i)
-       (unboxed-gensym-2 (unsafe-flreal-part unboxed-gensym-1))
-       (unboxed-gensym-3 (unsafe-flimag-part unboxed-gensym-1))
-       (unboxed-gensym-4 2.0+4.0i)
-       (unboxed-gensym-5 (unsafe-flreal-part unboxed-gensym-4))
-       (unboxed-gensym-6 (unsafe-flimag-part unboxed-gensym-4))
-       (unboxed-gensym-7 (unsafe-fl+ unboxed-gensym-2 unboxed-gensym-5)) ; t1-real
-       (unboxed-gensym-8 (unsafe-fl+ unboxed-gensym-3 unboxed-gensym-6))) ; t1-imag
-  (let* ((unboxed-gensym-1 3.0+6.0i)
-         (unboxed-gensym-2 (unsafe-flreal-part unboxed-gensym-1))
-         (unboxed-gensym-3 (unsafe-flimag-part unboxed-gensym-1))
-         (unboxed-gensym-4 (unsafe-fl- unboxed-gensym-7 unboxed-gensym-2)) ; t2-real
-         (unboxed-gensym-5 (unsafe-fl- unboxed-gensym-8 unboxed-gensym-3))) ; t2-imag
-    (let* ((unboxed-gensym-1 4.0+8.0i)
-           (unboxed-gensym-2 (unsafe-flreal-part unboxed-gensym-1)) ; t3-real
-           (unboxed-gensym-3 (unsafe-flimag-part unboxed-gensym-1)) ; t3-imag
-           (unboxed-gensym-4 (unsafe-fl+ unboxed-gensym-4 unboxed-gensym-2))
-           (unboxed-gensym-5 (unsafe-fl+ unboxed-gensym-5 unboxed-gensym-3)))
-      (unsafe-make-flrectangular unboxed-gensym-4 unboxed-gensym-5))))
+(let* ((unboxed-gensym-1 1.0)
+       (unboxed-gensym-2 2.0)
+       (unboxed-gensym-3 2.0)
+       (unboxed-gensym-4 4.0)
+       (unboxed-gensym-5 (unsafe-fl+ unboxed-gensym-1 unboxed-gensym-3)) ; t1-real
+       (unboxed-gensym-6 (unsafe-fl+ unboxed-gensym-2 unboxed-gensym-4))) ; t1-imag
+  (let* ((unboxed-gensym-1 3.0)
+         (unboxed-gensym-2 6.0)
+         (unboxed-gensym-3 (unsafe-fl- unboxed-gensym-5 unboxed-gensym-1)) ; t2-real
+         (unboxed-gensym-4 (unsafe-fl- unboxed-gensym-6 unboxed-gensym-2))) ; t2-imag
+    (let* ((unboxed-gensym-1 4.0) ; t3-real
+           (unboxed-gensym-2 8.0) ; t3-imag
+           (unboxed-gensym-3 (unsafe-fl+ unboxed-gensym-3 unboxed-gensym-1))
+           (unboxed-gensym-4 (unsafe-fl+ unboxed-gensym-4 unboxed-gensym-2)))
+      (unsafe-make-flrectangular unboxed-gensym-3 unboxed-gensym-4))))
 (void)
