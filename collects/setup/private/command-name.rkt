804ff115e7ff085e19a13d7ae595ed4bb532e6ac
Matthew Flatt <mflatt@racket-lang.org>
2010-08-06 13:54:18 -0600

    fix `raco setup' reported name for Windows

--- collects/setup/private/command-name.rkt
+++ collects/setup/private/command-name.rkt
@@ -2,28 +2,32 @@
 ;; so minimize its dependencies
 
 (module command-name '#%kernel
-  (#%require raco/command-name)
+  (#%require raco/command-name '#%utils)
   (#%provide get-names)
 
   (define-values (get-names)
     (lambda ()
       (let-values ([(p) (find-system-path 'run-file)])
-        (let-values ([(base name dir?) (split-path p)])
-          (if (current-command-name)
-              (values (format "~a ~a" name (current-command-name))
-                      (program+command-name))
-              ;; Hack for bootstrapping, if the program name is "raco",
-              ;; then claim to be the "setup" command:
-              ;; if the program name is "racket", assume that there's a "racket -l setup"
-              ;; going on in there and also claim to be the "raco setup" command
-              (if (if (equal? (path->string name) "raco")
-                      #t
-                      (equal? (path->string name) "racket"))
-                  (values "raco setup"
-                          (string-append (regexp-replace*
-                                          #rx"racket$"
-                                          (format "~a" p)
-                                          "raco")
-                                         " setup")
-                          #t)
-                  (values (path->string name) p #f))))))))
+        (let-values ([(p) (if (eq? (system-type) 'windows)
+                              (path-replace-suffix p #"")
+                              p)])
+          (let-values ([(base name dir?) (split-path p)])
+            (if (current-command-name)
+                (values (format "~a ~a" name (current-command-name))
+                        (program+command-name))
+                ;; Hack for bootstrapping, if the program name is "raco",
+                ;; then claim to be the "setup" command:
+                ;; if the program name is "racket", assume that there's a "racket -l setup"
+                ;; going on in there and also claim to be the "raco setup" command
+                (if (if (equal? (path->string name) "raco")
+                        #t
+                        (equal? (path->string name) "racket"))
+                    (values "raco setup"
+                            (string-append (regexp-replace*
+                                            #rx"racket$"
+                                            (format "~a" p)
+                                            "raco")
+                                           " setup")
+                            #t)
+                    (values (path->string name) p #f)))))))))
+
