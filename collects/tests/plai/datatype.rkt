9e2ee8ecc62d4a87ee4fe4de4289f8a9d9c6d920
Jay McCarthy <jay@racket-lang.org>
2010-08-20 10:20:38 -0600

    Fixing another print related test

--- collects/tests/plai/datatype.rkt
+++ collects/tests/plai/datatype.rkt
@@ -32,10 +32,10 @@
 (eli:test
  (i 4)
  
- (regexp-match "\\(exception \\(make-i #f\\) \".+/collects/tests/plai/datatype\\.rkt:13\\.3: use broke the contract \\(-> number\\? i\\?\\) on make-i given to \\\\n  \\(file\\\\n   .+/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <number\\?>, given: #f\" <no-expected-value> \"at line 36\"\\)"
+ (regexp-match "\\(exception \\(make-i #f\\) \".+/collects/tests/plai/datatype\\.rkt:13\\.3: use broke the contract \\(-> number\\? i\\?\\) on make-i given to \\\\n  \\(file\\\\n   .+/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <number\\?>, given: #f\" '<no-expected-value> \"at line 36\"\\)"
                (with-output-to-string (λ () (test/exn (make-i #f) "contract"))))
  
- (regexp-match "\\(exception \\(i-f #f\\) \".+/collects/tests/plai/datatype\\.rkt:13\\.6: use broke the contract \\(-> i\\? number\\?\\) on i-f given to \\\\n  \\(file\\\\n   .+/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <i\\?>, given: #f\" <no-expected-value> \"at line 39\"\\)"
+ (regexp-match "\\(exception \\(i-f #f\\) \".+/collects/tests/plai/datatype\\.rkt:13\\.6: use broke the contract \\(-> i\\? number\\?\\) on i-f given to \\\\n  \\(file\\\\n   .+/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <i\\?>, given: #f\" '<no-expected-value> \"at line 39\"\\)"
                (with-output-to-string (λ () (test/exn (i-f #f) "contract"))))
  
  
@@ -45,10 +45,10 @@
  =>
  1
  
- (regexp-match "\\(exception \\(c1 \\(quote not-a-number\\)\\) \".+/collects/tests/plai/datatype\\.rkt:29\\.17: use broke the contract \\(-> number\\? c1\\?\\) on c1 given to \\\\n  \\(file\\\\n   .+/plt/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <number\\?>, given: 'not-a-number\" <no-expected-value> \"at line 49\"\\)"
+ (regexp-match "\\(exception \\(c1 \\(quote not-a-number\\)\\) \".+/collects/tests/plai/datatype\\.rkt:29\\.17: use broke the contract \\(-> number\\? c1\\?\\) on c1 given to \\\\n  \\(file\\\\n   .+/plt/collects/tests/plai/datatype\\.rkt\\)\\\\n; expected <number\\?>, given: 'not-a-number\" '<no-expected-value> \"at line 49\"\\)"
                (with-output-to-string (λ () (test (c1 'not-a-number) (list 5)))))
  
- (regexp-match (regexp-quote "(exception (type-case t (list 1) (c () 1)) \"type-case: expected a value from type t, got: (1)\" <no-expected-value> \"at line 53\")")
+ (regexp-match (regexp-quote "(exception (type-case t (list 1) (c () 1)) \"type-case: expected a value from type t, got: (1)\" '<no-expected-value> \"at line 53\")")
                (with-output-to-string (λ ()
                                         (test/exn
                                          (type-case t (list 1) (c () 1))
