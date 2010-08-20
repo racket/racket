d93fc805f37b40db8bacbd20b980e599f86d4443
Robby Findler <robby@racket-lang.org>
2010-08-20 16:23:57 -0500

    maybe this will help with some of the race conditions showing up in drdr

--- collects/tests/drracket/drracket-test-util.rkt
+++ collects/tests/drracket/drracket-test-util.rkt
@@ -151,7 +151,8 @@
 	      (fw:test:reraise-error)
 	      (send (send frame get-execute-button) is-enabled?))])
       ;(poll-until wait-for-computation-to-start 60) ;; hm.
-      (poll-until wait-for-computation-to-finish 60)))
+      (poll-until wait-for-computation-to-finish 60)
+      (sync (system-idle-evt))))
 
   (define do-execute 
     (case-lambda
