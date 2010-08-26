f38ec26ea5df762525ac575f860285923f7c69e3
Eli Barzilay <eli@barzilay.org>
2010-08-26 12:10:48 -0400

    More "~n" -> "\n" changes
    original commit: 8e0f8dd39c3744472b450021f003f9cbe8cbcb62

--- collects/tests/racket/embed-me2.rkt
+++ collects/tests/racket/embed-me2.rkt
@@ -2,8 +2,5 @@
   (require "embed-me1.ss"
 	   mzlib/etc)
   (with-output-to-file "stdout"
-    (lambda () (printf "This is 2: ~a~n" true))
+    (lambda () (printf "This is 2: ~a\n" true))
     'append))
-
-
-
