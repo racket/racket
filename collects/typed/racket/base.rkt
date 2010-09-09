5302b3e87c01be9c11e5f2098c90615bf5c883bc
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-09-09 14:11:58 -0400

    Remove requires for environments now handled dynamically.

--- collects/typed/racket/base.rkt
+++ collects/typed/racket/base.rkt
@@ -10,11 +10,7 @@
 		   #%top-interaction
 		   lambda
 		   #%app))
-(require typed-scheme/private/base-env
-	 typed-scheme/private/base-special-env
-	 typed-scheme/private/base-env-numeric
-	 typed-scheme/private/base-env-indexing
-	 typed-scheme/private/extra-procs
+(require typed-scheme/private/extra-procs
 	 (for-syntax typed-scheme/private/base-types-extra))
 (provide (rename-out [with-handlers: with-handlers]
                      [define-type-alias define-type])
