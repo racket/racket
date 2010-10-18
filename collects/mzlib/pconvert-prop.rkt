499fe40f8af9c9e0036dbdb91c4c4988e9e6496a
Robby Findler <robby@racket-lang.org>
2010-10-18 16:23:18 -0500

    minimize dependencies

--- collects/mzlib/pconvert-prop.rkt
+++ collects/mzlib/pconvert-prop.rkt
@@ -1,12 +1,12 @@
 
-(module pconvert-prop mzscheme
+(module pconvert-prop '#%kernel
 
-  (provide prop:print-convert-constructor-name
-	   print-convert-named-constructor?
-	   print-convert-constructor-name
-           prop:print-converter
-           print-converter?
-           print-converter-proc)
+  (#%provide prop:print-convert-constructor-name
+             print-convert-named-constructor?
+             print-convert-constructor-name
+             prop:print-converter
+             print-converter?
+             print-converter-proc)
 
   ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; property recognized by print convert to set a value's constructor name:
@@ -17,10 +17,11 @@
 		  print-convert-constructor-name)
     (make-struct-type-property 'print-convert-constructor-name
 			       (lambda (s info)
-				 (unless (symbol? s)
-				   (raise-type-error '|prop:print-convert-constructor-name guard|
-						     "symbol"
-						     s))
+				 (if (symbol? s)
+                                     (void)
+                                     (raise-type-error '|prop:print-convert-constructor-name guard|
+                                                       "symbol"
+                                                       s))
 				 s)))
 
   (define-values (prop:print-converter
@@ -28,9 +29,11 @@
 		  print-converter-proc)
     (make-struct-type-property 'print-converter
 			       (lambda (p info)
-				 (unless (and (procedure? p)
-                                              (procedure-arity-includes? p 2))
-				   (raise-type-error '|prop:print-converter|
-						     "procedure (arity 2)"
-						     p))
+                                 (if (if (procedure? p)
+                                         (procedure-arity-includes? p 2)
+                                         #f)
+                                     (void)
+                                     (raise-type-error '|prop:print-converter|
+                                                       "procedure (arity 2)"
+                                                       p))
 				 p))))
