3b23f247152230fb1feb7d79696478c9ef2dc600
Vincent St-Amour <stamourv@racket-lang.org>
2010-10-20 14:31:52 -0400

    Fix opt-lambda:. Merge to 5.0.2. (cherry picked from commit a15236ea4f1ba84c9351632e9469e1cd34b5375b)

--- collects/typed-scheme/private/prims.rkt
+++ collects/typed-scheme/private/prims.rkt
@@ -34,6 +34,7 @@ This file defines two sorts of primitives. All of them are provided into any mod
          "base-types.rkt"
          "base-types-extra.rkt"
          racket/flonum ; for for/flvector and for*/flvector
+         mzlib/etc
          (for-syntax 
           syntax/parse
 	  syntax/private/util
