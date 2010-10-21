b37187da59c401d223137bfe2fe4194ab7deda45
John Clements <clements@racket-lang.org>
2010-10-21 07:18:08 -0700

    added universe-test

---
+++ collects/tests/stepper/universe-test.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname universe-test) (read-case-sensitive #t) (teachpacks ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp")))))
+(define (zug state)
+  (overlay
+   (rectangle 100 state "solid" "blue")
+   (rectangle 100 200 "solid" "red")))
+
+(big-bang 14
+          (on-tick add1)
+          (to-draw zug))
