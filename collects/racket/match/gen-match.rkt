7802f1deb70a9fd11cfc8130bb43a58eb854fb97
Sam Tobin-Hochstadt <samth@racket-lang.org>
2010-10-26 16:38:17 -0400

    Add source locations in match -> match* conversion

--- collects/racket/match/gen-match.rkt
+++ collects/racket/match/gen-match.rkt
@@ -9,10 +9,14 @@
 ;; this transforms `match'-style clauses into ones acceptable to `go'
 ;; go : syntax syntax syntax [certifier] -> syntax
 (define (go/one parse/cert stx expr clauses [cert (syntax-local-certifier)])
+  (define-syntax-class cl
+    (pattern [p . rhs]
+             #:with res (syntax/loc this-syntax [(p) . rhs])))
   (syntax-parse clauses
-    [([p . rhs] ...) 
+    [(c:cl ...)
      (go parse/cert stx (quasisyntax/loc expr (#,expr))
-         #'([(p) . rhs] ...) cert)]))
+         #'(c.res ...)
+         cert)]))
 
 ;; this parses the clauses using parse/cert, then compiles them
 ;; go : syntax syntax syntax [certifier] -> syntax
