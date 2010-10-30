c428f6cafe9a5659ed54d8666e39b2578ae021f5
Jay McCarthy <jay@racket-lang.org>
2010-10-30 09:14:58 -0600

    Exposing more values to GC by not making them toplevels
    original commit: 26c7625c7903b4edb74d745ff9737fc7ab1e0021

--- collects/compiler/demodularizer/batch.rkt
+++ collects/compiler/demodularizer/batch.rkt
@@ -40,6 +40,7 @@ Here's the idea:
 
 (require racket/pretty
          racket/system
+         "mpi.rkt"
          "util.rkt"
          "nodep.rkt"
          "merge.rkt"
@@ -51,61 +52,62 @@ Here's the idea:
          compiler/zo-marshal
          racket/set)
 
-(define excluded-modules (make-parameter (set)))
-(define file-to-batch
-  (command-line #:program "batch" 
-                #:multi
-                [("-e" "--exclude-modules") mod
-                                            "Exclude a module from being batched"
-                                            (excluded-modules (set-add (excluded-modules) mod))]
-                #:args (filename) filename))
+(define (main file-to-batch)
+  (define-values (base name dir?) (split-path file-to-batch))
+  (when (or (eq? base #f) dir?)
+    (error 'batch "Cannot run on directory"))
+  
+  ;; Compile 
+  
+  (log-info "Compiling module")
+  (void (system* (find-executable-path "raco") "make" file-to-batch)) 
+  
+  (define merged-zo-path (path-add-suffix file-to-batch #"_merged.zo"))
+  
+  ;; Transformations
+  (define path-cache (make-hash))
+  
+  (log-info "Removing dependencies")
+  (define-values (batch-nodep top-lang-info top-self-modidx get-modvar-rewrite)
+    (parameterize ([MODULE-PATHS path-cache])
+      (nodep-file file-to-batch)))
+  
+  (log-info "Merging modules")
+  (define batch-merge
+    (parameterize ([MODULE-PATHS path-cache])
+      (merge-compilation-top get-modvar-rewrite batch-nodep)))
+  
+  ; Not doing this for now
+  ;(log-info "GC-ing top-levels")
+  (define batch-gcd
+    batch-merge
+    #;(gc-toplevels batch-merge))
+  
+  (log-info "Alpha-varying top-levels")
+  (define batch-alpha
+    (alpha-vary-ctop batch-gcd))
+  
+  (log-info "Replacing self-modidx")
+  (define batch-replace-modidx
+    (replace-modidx batch-alpha top-self-modidx))
+  
+  (define batch-modname
+    (string->symbol (regexp-replace #rx"\\.zo$" (path->string merged-zo-path) "")))
+  (log-info (format "Modularizing into ~a" batch-modname))
+  (define batch-mod
+    (wrap-in-kernel-module batch-modname batch-modname top-lang-info top-self-modidx batch-replace-modidx))
+  
+  (log-info "Writing merged zo")
+  (void
+   (with-output-to-file 
+       merged-zo-path
+     (lambda ()
+       (zo-marshal-to batch-mod (current-output-port)))
+     #:exists 'replace)))
 
-(define-values (base name dir?) (split-path file-to-batch))
-(when (or (eq? base #f) dir?)
-  (error 'batch "Cannot run on directory"))
-
-
-;; Compile 
-
-(log-info "Compiling module")
-(void (system* (find-executable-path "raco") "make" file-to-batch)) 
-
-
-(define merged-zo-path (path-add-suffix file-to-batch #"_merged.zo"))
-
-;; Transformations
-(log-info "Removing dependencies")
-(define-values (batch-nodep top-lang-info top-self-modidx get-modvar-rewrite)
-  (nodep-file file-to-batch (excluded-modules)))
-
-(log-info "Merging modules")
-(define batch-merge
-  (merge-compilation-top get-modvar-rewrite batch-nodep))
-
-; Not doing this for now
-;(log-info "GC-ing top-levels")
-(define batch-gcd
-  batch-merge
-  #;(gc-toplevels batch-merge))
-
-(log-info "Alpha-varying top-levels")
-(define batch-alpha
-  (alpha-vary-ctop batch-gcd))
-
-(log-info "Replacing self-modidx")
-(define batch-replace-modidx
-  (replace-modidx batch-alpha top-self-modidx))
-
-(define batch-modname
-  (string->symbol (regexp-replace #rx"\\.zo$" (path->string merged-zo-path) "")))
-(log-info (format "Modularizing into ~a" batch-modname))
-(define batch-mod
-  (wrap-in-kernel-module batch-modname batch-modname top-lang-info top-self-modidx batch-replace-modidx))
-
-(log-info "Writing merged zo")
-(void
- (with-output-to-file 
-     merged-zo-path
-   (lambda ()
-     (zo-marshal-to batch-mod (current-output-port)))
-   #:exists 'replace))
+(command-line #:program "batch" 
+              #:multi
+              [("-e" "--exclude-modules") mod
+                                          "Exclude a module from being batched"
+                                          (current-excluded-modules (set-add (current-excluded-modules) mod))]
+              #:args (filename) (main filename))
