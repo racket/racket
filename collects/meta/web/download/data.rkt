371b00afd89c3587e3822c7271254e5895bb34a8
Eli Barzilay <eli@barzilay.org>
2010-11-07 00:46:22 -0400

    v5.0.2 stuff (cherry picked from commit c195e2b2013ed16a5733a475f01dd0695267ac10)

--- collects/meta/web/download/data.rkt
+++ collects/meta/web/download/data.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (define -versions+dates-
-  '(["5.0.1" "August 2010"]
+  '(["5.0.2" "November 2010"]
+    ["5.0.1" "August 2010"]
     ["5.0"   "June 2010"]
     ["4.2.5" "April 2010"]
     ["4.2.4" "January 2010"]
