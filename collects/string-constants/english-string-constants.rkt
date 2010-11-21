3b56f81b101bc518f9252186ea413b7e8abfc4f6
Robby Findler <robby@eecs.northwestern.edu>
2010-11-21 07:15:02 -0600

    adjusted case of 'show line numbers' to match menu / prefs distinction

--- collects/string-constants/english-string-constants.rkt
+++ collects/string-constants/english-string-constants.rkt
@@ -448,8 +448,9 @@ please adhere to these guidelines:
  (show-interactions-on-execute "Automatically open interactions window when running a program")
  (switch-to-module-language-automatically "Automatically switch to the module language when opening a module")
  (interactions-beside-definitions "Put the interactions window beside the definitions window") ;; in preferences, below the checkbox one line above this one
- (show-line-numbers "Show Line Numbers")
- (hide-line-numbers "Hide Line Numbers")
+ (show-line-numbers "Show line numbers")
+ (show-line-numbers/menu "Show Line Numbers")  ;; just like the above, but capitalized for appearance in a menu item
+ (hide-line-numbers/menu "Hide Line Numbers")
  (limit-interactions-size "Limit interactions size")
  (background-color "Background Color")
  (default-text-color "Default text") ;; used for configuring colors, but doesn't need the word "color"
