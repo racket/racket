78a65f0662edc15e5ee314d141751b5c3194f54a
Jay McCarthy <jay@racket-lang.org>
2010-12-04 23:17:25 -0700

    More exhaustive test

--- collects/tests/web-server/pr/compat0.rkt
+++ collects/tests/web-server/pr/compat0.rkt
@@ -1,3 +1,6 @@
 #lang racket/base
-(require web-server/http/response-structs
-         web-server/compat/0/http/response-structs)
+(require web-server/compat/0/coerce
+         web-server/http/response-structs
+         web-server/compat/0/http/response-structs
+         web-server/http/cookie
+         web-server/compat/0/http/cookie)
