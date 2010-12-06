45a97cb561628f7243c8a3e6868b7170af1693d0
Robby Findler <robby@eecs.northwestern.edu>
2010-12-06 09:17:19 -0600

    off by one error in 2htdp/image error messages closes PR 11476

--- collects/2htdp/private/img-err.rkt
+++ collects/2htdp/private/img-err.rkt
@@ -49,7 +49,7 @@
          (with-syntax ([(i ...) (build-list len add1)])
            #`(define (fn-name args ... . final-arg)
                (let ([args (check/normalize 'fn-name 'args args i)] ...
-                     [final-arg (map/i (λ (x j) (check/normalize 'fn-name 'final-arg x (+ #,len j)))
+                     [final-arg (map/i (λ (x j) (check/normalize 'fn-name 'final-arg x (+ #,(+ len 1) j)))
                                        final-arg)])
                  body ...))))]
       [(define/chk (fn-name args ...) body ...)
