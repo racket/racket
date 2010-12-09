365d2726660ab9342f229789f268e2461cdd2063
Vincent St-Amour <stamourv@racket-lang.org>
2010-12-08 20:09:31 -0500

    Test for errors in rhs.

--- collects/tests/typed-scheme/fail/safe-letrec.rkt
+++ collects/tests/typed-scheme/fail/safe-letrec.rkt
@@ -1,7 +1,9 @@
 #;
-(exn-pred 1)
+(exn-pred 2)
 #lang typed/racket
 
 ;; make sure letrec takes into account that some bidings may be undefined
 
 (+ (letrec: ([x : Float x]) x) 1) ; PR 11511
+
+(letrec: ([x : Float (+ x 1)]) 0) ; error in rhs
