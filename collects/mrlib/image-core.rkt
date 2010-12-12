91aa9f756a258a8b9de624024fe81c4864f1b1dd
Robby Findler <robby@eecs.northwestern.edu>
2010-12-12 15:13:24 -0600

    finally, a fix that makes the 2htdp/image test suite pass!

--- collects/mrlib/image-core.rkt
+++ collects/mrlib/image-core.rkt
@@ -977,6 +977,7 @@ has been moved out).
          (send dc draw-path path dx dy)))]
     [(flip? np-atomic-shape) 
      (let ([bm (get-rendered-bitmap np-atomic-shape)]) 
+       (send dc set-smoothing 'smoothed)
        (send dc draw-bitmap 
              bm
              (- dx (/ (send bm get-width) 2))
@@ -988,6 +989,7 @@ has been moved out).
      (let ([θ (degrees->radians (text-angle np-atomic-shape))]
            [font (send dc get-font)])
        (send dc set-font (text->font np-atomic-shape))
+       (send dc set-smoothing 'aligned) ;; should this be smoothed?
        (let ([color (get-color-arg (text-color np-atomic-shape))])
          (send dc set-text-foreground 
                (cond
