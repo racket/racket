8368dcaca12997073f1a27e5ba6bc3f445ba8cf4
Casey Klein <clklein@racket-lang.org>
2010-12-14 12:26:11 -0600

    Fixes apparent copy-paste error

--- collects/deinprogramm/signature/info.rkt
+++ collects/deinprogramm/signature/info.rkt
@@ -4,9 +4,3 @@
 
 (define drracket-tools '("tool.rkt"))
 (define drracket-tool-names '("Signatures"))
-#lang setup/infotab
-
-(define name "DeinProgramm - Signatures")
-
-(define drracket-tools '("tool.rkt"))
-(define drracket-tool-names '("Signatures"))
