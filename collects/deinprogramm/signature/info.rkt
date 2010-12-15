3f74e662ed510b5e1bb4f91ec017322605688418
Mike Sperber <sperber@deinprogramm.de>
2010-12-15 08:17:57 +0100

    Add tool.rkt needed for "Disable Signature Check".
    I'd screwed up previously.

--- collects/deinprogramm/signature/info.rkt
+++ collects/deinprogramm/signature/info.rkt
@@ -2,6 +2,5 @@
 
 (define name "DeinProgramm - Signatures")
 
-;; Disable until "too.rkt" is added (or otherwise fixed)
-;(define drracket-tools '("tool.rkt"))
-;(define drracket-tool-names '("Signatures"))
+(define drracket-tools '("tool.rkt"))
+(define drracket-tool-names '("Signatures"))
