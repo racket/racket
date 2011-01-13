045aac4dafca91ed3dec1bf44eabfdcaaaad8631
Matthew Flatt <mflatt@racket-lang.org>
2011-01-13 13:08:42 -0700

    fix tools doc  --- hopefully the switch from `scheme' to `racket' doesn't      change the intended reference of anything in the docs

--- collects/scribblings/tools/common.rkt
+++ collects/scribblings/tools/common.rkt
@@ -4,27 +4,27 @@
 (require scribble/manual
          scribble/basic
          scribble/extract
-         scheme/class
-         scheme/contract)
+         racket/class
+         racket/contract)
 (provide (all-from-out scribble/manual)
          (all-from-out scribble/basic)
          (all-from-out scribble/extract)
-         (all-from-out scheme/class)
-         (all-from-out scheme/contract))
+         (all-from-out racket/class)
+         (all-from-out racket/contract))
 
-(require (for-label scheme/gui/base
+(require (for-label racket/gui/base
                     racket/snip
-                    scheme/class
-                    scheme/contract
-                    scheme/base
+                    racket/class
+                    racket/contract
+                    racket/base
                     drscheme/tool-lib
 		    mrlib/switchable-button
                     framework))
-(provide (for-label (all-from-out scheme/gui/base)
+(provide (for-label (all-from-out racket/gui/base)
                     (all-from-out racket/snip)
-                    (all-from-out scheme/class)
-                    (all-from-out scheme/contract)
-                    (all-from-out scheme/base)
+                    (all-from-out racket/class)
+                    (all-from-out racket/contract)
+                    (all-from-out racket/base)
                     (all-from-out drscheme/tool-lib)
                     (all-from-out mrlib/switchable-button)
                     (all-from-out framework)))
