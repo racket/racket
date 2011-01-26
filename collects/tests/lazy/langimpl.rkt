f60957282f6a4d948642ca1f329de7b652327f19
Stephen Chang <stchang216@gmail.com>
2011-01-26 18:14:34 -0500

    fix (take 0 nonemptylist) test in tests/lazy/langimpl.rkt

--- collects/tests/lazy/langimpl.rkt
+++ collects/tests/lazy/langimpl.rkt
@@ -5,8 +5,8 @@
 ;; tests for lazy language constructs
 
 (define (test-take)
-  (test (take 0 '(1 2 3)) => '())) ; test for push#22080
+  (test (! (take 0 '(1 2 3))) => '())) ; test for push#22080
 
 (provide langimpl-tests)
 (define (langimpl-tests)
-  (test do (test-take)))
+  (test (test-take)))
--- collects/tests/lazy/main.rkt
+++ collects/tests/lazy/main.rkt
@@ -3,5 +3,6 @@
 (require tests/eli-tester "promise.rkt" "lang.rkt" "langimpl.rkt")
 
 (test do (lang-tests)
+      do (langimpl-tests)
       do (promise-tests)
-      do (langimpl-tests))
+)
