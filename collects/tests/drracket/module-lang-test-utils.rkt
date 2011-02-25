218777347401980f638f3853838d4914b6e9aa28
Robby Findler <robby@racket-lang.org>
2011-02-25 09:40:52 -0600

    adjusted to change in the module language dialog

--- collects/tests/drracket/module-lang-test-utils.rkt
+++ collects/tests/drracket/module-lang-test-utils.rkt
@@ -180,7 +180,7 @@
       (let ([got (setup-dialog/run 
                   (λ () 
                     (test:set-radio-box-item! radio-box)
-                    (test:set-check-box! "Populate compiled/ directories (for faster loading)" #f)))])
+                    (test:set-check-box! "Populate \"compiled\" directories (for faster loading)" #f)))])
         (unless (equal? got (format "~s" no-check-expected))
           (error 'r-u-c-f-p-t.2 "got ~s expected ~s"
                  got
