ba70e05ae24728a30e85618657b52c8da8d08b4a
Matthew Flatt <mflatt@racket-lang.org>
2011-03-29 08:31:10 -0600

    Scribble: fix Latex-renderer bug introduced by 67b7c8f3f4e54

--- collects/scribble/latex-render.rkt
+++ collects/scribble/latex-render.rkt
@@ -259,8 +259,9 @@
                                        (let* ([bstr (list-ref (list-ref bstr+info+suffix 0) 0)]
                                               [suffix (list-ref bstr+info+suffix 1)]
                                               [height (list-ref (list-ref bstr+info+suffix 0) 2)]
-                                              [descent (+ (list-ref (list-ref bstr+info+suffix 0) 3)
-                                                          (- (ceiling height) height))]
+                                              [descent (and height
+                                                            (+ (list-ref (list-ref bstr+info+suffix 0) 3)
+                                                               (- (ceiling height) height)))]
                                               [fn (install-file (format "pict~a" suffix) bstr)])
                                          (if descent
                                              (printf "\\raisebox{-~apx}{\\includegraphics{~a}}" descent fn)
