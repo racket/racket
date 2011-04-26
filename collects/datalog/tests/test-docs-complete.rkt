c47a16c89b0388e7750764b16c9c824a250e409a
Jay McCarthy <jay@racket-lang.org>
2011-04-26 16:32:26 -0600

    All public bindings come from this module

--- collects/datalog/tests/test-docs-complete.rkt
+++ collects/datalog/tests/test-docs-complete.rkt
@@ -1,9 +1,3 @@
 #lang racket/base
 (require tests/utils/docs-complete)
-(check-docs (quote datalog/stx))
-(check-docs (quote datalog/runtime))
-(check-docs (quote datalog/pretty))
-(check-docs (quote datalog/parse))
 (check-docs (quote datalog))
-(check-docs (quote datalog/eval))
-(check-docs (quote datalog/ast))
