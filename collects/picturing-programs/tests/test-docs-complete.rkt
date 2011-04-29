0417e6b1d3086b25c016c14f36b8be89b78f74c1
Stephen Bloch <sbloch@adelphi.edu>
2011-04-29 07:45:08 -0400

    Took private files out of test-docs-complete.

--- collects/picturing-programs/tests/test-docs-complete.rkt
+++ collects/picturing-programs/tests/test-docs-complete.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require tests/utils/docs-complete)
-(check-docs (quote picturing-programs/private/tiles))
-(check-docs (quote picturing-programs/private/map-image))
+;(check-docs (quote picturing-programs/private/tiles))
+;(check-docs (quote picturing-programs/private/map-image))
 (check-docs (quote picturing-programs))
-(check-docs (quote picturing-programs/private/io-stuff))
-(check-docs (quote picturing-programs/private/book-pictures))
+;(check-docs (quote picturing-programs/private/io-stuff))
+;(check-docs (quote picturing-programs/private/book-pictures))
