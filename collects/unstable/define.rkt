01f1add1cbcf4ec8b0a057cbe88e956f520af22c
Carl Eastlund <cce@racket-lang.org>
2011-04-30 16:37:37 -0400

    Made define-syntax-block less restrictive about source of bindings.

--- collects/unstable/define.rkt
+++ collects/unstable/define.rkt
@@ -54,20 +54,7 @@
                      (syntax-list decl.external ...))
        "duplicate defined name"
        #'(define-syntaxes [decl.external ...]
-           ;; Easier way to ensure the internal names are bound than
-           ;; local-expand: bind them to an error macro and force the
-           ;; user to shadow them.
-           (let-syntax
-               ([decl.internal
-                 (make-set!-transformer
-                   (lambda (stx)
-                     (raise-syntax-error #f
-                       "transformer must be defined within define-syntax-block"
-                       stx)))]
-                ...)
-             (block
-               body ...
-               (values decl.internal ...))))])))
+           (block body ... (values decl.internal ...)))])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
