44f377bcd40b88147b606dbe1e8950e59b40e07d
Eric Dobson <eric.n.dobson@gmail.com>
2011-05-03 14:59:47 -0400

    Correctly instantiate poly-dotted functions with zero arguments. Closes PR 11776. Closes PR 11545.

---
+++ collects/tests/typed-scheme/succeed/pr11545+11776.rkt
@@ -0,0 +1,10 @@
+#lang typed/racket
+
+(: stuff (All [X ...] (X ... X -> (Listof Any))))
+(define (stuff . xs) xs)
+
+(: thing (-> (Listof Any)))
+(define (thing)
+  ((inst stuff)))
+
+(inst values)
