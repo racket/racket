7ae67dff356e4436fd13583409f7127e9a1df7d6
Eric Dobson <eric.n.dobson@gmail.com>
2011-05-03 16:48:45 -0400

    Changed Paths to PathStrings in types where appropriate

---
+++ collects/tests/typed-scheme/succeed/pathstrings.rkt
@@ -0,0 +1,25 @@
+#lang typed/racket
+
+(: no-exec (-> Void))
+(define (no-exec)
+ 
+
+ (call-with-output-file "file.tmp"
+  (lambda: ((port : Output-Port))
+   (write "hello world" port)))
+ (call-with-input-file "file.tmp"
+  (lambda: ((port : Input-Port))
+   (read port)))
+
+ (directory-list "/")
+ (make-directory "tmp-dir")
+
+ (path-only "file.tmp")
+ 
+ (system #"echo foo")
+ (system* "/bin/echo" "zzz" #"foo" (string->path "/"))
+ (system/exit-code #"echo foo")
+ (system*/exit-code "/bin/echo" "zzz" #"foo" (string->path "/"))
+
+ (void))
+
