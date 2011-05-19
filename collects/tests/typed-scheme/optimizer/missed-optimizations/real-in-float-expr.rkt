0432ea16cb75bfb897d915c9d6d50d93db15c477
Vincent St-Amour <stamourv@racket-lang.org>
2011-05-19 12:10:20 -0400

    Add an extra test that checks for non-reporting of false positives.

--- collects/tests/typed-scheme/optimizer/missed-optimizations/real-in-float-expr.rkt
+++ collects/tests/typed-scheme/optimizer/missed-optimizations/real-in-float-expr.rkt
@@ -1,12 +1,13 @@
 #;
 (
- real-in-float-expr.rkt 14:0 (#%app * (quote 3) (quote 2.3)) -- binary, args all float-arg-expr, return type not Float -- caused by: 14:8 (quote 3)
- real-in-float-expr.rkt 22:0 (#%app * (quote 2) (quote 2.0)) -- binary, args all float-arg-expr, return type not Float -- caused by: 22:8 (quote 2)
+ real-in-float-expr.rkt 15:0 (#%app * (quote 3) (quote 2.3)) -- binary, args all float-arg-expr, return type not Float -- caused by: 15:8 (quote 3)
+ real-in-float-expr.rkt 24:0 (#%app * (quote 2) (quote 2.0)) -- binary, args all float-arg-expr, return type not Float -- caused by: 24:8 (quote 2)
  6.8999999999999995
  6
  5
  6
  5
+ 17/12
  4.0
  )
 #lang typed/racket
@@ -18,5 +19,6 @@
 (+ (ann 2 Integer) (ann 3 Integer))
 (* 2 3)
 (+ 2 3)
+(+ 2/3 3/4)
 
 (* (ann 2 Natural) 2.0) ; close calls that result in Nonnegative-Real and co (i.e. not directly Real) should be reported too
