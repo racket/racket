fc705c6e29b585ee8f774175d55cabf52035e061
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-01 15:34:01 -0400

    Implement with-logging-to-port in terms of something more general.

--- collects/unstable/logging.rkt
+++ collects/unstable/logging.rkt
@@ -4,25 +4,22 @@
 
 ;; Known limitations:
 ;; - If another thread is logging while t is running, these messages will be
-;;   sent to the port as well, even if they don't come from proc.
+;;   intercepted as well, even if they don't come from proc.
 ;; - In the following example:
 ;;     (with-logging-to-port port level
 ;;       (lambda () (log-warning "ok") 3))
 ;;     (log-warning "not ok")
 ;;   If the logging on the last line is executed before the thread listening
 ;;   to the logs sees the stop message, "not ok" will also be sent to port.
-(define (with-logging-to-port port proc #:level [level 'debug])
+(define (with-intercepted-logging interceptor proc #:level [level 'debug])
   (let* ([logger    (make-logger #f (current-logger))]
          [receiver  (make-log-receiver logger level)]
          [stop-chan (make-channel)]
          [t (thread (lambda ()
-                      (define (output-event l)
-                        (displayln (vector-ref l 1) ; actual message
-                                   port))
                       (define (clear-events)
                         (let ([l (sync/timeout 0 receiver)])
                           (when l ; still something to read
-                            (output-event l)
+                            (interceptor l) ; interceptor get the whole vector
                             (clear-events))))
                       (let loop ()
                         (let ([l (sync receiver stop-chan)])
@@ -32,13 +29,19 @@
                                  ;; stop
                                  (clear-events)]
                                 [else ; keep going
-                                 (output-event l)
+                                 (interceptor l)
                                  (loop)])))))])
     (begin0
         (parameterize ([current-logger logger])
           (proc))
       (channel-put stop-chan 'stop) ; stop the receiver thread
       (thread-wait t))))
+
+(define (with-logging-to-port port proc #:level [level 'debug])
+  (with-intercepted-logging
+   (lambda (l) (displayln (vector-ref l 1) ; actual message
+                          port))
+   proc #:level level))
 
 (provide/contract [with-logging-to-port
                    (->* (output-port? (-> any))
