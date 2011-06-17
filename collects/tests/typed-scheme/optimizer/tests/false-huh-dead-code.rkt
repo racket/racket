88dafca69fae596763037c22f9557e5f875de87c
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-17 18:05:09 -0400

    Update test format.

--- collects/tests/typed-scheme/optimizer/tests/false-huh-dead-code.rkt
+++ collects/tests/typed-scheme/optimizer/tests/false-huh-dead-code.rkt
@@ -1,10 +1,10 @@
 #;
 (
-false-huh-dead-code.rkt line 10 col 16 - (quote 1) - dead then branch
-false-huh-dead-code.rkt line 11 col 13 - (quote 1) - dead then branch
+TR opt: false-huh-dead-code.rkt 10:16 1 -- dead then branch
+TR opt: false-huh-dead-code.rkt 11:13 1 -- dead then branch
 2
 2
- )
+)
 
 #lang typed/racket
 (if (false? #t) 1 2)
