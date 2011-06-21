2f274b789278c9c65b13fe55138e0ba9c600a866
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-06-21 15:12:16 -0400

    Include `typed-scheme' language in doc check.

--- collects/typed/tests/test-docs-complete.rkt
+++ collects/typed/tests/test-docs-complete.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 (require rackunit/docs-complete)
+(check-docs (quote typed-scheme))
 (check-docs (quote typed/scheme))
 (check-docs (quote typed/scheme/base))
 (check-docs (quote typed/racket))
