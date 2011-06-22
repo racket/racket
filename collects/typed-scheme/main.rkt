7b9cd61cb3c21b32891d74776a76398d9f63d987
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-06-22 12:39:54 -0400

    Fix requires/provides of `typed-scheme' language.

--- collects/typed-scheme/main.rkt
+++ collects/typed-scheme/main.rkt
@@ -1,17 +1,17 @@
 #lang s-exp "minimal.rkt"
 
-
-
-(providing (libs (except scheme/base #%module-begin #%top-interaction with-handlers lambda #%app for for*)
-                 (except "base-env/prims.rkt")
-                 (except "base-env/base-types.rkt")
-                 (except "base-env/base-types-extra.rkt"))
-	   (basics #%module-begin
-		   #%top-interaction
-		   lambda
-		   #%app))
-(require "base-env/extra-procs.rkt"
-         (for-syntax "base-env/base-types-extra.rkt"))
-(provide (rename-out [with-handlers: with-handlers])
-         (for-syntax (all-from-out "base-env/base-types-extra.rkt"))
-	 assert defined? with-type for for*)
+(providing (libs (except scheme/base #%module-begin #%top-interaction with-handlers lambda #%app for for*))
+	   (basics #%module-begin #%top-interaction lambda #%app))
+(require typed-scheme/base-env/extra-procs
+         typed-scheme/base-env/prims
+         typed-scheme/base-env/base-types
+         typed-scheme/base-env/base-types-extra
+	 (for-syntax typed-scheme/base-env/base-types-extra))
+(provide (rename-out [with-handlers: with-handlers]
+                     [define-type-alias define-type])
+         (except-out (all-from-out typed-scheme/base-env/prims)
+                     with-handlers: for/annotation for*/annotation)
+         (all-from-out typed-scheme/base-env/base-types)
+         (all-from-out typed-scheme/base-env/base-types-extra)
+	 assert defined? with-type for for*
+         (for-syntax (all-from-out typed-scheme/base-env/base-types-extra)))
