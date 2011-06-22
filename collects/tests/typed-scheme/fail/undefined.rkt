0df00e31d53ceb8da8c1f1e00e3d638b80e7ce9f
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-06-22 17:36:03 -0400

    Add test for PR 10794.

---
+++ collects/tests/typed-scheme/fail/undefined.rkt
@@ -0,0 +1,22 @@
+#;
+(exn-pred "Undefined")
+#lang scheme/load
+
+(module A scheme
+ 
+  (define (f x) (add1 x))
+ 
+  (provide f))
+
+(module B typed/scheme
+ 
+  (require/typed 'A [f (Integer -> Integer)])
+ 
+  (let ()
+   
+    (: x Integer)
+    (define x (f x))
+   
+    (void)))
+
+(require 'B)
