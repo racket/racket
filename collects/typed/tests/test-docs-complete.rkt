d05d54db8efa0db72c23ece4d049866d3e13b9c4
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-06-22 12:38:54 -0400

    Ignore undocumented exports that are from `racket'.

--- collects/typed/tests/test-docs-complete.rkt
+++ collects/typed/tests/test-docs-complete.rkt
@@ -1,7 +1,14 @@
 #lang racket/base
 (require rackunit/docs-complete)
-(check-docs (quote typed-scheme))
-(check-docs (quote typed/scheme))
-(check-docs (quote typed/scheme/base))
-(check-docs (quote typed/racket))
-(check-docs (quote typed/racket/base))
+;; these are currently undocumented in `racket' as well
+(define exclude '(define-place
+                  deserialize-info:set-v0
+                  make-primitive-class
+                  place-sleep
+                  procedure-closure-contents-eq?
+                  processor-count))
+(check-docs (quote typed-scheme) #:skip exclude)
+(check-docs (quote typed/scheme) #:skip exclude)
+(check-docs (quote typed/scheme/base) #:skip exclude)
+(check-docs (quote typed/racket) #:skip exclude)
+(check-docs (quote typed/racket/base) #:skip exclude)
