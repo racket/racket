2ecec626b35432408d9d558fc4aac66ce3cf4dba
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-23 17:00:57 -0400

    Remove highlights after editing.

--- collects/typed-scheme/optimizer/tool/tool.rkt
+++ collects/typed-scheme/optimizer/tool/tool.rkt
@@ -1,12 +1,14 @@
 #lang racket/base
 
-(require racket/unit racket/class racket/port racket/list
-         racket/gui/base mrlib/switchable-button)
+(require racket/class racket/port racket/list
+         racket/gui/base mrlib/switchable-button
+         racket/unit drracket/tool)
 
 (require (prefix-in tr: typed-scheme/typed-reader)
          typed-scheme/optimizer/logging)
 
-(provide performance-report-drracket-button)
+(provide performance-report-drracket-button
+         tool@)
 
 ;; DrRacket tool for reporting missed optimizations in the editor.
 
@@ -22,6 +24,8 @@
     (send bdc draw-ellipse 6 6 8 8)
     (send bdc set-bitmap #f)
     bmp))
+
+(define highlights '())
 
 ;; performance-report-callback : drracket:unit:frame<%> -> void
 (define (performance-report-callback drr-frame)
@@ -40,21 +44,45 @@
        (expand (tr:read-syntax portname input)))))
   (set! log (reverse log))
   ;; highlight
-  (for ([l (in-list log)])
-    (let ([stx (log-entry-stx l)]
-          [pos (sub1 (log-entry-pos l))]
-          ;; opt or missed opt?
-          [opt? (regexp-match #rx"^TR opt:" (log-entry-msg l))])
-      (send defs highlight-range
-            pos
-            (+ pos (syntax-span stx))
-            (if opt? "lightgreen" "pink"))))
+  (define new-highlights
+    (for/list ([l (in-list log)])
+      (let* ([stx (log-entry-stx l)]
+             [pos (sub1 (log-entry-pos l))]
+             [end (+ pos (syntax-span stx))]
+             ;; opt or missed opt?
+             [opt? (regexp-match #rx"^TR opt:" (log-entry-msg l))]
+             [color (if opt? "lightgreen" "pink")])
+        (send defs highlight-range pos end color)
+        (list pos end color)))) ; record the highlight, to undo it later
+  (set! highlights (append new-highlights highlights))
   (message-box
    "Performance Report"
    (with-output-to-string
     (lambda ()
       (for ([l (in-list log)])
         (displayln (log-entry-msg l)))))))
+
+(define remove-highlights-mixin
+  (mixin ((class->interface text%)) ()
+    (inherit begin-edit-sequence
+             end-edit-sequence
+             insert
+             get-text)
+    (define (clear-highlights)
+      (for ([h (in-list highlights)])
+        (send this unhighlight-range . h)))
+    (define/augment (after-insert start len)
+      (clear-highlights))
+    (define/augment (after-delete start len)
+      (clear-highlights))
+    (super-new)))
+
+(define-unit tool@
+  (import drracket:tool^)
+  (export drracket:tool-exports^)
+  (define (phase1) (void))
+  (define (phase2) (void))
+  (drracket:get/extend:extend-definitions-text remove-highlights-mixin))
 
 (define performance-report-drracket-button
   (list
