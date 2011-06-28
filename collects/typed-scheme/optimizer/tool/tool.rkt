092df83c18d16e1c8f74e0c2da0e5b74f42c1dd7
Vincent St-Amour <stamourv@racket-lang.org>
2011-06-28 17:46:31 -0400

    Have an icon for performance-report.

--- collects/typed-scheme/optimizer/tool/tool.rkt
+++ collects/typed-scheme/optimizer/tool/tool.rkt
@@ -12,18 +12,10 @@
 
 ;; DrRacket tool for reporting missed optimizations in the editor.
 
-(define reverse-content-bitmap
-  (let* ((bmp (make-bitmap 16 16))
-         (bdc (make-object bitmap-dc% bmp)))
-    (send bdc erase)
-    (send bdc set-smoothing 'smoothed)
-    (send bdc set-pen "black" 1 'transparent)
-    (send bdc set-brush "blue" 'solid)
-    (send bdc draw-ellipse 2 2 8 8)
-    (send bdc set-brush "red" 'solid)
-    (send bdc draw-ellipse 6 6 8 8)
-    (send bdc set-bitmap #f)
-    bmp))
+(define performance-report-bitmap
+  (make-object
+   bitmap%
+   (collection-file-path "performance-report.png" "icons") 'png/mask))
 
 (define highlights '())
 
@@ -106,5 +98,5 @@
 (define performance-report-drracket-button
   (list
    "Performance Report"
-   reverse-content-bitmap
+   performance-report-bitmap
    performance-report-callback))
