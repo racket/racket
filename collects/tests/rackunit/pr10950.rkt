bd46637210357b72d92a08a1b4a8c83fc0169b6b
Jay McCarthy <jay@racket-lang.org>
2011-07-02 13:51:31 -0700

    This test is sensitive to the JIT, it seems

--- collects/tests/rackunit/pr10950.rkt
+++ collects/tests/rackunit/pr10950.rkt
@@ -15,8 +15,8 @@
 
 (test
  (regexp-match
-  (regexp (format "~a.*~a"
-                 (regexp-quote "--------------------\ntests > #f\nUnnamed test \nFAILURE\nname:       check3\nlocation:   ")
-                 
-                 (regexp-quote "/collects/tests/rackunit/pr10950.rkt:14:51\nparams:     \n--------------------\n0 success(es) 1 failure(s) 0 error(s) 1 test(s) run\n")))
+  (regexp (format "~a.*~a.*~a"
+                 (regexp-quote "--------------------\ntests > #f\nUnnamed test \nFAILURE\nname:       check3\nlocation:   ")                 
+                 (regexp-quote "pr10950.rkt:14:51")
+                 (regexp-quote "0 success(es) 1 failure(s) 0 error(s) 1 test(s) run\n")))
   output))
