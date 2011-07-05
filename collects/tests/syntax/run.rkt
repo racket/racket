867f1e9097cc43984ae77237c4312668d22c2b1a
Vincent St-Amour <stamourv@racket-lang.org>
2011-07-05 16:28:25 -0400

    Fix syntax test harness to work with compiled tests.

--- collects/tests/syntax/run.rkt
+++ collects/tests/syntax/run.rkt
@@ -10,9 +10,11 @@
 (define tests
   (make-test-suite
    "syntax tests"
-   (for/list ([t (in-directory tests-dir)])
+   (for/list ([t (directory-list tests-dir)]
+              #:when (regexp-match ".*rkt$" t))
      (test-suite
       (path->string t)
-      (check-not-exn (lambda () (dynamic-require t #f)))))))
+      (check-not-exn (lambda ()
+                       (dynamic-require (build-path tests-dir t) #f)))))))
 
 (run-tests tests)
