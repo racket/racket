cf04739f93dd2a6e16bd022431a123c1a25865cb
Vincent St-Amour <stamourv@racket-lang.org>
2011-07-13 12:52:28 -0400

    Fix unsafe multiplication.

--- collects/typed-scheme/types/numeric-predicates.rkt
+++ collects/typed-scheme/types/numeric-predicates.rkt
@@ -9,7 +9,6 @@
 ;; we assume indexes are 2 bits shorter than fixnums
 ;; We're generating a reference to fixnum? rather than calling it, so
 ;; we're safe from fixnum size issues on different platforms.
-(define (index? x)
-  (and (fixnum? x) (unsafe-fx>= x 0) (fixnum? (unsafe-fx* x 4))))
+(define (index? x) (and (fixnum? x) (unsafe-fx>= x 0) (fixnum? (* x 4))))
 
 (define exact-rational? (conjoin rational? exact?))
