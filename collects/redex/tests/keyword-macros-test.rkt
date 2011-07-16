6ff5dc779b2611821f008e7312236dc72d70c62e
Casey Klein <clklein@racket-lang.org>
2011-07-16 12:38:10 -0500

    Fixes keyword tests

--- collects/redex/tests/keyword-macros-test.rkt
+++ collects/redex/tests/keyword-macros-test.rkt
@@ -22,23 +22,23 @@
       (test (parse #'(a #:c 3 #:b 4)) '(4 3))
       (test (parse #'(a #:b 4 #:c 3)) '(4 3))
       (test (parse #'(a #:c 3)) '(1 3))
-      (let*-values ([(kw) #'#:b]
+      (let*-values ([(kw) (syntax-taint #'#:b)]
                     [(msg src) (msg-src (parse #`(a #,kw)))])
         (test msg #rx"a: missing argument expression after keyword")
         (test src (list kw)))
-      (let*-values ([(arg) #'1]
+      (let*-values ([(arg) (syntax-taint #'1)]
                     [(msg src) (msg-src (parse #`(a #:b 1 #,arg)))])
         (test msg #rx"a: expected a keyword")
         (test src (list arg)))
-      (let*-values ([(kw) #'#:c]
+      (let*-values ([(kw) (syntax-taint #'#:c)]
                     [(msg src) (msg-src (parse #`(a #:c 1 #:b 2 #,kw 3)))])
         (test msg #rx"a: repeated keyword")
         (test src (list kw)))
-      (let*-values ([(kw) #'#:c]
+      (let*-values ([(kw) (syntax-taint #'#:c)]
                     [(msg src) (msg-src (parse #`(a #:b #,kw 3)))])
         (test msg #rx"a: expected an argument expression")
         (test src (list kw)))
-      (let*-values ([(kw) #'#:typo]
+      (let*-values ([(kw) (syntax-taint #'#:typo)]
                     [(msg src) (msg-src (parse #`(a #:b 3 #,kw 4)))])
         (test msg #rx"a: invalid keyword")
         (test src (list kw))))))
