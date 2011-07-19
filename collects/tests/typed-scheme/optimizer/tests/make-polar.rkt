419398ce1a1662cd6810bd5a100e65367e8855f8
Vincent St-Amour <stamourv@racket-lang.org>
2011-07-19 15:06:43 -0400

    Fix test to not be dependent on floating-point printing.

--- collects/tests/typed-scheme/optimizer/tests/make-polar.rkt
+++ collects/tests/typed-scheme/optimizer/tests/make-polar.rkt
@@ -1,8 +1,8 @@
 #;
 (
-TR opt: make-polar.rkt 27:0 (make-polar 1.0 1.0) -- make-polar
-TR opt: make-polar.rkt 27:0 (make-polar 1.0 1.0) -- make-rectangular elimination
-TR opt: make-polar.rkt 30:0 (let ((p (+ 1.0+2.0i (make-polar 2.0 4.0)))) (string-append (real->decimal-string (real-part p) 10) (real->decimal-string (imag-part p) 10))) -- unboxed let bindings
+TR opt: make-polar.rkt 27:6 (make-polar 1.0 1.0) -- make-polar
+TR opt: make-polar.rkt 27:6 (make-polar 1.0 1.0) -- make-rectangular elimination
+TR opt: make-polar.rkt 30:0 (let ((p (+ 1.0+2.0i (make-polar 2.0 4.0)))) (string-append (real->decimal-string (real-part p) 3) (real->decimal-string (imag-part p) 3))) -- unboxed let bindings
 TR opt: make-polar.rkt 30:9 (+ 1.0+2.0i (make-polar 2.0 4.0)) -- unboxed binary float complex
 TR opt: make-polar.rkt 30:12 1.0+2.0i -- unboxed literal
 TR opt: make-polar.rkt 30:21 (make-polar 2.0 4.0) -- make-rectangular elimination
@@ -14,8 +14,7 @@ TR opt: make-polar.rkt 31:50 p -- unbox float-complex
 TR opt: make-polar.rkt 32:39 (imag-part p) -- complex accessor elimination
 TR opt: make-polar.rkt 32:50 p -- leave var unboxed
 TR opt: make-polar.rkt 32:50 p -- unboxed complex variable
-0.5403023058681398+0.8414709848078965i
-"-0.30728724170.4863950094"
+"-0.3070.486"
 )
 
 #lang typed/scheme
@@ -23,10 +22,11 @@ TR opt: make-polar.rkt 32:50 p -- unboxed complex variable
 
 
 
+
 ;; top level
-(make-polar 1.0 1.0)
+(void (make-polar 1.0 1.0))
 
 ;; nested
 (let ((p (+ 1.0+2.0i (make-polar 2.0 4.0))))
-  (string-append (real->decimal-string (real-part p) 10)
-                 (real->decimal-string (imag-part p) 10)))
+  (string-append (real->decimal-string (real-part p) 3)
+                 (real->decimal-string (imag-part p) 3)))
