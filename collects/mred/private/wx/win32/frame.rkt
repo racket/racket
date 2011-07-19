255549c8c8fb751a2128478179cb078a70218f3b
Matthew Flatt <mflatt@racket-lang.org>
2011-07-19 08:54:38 -0600

    win32: fix `is-maximized' in `frame%'
     Merge to 5.1.2

--- collects/mred/private/wx/win32/frame.rkt
+++ collects/mred/private/wx/win32/frame.rkt
@@ -482,8 +482,8 @@
 
   (define/public (is-maximized?)
     (if (is-shown?)
-        hidden-zoomed?
-        (IsZoomed hwnd)))
+        (IsZoomed hwnd)
+        hidden-zoomed?))
   
   (define/public (maximize on?)
     (if (is-shown?)
