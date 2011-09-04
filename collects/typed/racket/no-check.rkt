2faa761f533604d99df08a041aa244a45d23495c
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-09-04 13:25:14 -0400

    Use better language for lang-info

--- collects/typed/racket/no-check.rkt
+++ collects/typed/racket/no-check.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang typed-racket/minimal
 
 (require racket/require typed-scheme/no-check (subtract-in typed/racket typed-scheme/no-check))
 (provide (all-from-out typed/racket typed-scheme/no-check))
