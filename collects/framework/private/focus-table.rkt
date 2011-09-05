8a30ed73b7e569faf809ca4cd4fa76b61bbd66c6
Robby Findler <robby@racket-lang.org>
2011-09-05 09:41:49 -0500

    add forgotten file

---
+++ collects/framework/private/focus-table.rkt
@@ -0,0 +1,13 @@
+#lang racket/base
+(require racket/gui/base racket/class)
+(provide frame:lookup-focus-table
+         frame:set-focus-table)
+
+;; focus-table : hash[eventspace -o> (listof frame)]
+(define focus-table (make-hash))
+(define (frame:lookup-focus-table [eventspace (current-eventspace)])
+  (hash-ref focus-table eventspace '()))
+(define (frame:set-focus-table eventspace new)
+  (if (null? new)
+      (hash-remove! focus-table eventspace)
+      (hash-set! focus-table eventspace new)))
