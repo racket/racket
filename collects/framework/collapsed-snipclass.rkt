14441264efead613c7f21ce0ec6f486bdc81dc41
Robby Findler <robby@racket-lang.org>
2011-09-11 18:26:00 -0500

    Rackety

--- collects/framework/collapsed-snipclass.rkt
+++ collects/framework/collapsed-snipclass.rkt
@@ -1,29 +1,28 @@
-(module collapsed-snipclass mzscheme
-  (require mred
-           mzlib/class
-           "private/collapsed-snipclass-helpers.rkt")
-  
-  (provide snip-class)
+#lang racket/base
+(require racket/gui/base
+         racket/class
+         "private/collapsed-snipclass-helpers.rkt")
 
-  (define simple-sexp-snip%
-    (class* snip% (readable-snip<%>)
-      (init-field left-bracket right-bracket saved-snips)
-      (define/public (read-special file line col pos)
-        (let ([text (make-object text%)])
-          (for-each
-           (λ (s) (send text insert (send s copy)
-                             (send text last-position)
-                             (send text last-position)))
-           saved-snips)
-	  (datum->syntax-object
-	   #f
-	   (read (open-input-text-editor text))
-	   (list file line col pos 1))))
-      (super-instantiate ())))
-  
-  (define sexp-snipclass% (make-sexp-snipclass% simple-sexp-snip%))
-  
-  (define snip-class (make-object sexp-snipclass%))
-  (send snip-class set-classname (format "~s" '(lib "collapsed-snipclass.ss" "framework")))
-  (send snip-class set-version 0)
-  (send (get-the-snip-class-list) add snip-class))
+(provide snip-class)
+
+(define simple-sexp-snip%
+  (class* snip% (readable-snip<%>)
+    (init-field left-bracket right-bracket saved-snips)
+    (define/public (read-special file line col pos)
+      (define text (make-object text%))
+      (for ([s (in-list saved-snips)])
+        (send text insert (send s copy)
+              (send text last-position)
+              (send text last-position)))
+      (datum->syntax
+       #f
+       (read (open-input-text-editor text))
+       (list file line col pos 1)))
+    (super-new)))
+
+(define sexp-snipclass% (make-sexp-snipclass% simple-sexp-snip%))
+
+(define snip-class (make-object sexp-snipclass%))
+(send snip-class set-classname (format "~s" '(lib "collapsed-snipclass.ss" "framework")))
+(send snip-class set-version 0)
+(send (get-the-snip-class-list) add snip-class)
