84d24bac80b37803263367df72696a2345457469
Eli Barzilay <eli@barzilay.org>
2011-09-14 16:54:13 -0400

    Works up to the current version number, which is "1.0.0e".

--- collects/openssl/libssl.rkt
+++ collects/openssl/libssl.rkt
@@ -22,4 +22,6 @@
                         (lambda (x)
                           (set! libssl-load-fail-reason (exn-message x))
                           #f)])
-         (ffi-lib libssl-so '("" "1.0" "1.0.0a" "0.9.8e" "0.9.8b" "0.9.8" "0.9.7")))))
+         (ffi-lib libssl-so
+                  '("" "1.0" "1.0.0e" "1.0.0d" "1.0.0c" "1.0.0b" "1.0.0a"
+                    "0.9.8e" "0.9.8b" "0.9.8" "0.9.7")))))
