d02d7aae9787801230c0c67e4790ba19f96d56c7
Matthew Flatt <mflatt@racket-lang.org>
2011-10-13 06:03:10 -0600

    add missing test for text% tab fix
    Merge to 5.2
    (cherry picked from commit 6d608e392c301cfcff4529445a74788745ad79cc)

--- collects/tests/gracket/wxme.rkt
+++ collects/tests/gracket/wxme.rkt
@@ -1415,5 +1415,15 @@
   (expect (send t line-end-position 3) 9))
 
 ;; ----------------------------------------
+;; tabs
+
+(let ([t1 (new text%)])
+  (send t1 set-admin (new test-editor-admin%))  
+  (send t1 set-tabs '(100 200 300 400 500 600 700 800 900 1000 100) 1 #t)
+  (send t1 insert "Hello\tWorld")
+  (send t1 get-extent (box 0) (box 0)))
+
+
+;; ----------------------------------------
 
 (done)
