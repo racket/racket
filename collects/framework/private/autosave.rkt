4a3997ac1c21282c324e125ffad835f8d8371cbe
Robby Findler <robby@racket-lang.org>
2011-10-17 17:31:57 -0500

    use the focus table mixin for the autosave window so that drdr tests work right

--- collects/framework/private/autosave.rkt
+++ collects/framework/private/autosave.rkt
@@ -144,7 +144,7 @@
                [filtered-table
                 (filter (λ (x) (file-exists? (cadr x))) table)])
           (unless (null? filtered-table)
-            (let* ([dlg (new dialog%
+            (let* ([dlg (new (frame:focus-table-mixin dialog%)
                              (label (string-constant recover-autosave-files-frame-title)))]
                    [t (new text% (auto-wrap #t))]
                    [ec (new editor-canvas%
