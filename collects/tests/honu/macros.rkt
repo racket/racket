786436b314f39a115381da9791fe86a7559fc16b
Jon Rafkind <rafkind@racket-lang.org>
2011-11-10 20:49:57 -0700

    [honu] add some simple macro examples

--- collects/tests/honu/macros.rkt
+++ collects/tests/honu/macros.rkt
@@ -6,3 +6,14 @@ testx 5 * 2
 
 for z = 1 to testx 5 * 2 do
   printf("z is ~a\n", z)
+
+macro testfor () {x:expression} {
+  syntax(for z = 1 to x_result do
+    printf("z is ~a\n" z))
+}
+
+macro testfor2 () {x:expression}{
+  syntax(testfor x_result * 2)
+}
+
+testfor2 1 + 2
