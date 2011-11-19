40eeeb6a72a962dfd37a8aabd3548337a26f2a19
Jon Rafkind <rafkind@racket-lang.org>
2011-11-18 22:00:08 -0700

    [honu] dont need extra prefixes for require

--- collects/honu/core/main.rkt
+++ collects/honu/core/main.rkt
@@ -3,57 +3,57 @@
 (define-syntax-rule (standard-honu meta-level)
   (begin
     (require (for-meta meta-level
-                       (prefix-in racket: racket/base)
-                       (prefix-in honu: "private/macro2.rkt")
-                       (prefix-in honu: "private/class.rkt")
+                       racket/base
+                       "private/macro2.rkt"
+                       "private/class.rkt"
                        (prefix-in literal: "private/literals.rkt")
                        (prefix-in syntax-parse: syntax/parse)
                        (prefix-in racket: racket/base)
                        (prefix-in parse: "private/parse2.rkt")
-                       (prefix-in honu: "private/honu2.rkt")))
+                       "private/honu2.rkt"))
     (provide (for-meta meta-level 
                        (rename-out [parse:honu-expression expression]
                                    [parse:honu-identifier identifier]
-                                   [honu:honu-function function]
-                                   [honu:honu-var var]
-                                   [honu:honu-equal =]
-                                   [honu:honu--> %arrow]
-                                   [honu:honu-class class]
-                                   [honu:honu-require require]
-                                   [honu:honu-new new]
-                                   [honu:honu-while while]
-                                   [honu:honu-macro macro]
-                                   [honu:honu-with-input-from-file with_input_from_file]
-                                   [honu:define-make-honu-operator operator]
-                                   [honu:honu-match match]
-                                   [honu:honu-with with]
-                                   [honu:honu-var var]
-                                   [honu:honu-val val]
-                                   [honu:honu-for for]
-                                   [honu:honu-if if]
-                                   [honu:honu-quote quote]
-                                   [honu:honu-quasiquote quasiquote]
-                                   [honu:honu-+ +] [honu:honu-- -]
-                                   [honu:honu-* *] [honu:honu-/ /]
-                                   [honu:honu-modulo %]
-                                   [honu:honu-^ ^]
-                                   [honu:honu-> >] [honu:honu-< <]
-                                   [honu:honu->= >=]
-                                   [honu:honu-<= <=]
-                                   [honu:honu-equal =]
-                                   [honu:honu-assignment :=]
-                                   [honu:honu-map map]
-                                   [honu:honu-flow \|]
-                                   [honu:honu-dot %dot]
-                                   [honu:honu--> %arrow]
-                                   [honu:honu-string=? string_equal]
-                                   [honu:honu-cons ::]
-                                   [honu:honu-and and] [honu:honu-and &&]
-                                   [honu:honu-or or] [honu:honu-or \|\|]
-                                   [honu:honu-not not] [honu:honu-not !]
-                                   [honu:honu-structure structure]
-                                   [honu:honu-structure struct]
-                                   [honu:honu-syntax syntax]
+                                   [honu-function function]
+                                   [honu-var var]
+                                   [honu-equal =]
+                                   [honu--> %arrow]
+                                   [honu-class class]
+                                   [honu-require require]
+                                   [honu-new new]
+                                   [honu-while while]
+                                   [honu-macro macro]
+                                   [honu-with-input-from-file with_input_from_file]
+                                   [define-make-honu-operator operator]
+                                   [honu-match match]
+                                   [honu-with with]
+                                   [honu-var var]
+                                   [honu-val val]
+                                   [honu-for for]
+                                   [honu-if if]
+                                   [honu-quote quote]
+                                   [honu-quasiquote quasiquote]
+                                   [honu-+ +] [honu-- -]
+                                   [honu-* *] [honu-/ /]
+                                   [honu-modulo %]
+                                   [honu-^ ^]
+                                   [honu-> >] [honu-< <]
+                                   [honu->= >=]
+                                   [honu-<= <=]
+                                   [honu-equal =]
+                                   [honu-assignment :=]
+                                   [honu-map map]
+                                   [honu-flow \|]
+                                   [honu-dot %dot]
+                                   [honu--> %arrow]
+                                   [honu-string=? string_equal]
+                                   [honu-cons ::]
+                                   [honu-and and] [honu-and &&]
+                                   [honu-or or] [honu-or \|\|]
+                                   [honu-not not] [honu-not !]
+                                   [honu-structure structure]
+                                   [honu-structure struct]
+                                   [honu-syntax syntax]
                                    [literal:honu-prefix prefix]
                                    [literal:honu-then then]
                                    [literal:colon %colon]
@@ -65,10 +65,10 @@
                                    [literal:#%brackets #%brackets]
                                    [literal:#%braces #%braces]
                                    [literal:#%parens #%parens])
-                       racket:print racket:printf
-                       honu:true honu:false
-                       honu:withSyntax
-                       honu:mergeSyntax
+                       print printf
+                       true false
+                       withSyntax
+                       mergeSyntax
                        ))))
 
 (require "private/honu-typed-scheme.rkt")
