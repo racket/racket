331d47eb090fb6da4ca0e51309d0270b9673a93e
Vincent St-Amour <stamourv@racket-lang.org>
2011-11-30 17:56:06 -0500

    Remove vestigial file.

--- collects/typed-racket/optimizer/tool/utilities.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-;; TODO all this stuff should make it into unstable/X
-
-(provide (all-defined-out))
-
-(define (regexp-filter r log)
-  (for/list ([l (in-list log)] #:when (regexp-match r l))
-    l))
