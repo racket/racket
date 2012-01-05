061007f30f13f67a59665fdebebfdb07597bc6b9
Jay McCarthy <jay@racket-lang.org>
2012-01-05 14:57:08 -0700

    Fixing last dumb mistake?

--- collects/tests/plai/gc2/bad-collectors/no-collection-collector.rkt
+++ collects/tests/plai/gc2/bad-collectors/no-collection-collector.rkt
@@ -13,6 +13,10 @@
 (define (gc:cons? loc) #f)
 (define (gc:set-first! pr new) (void))
 (define (gc:set-rest! pr new) (void))
+(define (gc:closure code-ptr env) 0)
+(define (gc:closure-code-ptr ptr) #f)
+(define (gc:closure-env-ref ptr i) 0)
+(define (gc:closure? ptr) #f)
 
 (with-heap (vector 1 2 3)
   (test (gc:deref 0) #f))
