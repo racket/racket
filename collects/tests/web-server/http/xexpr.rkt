8be9abf1d4e05dae6fefc94a9aac6a28b700a1f8
Jay McCarthy <jay@racket-lang.org>
2012-01-25 16:48:01 -0700

    Verify that the HTML tags really do have their empty-tag behavior

--- collects/tests/web-server/http/xexpr.rkt
+++ collects/tests/web-server/http/xexpr.rkt
@@ -21,6 +21,16 @@
  =>
  (bytes-sort
   #"HTTP/1.1 200 Okay\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n<a href=\"#\">link</a>")
+
+(write-response (response/xexpr '(html)))
+ =>
+ (bytes-sort
+  #"HTTP/1.1 200 Okay\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n<html></html>")
+
+ (write-response (response/xexpr '(img)))
+ =>
+ (bytes-sort
+  #"HTTP/1.1 200 Okay\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n<img />")
  
  (write-response (response/xexpr '(a ([href "#"]) "link")
                                  #:code 404))
