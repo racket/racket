dc6522b153ca67755150b0683d0ea7f4283098f7
Robby Findler <robby@racket-lang.org>
2012-02-14 12:30:23 -0600

    added racket/future to racket (the docs already say that it is included)

--- collects/racket/main.rkt
+++ collects/racket/main.rkt
@@ -21,6 +21,7 @@
          racket/path
          racket/file
          racket/place
+         racket/future
          racket/port
          racket/cmdline
          racket/promise
@@ -52,6 +53,7 @@
                        racket/path
                        racket/file
                        racket/place
+                       racket/future
                        racket/port
                        racket/cmdline
                        racket/promise
