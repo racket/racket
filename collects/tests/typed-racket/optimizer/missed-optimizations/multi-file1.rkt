eca43387f60b3ecb33aa22ff011eb76d2c1a7cb8
Vincent St-Amour <stamourv@racket-lang.org>
2012-02-14 18:09:30 -0500

    Fix tests.

--- collects/tests/typed-racket/optimizer/missed-optimizations/multi-file1.rkt
+++ collects/tests/typed-racket/optimizer/missed-optimizations/multi-file1.rkt
@@ -1,6 +1,6 @@
 #;
 (
-TR missed opt: multi-file1.rkt 12:2 (* x 3) -- all args float-arg-expr, result not Float -- caused by: 12:7 3
+TR missed opt: multi-file1.rkt 12:2 (* x (ann 3 Integer)) -- all args float-arg-expr, result not Float -- caused by: 12:12 3
 )
 
 #lang typed/racket
@@ -9,4 +9,4 @@ TR missed opt: multi-file1.rkt 12:2 (* x 3) -- all args float-arg-expr, result n
 
 (: f : Float -> Real)
 (define (f x)
-  (* x 3))
+  (* x (ann 3 Integer)))
