f3a21c6aba515db8f5dc3bf320a7c0ac6ba8d1fc
Eli Barzilay <eli@barzilay.org>
2012-03-16 03:45:35 -0400

    Remove typed versions of the cgi exceptions.

--- collects/typed/net/cgi.rkt
+++ collects/typed/net/cgi.rkt
@@ -2,12 +2,6 @@
 
 (require typed/private/utils)
 
-(require-typed-struct cgi-error () #:extra-constructor-name make-cgi-error net/cgi)
-
-(require-typed-struct (incomplete-%-suffix cgi-error)  ([chars : (Listof Char)]) #:extra-constructor-name make-incomplete-%-suffix net/cgi)
-(require-typed-struct (invalid-%-suffix cgi-error)  ([char : Char]) #:extra-constructor-name make-invalid-%-suffix net/cgi)
-
-
 (require/typed/provide net/cgi
   [get-bindings (-> (Listof (cons (U Symbol String) String)))]
   [get-bindings/post (-> (Listof (Pair (U Symbol String) String)))]
@@ -22,9 +16,3 @@
   [get-cgi-method (-> (U "GET" "POST"))]
   [string->html (String -> String)]
   [generate-link-text (String String -> String)])
-
-
-(provide
- (struct-out cgi-error)
- (struct-out incomplete-%-suffix)
- (struct-out invalid-%-suffix))
