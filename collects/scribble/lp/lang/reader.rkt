4fe8cd344c09390b20eb7c9cca415d79753d642b
Robby Findler <robby@racket-lang.org>
2012-03-17 19:24:33 -0500

    disable the drracket toolbar buttons for scribble/lp files

--- collects/scribble/lp/lang/reader.rkt
+++ collects/scribble/lp/lang/reader.rkt
@@ -5,7 +5,8 @@ scribble/lp/lang/lang
 #:read read-inside
 #:read-syntax read-syntax-inside
 #:whole-body-readers? #t
-#:info (scribble-base-info)
+;; don't use scribble-base-info for the #:info arg, since
+;; scribble/lp files are not directly scribble'able.
 #:language-info (scribble-base-language-info)
 
 (require scribble/reader
