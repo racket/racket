a2a60cb92406ec5319ea3323f30147992bd71b84
Vincent St-Amour <stamourv@racket-lang.org>
2012-05-02 15:04:10 -0400

    Fix TR test for new contract error message format.

--- collects/tests/typed-racket/fail/back-and-forth.rkt
+++ collects/tests/typed-racket/fail/back-and-forth.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred exn:fail:contract? #rx".*contract violation.*contract.*f.*\\(-> Number Number\\).*")
+(exn-pred exn:fail:contract? #rx".*contract violation.*\\(-> Number Number\\).*contract.*f.*")
 
 #lang scheme/load
 
