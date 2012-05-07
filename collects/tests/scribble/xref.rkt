a4381dd1f62f701227ead8efa6be15d5b950dc7d
Eli Barzilay <eli@barzilay.org>
2012-05-07 09:11:36 -0400

    Do the xref test only if "doc/reference/out.sxref" exists/
    (Since the nightly build includes the documentation only on the main
    machine.)

--- collects/tests/scribble/xref.rkt
+++ collects/tests/scribble/xref.rkt
@@ -7,13 +7,10 @@
 (provide xref-tests)
 (module+ main (xref-tests))
 (define (xref-tests)
-  (define xref
-    (load-xref (list
-                (lambda ()
-                  (cadr
-                   (call-with-input-file*
-                       (build-path (find-doc-dir) "reference" "out.sxref")
-                     fasl->s-exp))))))
-  (test (xref-binding->definition-tag
-         xref (list '(lib "contract.rkt" "racket") '->) #f)
-        => '(form ((lib "racket/contract/base.rkt") ->))))
+  (define sxref (build-path (find-doc-dir) "reference" "out.sxref"))
+  (when (file-exists? sxref)
+    (define xref
+      (load-xref (list (λ() (cadr (call-with-input-file* sxref fasl->s-exp))))))
+    (test (xref-binding->definition-tag
+           xref (list '(lib "contract.rkt" "racket") '->) #f)
+          => '(form ((lib "racket/contract/base.rkt") ->)))))
