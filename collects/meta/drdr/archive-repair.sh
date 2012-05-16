b752d2e09a2f89564bd56a7f21583f476ccf4fcd
Jay McCarthy <jay@racket-lang.org>
2012-05-16 09:54:13 -0600

    Only repair the big archives

--- collects/meta/drdr/archive-repair.sh
+++ collects/meta/drdr/archive-repair.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 cd /opt/plt/builds
-for i in * ; do
-    if [ -f ${i}/archive.db ] ; then
-        /opt/plt/plt/bin/racket -t /opt/svn/drdr/archive-repair.rkt -- $i
-    fi
+
+du -h */archive.db | awk '{print $1}' | sort -n | uniq -c
+echo
+
+for rev in $(du -h */archive.db | sort -n | tail | awk '{print $2}' | awk -F/ '{print $1}' | tac) ; do
+    du -h ${rev}/archive.db
+    /opt/plt/plt/bin/racket -t /opt/svn/drdr/archive-repair.rkt -- $rev > /dev/null
 done
+echo
+
+du -h */archive.db | awk '{print $1}' | sort -n | uniq -c
+echo
+
+df -h
