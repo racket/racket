73ce5a4767a7685e5a922dcd0eec5da4dd62556d
Matthew Flatt <mflatt@racket-lang.org>
2012-05-27 14:39:51 -0600

    racket/serialize: fix for submodules
    Closes PR 12795

--- collects/racket/private/serialize.rkt
+++ collects/racket/private/serialize.rkt
@@ -56,13 +56,17 @@
   ;;  then it can cause simplified module paths to be paths;
   ;;  keep the literal path, but marshal it to bytes.
   (define (protect-path p)
-    (if (path? p)
-	(path->bytes p)
-	p))
+    (cond
+     [(path? p) (path->bytes p)]
+     [(and (pair? p) (eq? (car p) 'submod) (path? (cadr p)))
+      `(submod ,(protect-path (cadr p)) . ,(cddr p))]
+     [else p]))
   (define (unprotect-path p)
-    (if (bytes? p)
-	(bytes->path p)
-	p))
+    (cond
+     [(bytes? p) (bytes->path p)]
+     [(and (pair? p) (eq? (car p) 'submod) (bytes? (cadr p)))
+      `(submod ,(unprotect-path (cadr p)) . ,(cddr p))]
+     [else p]))
 
   (define (revive-symbol s)
     (if (string? s)
