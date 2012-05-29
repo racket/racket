38ae869d66e57e0e26f2ecdd6615fd436d3dc328
Jay McCarthy <jay@racket-lang.org>
2012-05-29 13:26:53 -0600

    Fixing error introduced by 9e7548de

--- collects/tests/web-server/pr/12658.rkt
+++ collects/tests/web-server/pr/12658.rkt
@@ -6,6 +6,6 @@
  (with-handlers ([exn? (λ (x) (exn-message x))])
    (go 42)
    (error 'go "Failed to throw exn"))
- "procedure go: expects no arguments, given 1: 42")
+ "application: wrong number of arguments\n  procedure: go\n  expected number of arguments: 0\n  given number of arguments: 1\n  arguments:\n   42")
 
 
