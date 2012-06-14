ab0d3d64926701cf6fab81c4f0c14348ab9c2b97
Asumu Takikawa <asumu@ccs.neu.edu>
2012-06-14 18:22:42 -0400

    Fix TR optimizer test
    (round to avoid FPU sensitivity)

--- collects/tests/typed-racket/optimizer/tests/unboxed-exp.rkt
+++ collects/tests/typed-racket/optimizer/tests/unboxed-exp.rkt
@@ -1,27 +1,36 @@
 #;
 (
-TR opt: unboxed-exp.rkt 23:5 2.0+3.4i -- unboxed literal
-TR opt: unboxed-exp.rkt 23:0 (exp 2.0+3.4i) -- unboxed unary float complex
-TR opt: unboxed-exp.rkt 24:5 0.0+0.0i -- unboxed literal
-TR opt: unboxed-exp.rkt 24:0 (exp 0.0+0.0i) -- unboxed unary float complex
-TR opt: unboxed-exp.rkt 25:5 -12.2-4.7i -- unboxed literal
-TR opt: unboxed-exp.rkt 25:0 (exp -12.2-4.7i) -- unboxed unary float complex
-TR opt: unboxed-exp.rkt 26:5 12.2-4.7i -- unboxed literal
-TR opt: unboxed-exp.rkt 26:0 (exp 12.2-4.7i) -- unboxed unary float complex
-TR opt: unboxed-exp.rkt 27:5 -12.2+4.7i -- unboxed literal
-TR opt: unboxed-exp.rkt 27:0 (exp -12.2+4.7i) -- unboxed unary float complex
--7.143726081314396-1.8882075384588168i
-1.0+0.0i
--6.232062158151458e-08+5.030069557694479e-06i
--2462.731893583747+198773.89557926523i
--6.232062158151458e-08-5.030069557694479e-06i
+TR opt: unboxed-exp.rkt 29:50 p -- unbox float-complex
+TR opt: unboxed-exp.rkt 29:39 (real-part p) -- complex accessor elimination
+TR opt: unboxed-exp.rkt 30:50 p -- unbox float-complex
+TR opt: unboxed-exp.rkt 30:39 (imag-part p) -- complex accessor elimination
+TR opt: unboxed-exp.rkt 32:30 2.0+3.4i -- unboxed literal
+TR opt: unboxed-exp.rkt 32:25 (exp 2.0+3.4i) -- unboxed unary float complex
+TR opt: unboxed-exp.rkt 33:30 0.0+0.0i -- unboxed literal
+TR opt: unboxed-exp.rkt 33:25 (exp 0.0+0.0i) -- unboxed unary float complex
+TR opt: unboxed-exp.rkt 34:30 -12.2-4.7i -- unboxed literal
+TR opt: unboxed-exp.rkt 34:25 (exp -12.2-4.7i) -- unboxed unary float complex
+TR opt: unboxed-exp.rkt 35:30 12.2-4.7i -- unboxed literal
+TR opt: unboxed-exp.rkt 35:25 (exp 12.2-4.7i) -- unboxed unary float complex
+TR opt: unboxed-exp.rkt 36:30 -12.2+4.7i -- unboxed literal
+TR opt: unboxed-exp.rkt 36:25 (exp -12.2+4.7i) -- unboxed unary float complex
+"-7.14373-1.88821"
+"1.000000.00000"
+"-0.000000.00001"
+"-2462.73189198773.89558"
+"-0.00000-0.00001"
 )
 
 #lang typed/racket
 #:optimize
 
-(exp 2.0+3.4i)
-(exp 0.0+0.0i)
-(exp -12.2-4.7i)
-(exp 12.2-4.7i)
-(exp -12.2+4.7i)
+(: complex->decimal-string (Float-Complex -> String))
+(define (complex->decimal-string p)
+  (string-append (real->decimal-string (real-part p) 5)
+                 (real->decimal-string (imag-part p) 5)))
+
+(complex->decimal-string (exp 2.0+3.4i))
+(complex->decimal-string (exp 0.0+0.0i))
+(complex->decimal-string (exp -12.2-4.7i))
+(complex->decimal-string (exp 12.2-4.7i))
+(complex->decimal-string (exp -12.2+4.7i))
