a929bb21fc044da94bf82a909e1ccb494ec2e917
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-06-15 14:27:59 +0800

    Don't export internal names for define & lambda.

--- collects/typed-racket/base-env/prims.rkt
+++ collects/typed-racket/base-env/prims.rkt
@@ -19,7 +19,9 @@ This file defines two sorts of primitives. All of them are provided into any mod
 |#
 
 
-(provide (except-out (all-defined-out) dtsi* let-internal: define-for-variants define-for*-variants with-handlers: for/annotation for*/annotation define-for/sum:-variants)
+(provide (except-out (all-defined-out) dtsi* let-internal: define-for-variants define-for*-variants 
+                     with-handlers: for/annotation for*/annotation define-for/sum:-variants
+                     -lambda -define)
          :
          (rename-out [define-typed-struct define-struct:]
                      [lambda: λ:]
