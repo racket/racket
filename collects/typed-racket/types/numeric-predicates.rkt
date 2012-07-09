419b59e88ea025a4d6cd2fa2cdbe62eb64529c78
Neil Toronto <ntoronto@racket-lang.org>
2012-07-09 14:28:36 -0700

    Added inline hint around `index?' definition; my own timing suggests 2x speedup

--- collects/typed-racket/types/numeric-predicates.rkt
+++ collects/typed-racket/types/numeric-predicates.rkt
@@ -1,10 +1,13 @@
 #lang racket/base
 
-(require racket/unsafe/ops)
+(require racket/unsafe/ops
+         racket/performance-hint)
 
 (provide index? exact-rational?)
 
 ;; this is required for template in numeric-tower.rkt
+
+(begin-encourage-inline
 
 ;; we assume indexes are 2 bits shorter than fixnums
 ;; We're generating a reference to fixnum? rather than calling it, so
@@ -12,3 +15,5 @@
 (define (index? x) (and (fixnum? x) (unsafe-fx>= x 0) (fixnum? (* x 4))))
 
 (define (exact-rational? x) (and (rational? x) (exact? x)))
+
+) ; begin-encourage-inline
