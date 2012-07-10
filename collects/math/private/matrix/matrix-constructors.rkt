f8c4e2e832fa5d268a45b27c7f356312ee0e81e6
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-10 12:22:00 +0200

    Added tests to matrix-constructors.

--- collects/math/private/matrix/matrix-constructors.rkt
+++ collects/math/private/matrix/matrix-constructors.rkt
@@ -9,3 +9,19 @@
 
 (: flidentity-matrix (Integer -> (lazy-array Float)))
 (define (flidentity-matrix size) (diagonal-array 2 size 1.0 0.0))
+
+(module* test typed/racket
+  (require typed/rackunit 
+           (submod "..")
+           "../../array.rkt")
+  (check-equal? (array->list (identity-matrix 1)) '[[1]])
+  (check-equal? (array->list (identity-matrix 2)) '[[1 0] [0 1]])
+  (check-equal? (array->list (identity-matrix 3)) '[[1 0 0] [0 1 0] [0 0 1]]) 
+  (check-equal? (array->list (flidentity-matrix 1)) '[[1.]])
+  (check-equal? (array->list (flidentity-matrix 2)) '[[1. 0.] [0. 1.]])
+  (check-equal? (array->list (flidentity-matrix 3)) '[[1. 0. 0.] [0. 1. 0.] [0. 0. 1.]]))
+  
+  
+
+  
+
