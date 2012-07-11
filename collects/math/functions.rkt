431d58c92599792aae1c6c4c59074498962ce5af
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-11 12:22:22 +0200

    Added tests for hypot, flhypot and fllog/base.

--- collects/math/functions.rkt
+++ collects/math/functions.rkt
@@ -47,3 +47,28 @@ Move racket/math functions here; racket/math reexports
 ;; from plot:
 ;; floor-log/base
 ;; ceiling-log/base
+
+(module* test typed/racket/base
+  (require (submod "..") 
+           typed/rackunit
+           math/constants)
+  
+  (define ε (* 2 +epsilon.0))
+  
+  (check-equal? (flhypot 0. 0.) 0.)
+  (check-equal? (flhypot 3. 0.) 3.)
+  (check-equal? (flhypot 3. 4.) 5.)
+  (check-equal? (flhypot -3. -4.) 5.)
+  (check-equal? (flhypot 4. 3.) 5.)
+  (check-= (flhypot 2. 3.) 3.6055512754639892931 ε)
+  (check-= (flhypot 3. 2.) 3.6055512754639892931 ε)
+  
+  (check-equal? (hypot 0 0) 0)
+  (check-equal? (hypot 3 0) 3)
+  (check-equal? (hypot 3 4) 5)
+  (check-equal? (hypot -3 -4) 5)
+  (check-equal? (hypot 4 3) 5)
+  (check-= (hypot 2 3) 3.6055512754639892931 ε)
+  (check-= (hypot 3 2) 3.6055512754639892931 ε)
+  
+  (check-equal? (fllog/base 2.0 (expt 2.0 5.0)) 5.0))
