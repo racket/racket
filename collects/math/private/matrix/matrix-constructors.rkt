6b5ff5b65a6a7d499e42eb8ed92a2ce1a31cb62c
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-12 14:37:57 +0200

    Added const-matrix.

--- collects/math/private/matrix/matrix-constructors.rkt
+++ collects/math/private/matrix/matrix-constructors.rkt
@@ -2,7 +2,7 @@
 
 (require "../../array.rkt")
 
-(provide identity-matrix flidentity-matrix)
+(provide identity-matrix flidentity-matrix const-matrix)
 
 (: identity-matrix (Integer -> (lazy-array Real)))
 (define (identity-matrix size) (diagonal-array 2 size 1 0))
@@ -10,18 +10,21 @@
 (: flidentity-matrix (Integer -> (lazy-array Float)))
 (define (flidentity-matrix size) (diagonal-array 2 size 1.0 0.0))
 
+(: const-matrix (All (A) (Integer Integer A -> (lazy-array A))))
+(define (const-matrix m n x)
+  (const-array (list m n) x))
+
+
 (module* test typed/racket
   (require typed/rackunit 
            (submod "..")
            "../../array.rkt")
+  
   (check-equal? (array->list (identity-matrix 1)) '[[1]])
   (check-equal? (array->list (identity-matrix 2)) '[[1 0] [0 1]])
   (check-equal? (array->list (identity-matrix 3)) '[[1 0 0] [0 1 0] [0 0 1]]) 
   (check-equal? (array->list (flidentity-matrix 1)) '[[1.]])
   (check-equal? (array->list (flidentity-matrix 2)) '[[1. 0.] [0. 1.]])
-  (check-equal? (array->list (flidentity-matrix 3)) '[[1. 0. 0.] [0. 1. 0.] [0. 0. 1.]]))
+  (check-equal? (array->list (flidentity-matrix 3)) '[[1. 0. 0.] [0. 1. 0.] [0. 0. 1.]])
   
-  
-
-  
-
+  (check-equal? (array->list (const-matrix 2 3 0)) '((0 0 0) (0 0 0))))
