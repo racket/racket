eb44f2897586772a6a6677b69fda9843f45b24d6
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-12 15:15:24 +0200

    More constructors: list->matrix, fllist->matrix, vector->matrix, flvector->matrix.

--- collects/math/private/matrix/matrix-constructors.rkt
+++ collects/math/private/matrix/matrix-constructors.rkt
@@ -2,7 +2,10 @@
 
 (require "../../array.rkt")
 
-(provide identity-matrix flidentity-matrix const-matrix)
+(provide identity-matrix flidentity-matrix 
+         matrix->list list->matrix fllist->matrix
+         matrix->vector vector->matrix flvector->matrix
+         const-matrix)
 
 (: identity-matrix (Integer -> (lazy-array Real)))
 (define (identity-matrix size) (diagonal-array 2 size 1 0))
@@ -13,6 +16,30 @@
 (: const-matrix (All (A) (Integer Integer A -> (lazy-array A))))
 (define (const-matrix m n x)
   (const-array (list m n) x))
+
+(: list->matrix : (Listof* Number) -> (lazy-array Number))
+(define (list->matrix rows)
+  (list->array number? rows))
+
+(: fllist->matrix : (Listof* Flonum) -> (lazy-array Flonum))
+(define (fllist->matrix rows)
+  (list->array flonum? rows))
+
+(: matrix->list : (All (A) (Array A) -> (Listof* A)))
+(define (matrix->list a)
+  (array->list a))
+
+(: vector->matrix : (Vectorof* Number) -> (lazy-array Number))
+(define (vector->matrix rows)
+  (vector->array number? rows))
+
+(: flvector->matrix : (Vectorof* Flonum) -> (lazy-array Flonum))
+(define (flvector->matrix rows)
+  (vector->array flonum? rows))
+
+(: matrix->vector : (All (A) (Array A) -> (Vectorof* A)))
+(define (matrix->vector a)
+  (array->vector a))
 
 (module* test typed/racket
   (require typed/rackunit 
@@ -27,4 +54,10 @@
   (check-equal? (array->list (flidentity-matrix 3)) '[[1. 0. 0.] [0. 1. 0.] [0. 0. 1.]])
   
   (check-equal? (array->list (const-matrix 2 3 0)) '((0 0 0) (0 0 0)))
-  (check-equal? (array->list (const-matrix 2 3 0.)) '((0. 0. 0.) (0. 0. 0.))))
+  (check-equal? (array->list (const-matrix 2 3 0.)) '((0. 0. 0.) (0. 0. 0.)))
+  
+  (check-equal? (matrix->list (list->matrix '((1 2) (3 4)))) '((1 2) (3 4)))
+  (check-equal? (matrix->list (fllist->matrix '((1. 2.) (3. 4.)))) '((1. 2.) (3. 4.)))
+  
+  (check-equal? (matrix->vector (vector->matrix '#(#(1 2) #(3 4)))) '#(#(1 2) #(3 4)))
+  (check-equal? (matrix->vector (flvector->matrix '#(#(1. 2.) #(3. 4.)))) '#(#(1. 2.) #(3. 4.))))
