8ac39ac0f64711cbf7435d82eca08d8960361949
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-13 15:40:54 +0200

    Introduced Matrix and Result-Matrix. Moved tests to matrix-tests.rkt. Removed submodules.

--- collects/math/private/matrix/matrix-multiply.rkt
+++ collects/math/private/matrix/matrix-multiply.rkt
@@ -2,9 +2,9 @@
 
 (require racket/unsafe/ops
          "../../array.rkt"
-         "utils.rkt")
+         "matrix-types.rkt")
 
-(provide matrix* matrix-fl*)
+(provide matrix*)
 
 ;; The `make-matrix-*' operators have to be macros; see ../array/array-pointwise.rkt for an
 ;; explanation.
@@ -51,43 +51,10 @@
 
 ;; ---------------------------------------------------------------------------------------------------
 
-(: matrix* (case-> ((Array Real)   (Array Real)   -> (lazy-array Real))
-                   ((Array Number) (Array Number) -> (lazy-array Number))))
+(: matrix* (case-> ((Matrix Real)   (Matrix Real)   -> (Result-Matrix Real))
+                   ((Matrix Number) (Matrix Number) -> (Result-Matrix Number))))
 (define matrix* (make-matrix-multiply 'matrix* array-axis-sum array*))
 
-(: matrix-fl* ((Array Float) (Array Float) -> (lazy-array Float)))
-(define matrix-fl* (make-matrix-multiply 'matrix-fl* array-axis-flsum array-fl*))
+;(: matrix-fl* ((Array Float) (Array Float) -> (lazy-array Float)))
+;(define matrix-fl* (make-matrix-multiply 'matrix-fl* array-axis-flsum array-fl*))
 
-(module* test typed/racket
-  (require typed/rackunit 
-           (submod "..")
-           "../../array.rkt")
-  
-  (define-syntax (check-matrix= stx)
-    ; Note: Does define-simple-check work with Typed Racket?
-    (syntax-case stx ()
-      [(_ a b) 
-       (syntax/loc stx (check-equal? (array->list a) (array->list b)))]))
-  
-  (: matrix : (Rec T (U Real (Listof T))) -> (Array Real))
-  (define (matrix rows) (list->array real? rows))
-  
-  (: flmatrix : (Rec T (U Float (Listof T))) -> (Array Float))
-  (define (flmatrix rows) (list->array flonum? rows))
-  
-  (: rows->flrows : (Listof (Listof Real)) -> (Listof (Listof Float)))
-  (define (rows->flrows rows)
-    (: row->flrow : (Listof Real) -> (Listof Float))
-    (define (row->flrow row) (map real->double-flonum row))
-    (map row->flrow rows))
-  
-  (let ()
-    (define-values (A B AB) (values '[[1 2] [3 4]] '[[5 6] [7 8]] '[[19 22] [43 50]]))
-    (define-values (A.0 B.0 AB.0) (values (rows->flrows A) (rows->flrows B) (rows->flrows AB)))
-    (check-matrix= (matrix* (matrix A) (matrix B)) (matrix AB))
-    (check-matrix= (matrix-fl* (flmatrix A.0) (flmatrix B.0)) (flmatrix AB.0)))
-  (let () 
-    (define-values (A B AB) (values '[[1 2] [3 4]] '[[5 6 7] [8 9 10]] '[[21 24 27] [47 54 61]]))
-    (define-values (A.0 B.0 AB.0) (values (rows->flrows A) (rows->flrows B) (rows->flrows AB)))
-    (check-matrix= (matrix* (matrix A) (matrix B)) (matrix AB))
-    (check-matrix= (matrix-fl* (flmatrix A.0) (flmatrix B.0)) (flmatrix AB.0))))
--- collects/math/private/matrix/utils.rkt
+++ collects/math/private/matrix/utils.rkt
@@ -4,6 +4,3 @@
 
 (provide (all-defined-out))
 
-(: array-matrix? (All (A) ((Array A) -> Boolean)))
-(define (array-matrix? x)
-  (= 2 (array-dims x)))
