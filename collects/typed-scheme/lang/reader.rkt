24b314a40ade5c576912eea416a68d78e09ca01b
Vincent St-Amour <stamourv@racket-lang.org>
2012-07-19 15:57:03 -0400

    Only enable Optimization Coach button in Typed Racket.
    Other languages can access it through the View menu.
    
    Please merge to release.

--- collects/typed-scheme/lang/reader.rkt
+++ collects/typed-scheme/lang/reader.rkt
@@ -10,4 +10,7 @@ typed-scheme
 
 (define (make-info key default use-default)
   (case key
+    [(drscheme:toolbar-buttons)
+     (list (dynamic-require 'typed-racket/optimizer/tool/tool
+                            'optimization-coach-drracket-button))]
     [else (use-default key default)]))
