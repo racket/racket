b7ef0a11b78f7268b159717a55a75ad10a398039
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-08-01 19:29:45 +0200

    Fix type error

--- collects/math/private/matrix/matrix-2d.rkt
+++ collects/math/private/matrix/matrix-2d.rkt
@@ -16,31 +16,27 @@
 (define (matrix-2d-rotation θ)
   (define cosθ (cos θ))
   (define sinθ (sin θ))
-  (vector->matrix
-   (vector 
-    (vector cosθ (- sinθ))
-    (vector sinθ cosθ))))
+  (matrix/dim 2 2 
+              cosθ (- sinθ)
+              sinθ cosθ))
 
 (: matrix-2d-scaling : Real Real -> (Result-Matrix Number))
 (define (matrix-2d-scaling sx sy)
-  (vector->matrix
-   (vector 
-    (vector sx 0)
-    (vector 0 sy))))
+  (matrix/dim 2 2 
+              sx 0
+              0 sy))
 
 (: matrix-2d-shear-x : Real -> (Result-Matrix Number))
 (define (matrix-2d-shear-x k)
-  (vector->matrix
-   (vector 
-    (vector 1 k)
-    (vector 0 1))))
+  (matrix/dim 2 2 
+              1 k
+              0 1))
 
 (: matrix-2d-shear-y : Real -> (Result-Matrix Number))
 (define (matrix-2d-shear-y k)
-  (vector->matrix
-   (vector 
-    (vector 1 0)
-    (vector k 1))))
+  (matrix/dim 2 2 
+              1 0
+              k 1))
 
 (: matrix-2d-reflection : Real Real -> (Result-Matrix Number))
 (define (matrix-2d-reflection a b)
@@ -50,10 +46,9 @@
   (define 2ab (* 2 a b))
   (define norm2 (+ a2 b2))
   (define 2ab/norm2 (/ 2ab norm2))
-  (vector->matrix
-   (vector
-    (vector (/ (- a2 b2) norm2) 2ab/norm2)
-    (vector 2ab/norm2           (/ (- b2 a2) norm2)))))
+  (matrix/dim 2 2 
+              (/ (- a2 b2) norm2) 2ab/norm2
+              2ab/norm2           (/ (- b2 a2) norm2)))
 
 (: matrix-2d-orthogonal-projection : Real Real -> (Result-Matrix Number))
 ; orthogonal projection onto the line through (0,0) and (a,b)
@@ -63,8 +58,6 @@
   (define ab (* a b))
   (define norm2 (+ a2 b2))
   (define ab/norm2 (/ ab norm2))
-  (vector->matrix
-   (vector
-    (vector (/ a2 norm2) ab/norm2)
-    (vector ab/norm2     (/ b2 norm2)))))
-
+  (matrix/dim 2 2 
+              (/ a2 norm2) ab/norm2
+              ab/norm2     (/ b2 norm2)))
