3e8bd25cc4ce6ff23fb46fa3c35479f067d570c3
Eric Dobson <eric.n.dobson@gmail.com>
2012-08-15 19:44:01 -0700

    Move TR list special cases to tc-app-list.rkt.

--- collects/typed-racket/typecheck/typechecker.rkt
+++ collects/typed-racket/typecheck/typechecker.rkt
@@ -6,7 +6,9 @@
                   provide-signature-elements
                   define-values/invoke-unit/infer link)
          "signatures.rkt"
-         "tc-app/tc-app-hetero.rkt" "signatures.rkt"
+         "tc-app/tc-app-hetero.rkt"
+         "tc-app/tc-app-list.rkt"
+         "signatures.rkt"
          "tc-if.rkt" "tc-lambda-unit.rkt" "tc-app.rkt"
          "tc-let-unit.rkt" "tc-apply.rkt"
          "tc-expr-unit.rkt" "check-subforms-unit.rkt")
@@ -15,4 +17,4 @@
 
 (define-values/invoke-unit/infer
   (link tc-if@ tc-lambda@ tc-app@ tc-let@ tc-expr@ check-subforms@ tc-apply@
-        tc-app-hetero@))
+        tc-app-hetero@ tc-app-list@))
