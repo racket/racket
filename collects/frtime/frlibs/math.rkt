4ecc0525e73800ea7d964340b500e3c96338e6ec
Patrick Mahoney <paddy.mahoney@gmail.com>
2012-08-21 14:34:57 -0400

    Use lifted Racket math definitions
    Continue to provide the same definitions, but lift the Racket ones
    rather than rewrite and redefine them.

--- collects/frtime/frlibs/math.rkt
+++ collects/frtime/frlibs/math.rkt
@@ -4,47 +4,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (module math frtime/frtime-lang-only
-  (provide e 
-	  pi 
+  (require (only-in racket/math pi sqr sgn conjugate sinh cosh))
+  
+  (provide (lifted
 	  sqr
 	  sgn conjugate
-	  sinh cosh)
-
-  (define (sqr z) (* z z))
+	  sinh cosh))
+  
+  (provide pi e)
   
   ;; circular constants and aliases
-  (define e (exp 1.0)) 
-  (define pi (atan 0 -1))
+  (define e (exp 1.0)))
   
-  ;; sgn function 
-  (define sgn
-    (lambda (x)
-      (if (exact? x)
-	  (cond
-	   ((< x 0) -1)
-	   ((> x 0)  1)
-	   (else 0))
-	  (cond
-	   ((< x 0.0) -1.0)
-	   ((> x 0.0)  1.0)
-	   (else 0.0)))))
-  
-  ;; complex conjugate
-  (define conjugate
-    (lambda (z)
-      (make-rectangular 
-       (real-part z)
-       (- (imag-part z)))))
-  
-  ;; real hyperbolic functions
-  (define sinh 
-    (lambda (x)
-      (/
-       (- (exp x) (exp (- x)))
-       2.0)))
-  
-  (define cosh
-    (lambda (x)
-      (/
-       (+ (exp x) (exp (- x)))
-       2.0))))
