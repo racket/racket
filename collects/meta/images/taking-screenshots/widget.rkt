e4f03a404595f3c886110f55a50542bc8e3fe3ec
Eli Barzilay <eli@barzilay.org>
2012-09-19 23:04:25 -0400

    Improvements to "meta/images/taking-screenshots".
    Improve text layout and avoid enforcing org-mode, improve the racket
    script.

--- collects/meta/images/taking-screenshots/widget.rkt
+++ collects/meta/images/taking-screenshots/widget.rkt
@@ -2,17 +2,13 @@
 
 (provide showcase)
 
-(define showcase (lambda (#:hmult (hmult 1) . name)
-                   (define frame (new frame% (label (if (null? name)
-                                                        "widget"
-                                                        (car name)))))
-                   (define panel (new panel% (parent frame)))
-                   (define showcase-panel (new horizontal-panel%
-                                               (parent panel)
-                                               (alignment '(center center))
-                                               (style '(border))
-                                               (min-width 242)
-                                               (min-height (* hmult 75))
-                                               (border 20)))
-                   (send frame show #t)
-                   showcase-panel))
+(define (showcase [name "widget"] #:hmult (hmult 1))
+  (define frame (new frame% [label name]))
+  (define panel (new panel% [parent frame]))
+  (define showcase-panel
+    (new horizontal-panel%
+         [parent panel] [alignment '(center center)]
+         [min-width 242] [min-height (* hmult 75)]
+         [style '(border)] [border 20]))
+  (send frame show #t)
+  showcase-panel)
