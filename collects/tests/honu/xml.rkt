59ec52dd8b7e452740a407706ab7da80f4c29ebe
Jon Rafkind <rafkind@racket-lang.org>
2012-10-11 14:31:12 -0600

    [honu] remove debugging lines

--- collects/tests/honu/xml.rkt
+++ collects/tests/honu/xml.rkt
@@ -36,18 +36,14 @@
     (define-literal-set xml-literals (honu-< honu-> honu-/))
 
     (define-splicing-syntax-class non-xml-id #:literal-sets (xml-literals)
-      [pattern (~and (~not honu-<) x:id) #:with q (begin (debug "non xml id ~a\n" #'x) #'x)]
-      [pattern x #:with q #'0 #:when (begin (debug "not an xml id ~a\n" #'x) #f)]
-      )
+      [pattern (~and (~not honu-<) x:id)])
 
     (define-splicing-syntax-class node
       #:literal-sets (xml-literals)
-      [pattern x #:with result #'0 #:when (begin (debug "xml node ~a\n" #'x) #f)]
       [pattern (~seq honu-< start:id honu-> more:node ... honu-< honu-/ end:id honu->)
                #:with result (racket-syntax (make-xml 'start more.result ...))]
       [pattern body:honu-body #:with result #'body.result]
       [pattern (~seq plain:non-xml-id plain*:non-xml-id ...) #:with result #''(plain plain* ...)]
-      [pattern x #:with result #'0 #:when (begin (debug "xml2 node ~a\n" #'x) #f)]
       )
 
     (debug "Parse xml ~a\n" code)
