1151094929265b66481e72a85d4654511f8b160e
Neil Toronto <ntoronto@racket-lang.org>
2012-10-17 15:36:37 -0600

    Made for/array body type optional (returns (Array Any), but at least it "works")

--- collects/math/private/array/array-comprehension.rkt
+++ collects/math/private/array/array-comprehension.rkt
@@ -14,21 +14,23 @@
 (define-syntax (base-for/array: stx)
   (syntax-parse stx #:literals (:)
     [(_ name:id for/vector:id #:shape ds-expr:expr (~optional (~seq #:fill fill-expr:expr))
-        (clause ...) : A:expr body:expr ...+)
-     (with-syntax ([(maybe-fill ...)  (if (attribute fill-expr) #'(#:fill fill-expr) #'())])
+        (clause ...) (~optional (~seq : A:expr)) body:expr ...+)
+     (with-syntax ([(maybe-fill ...)  (if (attribute fill-expr) #'(#:fill fill-expr) #'())]
+                   [(maybe-type ...)  (if (attribute A) #'(: A) #'())])
        (syntax/loc stx
          (let*: ([ds : User-Indexes  ds-expr]
                  [ds : Indexes  (check-array-shape
                                  ds (λ () (raise-argument-error 'name "Indexes" ds)))])
            (define vs (for/vector #:length (array-shape-size ds) maybe-fill ...
-                        (clause ...) : A body ...))
+                        (clause ...) maybe-type ... body ...))
            (unsafe-mutable-array ds vs))))]
-    [(_ name:id for/vector:id (clause ...) : A:expr body:expr ...+)
-     (syntax/loc stx
-       (let ()
-         (define vs (for/vector (clause ...) : A body ...))
-         (define ds ((inst vector Index) (vector-length vs)))
-         (unsafe-mutable-array ds vs)))]))
+    [(_ name:id for/vector:id (clause ...) (~optional (~seq : A:expr)) body:expr ...+)
+     (with-syntax ([(maybe-type ...)  (if (attribute A) #'(: A) #'())])
+       (syntax/loc stx
+         (let ()
+           (define vs (for/vector (clause ...) maybe-type ... body ...))
+           (define ds ((inst vector Index) (vector-length vs)))
+           (unsafe-mutable-array ds vs))))]))
 
 (define-syntax-rule (for/array: e ...)
   (base-for/array: for/array: for/vector: e ...))
