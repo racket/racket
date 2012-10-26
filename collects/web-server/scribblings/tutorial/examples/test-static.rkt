7f23a85e1598190bea7276623a48af058e3f897e
Jay McCarthy <jay@racket-lang.org>
2012-10-26 14:46:32 -0600

    fixing PR13210

--- collects/web-server/scribblings/tutorial/examples/test-static.rkt
+++ collects/web-server/scribblings/tutorial/examples/test-static.rkt
@@ -1,10 +1,10 @@
 #lang web-server/insta
 (define (start request)
   (response/xexpr
-   '(html (head (title "Testing"))
-          (link ((rel "stylesheet")
-                 (href "/test-static.css")
-                 (type "text/css")))
+   '(html (head (title "Testing")
+                (link ((rel "stylesheet")
+                       (href "/test-static.css")
+                       (type "text/css"))))          
           (body (h1 "This is a header")
                 (p "This is " (span ((class "hot")) "hot") ".")))))
 (static-files-path "htdocs")
