eb97b2f193024b1314c5ecc058ab449b228723fc
Matthew Flatt <mflatt@racket-lang.org>
2012-10-29 07:31:54 -0600

    fix in srfi/1
    Closes PR 13214

--- collects/srfi/1/fold.rkt
+++ collects/srfi/1/fold.rkt
@@ -54,7 +54,7 @@
   (check-arg procedure? p 'unfold-right)
   (check-arg procedure? f 'unfold-right)
   (check-arg procedure? g 'unfold-right)
-  (let lp ((seed seed) (ans maybe-tail))
+  (let lp ((seed seed) (ans (if (pair? maybe-tail) (car maybe-tail) '())))
     (if (p seed) ans
         (lp (g seed)
             (cons (f seed) ans)))))
@@ -249,3 +249,9 @@
         (recur (cdr lis))))))
 
 ;;; fold.rkt ends here
+
+(module+ test
+  (define (test a b)
+    (unless (equal? a b) (error 'test "failed: ~e vs. ~e" a b)))
+  (test (unfold-right null? car cdr '(2 3) '(4 5))
+        '(3 2 4 5)))
