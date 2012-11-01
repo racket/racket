0e71f2d5dc58bd497a686f23c0ed0781590a3dc6
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-10-31 21:13:01 -0400

    Fix accidental use of the wrong letrec-bound variable.

---
+++ collects/tests/typed-racket/succeed/exn-any.rkt
@@ -0,0 +1,15 @@
+#lang racket/load
+
+(module m typed/racket
+  (struct: s ())
+  
+  (struct: s2 s ())
+  (define:  v : Any (s2))
+  (provide v))
+
+(module n racket
+  (require 'm)
+  v)
+
+(require 'n)
+
