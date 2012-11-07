racket/racket

commit 733d95bb893e986d4768504ce51818c214ffde9e

path->url: ensure directory paths have a trailing slash
           If a path to a directory is converted to an URL it loses
           its trailing path separator.
           This can lead to a bug if the URL is used as a base URL to
           build other URLs (e.g. combine-url/relative).

collects/net/url.rkt
@@ -558,47 +558,53 @@
                ";"))
 
 (define (path->url path)
-  (let ([url-path
-         (let loop ([path (simplify-path path #f)][accum null])
-           (let-values ([(base name dir?) (split-path path)])
-             (cond
-               [(not base)
-                (append (map
-                         (lambda (s)
-                           (make-path/param s null))
-                         (if (eq? (path-convention-type path) 'windows)
-                           ;; For Windows, massage the root:
-                           (let ([s (regexp-replace
-                                     #rx"[/\\\\]$"
-                                     (bytes->string/utf-8 (path->bytes name))
-                                     "")])
-                             (cond
-                               [(regexp-match? #rx"^\\\\\\\\[?]\\\\[a-zA-Z]:" s)
-                                ;; \\?\<drive>: path:
-                                (regexp-split #rx"[/\\]+" (substring s 4))]
-                               [(regexp-match? #rx"^\\\\\\\\[?]\\\\UNC" s)
-                                ;; \\?\ UNC path:
-                                (regexp-split #rx"[/\\]+" (substring s 7))]
-                               [(regexp-match? #rx"^[/\\]" s)
-                                ;; UNC path:
-                                (regexp-split #rx"[/\\]+" s)]
-                               [else
-                                (list s)]))
-                           ;; On other platforms, we drop the root:
-                           null))
-                        accum)]
-               [else
-                (let ([accum (cons (make-path/param
-                                    (if (symbol? name)
-                                      name
-                                      (bytes->string/utf-8
-                                       (path-element->bytes name)))
-                                    null)
-                                   accum)])
-                  (if (eq? base 'relative)
-                    accum
-                    (loop base accum)))])))])
-    (make-url "file" #f "" #f (absolute-path? path) url-path '() #f)))
+  (let* ([spath (simplify-path path #f)]
+         [dir? (let-values ([(b n dir?) (split-path spath)]) dir?)]
+         ;; If original path is a directory the resulting URL
+         ;; should have a trailing forward slash
+         [url-tail (if dir? (list (make-path/param "" null)) null)]
+         [url-path
+          (let loop ([path spath][accum null])
+            (let-values ([(base name dir?) (split-path path)])
+              (cond
+                [(not base)
+                 (append (map
+                          (lambda (s)
+                            (make-path/param s null))
+                          (if (eq? (path-convention-type path) 'windows)
+                              ;; For Windows, massage the root:
+                              (let ([s (regexp-replace
+                                        #rx"[/\\\\]$"
+                                        (bytes->string/utf-8 (path->bytes name))
+                                        "")])
+                                (cond
+                                  [(regexp-match? #rx"^\\\\\\\\[?]\\\\[a-zA-Z]:" s)
+                                   ;; \\?\<drive>: path:
+                                   (regexp-split #rx"[/\\]+" (substring s 4))]
+                                  [(regexp-match? #rx"^\\\\\\\\[?]\\\\UNC" s)
+                                   ;; \\?\ UNC path:
+                                   (regexp-split #rx"[/\\]+" (substring s 7))]
+                                  [(regexp-match? #rx"^[/\\]" s)
+                                   ;; UNC path:
+                                   (regexp-split #rx"[/\\]+" s)]
+                                  [else
+                                   (list s)]))
+                              ;; On other platforms, we drop the root:
+                              null))
+                         accum)]
+                [else
+                 (let ([accum (cons (make-path/param
+                                     (if (symbol? name)
+                                         name
+                                         (bytes->string/utf-8
+                                          (path-element->bytes name)))
+                                     null)
+                                    accum)])
+                   (if (eq? base 'relative)
+                       accum
+                       (loop base accum)))])))])
+    (make-url "file" #f "" #f (absolute-path? path) (append url-path url-tail) '() #f)))
+
 
 (define (url->path url [kind (system-path-convention-type)])
   (file://->path url kind))

commit 45cff28b2aea83d80adc88c49e75a9d79c9b74eb

Added path->url tests for directory paths
      This check that no regression was introduced with 733d95b.

collects/tests/net/url.rkt
@@ -181,22 +181,40 @@
     (test (string->url/vec "http://foo:/abc/def.html")
           => #("http" #f "foo" #f #t (#("abc") #("def.html")) () #f)))
 
-  (test (url->string (path->url (bytes->path #"c:\\a\\b" 'windows)))
+  (test (url->string (path->url (bytes->path #"/a/b" 'unix)))
+        => "file:///a/b"
+        (url->string (path->url (bytes->path #"/a/b/" 'unix)))
+        => "file:///a/b/"
+        (url->string (path->url (bytes->path #"c:\\a\\b" 'windows)))
         => "file:///c:/a/b"
+        (url->string (path->url (bytes->path #"c:\\a\\b\\" 'windows)))
+        => "file:///c:/a/b/"
         (url->string (path->url (bytes->path #"\\\\?\\c:\\a\\b" 'windows)))
-        => "file:///c:/a/b")
+        => "file:///c:/a/b"
+        (url->string (path->url (bytes->path #"\\\\?\\c:\\a\\b\\" 'windows)))
+        => "file:///c:/a/b/")
 
   (test
    (path->bytes (url->path (path->url (bytes->path #"/a/b/c" 'unix)) 'unix))
    => #"/a/b/c"
+   (path->bytes (url->path (path->url (bytes->path #"/a/b/c/" 'unix)) 'unix))
+   => #"/a/b/c/."
    (path->bytes (url->path (path->url (bytes->path #"a/b/c" 'unix)) 'unix))
    => #"a/b/c"
+   (path->bytes (url->path (path->url (bytes->path #"a/b/c/" 'unix)) 'unix))
+   => #"a/b/c/."
    (path->bytes (url->path (path->url (bytes->path #"c:/a/b" 'windows)) 'windows))
    => #"c:\\a\\b"
+   (path->bytes (url->path (path->url (bytes->path #"c:/a/b/" 'windows)) 'windows))
+   => #"c:\\a\\b\\."
    (path->bytes (url->path (path->url (bytes->path #"a/b" 'windows)) 'windows))
    => #"a\\b"
+   (path->bytes (url->path (path->url (bytes->path #"a/b/" 'windows)) 'windows))
+   => #"a\\b\\."
    (path->bytes (url->path (path->url (bytes->path #"//d/c/a" 'windows)) 'windows))
    => #"\\\\d\\c\\a"
+   (path->bytes (url->path (path->url (bytes->path #"//d/c/a/" 'windows)) 'windows))
+   => #"\\\\d\\c\\a\\."
    (path->bytes (url->path (path->url (bytes->path #"\\\\?\\c:\\a\\b" 'windows)) 'windows))
    => #"c:\\a\\b"
    (path->bytes (url->path (path->url (bytes->path #"\\\\?\\UNC\\d\\c\\a\\b" 'windows)) 'windows))