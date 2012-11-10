b4a98af3197ec0828dbe83ce835453598459bd22
Asumu Takikawa <asumu@ccs.neu.edu>
2012-11-10 12:03:47 -0500

    Fix whitespace in test

--- collects/tests/typed-racket/succeed/custodian.rkt
+++ collects/tests/typed-racket/succeed/custodian.rkt
@@ -1,6 +1,7 @@
-#lang typed/racket                                                                                                                                                                                                                        
+#lang typed/racket
 
 ;; Make sure the Custodian type is bound
 
 (: cust Custodian)
 (define cust (current-custodian))
+
