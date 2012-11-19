3e505af4f95ba07e8e2a760e2dd391aeb4969c47
Matthew Flatt <mflatt@racket-lang.org>
2012-11-19 10:54:57 -0700

    remove `sirmail'
    SirMail is moving to its own repo:
     https://github.com/mflatt/sirmail
    Install with
     raco pkg install sirmail

--- collects/sirmail/info.rkt
+++
@@ -1,6 +0,0 @@
-#lang setup/infotab
-
-(define mred-launcher-libraries (list "sirmail.rkt"))
-(define mred-launcher-names (list "SirMail"))
-(define compile-omit-paths '("recover.rkt"))
-(define requires '(("mred") ("openssl")))
