d5f605f58d4fc99ee2b17b0f44f20f0a8c9b72fe
Robby Findler <robby@racket-lang.org>
2012-11-22 09:25:50 -0600

    fix dynamic require for moved file

--- collects/framework/private/frame.rkt
+++ collects/framework/private/frame.rkt
@@ -800,7 +800,7 @@
                          (cond
                            [(or (send evt get-alt-down)
                                 (send evt get-control-down))
-                            (dynamic-require 'framework/private/follow-log #f)]
+                            (dynamic-require 'tests/drracket/follow-log #f)]
                            [else
                             (collect-garbage)
                             (update-memory-text)]))]
