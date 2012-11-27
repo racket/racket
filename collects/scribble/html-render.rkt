racket/racket

commit 8653bc6792b614c69b7d0aab7e329591735a0dad

scribble/html-render: Add missing sep-element for non-CSS browsers

collects/scribble/html-render.rkt
@@ -871,7 +871,7 @@
                  next-content)))))
       (define navbar
         `(div ([class ,(if top? "navsettop" "navsetbottom")])
-           ,navleft ,navright nbsp)) ; need nbsp to make the navset bg visible
+           ,navleft ,sep-element ,navright nbsp)) ; need nbsp to make the navset bg visible
       (if (include-navigation?)
           (list navbar)
           null))

commit 3e81924f848731abda7415d89b4db0ada6ecb308

Fix nav separator for Opera
Commit 8653bc6792b614c69b7d0aab7e329591735a0dad caused
the doc navigation to render oddly on Opera due to how
it handles &nbsp;. This commit retains the spacing
for text browsers, but fixes the navigation on Opera.

collects/scribble/html-render.rkt
@@ -843,6 +843,8 @@
           ""
           `(span ([class "navright"])
              ,@(render
+                ;; put space here for text browsers and to avoid an Opera issue
+                sep-element
                 (make-element
                  (cond [(not parent) "nonavigation"]
                        [prev (titled-url "backward" prev)]
@@ -871,7 +873,7 @@
                  next-content)))))
       (define navbar
         `(div ([class ,(if top? "navsettop" "navsetbottom")])
-           ,navleft ,sep-element ,navright nbsp)) ; need nbsp to make the navset bg visible
+           ,navleft ,navright nbsp)) ; need nbsp to make the navset bg visible
       (if (include-navigation?)
           (list navbar)
           null))