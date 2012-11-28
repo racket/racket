b16679266a04bd7d95a146ba5eff6b6666768d59
Matthew Flatt <mflatt@racket-lang.org>
2012-11-28 10:53:48 -0700

    raco pkg: `--dont-setup' to `--no-setup' and help text adjustments
    Adjustements are for case and setence consistency and to fit
    in 80 columns.

--- collects/planet2/main.rkt
+++ collects/planet2/main.rkt
@@ -4,29 +4,36 @@
          "commands.rkt"
          (prefix-in setup: setup/setup))
 
-(define (setup dont-setup)
-  (unless (or dont-setup
-              (equal? "1" (getenv "PLT_PLANET2_DONTSETUP")))
+(define (setup no-setup)
+  (unless (or no-setup
+              (equal? "1" (getenv "PLT_PLANET2_NOSETUP")))
     (setup:setup)))
 
 (commands
  "This tool is used for managing installed packages."
  [install
   "Install packages"
-  [(#:sym #f) type ("-t") ("type of <pkg-source>: file, dir, url, github, or name"
-                           "If not specified, the type is inferred syntactically")]
-  [#:bool dont-setup () "Don't run 'raco setup' after changing packages (generally not a good idea)"]
+  [(#:sym #f) type ("-t") ("Type of <pkg-source>;"
+                           "options are: file, dir, url, github, or name;"
+                           "if not specified, the type is inferred syntactically")]
+  [#:bool no-setup () ("Don't run 'raco setup' after changing packages"
+                       "(generally not a good idea)")]
   [#:bool installation ("-i") "Operate on the installation-wide package database"]
   [(#:sym #f) deps ()
-   ("Specify the behavior for dependencies."
-    "Options are: fail, force, search-ask, search-auto."
-    "  'fail' cancels the installation if dependencies are unmet (default for most packages)."
-    "  'force' installs the package despite missing dependencies."
-    "  'search-ask' looks for the dependencies on your package naming services (default if package is an indexed name) and asks if you would like it installed."
-    "  'search-auto' is like 'search-ask' but does not ask for permission to install.")]
+   ("Specify the behavior for dependencies;"
+    "options are:"
+    "  fail: cancels the installation if dependencies are unmet"
+    "        (default for most packages)"
+    "  force: installs the package despite missing dependencies"
+    "  search-ask: looks for the dependencies on your package naming services"
+    "              (default if package is an indexed name) and asks if you would"
+    "              like it installed"
+    "  search-auto: like 'search-ask' but does not ask for permission to install")]
   [#:bool force () "Ignores conflicts"]
   [#:bool ignore-checksums () "Ignores checksums"]
-  [#:bool link () "When used with a directory package, leave the directory in place, but add a link to it in the package directory. This is a global setting for all installs for this command, which means it affects dependencies... so make sure the dependencies exist first."]
+  [#:bool link () ("Link a directory package source in place;"
+                   "this is a global setting for all installs for this command, which means"
+                   "that it affects dependencies... so make sure the dependencies exist first")]
   #:args pkg-source
   (parameterize ([current-install-system-wide? installation])
     (with-package-lock
@@ -37,19 +44,24 @@
                   #:type (or type 
                              (and link 'dir))
                   (map (curry cons #f) pkg-source))
-     (setup dont-setup)))]
+     (setup no-setup)))]
  [update
   "Update packages"
-  [#:bool dont-setup () "Don't run 'raco setup' after changing packages (generally not a good idea)"]
+  [#:bool no-setup () ("Don't run 'raco setup' after changing packages"
+                       "(generally not a good idea)")]
   [#:bool installation ("-i") "Operate on the installation-wide package database"]
-  [#:bool all ("-a") "Update all packages (if no packages are given on the command line)"]
+  [#:bool all ("-a") ("Update all packages;"
+                      "only if no packages are given on the command line")]
   [(#:sym #f) deps ()
-   ("Specify the behavior for dependencies."
-    "Options are: fail, force, search-ask, search-auto."
-    "  'fail' cancels the installation if dependencies are unmet (default for most packages)."
-    "  'force' installs the package despite missing dependencies."
-    "  'search-ask' looks for the dependencies on your package naming services (default if package is an indexed name) and asks if you would like it installed."
-    "  'search-auto' is like 'search-ask' but does not ask for permission to install.")]
+   ("Specify the behavior for dependencies;"
+    "options are:"
+    "  fail: cancels the installation if dependencies are unmet"
+    "        (default for most packages)"
+    "  force: installs the package despite missing dependencies"
+    "  search-ask: looks for the dependencies on your package naming services"
+    "              (default if package is an indexed name) and asks if you would"
+    "              like it installed"
+    "  search-auto: like 'search-ask' but does not ask for permission to install")]
   [#:bool update-deps () "Check named packages' dependencies for updates"]
   #:args pkgs
   (parameterize ([current-install-system-wide? installation])
@@ -58,10 +70,11 @@
                             #:all? all
                             #:dep-behavior deps
                             #:deps? update-deps)
-       (setup dont-setup))))]
+       (setup no-setup))))]
  [remove
   "Remove packages"
-  [#:bool dont-setup () "Don't run 'raco setup' after changing packages (generally not a good idea)"]
+  [#:bool no-setup () ("Don't run 'raco setup' after changing packages"
+                       "(generally not a good idea)")]
   [#:bool installation ("-i") "Operate on the installation-wide package database"]
   [#:bool force () "Force removal of packages"]
   [#:bool auto () "Remove automatically installed packages with no dependencies"]
@@ -71,7 +84,7 @@
      (remove-packages pkgs
                       #:auto? auto
                       #:force? force)
-     (setup dont-setup)))]
+     (setup no-setup)))]
  [show
   "Show information about installed packages"
   [#:bool installation ("-i") "Operate on the installation-wide package database"]
@@ -90,10 +103,10 @@
  [create
   "Bundle a new package"
   [(#:str #f) format ()
-   ("Select the format of the package to be created."
-    "Options are: tgz, zip, plt")]
+   ("Select the format of the package to be created;"
+    "options are: tgz, zip, plt")]
   [#:bool manifest () "Creates a manifest file for a directory, rather than an archive"]
   #:args (maybe-dir)
   (unless (or manifest format)
-    (error 'planet2 "You must specify an archive format."))
+    (error 'planet2 "You must specify an archive format"))
   (create-cmd (if manifest "MANIFEST" format) maybe-dir)])
