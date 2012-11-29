ad7ee1d9482e4c13f01879b319816068a7216526
Ryan Culpepper <ryanc@racket-lang.org>
2012-11-29 15:40:26 -0500

    split off common certificate-adding code

--- collects/openssl/private/win32.rkt
+++ collects/openssl/private/win32.rkt
@@ -5,34 +5,9 @@
          ffi/unsafe/define
          ffi/unsafe/alloc
          "../libssl.rkt"
-         "../libcrypto.rkt")
+         "../libcrypto.rkt"
+         "add-cert.rkt")
 (provide load-win32-root-certificates)
-
-;; -- libcrypto
-(define-ffi-definer define-crypto libcrypto
-  #:default-make-fail make-not-available)
-(define-cpointer-type _X509*)
-(define-cpointer-type _X509_STORE*)
-
-;; FIXME: refcounting?
-(define-crypto d2i_X509
-  (_fun (buf) ::
-        (_pointer = #f)
-        ((_ptr i _pointer) = buf)
-        (_int32 = (bytes-length buf))
-        -> _X509*))
-
-(define-crypto X509_STORE_add_cert
-  (_fun _X509_STORE* _X509* -> _int))
-
-;; -- libssl
-
-(define-ffi-definer define-ssl libssl
-  #:default-make-fail make-not-available)
-(define _SSL_CTX* _pointer)
-
-(define-ssl SSL_CTX_get_cert_store
-  (_fun _SSL_CTX* -> _X509_STORE*))
 
 ;; -- Windows CryptoAPI
 
