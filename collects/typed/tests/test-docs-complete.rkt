eb16dd4e1572868929e771c54396aafcf9bbb06d
Asumu Takikawa <asumu@ccs.neu.edu>
2012-11-30 23:43:41 -0500

    Ignore contracted proxies from `racket`

--- collects/typed/tests/test-docs-complete.rkt
+++ collects/typed/tests/test-docs-complete.rkt
@@ -1,12 +1,16 @@
 #lang racket/base
 (require rackunit/docs-complete)
-;; these are currently undocumented in `racket' as well
-(define exclude '(define-place
-                  deserialize-info:set-v0
-                  make-primitive-class
-                  place-sleep
-                  procedure-closure-contents-eq?
-                  processor-count))
+(define exclude
+  '(;; these are currently undocumented in `racket' as well
+    define-place
+    deserialize-info:set-v0
+    make-primitive-class
+    place-sleep
+    procedure-closure-contents-eq?
+    processor-count
+    ;; the following are contracted proxy values that are
+    ;; already documented in `racket'
+    default-continuation-prompt-tag))
 (check-docs (quote typed-scheme) #:skip exclude)
 (check-docs (quote typed/scheme) #:skip exclude)
 (check-docs (quote typed/scheme/base) #:skip exclude)
