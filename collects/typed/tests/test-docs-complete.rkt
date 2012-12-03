25ed52f9d6f407b5ab32ecf5dca70d853d679744
Asumu Takikawa <asumu@ccs.neu.edu>
2012-12-03 11:05:21 -0500

    Revert "Ignore contracted proxies from `racket`"
    This reverts commit 702676030b8d4c956d063b3ce4e3bf0d3dc2c594.
    
    (this should actually be documented)

--- collects/typed/tests/test-docs-complete.rkt
+++ collects/typed/tests/test-docs-complete.rkt
@@ -1,16 +1,12 @@
 #lang racket/base
 (require rackunit/docs-complete)
-(define exclude
-  '(;; these are currently undocumented in `racket' as well
-    define-place
-    deserialize-info:set-v0
-    make-primitive-class
-    place-sleep
-    procedure-closure-contents-eq?
-    processor-count
-    ;; the following are contracted proxy values that are
-    ;; already documented in `racket'
-    default-continuation-prompt-tag))
+;; these are currently undocumented in `racket' as well
+(define exclude '(define-place
+                  deserialize-info:set-v0
+                  make-primitive-class
+                  place-sleep
+                  procedure-closure-contents-eq?
+                  processor-count))
 (check-docs (quote typed-scheme) #:skip exclude)
 (check-docs (quote typed/scheme) #:skip exclude)
 (check-docs (quote typed/scheme/base) #:skip exclude)
