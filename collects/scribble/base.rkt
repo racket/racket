f29230f8f2710b58eae8646edd6eab9f6760deee
Matthew Flatt <mflatt@racket-lang.org>
2012-12-27 08:40:02 -0600

    add `scribble/tag' and clean up some tag-related documentation and functions
    Includes the addition of `make-section-tag' and `taglet?' to
    `scribble/base'.

--- collects/scribble/base.rkt
+++ collects/scribble/base.rkt
@@ -5,12 +5,14 @@
          "manual-struct.rkt"
          "decode-struct.rkt"
          "html-properties.rkt"
+         "tag.rkt"
          scheme/list
          scheme/class
          racket/contract/base
          racket/contract/combinator
-         setup/main-collects
          (for-syntax scheme/base))
+
+(provide (all-from-out "tag.rkt"))
 
 ;; ----------------------------------------
 
@@ -155,96 +157,6 @@
                                       " dot ")
                      (hyperlink (string-append "mailto:" email) email))
                  ">")))
-
-;; ----------------------------------------
-
-; XXX unknown contracts
-(provide intern-taglet
-         module-path-index->taglet
-         doc-prefix)
-(provide/contract
- [module-path-prefix->string (module-path? . -> . string?)])
-
-(require syntax/modcollapse
-         ;; Needed to normalize planet version numbers:
-         (only-in planet/resolver get-planet-module-path/pkg)
-         (only-in planet/private/data pkg-maj pkg-min))
-
-(define interned (make-weak-hash))
-  
-(define (intern-taglet v)
-  (let ([v (if (list? v)
-               (map intern-taglet v)
-               (datum-intern-literal v))])
-    (if (or (string? v)
-            (bytes? v)
-            (list? v))
-        (let ([b (hash-ref interned v #f)])
-          (if b
-              (or (weak-box-value b)
-                  ;; just in case the value is GCed before we extract it:
-                  (intern-taglet v))
-              (begin
-                (hash-set! interned v (make-weak-box v))
-                v)))
-        v)))
-
-(define (do-module-path-index->taglet mod)
-  ;; Derive the name from the module path:
-  (let ([p (collapse-module-path-index
-            mod
-            (lambda () (build-path (current-directory) "dummy")))])
-    (if (path? p)
-        ;; If we got a path back anyway, then it's best to use the resolved
-        ;; name; if the current directory has changed since we 
-        ;; the path-index was resolved, then p might not be right. Also,
-        ;; the resolved path might be a symbol instead of a path.
-        (let ([rp (resolved-module-path-name 
-                   (module-path-index-resolve mod))])
-          (if (path? rp)
-              (intern-taglet 
-               (path->main-collects-relative rp))
-              rp))
-        (let ([p (if (and (pair? p)
-                          (eq? (car p) 'planet))
-                     ;; Normalize planet verion number based on current
-                     ;; linking:
-                     (let-values ([(path pkg)
-                                   (get-planet-module-path/pkg p #f #f)])
-                       (list* 'planet
-                              (cadr p)
-                              (list (car (caddr p))
-                                    (cadr (caddr p))
-                                    (pkg-maj pkg)
-                                    (pkg-min pkg))
-                              (cdddr p)))
-                     ;; Otherwise the path is fully normalized:
-                     p)])
-          (intern-taglet p)))))
-
-(define collapsed (make-weak-hasheq))
-(define (module-path-index->taglet mod)
-  (or (hash-ref collapsed mod #f)
-      (let ([v (do-module-path-index->taglet mod)])
-        (hash-set! collapsed mod v)
-        v)))
-
-(define (module-path-prefix->string p)
-  (datum-intern-literal
-   (format "~a" (module-path-index->taglet (module-path-index-join p #f)))))
-
-(define doc-prefix
-  (case-lambda
-   [(doc s)
-    (if doc
-        (if (list? s)
-            (cons (module-path-prefix->string doc) s)
-            (list (module-path-prefix->string doc) s))
-        s)]
-   [(doc prefix s)
-    (doc-prefix doc (if prefix
-                        (append prefix (list s))
-                        s))]))
 
 ;; ----------------------------------------
 
@@ -525,7 +437,7 @@
   (make-link-element (if u? #f "plainlink") (decode-content body) `(elem ,t)))
 
 (define (secref s #:underline? [u? #t] #:doc [doc #f] #:tag-prefixes [prefix #f])
-  (make-link-element (if u? #f "plainlink") null `(part ,(doc-prefix doc prefix s))))
+  (make-link-element (if u? #f "plainlink") null (make-section-tag s #:doc doc #:tag-prefixes prefix)))
 (define (Secref s #:underline? [u? #t] #:doc [doc #f] #:tag-prefixes [prefix #f])
   (let ([le (secref s #:underline? u? #:doc doc #:tag-prefixes prefix)])
     (make-link-element
--- collects/scribblings/scribble/base.scrbl
+++ collects/scribblings/scribble/base.scrbl
@@ -6,8 +6,8 @@
 @(define-syntax def-section-like
    (syntax-rules ()
      [(_ id result/c x ...)
-      (defproc (id [#:tag tag (or/c false/c string? (listof string?)) #f]
-                   [#:tag-prefix tag-prefix (or/c false/c string? module-path?) #f]
+      (defproc (id [#:tag tag (or/c #f string? (listof string?)) #f]
+                   [#:tag-prefix tag-prefix (or/c #f string? module-path?) #f]
                    [#:style style (or/c style? #f string? symbol? (listof symbol?)) #f]
                    [pre-content pre-content?] (... ...+))
         result/c
@@ -60,11 +60,11 @@ have @racketmodname[scribble/manual]).
 
 @section{Document Structure}
 
-@defproc[(title [#:tag tag (or/c false/c string? (listof string?)) #f]
-                [#:tag-prefix tag-prefix (or/c false/c string? module-path?) #f]
+@defproc[(title [#:tag tag (or/c #f string? (listof string?)) #f]
+                [#:tag-prefix tag-prefix (or/c #f string? module-path?) #f]
                 [#:style style (or/c style? #f string? symbol? (listof symbol?)) #f]
-                [#:version vers (or/c string? false/c) #f]
-                [#:date date (or/c string? false/c) #f]
+                [#:version vers (or/c string? #f) #f]
+                [#:date date (or/c string? #f) #f]
                 [pre-content pre-content?] ...+)
          title-decl?]{
 
@@ -470,8 +470,8 @@ Generates a literal hyperlinked URL.}
 
 
 @defproc[(secref [tag string?]
-                 [#:doc module-path (or/c module-path? false/c) #f]
-                 [#:tag-prefixes prefixes (or/c (listof string?) false/c) #f]
+                 [#:doc module-path (or/c module-path? #f) #f]
+                 [#:tag-prefixes prefixes (or/c (listof string?) #f) #f]
                  [#:underline? underline? any/c #t])
          element?]{
 
@@ -508,8 +508,8 @@ title after the section number. Customize the output (see
 
 
 @defproc[(Secref [tag string?]
-                 [#:doc module-path (or/c module-path? false/c) #f]
-                 [#:tag-prefixes prefixes (or/c (listof string?) false/c) #f]
+                 [#:doc module-path (or/c module-path? #f) #f]
+                 [#:tag-prefixes prefixes (or/c (listof string?) #f) #f]
                  [#:underline? underline? any/c #t])
          element?]{
 
@@ -518,8 +518,8 @@ with a word (e.g., ``section''), then the word is capitalized.}
 
 
 @defproc[(seclink [tag string?] 
-                  [#:doc module-path (or/c module-path? false/c) #f]
-                  [#:tag-prefixes prefixes (or/c (listof string?) false/c) #f]
+                  [#:doc module-path (or/c module-path? #f) #f]
+                  [#:tag-prefixes prefixes (or/c (listof string?) #f) #f]
                   [#:underline? underline? any/c #t]
                   [pre-content pre-content?] ...) element?]{
 
@@ -548,11 +548,6 @@ The tag @racket[t] refers to the content form of
 
 The @tech{decode}d @racket[pre-content] is hyperlinked to @racket[t],
 which is normally defined using @racket[elemtag].}
-
-@defproc[(module-path-prefix->string [mod-path module-path?]) string?]{
-
-Converts a module path to a string by resolving it to a path, and
-using @racket[path->main-collects-relative].}
 
 @; ------------------------------------------------------------------------
 
@@ -601,7 +596,7 @@ section by @racket[decode]. The @racket[word]s serve as both the keys
 and as the rendered forms of the keys within the index.}
 
 
-@defproc[(index-section [#:tag tag (or/c false/c string?) "doc-index"])
+@defproc[(index-section [#:tag tag (or/c #f string?) "doc-index"])
          part?]{
 
 Produces a part that shows the index the enclosing document. The
@@ -634,3 +629,11 @@ also the @racket['quiet] style of @racket[part] (i.e., in a
 @racket[part] structure, not supplied as the @racket[style] argument
 to @racket[local-table-of-contents]), which normally suppresses
 sub-part entries in a table of contents.}
+
+@; ------------------------------------------------------------------------
+
+@section{Tags}
+
+The exports of @racketmodname[scribble/tag] are all re-exported by
+@racketmodname[scribble/base].
+
