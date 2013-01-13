f332092f588715048cc857d211d5b5beb5934297
Mike Sperber <sperber@deinprogramm.de>
2013-01-13 14:57:45 +0100

    Synch German string constants with latest.

--- collects/string-constants/private/german-string-constants.rkt
+++ collects/string-constants/private/german-string-constants.rkt
@@ -902,6 +902,10 @@
  ;;; executables
  (create-executable-menu-item-label "Programmdatei generieren...")
  (create-executable-title "Programmdatei generieren")
+ (drracket-creates-executables-only-in-some-languages
+  "Die Erzeugung von Programmdateien in DrRacket ist nur für die Lehrsprachen"
+  " und für “Die Sprache Racket” (also mit #lang-Zeile) unterstützt.\n\nZiehen"
+  " Sie das Kommandozeilenprogramm \"raco exe\" in Betracht.")
  (must-save-before-executable "Sie müssen vor der Generierung einer Programmdatei speichern.")
  (save-a-mred-launcher "GRacket-Launcher speichern")
  (save-a-mzscheme-launcher "Racket-Launcher speichern")
@@ -1598,6 +1602,9 @@
   (planet-finished "PLaneT: fertig mit ~a.")
   (planet-docs-building "PLaneT: Dokumentation bauen (ausgelöst durch ~a)...")
   (planet-no-status "PLaneT") ;; this can happen when there is status shown in a different and then the user switches to a tab where planet hasn't been used
+
+  (bug-report-field-planet2 "Info Package-System")
+  
 
  ;; string normalization. To see this, paste some text with a ligature into DrRacket
  ;; the first three strings are in the dialog that appears. The last one is in the preferences dialog
