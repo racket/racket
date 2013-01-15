racket/racket

commit f315f08dc3af4879b5456a1274b143e637cac47d

make the planet2 info in the bug report form have multiple lines in the GUI

collects/help/private/bug-report-controls.rkt
@@ -249,13 +249,9 @@
      #:top-panel synthesized-panel))
   
   (define planet2-info
-    (build/label
+    (make-big-text
      (string-constant bug-report-field-planet2)
-     (lambda (panel)
-       (keymap:call/text-keymap-initializer
-        (lambda ()
-          (make-object text-field% #f panel void ""))))
-     #f
+     #:stretch? #t
      #:top-panel synthesized-panel))
   
   (define collections

commit 131af9955df02b9feae3c7054c216309cad3fb50

fix bug introduced in f315f08dc3af4879b5456a1274b143e637cac47d
closes PR 13435

Also cleaned up one more threading problem (this one was
probably innocuous, but it is hard to be sure and the
rewrite is easier to see that it isn't problematic)

collects/help/bug-report.rkt
@@ -201,6 +201,7 @@
     (set! cancel-kill-cust (make-custodian))
     (define response-chan (make-channel))
     (define exn-chan (make-channel))
+    (define starter-query (get-query))
     (define worker-thread
       (parameterize ([current-custodian cancel-kill-cust]
                      [current-alist-separator-mode 'amp])
@@ -222,16 +223,15 @@
                      "Are you human?" ; FIXME: use string-constant
                      captcha-question bug-frame)))
              (define post-data
-               (let* ([q (get-query)]
-                      [q (if captcha-answer
+               (let* ([q (if captcha-answer
                              `([captcha . ,captcha-answer]
                                ;; send back the question too: if things get really
                                ;; bad, then the server can make up random captchas
                                ;; and check the reply against the challenge that
                                ;; was used
                                [captcha-question . ,captcha-question]
-                               ,@q)
-                             q)])
+                               ,@starter-query)
+                             starter-query)])
                  (string->bytes/utf-8 (alist->form-urlencoded q))))
 
              (call/input-url

collects/help/private/bug-report-controls.rkt
@@ -322,7 +322,7 @@
                    (format "Human Language: ~a\n" (send human-language get-value))
                    (format "(current-memory-use) ~a\n" (send memory-use get-value))
                    (format "Links: ~a\n" (send links-ctrl get-value))
-                   (format "Planet2 (show):\n~a\n" (send planet2-info get-value))
+                   (format "Planet2 (show):\n~a\n" (send (send planet2-info get-editor) get-text))
                    "\n"
                    "\nCollections:\n"
                    (format "~a" (send (send collections get-editor) get-text))