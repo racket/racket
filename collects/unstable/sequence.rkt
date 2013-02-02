2e8ffe74002e3de0a929c8786ac683e509dc2494
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-02-02 17:47:59 -0500

    Fix `sequence-lift` for multiple valued sequences.
    Also minor fixups of the rest of `unstable/sequence`.

--- collects/unstable/sequence.rkt
+++ collects/unstable/sequence.rkt
@@ -7,45 +7,44 @@
 (provide in-syntax in-pairs in-sequence-forever sequence-lift)
 
 (define-sequence-syntax in-syntax
-  (lambda () #'(lambda (e) (in-list (syntax->list e))))
-  (lambda (stx)
+  (λ () #'(λ (e) (in-list (syntax->list e))))
+  (λ (stx)
     (syntax-case stx ()
       [[ids (_ arg)]
        #'[ids (in-list (syntax->list arg))]])))
 
 (define (in-pairs seq)
   (make-do-sequence
-   (lambda ()
+   (λ ()
      (let-values ([(more? gen) (sequence-generate seq)])
-       (values (lambda (e) (let ([e (gen)]) (values (car e) (cdr e))))
-               (lambda (_) #t)
+       (values (λ (e) (let ([e (gen)]) (values (car e) (cdr e))))
+               (λ (_) #t)
                #t
-               (lambda (_) (more?))
-               (lambda _ #t)
-               (lambda _ #t))))))
+               (λ (_) (more?))
+               (λ _ #t)
+               (λ _ #t))))))
 
 (define (in-sequence-forever seq val)
   (make-do-sequence
-   (lambda ()
+   (λ ()
      (let-values ([(more? gen) (sequence-generate seq)])
-       (values (lambda (e) (let ([e (if (more?) (gen) val)]) e))
-               (lambda (_) #t)
+       (values (λ (e) (if (more?) (gen) val))
+               (λ (_) #t)
                #t
-               (lambda (_) #t)
-               (lambda _ #t)
-               (lambda _ #t))))))
+               (λ (_) #t)
+               (λ _ #t)
+               (λ _ #t))))))
 
 (define (sequence-lift f seq)
   (make-do-sequence
-   (lambda ()
+   (λ ()
      (let-values ([(more? gen) (sequence-generate seq)])
-       (values (lambda (e) (f (gen)))
-               (lambda (_) #t)
+       (values (λ (e) (call-with-values gen f))
+               (λ (_) #t)
                #t
-               (lambda (_) (more?))
-               (lambda _ #t)
-               (lambda _ #t))))))
-
+               (λ (_) (more?))
+               (λ _ #t)
+               (λ _ #t))))))
 
 ;; Added by stamourv (from David Vanderson (david.vanderson at gmail.com)):
 
@@ -55,15 +54,14 @@
 (define (in-slice k seq)
   (unless (sequence? seq) (raise-type-error 'in-slice "sequence" seq))
   (make-do-sequence
-   (lambda ()
+   (λ ()
      (define-values (more? get) (sequence-generate seq))
      (values
-      (lambda (_)
-        (for/list ((i k)
-                   #:when (more?))
+      (λ (_)
+        (for/list ([i k] #:when (more?))
           (get)))
       values
       #f
       #f
-      (lambda (val) (0 . < . (length val)))
+      (λ (val) (0 . < . (length val)))
       #f))))
