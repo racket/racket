b42034f98b548aa473384e06e2cd153ba9847858
Vincent St-Amour <stamourv@racket-lang.org>
2013-02-04 14:18:53 -0500

    Exclude the right zeroes when checking for equality.

--- collects/tests/typed-racket/unit-tests/typecheck-tests.rkt
+++ collects/tests/typed-racket/unit-tests/typecheck-tests.rkt
@@ -1467,6 +1467,12 @@
         [tc-e (let: ([x : Float 0.0])
                 (= 0 x))
               #:ret (ret -Boolean (-FS -top -top) (make-Empty))]
+        [tc-e (let: ([x : Inexact-Real 0.0])
+                (= 0 x))
+              #:ret (ret -Boolean (-FS -top -top) (make-Empty))]
+        [tc-e (let: ([x : Real 0.0])
+                (= 0 x))
+              #:ret (ret -Boolean (-FS -top -top) (make-Empty))]
         
         [tc-e/t (ann (lambda: ([x : Boolean]) (if x x #t)) (Boolean -> #t)) (t:-> -Boolean (-val #t))]
         
