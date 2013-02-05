adbc516edfc66c5990ae2271c29e991837332de0
Eric Dobson <eric.n.dobson@gmail.com>
2013-02-05 09:31:16 -0800

    Move tc-literal to seperate file, as it doesn't belong in tc-expr.

--- collects/typed-racket/typecheck/typechecker.rkt
+++ collects/typed-racket/typecheck/typechecker.rkt
@@ -9,10 +9,11 @@
          "tc-app-combined.rkt"
          "tc-if.rkt" "tc-lambda-unit.rkt"
          "tc-let-unit.rkt" "tc-apply.rkt"
+         "tc-literal.rkt"
          "tc-expr-unit.rkt" "check-subforms-unit.rkt")
 
-(provide-signature-elements tc-expr^ check-subforms^)
+(provide-signature-elements tc-expr^ check-subforms^ tc-literal^)
 
 (define-values/invoke-unit/infer
   (link tc-if@ tc-lambda@ tc-app-combined@ tc-let@ tc-expr@
-        check-subforms@ tc-apply@))
+        check-subforms@ tc-apply@ tc-literal@))
