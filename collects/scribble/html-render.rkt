db0234fb6f59ea8c8741ea68f630548d8e1e6cc1
Danny Yoo <dyoo@hashcollision.org>
2013-02-12 20:18:54 -0700

    Amend comment.

--- collects/scribble/html-render.rkt
+++ collects/scribble/html-render.rkt
@@ -1396,9 +1396,9 @@
            (if m
              (list* (substring i 0 (cdar m))
                     ;; Most browsers wrap after a hyphen. The one that
-                    ;; doesn't, Firefox, pays attention to wbr. Some
-                    ;; browsers ignore wbr, but at least they don't do
-                    ;; strange things with it.
+                    ;; doesn't, Firefox, pays attention to wbr.  But
+                    ;; almost all browsers pay attention to zero width
+                    ;; space, so we'll insert one here.
                     #x200b
                     (render-other (substring i (cdar m)) part ri))
              (ascii-ize i)))]
