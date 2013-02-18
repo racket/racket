e720d1df923080518d5a58a71b04d9cd0014aa96
Robby Findler <robby@racket-lang.org>
2013-02-18 13:37:48 -0600

    add parameterize* to tabing and smart opening square bracket pref defaults

--- collects/framework/private/main.rkt
+++ collects/framework/private/main.rkt
@@ -120,7 +120,7 @@
                                    "match-let" "match-let*" "match-letrec"
                                    "letrec"
                                    "letrec-syntaxes" "letrec-syntaxes+values" "letrec-values"
-                                   "parameterize"
+                                   "parameterize" "parameterize*"
                                    "with-syntax"))
                          (λ (x) (and (list? x) (andmap string? x))))
 
@@ -327,7 +327,7 @@
                unit/sig unit/lang
                with-handlers
                interface
-               parameterize
+               parameterize parameterize*
                call-with-input-file call-with-input-file* with-input-from-file
                with-input-from-port call-with-output-file
                with-output-to-file with-output-to-port 
