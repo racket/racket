8117b6b4716fb732a68bb2adf77029858c3306d3
Eli Barzilay <eli@barzilay.org>
2013-02-26 09:43:36 -0500

    Change the symlink that serves docs to the one that holds the tweaked version.

--- collects/meta/web/download/symlinks.rkt
+++ collects/meta/web/download/symlinks.rkt
@@ -7,7 +7,7 @@
 (define (in-ftp . paths)
   (string-join (cons "/var/ftp/pub/racket" paths) "/"))
 
-(define docs       (symlink (in-ftp "docs")))
+(define docs       (symlink "/www/docs"))
 (define installers (symlink (in-ftp "installers")))
 (define libs       (symlink (in-ftp "libs/tags") "libs"))
 (define stubs      (symlink "/www/stubs"))
