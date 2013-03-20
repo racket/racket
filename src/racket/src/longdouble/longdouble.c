1312c1786ee85f7e0c55f992d755880a0a9bca79
Michael Filonenko <filonenko.mikhail@gmail.com>
2013-03-20 10:55:37 +0300

    precision switching on every funcall on win32

--- src/racket/src/longdouble/longdouble.c
+++ src/racket/src/longdouble/longdouble.c
@@ -27,16 +27,12 @@ LDBL_DLL_API int get_x87_control()
 
 static void ext_mode()
 {
-#if defined(__x86_64)
   set_x87_control(0x37F);
-#endif
 }
 
 static void default_mode()
 {
-#if defined(__x86_64)
   set_x87_control(0x27F);
-#endif
 }
 
 long_double get_long_double_infinity_val()
