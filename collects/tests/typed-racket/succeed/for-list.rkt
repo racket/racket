eeeceedb8c0fb5102811ccdd1c4d52dc9efd813b
Eric Dobson <eric.n.dobson@gmail.com>
2013-03-24 20:42:53 -0700

    Reimplement for*/list: in TR so that it works.
    Closes PR 13253.

--- collects/tests/typed-racket/succeed/for-list.rkt
+++ collects/tests/typed-racket/succeed/for-list.rkt
@@ -6,3 +6,9 @@
 (define (explode s)
   (for/list ([i s]) i))
 
+(ann 
+  (for*/list: : (Listof Natural)
+    ((x : Natural (list 1 2 4))
+     (y : Natural (list 2 3 4)))
+    (+ x y))
+  (Listof Natural))
