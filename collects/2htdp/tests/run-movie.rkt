1ff3e3b9422dee5094dfa320b9c2097f9f01e738
Matthias Felleisen <matthias@ccs.neu.edu>
2013-03-30 18:06:14 -0400

    fixed rate argument for run-movie, Closes PR13642

---
+++ collects/2htdp/tests/run-movie.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require 2htdp/universe 2htdp/image)
+
+(define (make-images i)
+  (cond
+    [(zero? i) '()]
+    [else (cons (place-image DOT 50 (* 50 i) BACKGROUND) (make-images (sub1 i)))]))
+
+(define DOT (circle 3 'solid 'red))
+(define BACKGROUND (empty-scene 100 400))
+
+(run-movie 8 (make-images 8))
