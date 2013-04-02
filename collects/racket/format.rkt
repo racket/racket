448a62eae46cfee71548774459b519de280f4f02
Eli Barzilay <eli@barzilay.org>
2013-04-02 19:39:17 -0400

    Avoid non-ascii character in numeric formats.

--- collects/racket/format.rkt
+++ collects/racket/format.rkt
@@ -318,7 +318,7 @@
                           (string-append
                            (cond [(string? format-exponent) format-exponent]
                                  [(= base 10) "e"]
-                                 [else (format "×~s^" base)])
+                                 [else (format "x~s^" base)])
                            (if (negative? exponent) "-" "+")
                            (%pad (number->string (abs exponent))
                                  #:pad-to 2
