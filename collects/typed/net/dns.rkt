f77e8fe357e2f559136feedafc2870160b90e2ff
Asumu Takikawa <asumu@ccs.neu.edu>
2013-04-04 15:52:55 -0400

    Adjust type for `dns-get-address`

--- collects/typed/net/dns.rkt
+++ collects/typed/net/dns.rkt
@@ -3,7 +3,7 @@
 (require typed/private/utils)
 
 (require/typed/provide net/dns
-  [dns-get-address (String String -> String)]
+  [dns-get-address (String String [#:ipv6? Any] -> String)]
   [dns-get-name (String String -> String)]
   [dns-get-mail-exchanger (String String -> String )]
   [dns-find-nameserver (-> (Option String))])
