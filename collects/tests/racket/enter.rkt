9a52894b8bed6193b44b217cae3a5532ae2eb7bc
Matthew Flatt <mflatt@racket-lang.org>
2013-04-12 14:58:01 -0600

    racket/enter: fix exception handling & propagation for `enter!'
    Merge to v5.3.4

--- collects/tests/racket/enter.rkt
+++ collects/tests/racket/enter.rkt
@@ -1,4 +1,13 @@
 #lang racket
 (require racket/enter)
 
+;; Make sure that when a module has a syntax error, we still
+;; switch into the module's namespace:
+(parameterize ([current-namespace (make-base-namespace)])
+  (eval '(module f racket/base (define f 'yes) (raise-user-error 'oops "broken")))
+  (with-handlers ([exn:fail:user? void]) (enter! 'f))
+  (unless (eq? 'yes (eval 'f))
+    (error "not in f?")))
+
+;; Make sure that `enter!' can work on lots of modules:
 (enter! slideshow/pict)
