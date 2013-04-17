58ce10c2ee27e6db779322c2edca99fa748f485e
Matthew Flatt <mflatt@racket-lang.org>
2013-04-17 12:11:23 -0600

    demote some `raco' commands
    The commands `link', `pack', and `unpack' are now
    discouraged in favor of `pkg'.
    
    original commit: 3e1ecc41b442f127c61d262a6df3811a746c16ec

--- collects/compiler/commands/info.rkt
+++ collects/compiler/commands/info.rkt
@@ -3,7 +3,7 @@
 (define raco-commands
   '(("make" compiler/commands/make "compile source to bytecode" 100)
     ("exe" compiler/commands/exe "create executable" 20)
-    ("pack" compiler/commands/pack "pack files/collections into a .plt archive" 10)
+    ("pack" compiler/commands/pack "pack files/collections into a .plt archive" #f)
     ("unpack" compiler/commands/unpack "unpack files/collections from a .plt archive" #f)
     ("decompile" compiler/commands/decompile "decompile bytecode" #f)
     ("test" compiler/commands/test "run tests associated with files/directories" 15)
