341528065735f8390dbad87f49646580096a6a0d
Vincent St-Amour <stamourv@racket-lang.org>
2013-04-19 14:21:04 -0400

    Extra tests.

--- collects/tests/typed-racket/succeed/pathstrings.rkt
+++ collects/tests/typed-racket/succeed/pathstrings.rkt
@@ -20,6 +20,10 @@
  (system* "/bin/echo" "zzz" #"foo" (string->path "/"))
  (system/exit-code #"echo foo")
  (system*/exit-code "/bin/echo" "zzz" #"foo" (string->path "/"))
+ (system #:set-pwd? #t #"echo foo")
+ (system* #:set-pwd? #t "/bin/echo" "zzz" #"foo" (string->path "/"))
+ (system/exit-code #:set-pwd? #t #"echo foo")
+ (system*/exit-code #:set-pwd? #t "/bin/echo" "zzz" #"foo" (string->path "/"))
 
  (void))
 
