a8d981230c09f482ebabc1effc4fbb44892ffc07
James Swaine <james.swaine@gmail.com>
2013-05-16 16:09:34 -0500

    Make sure the Redex pattern language's 'boolean' is rendered as a nonterminal

--- collects/redex/private/underscore-allowed.rkt
+++ collects/redex/private/underscore-allowed.rkt
@@ -1,3 +1,3 @@
 #lang racket/base
 (provide underscore-allowed) 
-(define underscore-allowed '(any number string variable variable-not-otherwise-mentioned natural integer real))
+(define underscore-allowed '(any number string variable variable-not-otherwise-mentioned natural integer real boolean))
