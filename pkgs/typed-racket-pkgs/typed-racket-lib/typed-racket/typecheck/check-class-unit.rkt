d45251228e6c2ed92c33bfdf8fd167447c7fb573
Asumu Takikawa <asumu@ccs.neu.edu>
2013-05-18 12:53:30 -0400

    Move code around to accommodate non-expected checking

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/check-class-unit.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/check-class-unit.rkt
@@ -12,13 +12,15 @@
          "tc-metafunctions.rkt"
          "tc-funapp.rkt"
          "tc-subst.rkt"
+         (prefix-in c: racket/class)
          (private syntax-properties type-annotation)
+         (base-env class-prims)
          (env lexical-env)
          (types utils abbrev union subtype resolve)
          (utils tc-utils)
          (rep type-rep)
          (for-template racket/base
-                       racket/class
+                       (prefix-in c: racket/class)
                        (base-env class-prims)))
 
 (import tc-if^ tc-lambda^ tc-app^ tc-let^ tc-expr^)
@@ -34,137 +36,141 @@
 ;;
 ;;  we know by this point that #'form is an actual typed
 ;;  class produced by class: due to the syntax property
-(define (check-class form expected)
+(define (check-class form [expected #f])
   (match expected
     [(tc-result1: (and self-class-type (Class: _ inits fields methods)))
-     (syntax-parse form
-       #:literals (let-values #%plain-lambda quote-syntax begin
-                   #%plain-app values class:-internal letrec-syntaxes+values
-                   init init-field field public)
-       ;; Inspect the expansion of the class macro for the pieces that
-       ;; we need to type-check like superclass, methods, top-level
-       ;; expressions and so on
-       [(let-values ()
-          (letrec-syntaxes+values ()
-                                  ((()
-                                    ;; residual class: data
-                                    ;; FIXME: put in syntax class
-                                    (begin
-                                      (quote-syntax
-                                       (class:-internal
-                                        (init internal-init-names ...)
-                                        (init-field internal-init-field-names ...)
-                                        (field internal-field-names ...)
-                                        (public internal-public-names ...)))
-                                      (#%plain-app values))))
-                                  (let-values (((superclass) superclass-expr)
-                                               ((interfaces) interface-expr))
-                                    (?#%app compose-class
-                                            internal ...
-                                            (#%plain-lambda (local-accessor local-mutator ??? ...)
-                                                            (let-values ([(field-name) accessor-or-mutator]
-                                                                         ...)
-                                                              body))
-                                            ????))))
-        ;; Type for self in method calls
-        (define self-type (make-Instance self-class-type))
-        ;; Make sure the superclass is a class
-        ;; FIXME: maybe should check the property on this expression
-        ;;        as a sanity check too
-        (define super-type (tc-expr #'superclass-expr))
-        (define-values (super-inits super-fields super-methods)
-          (match super-type
-            ;; FIXME: should handle the case where the super class is
-            ;;        polymorphic
-            [(tc-result1: (Class: _ super-inits super-fields super-methods))
-             (values super-inits super-fields super-methods)]
-            [(tc-result1: t)
-             (tc-error/expr "expected a superclass but got ~a" t
-                            #:stx #'superclass-expr)
-             ;; FIXME: is this the right thing to do?
-             (values null null null)]))
-        ;; Define sets of names for use later
-        (define super-init-names (list->set (dict-keys super-inits)))
-        (define super-field-names (list->set (dict-keys super-fields)))
-        (define super-method-names (list->set (dict-keys super-methods)))
-        (define exp-init-names (list->set (dict-keys inits)))
-        (define exp-field-names (list->set (dict-keys fields)))
-        (define exp-method-names (list->set (dict-keys methods)))
-        (define this%-init-names
-          (list->set
-           (append (syntax->datum #'(internal-init-names ...))
-                   (syntax->datum #'(internal-init-field-names ...)))))
-        (define this%-field-names
-          (list->set
-           (append (syntax->datum #'(internal-field-names ...))
-                   (syntax->datum #'(internal-init-field-names ...)))))
-        (define this%-method-names
-          (list->set (syntax->datum #'(internal-public-names ...))))
-        ;; Use the internal class: information to check whether clauses
-        ;; exist or are absent appropriately
-        (check-exists (set-union this%-init-names super-init-names)
-                      exp-init-names
-                      "initialization argument")
-        (check-exists (set-union this%-method-names super-method-names)
-                      exp-method-names
-                      "public method")
-        (check-exists (set-union this%-field-names super-field-names)
-                      exp-field-names
-                      "public field")
-        (check-absent super-field-names this%-field-names "public field")
-        (check-absent super-method-names this%-method-names "public method")
-        ;; FIXME: the control flow for the failure of these checks is
-        ;;        still up in the air
-        #|
-        (check-no-extra (set-union this%-field-names super-field-names)
-                        exp-field-names)
-        (check-no-extra (set-union this%-method-names super-method-names)
-                        exp-method-names)
-        |#
-        ;; trawl the body for the local name table
-        (define locals (trawl-for-property #'body 'tr:class:local-table))
-        (define local-table
-         (syntax-parse (car locals)
-           #:literals (let-values #%plain-app #%plain-lambda)
-           [(let-values ([(method ...)
-                          (#%plain-app
-                           values
-                           (#%plain-lambda ()
-                             (#%plain-app (#%plain-app local-method self1) self2))
-                           ...)])
-               (#%plain-app void))
-             (map cons
-                 (syntax->datum #'(method ...))
-                 (syntax->list #'(local-method ...)))]))
-        (pretty-print (syntax->datum (car locals)))
-        ;; trawl the body and find methods and type-check them
-        (define meths (trawl-for-property #'body 'tr:class:method))
-        (with-lexical-env/extend (map (λ (m) (dict-ref local-table m))
-                                      (syntax->datum #'(internal-public-names ...)))
-                                 ;; FIXME: the types we put here are fine in the expected
-                                 ;;        case, but not if the class doesn't have an annotation.
-                                 ;;        Then we need to hunt down annotations in a first pass.
-                                 ;;        (should probably do this in expected case anyway)
-                                 ;; FIXME: this doesn't work because the names of local methods
-                                 ;;        are obscured and need to be reconstructed somehow
-                                 (map (λ (m) (->* (list (make-Univ))
-                                                  (fixup-method-type (car (dict-ref methods m))
-                                                                     self-type)))
-                                      (syntax->datum #'(internal-public-names ...)))
-         (for ([meth meths])
-           (pretty-print (syntax->datum meth))
-           (define method-name (syntax-property meth 'tr:class:method))
-           (define method-type
-             (fixup-method-type
-              (car (dict-ref methods method-name))
-              self-type))
-           (define expected (ret method-type))
-           (define annotated (annotate-method meth self-type))
-           (tc-expr/check annotated expected)))
-        ;; trawl the body for top-level expressions too
-        (define top-level-exprs (trawl-for-property #'body 'tr:class:top-level))
-        (pretty-print (map syntax->datum top-level-exprs))
-        ])]))
+     (do-check form #t self-class-type inits fields methods)]
+    [#f (do-check form #f #f null null null)]))
+
+;; Syntax Boolean Option<Type> Inits Fields Methods -> Type
+;; Do the actual type-checking
+(define (do-check form expected? self-class-type inits fields methods)
+  (syntax-parse form
+    #:literals (let-values #%plain-lambda quote-syntax begin
+                #%plain-app values class:-internal letrec-syntaxes+values
+                c:init c:init-field c:field c:public)
+    ;; Inspect the expansion of the class macro for the pieces that
+    ;; we need to type-check like superclass, methods, top-level
+    ;; expressions and so on
+    [(let-values ()
+       (letrec-syntaxes+values ()
+                               ((()
+                                 ;; residual class: data
+                                 ;; FIXME: put in syntax class
+                                 (begin
+                                   (quote-syntax
+                                    (class:-internal
+                                     (c:init internal-init-names ...)
+                                     (c:init-field internal-init-field-names ...)
+                                     (c:field internal-field-names ...)
+                                     (c:public internal-public-names ...)))
+                                   (#%plain-app values))))
+                               (let-values (((superclass) superclass-expr)
+                                            ((interfaces) interface-expr))
+                                 (?#%app compose-class
+                                         internal ...
+                                         (#%plain-lambda (local-accessor local-mutator ??? ...)
+                                                         (let-values ([(field-name) accessor-or-mutator]
+                                                                      ...)
+                                                           body))
+                                         ????))))
+     ;; Type for self in method calls
+     (define self-type (make-Instance self-class-type))
+     ;; Make sure the superclass is a class
+     ;; FIXME: maybe should check the property on this expression
+     ;;        as a sanity check too
+     (define super-type (tc-expr #'superclass-expr))
+     (define-values (super-inits super-fields super-methods)
+       (match super-type
+         ;; FIXME: should handle the case where the super class is
+         ;;        polymorphic
+         [(tc-result1: (Class: _ super-inits super-fields super-methods))
+          (values super-inits super-fields super-methods)]
+         [(tc-result1: t)
+          (tc-error/expr "expected a superclass but got ~a" t
+                         #:stx #'superclass-expr)
+          ;; FIXME: is this the right thing to do?
+          (values null null null)]))
+     ;; Define sets of names for use later
+     (define super-init-names (list->set (dict-keys super-inits)))
+     (define super-field-names (list->set (dict-keys super-fields)))
+     (define super-method-names (list->set (dict-keys super-methods)))
+     (define this%-init-names
+       (list->set
+        (append (syntax->datum #'(internal-init-names ...))
+                (syntax->datum #'(internal-init-field-names ...)))))
+     (define this%-field-names
+       (list->set
+        (append (syntax->datum #'(internal-field-names ...))
+                (syntax->datum #'(internal-init-field-names ...)))))
+     (define this%-method-names
+       (list->set (syntax->datum #'(internal-public-names ...))))
+     ;; Use the internal class: information to check whether clauses
+     ;; exist or are absent appropriately
+     (when expected?
+      (define exp-init-names (list->set (dict-keys inits)))
+      (define exp-field-names (list->set (dict-keys fields)))
+      (define exp-method-names (list->set (dict-keys methods)))
+      (check-exists (set-union this%-init-names super-init-names)
+                    exp-init-names
+                    "initialization argument")
+      (check-exists (set-union this%-method-names super-method-names)
+                    exp-method-names
+                    "public method")
+      (check-exists (set-union this%-field-names super-field-names)
+                    exp-field-names
+                    "public field"))
+     (check-absent super-field-names this%-field-names "public field")
+     (check-absent super-method-names this%-method-names "public method")
+     ;; FIXME: the control flow for the failure of these checks is
+     ;;        still up in the air
+     #|
+     (check-no-extra (set-union this%-field-names super-field-names)
+                     exp-field-names)
+     (check-no-extra (set-union this%-method-names super-method-names)
+                     exp-method-names)
+     |#
+     ;; trawl the body for the local name table
+     (define locals (trawl-for-property #'body 'tr:class:local-table))
+     (define local-table
+      (syntax-parse (car locals)
+        #:literals (let-values #%plain-app #%plain-lambda)
+        [(let-values ([(method ...)
+                       (#%plain-app
+                        values
+                        (#%plain-lambda ()
+                          (#%plain-app (#%plain-app local-method self1) self2))
+                        ...)])
+            (#%plain-app void))
+          (map cons
+              (syntax->datum #'(method ...))
+              (syntax->list #'(local-method ...)))]))
+     ;; trawl the body and find methods and type-check them
+     (define meths (trawl-for-property #'body 'tr:class:method))
+     (with-lexical-env/extend (map (λ (m) (dict-ref local-table m))
+                                   (syntax->datum #'(internal-public-names ...)))
+                              ;; FIXME: the types we put here are fine in the expected
+                              ;;        case, but not if the class doesn't have an annotation.
+                              ;;        Then we need to hunt down annotations in a first pass.
+                              ;;        (should probably do this in expected case anyway)
+                              ;; FIXME: this doesn't work because the names of local methods
+                              ;;        are obscured and need to be reconstructed somehow
+                              (map (λ (m) (->* (list (make-Univ))
+                                               (fixup-method-type (car (dict-ref methods m))
+                                                                  self-type)))
+                                   (syntax->datum #'(internal-public-names ...)))
+      (for ([meth meths])
+        (define method-name (syntax-property meth 'tr:class:method))
+        (define method-type
+          (fixup-method-type
+           (car (dict-ref methods method-name))
+           self-type))
+        (define expected (ret method-type))
+        (define annotated (annotate-method meth self-type))
+        (tc-expr/check annotated expected)))
+     ;; trawl the body for top-level expressions too
+     (define top-level-exprs (trawl-for-property #'body 'tr:class:top-level))
+     (void)]))
 
 ;; Syntax -> Listof<Syntax>
 ;; Look through the expansion of the class macro in search for
