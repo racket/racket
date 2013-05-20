3faf75bd72c7799d3841641305c385ae83e33128
Robby Findler <robby@racket-lang.org>
2013-05-19 21:21:29 -0500

    should have been part of 299063d

--- collects/drracket/private/syncheck/intf.rkt
+++ collects/drracket/private/syncheck/intf.rkt
@@ -41,15 +41,7 @@
     (define/public (syncheck:find-source-object stx) #f)
     (define/public (syncheck:add-background-color source start end color) (void))
     (define/public (syncheck:add-require-open-menu source start end key) (void))
-    (define/public (syncheck:add-id-set all-ids new-name-intereferes?) 
-      (define fst (car all-ids))
-      (define src (list-ref fst 0))
-      (define sym 
-        (cond
-          [(syntax? src) (syntax-e src)]
-          [(object? src) (send src get-text (list-ref fst 1) (list-ref fst 2))]
-          [else 'just-a-random-guess-as-to-a-good-id]))
-      (syncheck:add-rename-menu sym all-ids new-name-intereferes?))
+    (define/public (syncheck:add-id-set all-ids new-name-intereferes?) (void))
     (define/public (syncheck:add-rename-menu sym all-ids new-name-intereferes?) (void))
     (define/public (syncheck:add-docs-menu text start-pos end-pos key the-label path definition-tag tag) (void))
     (define/public (syncheck:add-arrow start-text start-pos-left start-pos-right
