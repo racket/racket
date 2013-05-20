6e94295c9679603eda6ae88d44b9bdcc2ed94faa
Asumu Takikawa <asumu@ccs.neu.edu>
2013-05-20 16:14:23 -0400

    Add a #:verbose option to :type.
    This prints the old way, expanding all aliases inside the type.
    (cherry picked from commit 1f5b262f6d5c048bf546e7b048ab342de29b27d7)

--- collects/tests/typed-racket/succeed/type-printer-single-level.rkt
+++ collects/tests/typed-racket/succeed/type-printer-single-level.rkt
@@ -21,3 +21,16 @@
 (tr-eval '(:type Bar))
 
 (check-equal? (get-output-string out) "(U Integer String)\n(Foo -> Foo)\n")
+
+;; if #:verbose, make sure it's the full type
+(tr-eval '(:type #:verbose Bar))
+(check-equal? (get-output-string out)
+              (string-append "(U Integer String)\n(Foo -> Foo)\n"
+                             "((U 0 1 Byte-Larger-Than-One Positive-Index-Not-Byte "
+                             "Positive-Fixnum-Not-Index Negative-Fixnum "
+                             "Positive-Integer-Not-Fixnum Negative-Integer-Not-Fixnum String) "
+                             "-> (U 0 1 Byte-Larger-Than-One Positive-Index-Not-Byte "
+                             "Positive-Fixnum-Not-Index Negative-Fixnum "
+                             "Positive-Integer-Not-Fixnum Negative-Integer-Not-Fixnum "
+                             "String))\n"))
+
