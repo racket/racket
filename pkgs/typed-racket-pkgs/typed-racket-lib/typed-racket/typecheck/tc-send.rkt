290b6aa79ae730c5ddcfb12bbb09211561d69192
Asumu Takikawa <asumu@ccs.neu.edu>
2013-05-24 20:54:05 -0400

    Attempt to support recursive types in classes better
    Still not sure this is quite right

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/tc-send.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typecheck/tc-send.rkt
@@ -12,15 +12,19 @@
 (export tc-send^)
 
 (define (tc/send form rcvr method args [expected #f])
-  (match (tc-expr rcvr)
-    [(tc-result1: (Instance: (and c (Class: _ _ _ methods))))
-     (match (tc-expr method)
-       [(tc-result1: (Value: (? symbol? s)))
-        (let* ([ftype (cond [(assq s methods) => cadr]
-                            [else (tc-error/expr "send: method ~a not understood by class ~a" s c)])]
-               [retval (tc/funapp rcvr args (ret ftype) (stx-map tc-expr args) expected)])
-          (add-typeof-expr form retval)
-          retval)]
-       [(tc-result1: t) (int-err "non-symbol methods not supported by Typed Racket: ~a" t)])]
-    [(tc-result1: t) (tc-error/expr #:return (or expected (ret -Bottom)) "send: expected a class instance, got ~a" t)]))
+  (define (do-check rcvr-type)
+   (match rcvr-type
+     [(tc-result1: (Instance: (? Mu? type)))
+      (do-check (ret (make-Instance (unfold type))))]
+     [(tc-result1: (Instance: (and c (Class: _ _ _ methods))))
+      (match (tc-expr method)
+        [(tc-result1: (Value: (? symbol? s)))
+         (let* ([ftype (cond [(assq s methods) => cadr]
+                             [else (tc-error/expr "send: method ~a not understood by class ~a" s c)])]
+                [retval (tc/funapp rcvr args (ret ftype) (stx-map tc-expr args) expected)])
+           (add-typeof-expr form retval)
+           retval)]
+        [(tc-result1: t) (int-err "non-symbol methods not supported by Typed Racket: ~a" t)])]
+     [(tc-result1: t) (tc-error/expr #:return (or expected (ret -Bottom)) "send: expected a class instance, got ~a" t)]))
+  (do-check (tc-expr rcvr)))
 
