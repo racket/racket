0e7940ab4943600e6f5c8f13ce7ee13e8af9a8f5
Vincent St-Amour <stamourv@racket-lang.org>
2013-05-28 15:44:53 -0400

    Fix broken test, again.

--- collects/tests/typed-racket/optimizer/tests/vector-length.rkt
+++ collects/tests/typed-racket/optimizer/tests/vector-length.rkt
@@ -1,7 +1,7 @@
 #;
 (
-TR opt: vector-length.rkt 14:0 (vector-length (vector 1 2 3)) -- known-length vector-length
-TR opt: vector-length.rkt 15:0 (vector-length (ann (vector 4 5 6) (Vectorof Integer))) -- known-length vector-length
+TR opt: vector-length.rkt 12:0 (vector-length (vector 1 2 3)) -- known-length vector-length
+TR opt: vector-length.rkt 13:0 (vector-length (ann (vector 4 5 6) (Vectorof Integer))) -- known-length vector-length
 3
 3
 )
