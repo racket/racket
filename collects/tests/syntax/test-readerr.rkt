racket/racket

commit 6b2a4ff54df535c0754083d164fd46bcd833a22e

include source-location in "missing module" exceptions
Also, add `current-directory-for-user' and `srcloc->string', and
adjust the way that source-location paths are reported to be
relative to `current-directory-for-user'.

collects/syntax/readerr.rkt
@@ -29,12 +29,10 @@
      (make-exn:fail:read
       (format "~a~a"
               (cond [(not (error-print-source-location)) ""]
-                    [(and line col)
-                     (format "~a:~a:~a: " source-name line col)]
-                    [pos
-                     (format "~a::~a: " source-name pos)]
-                    [else
-                     (format "~a: " source-name)])
+                    [(srcloc->string (srcloc source-name line col pos span))
+                     (lambda (s)
+                       (format "~a: " s))]
+                    [else ""])
               msg)
       (current-continuation-marks)
       (cons (make-srcloc source-name line col pos span)

collects/drracket/private/expanding-place.rkt
@@ -129,7 +129,8 @@
                                          #:use-use-current-security-guard? #t)
          (ep-log-info "expanding-place.rkt: 04 setting directories")
          (let ([init-dir (get-init-dir path)])
-           (current-directory init-dir))
+           (current-directory init-dir)
+           (current-directory-for-user init-dir))
          (current-load-relative-directory #f)
          (define sp (open-input-string program-as-string))
          (port-count-lines! sp)

collects/drracket/private/rep.rkt
@@ -1545,7 +1545,8 @@ TODO
         (current-rep this)
         (let ([dir (or (send context get-directory)
                        drracket:init:first-dir)])
-          (current-directory dir))
+          (current-directory dir)
+          (current-directory-for-user dir))
         
         (set! user-namespace-box (make-weak-box (current-namespace)))
         

collects/scribblings/reference/exns.scrbl
@@ -929,6 +929,15 @@ The fields of a @racket[srcloc] instance are as follows:
 ]}
 
 
+@defproc[(srcloc->string [srcloc srcloc?]) (or/c string? #f)]{
+
+Formats @racket[srcloc] as a string suitable for error reporting.  A
+path source in @racket[srcloc] is shown relative to the value of
+@racket[current-directory-for-user]. The result is @racket[#f] if
+@racket[srcloc] does not contain enough information to format a
+string.}
+
+
 @defthing[prop:exn:missing-module struct-type-property?]{
 
 A property that identifies structure types that provide a module path

collects/scribblings/reference/filesystem.scrbl
@@ -413,6 +413,18 @@ variable---if the value of the environment variable identifies the
 same directory as the operating system's report of the current
 directory.}
 
+@defparam*[current-directory-for-user path path-string? (and/c path? complete-path?)]{
+
+Like @racket[current-directory], but use only by
+@racket[srcloc->string] for reporting paths relative to a
+directory.
+
+Normally, @racket[current-directory-for-user] should stay at its
+initial value, reflecting the directory where a user started a
+process. A tool such as DrRacket, however, implicitly lets a user
+select a directory (for the file being edited), in which case updating 
+@racket[current-directory-for-user] makes sense.}
+
 
 @defproc[(current-drive) path?]{
 

collects/slideshow/main.rkt
@@ -8,4 +8,3 @@
 
 (module reader syntax/module-reader
   slideshow)
-

src/racket/include/scheme.h
@@ -1368,6 +1368,8 @@ enum {
   MZCONFIG_CURRENT_DIRECTORY,
   MZCONFIG_CURRENT_ENV_VARS,
 
+  MZCONFIG_CURRENT_USER_DIRECTORY,
+
   MZCONFIG_RANDOM_STATE,
 
   MZCONFIG_CURRENT_MODULE_RESOLVER,

commit 1f22800d517812c20d81a05e9403a8aa4759fbeb

fix bug introduced in 6b2a4ff5

collects/syntax/readerr.rkt
@@ -30,6 +30,7 @@
       (format "~a~a"
               (cond [(not (error-print-source-location)) ""]
                     [(srcloc->string (srcloc source-name line col pos span))
+                     =>
                      (lambda (s)
                        (format "~a: " s))]
                     [else ""])

collects/tests/syntax/test-readerr.rkt
@@ -1,0 +1,9 @@
+#lang racket/base
+(require rackunit syntax/readerr)
+
+(check-exn
+ (λ (x) 
+   (and (exn:fail:read:eof? x)
+        (regexp-match #rx"^y[01: ]* x" (exn-message x))))
+ (λ () (raise-read-eof-error
+        "x" "y" 1 1 1 1)))