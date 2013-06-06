4e360f8bc3762bac6eb25bdd16ac1abd016de7de
Vincent St-Amour <stamourv@racket-lang.org>
2013-06-06 13:08:37 -0400

    Fix unsound fxquotient optimization.
    Closes PR13827.
    (cherry picked from commit 5e304161100ee53c1150476b837e21a1baa820f3)

--- collects/tests/typed-racket/optimizer/tests/invalid-fxquotient.rkt
+++ collects/tests/typed-racket/optimizer/tests/invalid-fxquotient.rkt
@@ -1,11 +1,14 @@
 #;
 (
-TR missed opt: invalid-fxquotient.rkt 10:21 (quotient fixnum-min -1) -- out of fixnum range
+TR missed opt: invalid-fxquotient.rkt 12:21 (quotient fixnum-min -1) -- out of fixnum range
 #t
 )
 
 #lang typed/racket/base
 
+(require racket/fixnum)
+
 (define: fixnum-min : Nonpositive-Fixnum (assert (- (expt 2 30)) fixnum?))
 (define: q : Natural (quotient fixnum-min -1)) ; this can't be optimized safely
 (= 1073741824 q)
+(define (bad) (fxquotient 3 0)) ; can't be optimized
