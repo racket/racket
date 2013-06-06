fe96ba1516538dacc1c448fa86d67b897ee7aae2
Vincent St-Amour <stamourv@racket-lang.org>
2013-06-06 13:01:43 -0400

    Fix unsound unary fixnum subtraction optimization.
    Closes PR13826.
    (cherry picked from commit 93d4a5d4266511166de9e8549afec89922a4eb84)

--- collects/tests/typed-racket/optimizer/tests/unary-fixnum.rkt
+++ collects/tests/typed-racket/optimizer/tests/unary-fixnum.rkt
@@ -1,10 +1,15 @@
 #;
 (
-TR opt: unary-fixnum.rkt 10:0 (bitwise-not 4) -- unary fixnum
+TR opt: unary-fixnum.rkt 13:0 (bitwise-not 4) -- unary fixnum
+TR opt: unary-fixnum.rkt 14:0 (- (ann 2 Nonnegative-Fixnum)) -- unary fixnum
 -5
+-2
+2
 )
 
 #lang typed/scheme
 #:optimize
 
 (bitwise-not 4)
+(- (ann 2 Nonnegative-Fixnum))
+(- (ann -2 Negative-Fixnum)) ; can't optimize negatives
