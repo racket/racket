75f0c88feb1586adb445e892f44a19b97bfe4293
Asumu Takikawa <asumu@ccs.neu.edu>
2013-06-07 15:08:35 -0400

    Improve TR test case

--- collects/tests/typed-racket/fail/pr13209.rkt
+++ collects/tests/typed-racket/fail/pr13209.rkt
@@ -1,6 +1,11 @@
 #;
-(exn-pred exn:fail:syntax?)
+(exn-pred #rx"arguments for structure type constructor")
 #lang typed/racket
+
+;; Test for PR 13209
+;;
+;; The use of `node` at the end has the wrong number of
+;; type arguments. This should not raise an internal error.
 
 (struct: (α) leaf ({value : α}))
 (struct: (α) node ({left : [Tree α]} {right : [Tree α]}))
