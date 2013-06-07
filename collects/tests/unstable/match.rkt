12e5bc645bf6e4c8c74cd6460efefccbf5a5680d
Asumu Takikawa <asumu@ccs.neu.edu>
2013-06-07 14:31:46 -0400

    Add match*? to unstable/match

--- collects/tests/unstable/match.rkt
+++ collects/tests/unstable/match.rkt
@@ -21,4 +21,17 @@
      (test
       (match (list 1 2 3)
         [(as ([a 0]) (list b c d)) (list a b c d)])
-      (list 0 1 2 3)))))
+      (list 0 1 2 3)))
+   (test-suite "match*?"
+     (test
+      (check-true (match*? (1 2 3)
+                    (a b c)
+                    (#f y z))))
+     (test
+      (check-true (match*? (1 2 3)
+                    (a b #f)
+                    (x y z))))
+     (test
+      (check-false (match*? (1 2 3)
+                     (a #f c)
+                     (#f y z)))))))
