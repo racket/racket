4a4f21356490b2187ed1c70d1cdf676c3f8090a7
Asumu Takikawa <asumu@ccs.neu.edu>
2013-06-10 21:09:08 -0400

    Add a test for previous commit
    This commit will be fixuped when merged.

---
+++ collects/tests/pkg/test-pkgs/pkg-test3-v3/main.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+
+(printf "pkg-test3/main loaded\n")
+(exit 0)
--- collects/tests/pkg/tests-raco.rkt
+++ collects/tests/pkg/tests-raco.rkt
@@ -27,4 +27,10 @@
    $ "raco pkg create --format plt test-pkgs/raco-pkg"
    $ "raco raco-pkg" =exit> 1
    $ "raco pkg install test-pkgs/raco-pkg.plt"
-   $ "raco raco-pkg" =exit> 0)))
+   $ "raco raco-pkg" =exit> 0))
+
+ (with-fake-root
+  (shelly-case
+   "raco install uses raco setup with single collect"
+   $ "raco pkg install test-pkgs/pkg-test3-v3" =exit> 0)))
+
