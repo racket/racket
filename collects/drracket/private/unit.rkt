cf3c3a4a7f8a776cc90666b5d3307b827b3472ff
Robby Findler <robby@racket-lang.org>
2013-06-17 17:58:11 -0500

    remove bogus error (introduced to try out recent changes)

--- collects/drracket/private/unit.rkt
+++ collects/drracket/private/unit.rkt
@@ -1202,7 +1202,6 @@ module browser threading seems wrong.
                   (string-constant change-to-horizontal-alignment))]
              [callback
               (λ (a b) 
-                (car)
                 (preferences:set 'drracket:defs/ints-horizontal vertical?)
                 (set-orientation vertical?))])
         (popup-menu menu (send evt get-x) (send evt get-y)))
