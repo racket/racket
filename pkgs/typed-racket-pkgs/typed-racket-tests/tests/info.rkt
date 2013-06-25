1d89e18bca9ada96854d931dba392b930b2105d3
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-06-25 16:32:17 -0400

    Move Typed Racket tests to their own package.

---
+++ pkgs/typed-racket-pkgs/typed-racket-tests/tests/info.rkt
@@ -0,0 +1,12 @@
+#lang setup/infotab
+
+(define name "Typed Racket Test Suite")
+
+;; No need to compile the actual integration tests, just the harness.
+(define compile-omit-paths
+  '("typed-racket/succeed"
+    "typed-racket/fail"
+    "typed-racket/xfail"
+    "typed-racket/optimizer" ;; FIXME: should be improved by stamourv
+    ;; this file is weird
+    "typed-racket/unit-tests/special-env-typecheck-tests.rkt"))
