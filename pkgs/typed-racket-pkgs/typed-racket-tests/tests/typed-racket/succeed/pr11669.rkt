2cf5a42ac2673f36d8b35f475cbc45c7e66e5da2
Asumu Takikawa <asumu@ccs.neu.edu>
2013-06-28 15:15:29 -0400

    Revise test with racket/load

--- pkgs/typed-racket-pkgs/typed-racket-tests/tests/typed-racket/succeed/pr11669.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-tests/tests/typed-racket/succeed/pr11669.rkt
@@ -1,27 +1,16 @@
-#lang racket/base
+#lang racket/load
+
+(require typed/racket)
 
 ;; Test that struct: and define-struct: work at the
 ;; top-level.
 ;;
 ;; Test for PR 11669
-
-(require rackunit
-         racket/sandbox)
-
-(define out (open-output-string))
-
-(define tr-eval
-  (parameterize ([sandbox-output out])
-    (call-with-trusted-sandbox-configuration
-     (λ () (make-evaluator 'typed/racket)))))
-
-(check-not-exn
-  (λ ()
-    (tr-eval '(struct: Foo ([x : Integer])))
-    (tr-eval '(define-struct: Bar ([y : Foo])))
-    (tr-eval '(define-type Qux (U String Integer)))
-    (tr-eval '(struct: Quux ([qux : Qux])))
-    (tr-eval 'Quux-qux)
-    (tr-eval 'Foo)
-    (tr-eval 'make-Bar)))
+(struct: Foo ([x : Integer]))
+(define-struct: Bar ([y : Foo]))
+(define-type Qux (U String Integer))
+(struct: Quux ([qux : Qux]))
+Quux-qux
+Foo
+make-Bar
 
