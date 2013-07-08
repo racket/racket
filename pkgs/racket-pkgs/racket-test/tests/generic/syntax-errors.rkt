065622d64bf82a8f9fe5a02d31e3a9a0bd4e382d
Carl Eastlund <cce@racket-lang.org>
2013-07-08 00:18:18 -0400

    Added tests for varying the order of keyword options to define-generics.

--- pkgs/racket-pkgs/racket-test/tests/generic/syntax-errors.rkt
+++ pkgs/racket-pkgs/racket-test/tests/generic/syntax-errors.rkt
@@ -4,7 +4,16 @@
 
 (define-namespace-anchor generic-env)
 
-(define-syntax-rule (check-syntax exp ...)
+(define-syntax-rule (check-good-syntax exp ...)
+  (begin
+    (check-not-exn
+     (lambda () (eval '(module foo racket/base
+                         (require racket/generic)
+                         exp)
+                      (namespace-anchor->namespace generic-env))))
+    ...))
+
+(define-syntax-rule (check-bad-syntax exp ...)
   (begin
     (check-exn
      exn:fail:syntax?
@@ -14,7 +23,75 @@
                       (namespace-anchor->namespace generic-env))))
     ...))
 
-(check-syntax
+(check-good-syntax
+
+ (define-generics stream
+   (stream-first stream)
+   (stream-rest stream)
+   (stream-empty? stream))
+
+ (define-generics stream
+   #:defined-table stream-table
+   (stream-first stream)
+   (stream-rest stream)
+   (stream-empty? stream)
+   #:defaults
+   ([list?
+     (define stream-first car)
+     (define stream-rest cdr)
+     (define stream-empty? null?)]))
+
+ (define-generics stream
+   #:defined-table stream-table
+   #:defaults
+   ([list?
+     (define stream-first car)
+     (define stream-rest cdr)
+     (define stream-empty? null?)])
+   (stream-first stream)
+   (stream-rest stream)
+   (stream-empty? stream))
+
+ (define-generics stream
+   (stream-first stream)
+   (stream-rest stream)
+   (stream-empty? stream)
+   #:defined-table stream-table
+   #:defaults
+   ([list?
+     (define stream-first car)
+     (define stream-rest cdr)
+     (define stream-empty? null?)]))
+
+ (define-generics stream
+   (stream-first stream)
+   (stream-rest stream)
+   (stream-empty? stream)
+   #:defaults
+   ([list?
+     (define stream-first car)
+     (define stream-rest cdr)
+     (define stream-empty? null?)])
+   #:defined-table stream-table))
+
+(check-bad-syntax
+
+ (define-generics stream
+   (stream-first stream)
+   (stream-rest stream)
+   #:defaults
+   ([list?
+     (define stream-first car)
+     (define stream-rest cdr)
+     (define stream-empty? null?)])
+   (stream-empty? stream))
+
+ (define-generics stream
+   (stream-first stream)
+   (stream-rest stream)
+   #:defined-table stream-table
+   (stream-empty? stream))
+
  (define-generics stream
    (stream-first stream)
    (stream-rest stream)
