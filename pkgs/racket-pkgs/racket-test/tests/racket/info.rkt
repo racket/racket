47148430d2c8ed1547bc0d2454de6612ccf02041
Matthew Flatt <mflatt@racket-lang.org>
2013-07-16 15:56:24 -0600

    enable compilation of tests formerly in "racket-test"; reorganize
    A package like "gui" does not depend on a package like "gui-test",
    which means that you have to specifically ask for a test package if
    you want the tests. A new "main-distribution-test" package
    aggregates the tests for packages that are in "main-distribution".
    
    The "plt-services" package, meanwhile, depends on the
    "main-distribution-test" package, which means that all tests are
    compiled by default for an in-place build.
    
    original commit: d50d0f8bcab260f404049029b77407e0222f588a

---
+++ pkgs/racket-pkgs/racket-test/tests/racket/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define compile-omit-paths '("embed-me9.rkt"
+                             "embed-planet-1"
+                             
+                             ;; Could be compiled, but we skep them to avoid
+                             ;; dependencies. This needs to be cleaned up.
+                             "embed-me5.rkt"
+                             "embed-me19.rkt"
+                             "embed-bsl.rkt"
+                             "embed-bsla.rkt"
+                             "embed-isl.rkt"
+                             "embed-isll.rkt"
+                             "embed-asl.rkt"))
