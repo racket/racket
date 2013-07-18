074150e427193aaad9268f785ad48d9a0ad85599
Tim Köhlmann <tim.koehlmann@crono.co.uk>
2013-07-18 19:38:27 +0200

    Fixed file/md5's conversion for TR according to the documentation.

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed/file/md5.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed/file/md5.rkt
@@ -1,4 +1,6 @@
 #lang typed/racket/base
 (require/typed file/md5
-               [md5 ((U Bytes Input-Port) -> Bytes)])
+               [md5 (case->
+                      ((U Bytes String Input-Port) -> Bytes)
+                      ((U Bytes String Input-Port) Boolean -> Bytes))])
 (provide md5)
