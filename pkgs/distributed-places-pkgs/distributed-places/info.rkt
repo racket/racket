95532827a5b3dc6714772129dc879836eedf5178
Matthew Flatt <mflatt@racket-lang.org>
2013-07-28 14:25:57 -0600

    add "distributed-places" package
    Joins the "-lib" and "-doc" packages.

---
+++ pkgs/distributed-places-pkgs/distributed-places/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("distributed-places-lib"
+               "distributed-places-doc"))
+(define implies '("distributed-places-lib"
+                  "distributed-places-doc"))
