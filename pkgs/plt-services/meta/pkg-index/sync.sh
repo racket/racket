c56c72f64ee3fcaa02750bf411a6b24709de7b37
Jay McCarthy <jay@racket-lang.org>
2013-08-16 12:38:11 -0600

    Sync script update re reorg

--- pkgs/plt-services/meta/pkg-index/sync.sh
+++ pkgs/plt-services/meta/pkg-index/sync.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-for i in pkg tests/pkg meta/pkg-index ; do
-    rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../$i/ plt-etc:local/galaxy/$i/
-done
+rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../meta/pkg-index/ plt-etc:local/galaxy/meta/pkg-index/
+
+rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../../../racket/collects/pkg/ plt-etc:local/galaxy/pkg/
+
+rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../../../pkgs/racket-pkgs/racket-test/tests/pkg plt-etc:local/galaxy/tests/pkg/
 
 for i in official planet-compat ; do
     rsync -a --progress -h --delete plt-etc:local/galaxy/meta/pkg-index/$i/root/ $i/root/
 done
 
-rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../pkg/ plt-etc:local/plt/collects/pkg/
+rsync -a --progress -h --delete --exclude root --exclude compiled --exclude doc ../../../../racket/collects/pkg plt-etc:local/plt/collects/pkg/
 
-rsync -a --progress -h --delete --exclude compiled ../../web-server ../../net plt-etc:local/plt/collects/
+rsync -a --progress -h --delete --exclude compiled ../../../web-server-pkgs/web-server-lib/web-server ../../../../racket/collects/net plt-etc:local/plt/collects/
