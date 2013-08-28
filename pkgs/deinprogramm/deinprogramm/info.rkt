5e5aa7526f21b5cd35f14c93f8d235322e00ea2c
Robby Findler <robby@racket-lang.org>
2013-08-28 12:12:25 -0500

    move the teachpack/deinprogramm collection into the deinprogramm pkg
    This required making the deinprogramm pkg into a 'multi
    pkg, which is why all of those other files moved
    around too

---
+++ pkgs/deinprogramm/deinprogramm/info.rkt
@@ -0,0 +1,25 @@
+#lang info
+
+(define name "DeinProgramm")
+
+(define tools '("deinprogramm-langs.rkt"))
+
+(define tool-icons '(("logo-small.png" "deinprogramm")))
+(define tool-names '("DeinProgramm"))
+(define tool-urls '("http://www.deinprogramm.de/dmda/"))
+
+(define compile-omit-files
+  '("define-record-procedures.scm"
+    "convert-explicit.scm"
+    "line3d.scm"))
+
+(define get-textbook-pls
+  '("textbook-pls-spec.rkt" textbook-pls))
+(define build-deps '("rackunit-lib"
+                     "racket-doc"
+                     "at-exp-lib"
+                     "scribble-lib"))
+
+(define pkg-desc "Teaching languages for _Die Macht der Abstraktion_")
+
+(define pkg-authors '(sperber))
--- pkgs/deinprogramm/info.rkt
+++ pkgs/deinprogramm/info.rkt
@@ -1,6 +1,5 @@
 #lang info
-
-(define collection "deinprogramm")
+(define collection 'multi)
 
 (define deps '("scheme-lib"
                "base"
@@ -15,26 +14,8 @@
                "drracket"
                "htdp"))
 
-(define name "DeinProgramm")
-
-(define tools '("deinprogramm-langs.rkt"))
-
-(define tool-icons '(("logo-small.png" "deinprogramm")))
-(define tool-names '("DeinProgramm"))
-(define tool-urls '("http://www.deinprogramm.de/dmda/"))
-
-(define compile-omit-files
-  '("define-record-procedures.scm"
-    "convert-explicit.scm"
-    "line3d.scm"))
-
-(define get-textbook-pls
-  '("textbook-pls-spec.rkt" textbook-pls))
-(define build-deps '("rackunit-lib"
-                     "racket-doc"
-                     "at-exp-lib"
-                     "scribble-lib"))
-
-(define pkg-desc "Teaching languages for _Die Macht der Abstraktion_")
-
-(define pkg-authors '(sperber))
+(define build-deps
+  '("at-exp-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "scribble-lib"))
