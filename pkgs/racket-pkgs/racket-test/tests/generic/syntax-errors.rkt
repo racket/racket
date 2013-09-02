10bce2b4726582d83c44722cc769b213b6e72432
Carl Eastlund <cce@racket-lang.org>
2013-09-02 19:35:17 -0400

    Constructed a test related to PR 13982.

--- pkgs/racket-pkgs/racket-test/tests/generic/syntax-errors.rkt
+++ pkgs/racket-pkgs/racket-test/tests/generic/syntax-errors.rkt
@@ -166,3 +166,15 @@
       (define stream-first  first)
       (define stream-rest   rest)
       (define stream-empty? empty?)])))
+
+(check-good-syntax
+  (module gen racket
+    (require racket/generic)
+    (provide gen:foo (rename-out [*bar bar]))
+    (define-generics foo (bar foo))
+    (define *bar bar))
+  (module impl racket
+    (require racket/generic (submod ".." gen))
+    (struct thing []
+      #:methods gen:foo
+      [(define/generic gbar bar)])))
