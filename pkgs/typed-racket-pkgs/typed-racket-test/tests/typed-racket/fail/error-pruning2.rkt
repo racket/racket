86c87f5fda227ec574a0e47d619155f96a2cf8a4
Vincent St-Amour <stamourv@racket-lang.org>
2013-09-04 11:12:01 -0400

    Add tests for error message pruning.

---
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/fail/error-pruning2.rkt
@@ -0,0 +1,11 @@
+#;
+(exn-pred #px"^((?!Integer).)+$")
+#lang typed/racket
+
+;; Tests that function type pruning in errors is working. If the string
+;; "Integer" shows up the error (which should be about Number), then
+;; pruning is broken.
+;; Because `exn-pred' only checks the first line of the error, only one
+;; error expression per file (otherwise, the first line is a summary).
+
+(+ 'foo1 3)
