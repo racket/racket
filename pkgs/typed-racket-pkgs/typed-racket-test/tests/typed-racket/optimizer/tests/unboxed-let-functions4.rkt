cef3547252d818fc7e33b036479ca54f8cf96a40
Eric Dobson <eric.n.dobson@gmail.com>
2013-09-05 21:39:51 -0700

    Fixup logs.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/unboxed-let-functions4.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/unboxed-let-functions4.rkt
@@ -1,15 +1,15 @@
 #;#;
 #<<END
-TR opt: unboxed-let-functions4.rkt 26:32 x -- unboxed var -> table
-TR opt: unboxed-let-functions4.rkt 26:7 f -- fun -> unboxed fun
-TR opt: unboxed-let-functions4.rkt 27:18 (+ x y) -- unboxed binary float complex
-TR opt: unboxed-let-functions4.rkt 27:21 x -- leave var unboxed
-TR opt: unboxed-let-functions4.rkt 27:23 y -- float-arg-expr in complex ops
-TR opt: unboxed-let-functions4.rkt 28:2 (f 3.0 (+ 1.0+2.0i 2.0+4.0i)) -- call to fun with unboxed args
-TR opt: unboxed-let-functions4.rkt 28:2 (f 3.0 (+ 1.0+2.0i 2.0+4.0i)) -- unboxed call site
-TR opt: unboxed-let-functions4.rkt 29:17 2.0+4.0i -- unboxed literal
-TR opt: unboxed-let-functions4.rkt 29:5 (+ 1.0+2.0i 2.0+4.0i) -- unboxed binary float complex
-TR opt: unboxed-let-functions4.rkt 29:8 1.0+2.0i -- unboxed literal
+TR opt: unboxed-let-functions4.rkt 23:32 x -- unboxed var -> table
+TR opt: unboxed-let-functions4.rkt 23:7 f -- fun -> unboxed fun
+TR opt: unboxed-let-functions4.rkt 24:18 (+ x y) -- unboxed binary float complex
+TR opt: unboxed-let-functions4.rkt 24:21 x -- leave var unboxed
+TR opt: unboxed-let-functions4.rkt 24:23 y -- float-arg-expr in complex ops
+TR opt: unboxed-let-functions4.rkt 25:2 (f 3.0 (+ 1.0+2.0i 2.0+4.0i)) -- call to fun with unboxed args
+TR opt: unboxed-let-functions4.rkt 25:2 (f 3.0 (+ 1.0+2.0i 2.0+4.0i)) -- unboxed call site
+TR opt: unboxed-let-functions4.rkt 26:17 2.0+4.0i -- unboxed literal
+TR opt: unboxed-let-functions4.rkt 26:5 (+ 1.0+2.0i 2.0+4.0i) -- unboxed binary float complex
+TR opt: unboxed-let-functions4.rkt 26:8 1.0+2.0i -- unboxed literal
 END
 #<<END
 6.0+6.0i
--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/unboxed-let-functions6.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/unboxed-let-functions6.rkt
@@ -1,20 +1,20 @@
 #;#;
 #<<END
-TR opt: unboxed-let-functions6.rkt 30:0 (let: loop : Float-Complex ((z : Float-Complex 0.0+0.0i) (l : (Listof Integer) (quote (1 2 3)))) (if (null? l) (+ z 0.0+1.0i) (loop (+ z (car l)) (cdr l)))) -- unboxed call site
-TR opt: unboxed-let-functions6.rkt 30:31 z -- unboxed var -> table
-TR opt: unboxed-let-functions6.rkt 30:51 0.0+0.0i -- unboxed literal
-TR opt: unboxed-let-functions6.rkt 30:6 loop -- fun -> unboxed fun
-TR opt: unboxed-let-functions6.rkt 30:6 loop -- unboxed let loop
-TR opt: unboxed-let-functions6.rkt 33:10 (+ z 0.0+1.0i) -- unboxed binary float complex
-TR opt: unboxed-let-functions6.rkt 33:13 z -- leave var unboxed
-TR opt: unboxed-let-functions6.rkt 33:15 0.0+1.0i -- unboxed literal
-TR opt: unboxed-let-functions6.rkt 34:10 (loop (+ z (car l)) (cdr l)) -- call to fun with unboxed args
-TR opt: unboxed-let-functions6.rkt 34:10 (loop (+ z (car l)) (cdr l)) -- unboxed call site
-TR opt: unboxed-let-functions6.rkt 34:16 (+ z (car l)) -- unboxed binary float complex
-TR opt: unboxed-let-functions6.rkt 34:19 z -- leave var unboxed
-TR opt: unboxed-let-functions6.rkt 34:21 (car l) -- float-arg-expr in complex ops
-TR opt: unboxed-let-functions6.rkt 34:21 (car l) -- pair
-TR opt: unboxed-let-functions6.rkt 35:16 (cdr l) -- pair
+TR opt: unboxed-let-functions6.rkt 27:0 (let: loop : Float-Complex ((z : Float-Complex 0.0+0.0i) (l : (Listof Integer) (quote (1 2 3)))) (if (null? l) (+ z 0.0+1.0i) (loop (+ z (car l)) (cdr l)))) -- unboxed call site
+TR opt: unboxed-let-functions6.rkt 27:31 z -- unboxed var -> table
+TR opt: unboxed-let-functions6.rkt 27:51 0.0+0.0i -- unboxed literal
+TR opt: unboxed-let-functions6.rkt 27:6 loop -- fun -> unboxed fun
+TR opt: unboxed-let-functions6.rkt 27:6 loop -- unboxed let loop
+TR opt: unboxed-let-functions6.rkt 30:10 (+ z 0.0+1.0i) -- unboxed binary float complex
+TR opt: unboxed-let-functions6.rkt 30:13 z -- leave var unboxed
+TR opt: unboxed-let-functions6.rkt 30:15 0.0+1.0i -- unboxed literal
+TR opt: unboxed-let-functions6.rkt 31:10 (loop (+ z (car l)) (cdr l)) -- call to fun with unboxed args
+TR opt: unboxed-let-functions6.rkt 31:10 (loop (+ z (car l)) (cdr l)) -- unboxed call site
+TR opt: unboxed-let-functions6.rkt 31:16 (+ z (car l)) -- unboxed binary float complex
+TR opt: unboxed-let-functions6.rkt 31:19 z -- leave var unboxed
+TR opt: unboxed-let-functions6.rkt 31:21 (car l) -- float-arg-expr in complex ops
+TR opt: unboxed-let-functions6.rkt 31:21 (car l) -- pair
+TR opt: unboxed-let-functions6.rkt 32:16 (cdr l) -- pair
 END
 #<<END
 6.0+1.0i
