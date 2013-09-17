17477cbf0a166a5781fdb870887360982c061b4e
Eric Dobson <eric.n.dobson@gmail.com>
2013-09-17 09:52:38 -0700

    Make conjugate correctly optimize.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/float-complex-conjugate.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/float-complex-conjugate.rkt
@@ -1,13 +1,16 @@
 #;#;
 #<<END
-TR opt: float-complex-conjugate.rkt 17:0 (+ (conjugate 1.0+2.0i) (conjugate 2.0+4.0i)) -- unboxed binary float complex
-TR opt: float-complex-conjugate.rkt 17:14 1.0+2.0i -- unboxed literal
-TR opt: float-complex-conjugate.rkt 17:24 (conjugate 2.0+4.0i) -- unboxed unary float complex
-TR opt: float-complex-conjugate.rkt 17:3 (conjugate 1.0+2.0i) -- unboxed unary float complex
-TR opt: float-complex-conjugate.rkt 17:35 2.0+4.0i -- unboxed literal
+TR opt: float-complex-conjugate.rkt 20:0 (+ (conjugate 1.0+2.0i) (conjugate 2.0+4.0i)) -- unboxed binary float complex
+TR opt: float-complex-conjugate.rkt 20:14 1.0+2.0i -- unboxed literal
+TR opt: float-complex-conjugate.rkt 20:24 (conjugate 2.0+4.0i) -- unboxed unary float complex
+TR opt: float-complex-conjugate.rkt 20:3 (conjugate 1.0+2.0i) -- unboxed unary float complex
+TR opt: float-complex-conjugate.rkt 20:35 2.0+4.0i -- unboxed literal
+TR opt: float-complex-conjugate.rkt 21:0 (conjugate 1.0+0.0i) -- unboxed unary float complex
+TR opt: float-complex-conjugate.rkt 21:11 1.0+0.0i -- unboxed literal
 END
 #<<END
 3.0-6.0i
+1.0-0.0i
 
 END
 
@@ -15,3 +18,4 @@ END
 #:optimize
 
 (+ (conjugate 1.0+2.0i) (conjugate 2.0+4.0i))
+(conjugate 1.0+0.0i)
