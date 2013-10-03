8090a2d23aeb04bb2f6aca49e401f271a7dd5d62
Jay McCarthy <jay@racket-lang.org>
2013-10-03 14:30:02 -0600

    Moving planet-compat to S3

---
+++ pkgs/plt-services/meta/pkg-index/planet-compat/common.rkt
@@ -0,0 +1,24 @@
+#lang racket/base
+(require racket/runtime-path
+         racket/file)
+
+(define-runtime-path root "root")
+(make-directory* root)
+
+(define mins (build-path root "mins"))
+(make-directory* mins)
+(define orig-pkg (build-path root "orig-pkg"))
+(make-directory* orig-pkg)
+(define orig (build-path root "orig"))
+(make-directory* orig)
+(define work (build-path root "work"))
+(make-directory* work)
+(define pkg-depo (build-path root "cache"))
+(make-directory* pkg-depo)
+(define pkg-depo-dir "static")
+(make-directory* (build-path pkg-depo pkg-depo-dir))
+
+(define cache-dir pkg-depo)
+(make-directory* cache-dir)
+
+(provide (all-defined-out))
---
+++ pkgs/plt-services/meta/pkg-index/planet-compat/s3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PATH=~/local/new-plt/racket/bin:$PATH
+
+cd ~/local/new-plt/pkgs/plt-services/meta/pkg-index/planet-compat
+racket update.rkt
+racket static.rkt
+s3cmd -c ~/.s3cfg-plt sync --recursive --delete-removed root/cache/ s3://planet-compat/
