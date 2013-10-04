e9ee4c45043c7eafcaaf5ada84a3798ec992e4ae
Matthias Felleisen <matthias@ccs.neu.edu>
2013-10-04 15:07:01 -0400

    test for stop-when problem with booleans

---
+++ pkgs/htdp-pkgs/htdp-test/2htdp/tests/stop-when-not-boolean.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+;; ---------------------------------------------------------------------------------------------------
+;; the stop-when clause crashes. make sure that it signals a catchable error.
+
+(require 2htdp/universe 2htdp/image)
+
+(with-handlers ((exn:fail? void))
+  (big-bang 0
+            (on-draw (λ _ (empty-scene 500 500)))
+            (stop-when (λ _ 5)))
+  (displayln '(*** SOMETHING WENT WRONG IN STOP-WHEN NOT BOOLEAN ***)))
