bbe1cc9ab6c0cd8f88d54b7fc85ee81060d16d12
Matthias Felleisen <matthias@ccs.neu.edu>
2013-10-04 15:11:03 -0400

    fixed test with display line

--- pkgs/htdp-pkgs/htdp-test/2htdp/tests/stop-when-crash.rkt
+++ pkgs/htdp-pkgs/htdp-test/2htdp/tests/stop-when-crash.rkt
@@ -8,4 +8,5 @@
 (with-handlers ((exn:fail? void))
   (big-bang 0
             (on-draw (λ _ (empty-scene 500 500)))
-            (stop-when (λ _ (car '())))))
+            (stop-when (λ _ (car '()))))
+  (displayln '(*** something went wrong in stop-when crash ***)))
