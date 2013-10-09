384ff2aa364c2dc596deca8955f5889cb194f3ee
Neil Toronto <neil.toronto@gmail.com>
2013-10-09 17:51:05 -0600

    Removed dependency on "plot-compat" from "plot" package

--- pkgs/plot-pkgs/plot/info.rkt
+++ pkgs/plot-pkgs/plot/info.rkt
@@ -2,8 +2,8 @@
 
 (define collection 'multi)
 
-(define deps '("plot-lib" "plot-gui-lib" "plot-compat" "plot-doc" "plot-test"))
-(define implies '("plot-lib" "plot-gui-lib" "plot-compat" "plot-doc" "plot-test"))
+(define deps '("plot-lib" "plot-gui-lib" "plot-doc" "plot-test"))
+(define implies '("plot-lib" "plot-gui-lib" "plot-doc" "plot-test"))
 
 (define pkg-desc "Functions (and docs and tests) for plotting")
 
