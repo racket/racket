e1bb8f5d6c9dcc9b05c04a5c70cfbffc2b357e4b
Matthew Flatt <mflatt@racket-lang.org>
2013-10-15 16:19:27 -0600

    move some test & doc collections out of "racket-" pkgs to new pkgs
    original commit: 1920ac59ab0607502641298fc575b66e6acd8c73

---
+++ pkgs/net-pkgs/net-doc/info.rkt
@@ -0,0 +1,17 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"))
+
+(define pkg-desc "documentation part of \"net\"")
+
+(define pkg-authors '(mflatt))
+(define build-deps '("compatibility-lib"
+                     "net-lib"
+                     "racket-doc"
+                     "scribble-lib"
+                     "unstable-contract-lib"
+                     "unstable-doc"
+                     "web-server-doc"
+                     "web-server-lib"))
---
+++ pkgs/net-pkgs/net-test/info.rkt
@@ -0,0 +1,17 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"))
+
+(define pkg-desc "tests for \"net-lib\"")
+
+(define pkg-authors '(mflatt))
+(define build-deps '("at-exp-lib"
+                     "compatibility-lib"
+                     "eli-tester"
+                     "net-lib"
+                     "racket-test"
+                     "rackunit-lib"
+                     "scheme-lib"
+                     "web-server-lib"))
