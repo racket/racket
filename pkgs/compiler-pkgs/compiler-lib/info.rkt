afe7f46d35918ae14b83ab1ac076daadf0537aef
Matthew Flatt <mflatt@racket-lang.org>
2013-10-15 16:19:27 -0600

    move some test & doc collections out of "racket-" pkgs to new pkgs
    original commit: 1920ac59ab0607502641298fc575b66e6acd8c73

---
+++ pkgs/compiler-pkgs/compiler-lib/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"
+               "scheme-lib"
+               "rackunit-lib"))
+
+(define pkg-desc "implementation (no documentation) part of \"compiler\"")
+
+(define pkg-authors '(mflatt))
--- pkgs/compiler-pkgs/compiler/info.rkt
+++ pkgs/compiler-pkgs/compiler/info.rkt
@@ -2,9 +2,8 @@
 
 (define collection 'multi)
 
-(define deps '("base"
-               "scheme-lib"
-               "rackunit-lib"))
+(define deps '("compiler-lib"))
+(define implies '("compiler-lib"))
 
 (define pkg-desc "Racket compilation tools, such as `raco exe'")
 
