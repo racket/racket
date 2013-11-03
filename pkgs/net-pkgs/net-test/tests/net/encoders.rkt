331b6a99cd26fda5701b6d1601a4f51771a06a32
Robby Findler <robby@racket-lang.org>
2013-11-02 21:24:23 -0500

    fix a use of collection-path
    original commit: 86d9a2fff2cb0cad4337841d2a6ed53be401c3f1

--- pkgs/net-pkgs/net-test/tests/net/encoders.rkt
+++ pkgs/net-pkgs/net-test/tests/net/encoders.rkt
@@ -2,7 +2,7 @@
 (require net/base64 net/qp tests/eli-tester)
 
 (define tricky-strings
-  (let ([dir (collection-path "tests" "racket")])
+  (let ([net.rktl (collection-file-path "net.rktl" "tests" "racket")])
     (list (make-bytes 200 32)
           (make-bytes 200 9)
           (make-bytes 200 (char->integer #\x))
@@ -12,12 +12,12 @@
           (make-bytes 204 (char->integer #\x))
           (list->bytes (for/list ([i (in-range 256)]) i))
           ;; Something that doesn't end with a LF:
-          (bytes-append (with-input-from-file (build-path dir "net.rktl")
+          (bytes-append (with-input-from-file net.rktl
                           (lambda () (read-bytes 500)))
            #"xxx")
           ;; CRLF:
           (regexp-replace #rx#"\r?\n"
-                          (with-input-from-file (build-path dir "net.rktl")
+                          (with-input-from-file net.rktl
                             (lambda () (read-bytes 500)))
            #"\r\n"))))
 
