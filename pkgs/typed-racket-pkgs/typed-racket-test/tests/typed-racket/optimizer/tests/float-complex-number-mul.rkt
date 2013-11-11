d629614620655ccf0a030d2a687e9bf79bcf2209
Eric Dobson <eric.n.dobson@gmail.com>
2013-11-11 08:19:58 -0800

    Add missing logs.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/float-complex-number-mul.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/float-complex-number-mul.rkt
@@ -6,6 +6,7 @@ TR opt: float-complex-number-mul.rkt 16:14 0.0+1.0i -- unboxed literal
 TR opt: float-complex-number-mul.rkt 16:23 (* 1 2) -- fixnum bounded expr
 TR opt: float-complex-number-mul.rkt 16:23 (* 1 2) -- float-arg-expr in complex ops
 TR opt: float-complex-number-mul.rkt 16:26 1 -- float-arg-expr in complex ops
+TR opt: float-complex-number-mul.rkt 16:26 1 -- float-arg-expr in complex ops
 TR opt: float-complex-number-mul.rkt 16:28 2 -- float-arg-expr in complex ops
 END
 #<<END
