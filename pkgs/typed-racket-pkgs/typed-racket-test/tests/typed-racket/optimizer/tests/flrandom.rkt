936ad898ef3ecb4d4b3236098349bb3f662958ac
Vincent St-Amour <stamourv@racket-lang.org>
2013-11-14 16:36:43 -0500

    Fix test.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/flrandom.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/flrandom.rkt
@@ -1,5 +1,6 @@
 #;#;
 #<<END
+TR info: flrandom.rkt 15:7 random -- hidden parameter (random)
 TR info: flrandom.rkt 16:7 random -- hidden parameter (random)
 TR opt: flrandom.rkt 15:6 (random) -- float 0-arg random
 TR opt: flrandom.rkt 17:6 (random (current-pseudo-random-generator)) -- float random
@@ -11,7 +12,6 @@ END
 #lang typed/racket
 
 (require racket/flonum)
-
 (void (random)) ; yes
 (void (random 2)) ; no
 (void (random (current-pseudo-random-generator))) ; yes
