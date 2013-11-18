af6bc6e8e225cd5ce3bb2bdd2b64a6c362fb6d32
Eric Dobson <eric.n.dobson@gmail.com>
2013-11-18 08:40:09 -0800

    Make unboxed let-bindings only log their clause.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/nested-unboxed-let.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/nested-unboxed-let.rkt
@@ -1,13 +1,13 @@
 #;#;
 #<<END
-TR opt: nested-unboxed-let.rkt 23:0 (let ((x (+ 1.0+2.0i 2.0+3.0i))) (let ((x (+ x 2.0+3.0i))) (+ x 3.0+6.0i))) -- unboxed let bindings
 TR opt: nested-unboxed-let.rkt 23:12 1.0+2.0i -- unboxed literal
 TR opt: nested-unboxed-let.rkt 23:21 2.0+3.0i -- unboxed literal
+TR opt: nested-unboxed-let.rkt 23:6 (x (+ 1.0+2.0i 2.0+3.0i)) -- unboxed let bindings
 TR opt: nested-unboxed-let.rkt 23:9 (+ 1.0+2.0i 2.0+3.0i) -- unboxed binary float complex
 TR opt: nested-unboxed-let.rkt 24:11 (+ x 2.0+3.0i) -- unboxed binary float complex
 TR opt: nested-unboxed-let.rkt 24:14 x -- leave var unboxed
 TR opt: nested-unboxed-let.rkt 24:16 2.0+3.0i -- unboxed literal
-TR opt: nested-unboxed-let.rkt 24:2 (let ((x (+ x 2.0+3.0i))) (+ x 3.0+6.0i)) -- unboxed let bindings
+TR opt: nested-unboxed-let.rkt 24:8 (x (+ x 2.0+3.0i)) -- unboxed let bindings
 TR opt: nested-unboxed-let.rkt 25:4 (+ x 3.0+6.0i) -- unboxed binary float complex
 TR opt: nested-unboxed-let.rkt 25:7 x -- leave var unboxed
 TR opt: nested-unboxed-let.rkt 25:9 3.0+6.0i -- unboxed literal
