cca6dab472894fe98ef3e12bf65d5d0aa4129652
Eric Dobson <eric.n.dobson@gmail.com>
2013-11-17 20:50:25 -0800

    Remove unused require.

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed/racket/base.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed/racket/base.rkt
@@ -3,18 +3,16 @@
 (providing (libs (except racket/base #%module-begin #%top-interaction
                          with-handlers default-continuation-prompt-tag
                          define λ lambda define-struct for for*))
-	   (basics #%module-begin #%top-interaction))
+           (basics #%module-begin #%top-interaction))
 
 (require typed-racket/base-env/extra-procs
          (except-in typed-racket/base-env/prims
            require-typed-struct-legacy
            require/typed-legacy)
          typed-racket/base-env/base-types
-         typed-racket/base-env/base-types-extra
-	 (for-syntax typed-racket/base-env/base-types-extra))
+         typed-racket/base-env/base-types-extra)
 (provide (rename-out [define-type-alias define-type])
          (all-from-out typed-racket/base-env/prims)
          (all-from-out typed-racket/base-env/base-types)
          (all-from-out typed-racket/base-env/base-types-extra)
-	 assert defined? with-type for for*
-         (for-syntax (all-from-out typed-racket/base-env/base-types-extra)))
+         assert defined? with-type for for*)
