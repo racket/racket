2ae4d3aef4f5546eb2b33ab4e077b4e1ddfd9ab5
Jay McCarthy <jay@racket-lang.org>
2013-11-19 14:31:34 -0700

    Fixing GC test

--- pkgs/plai/tests/gc2/bad-mutators/setbang.rkt
+++ pkgs/plai/tests/gc2/bad-mutators/setbang.rkt
@@ -1,16 +1,21 @@
 #lang plai/gc2/mutator
 (allocator-setup "../good-collectors/good-collector.rkt" 1024)
 
-(test/value=?
- (let ([f 0])
-   (let ([g (lambda (n) f)])
-     (set! f 1)
-     (g 11)))
- 1)
+;; xxx someday we should move this to good by fixing set! or rejecting
+;; the program
 
-(test/value=?
- (let ([f (lambda (n) 'wrong-answer)])
-   (let ([g (lambda (n) (f n))])
-     (set! f (lambda (n) 'right-answer))
-     (g 11)))
- 'right-answer)
+(if (= (let ([f 0])
+         (let ([g (lambda (n) f)])
+           (set! f 1)
+           (g 11)))
+       1)
+  "okay"
+  (/ 1 0))
+
+(if (= (let ([f (lambda (n) 'wrong-answer)])
+         (let ([g (lambda (n) (f n))])
+           (set! f (lambda (n) 'right-answer))
+           (g 11)))
+       'right-answer)
+  "okay"
+  (/ 1 0))
