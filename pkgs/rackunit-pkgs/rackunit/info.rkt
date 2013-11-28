ae6bdd0168ec91652364330dd4c59e19a250415b
Robby Findler <robby@racket-lang.org>
2013-11-28 11:57:34 -0600

    remove dependency on rackunit-test from rackunit
    Please include on the release branch

--- pkgs/rackunit-pkgs/rackunit/info.rkt
+++ pkgs/rackunit-pkgs/rackunit/info.rkt
@@ -5,13 +5,11 @@
 (define deps '("rackunit-lib"
                "rackunit-doc"
                "rackunit-gui"
-               "rackunit-plugin-lib"
-               "rackunit-test"))
+               "rackunit-plugin-lib"))
 (define implies '("rackunit-lib"
                   "rackunit-doc"
                   "rackunit-gui"
-                  "rackunit-plugin-lib"
-                  "rackunit-test"))
+                  "rackunit-plugin-lib"))
 
 (define pkg-desc "RackUnit testing framework")
 
