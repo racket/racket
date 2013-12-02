33f13a33de25a58c60d1b497c97f2e82ed2caf2b
Eric Dobson <eric.n.dobson@gmail.com>
2013-12-01 18:27:01 -0800

    Make tr unit tests not run TR printer on test creation.
    This makes it so that only running a subset of the tests is faster.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/special-env-typecheck-tests.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/special-env-typecheck-tests.rkt
@@ -34,16 +34,17 @@
     [(_ expr ty) (syntax/loc stx (tc-e expr #:ret (ret ty)))]
     [(id a #:ret b)
      (syntax/loc stx
-       (let-values
-         ([(res1 expanded)
-           (phase1-phase0-eval
-             (let ([ex (local-expand #'a 'expression null)])
-               (find-mutated-vars ex mvar-env)
-               #`(values '#,(tc-expr ex) '#,(syntax->datum ex))))]
-          [(res2) (phase1-phase0-eval #`'#,b)])
-         (with-check-info (['expanded expanded])
-           (check-tc-result-equal? (format "~a ~a" (quote-line-number id) 'a)
-                                   res1 res2))))]))
+       (test-case (format "~a ~a" (quote-line-number id) 'a)
+         (let-values
+           ([(res1 expanded)
+             (phase1-phase0-eval
+               (let ([ex (local-expand #'a 'expression null)])
+                 (find-mutated-vars ex mvar-env)
+                 #`(values '#,(tc-expr ex) '#,(syntax->datum ex))))]
+            [(res2) (phase1-phase0-eval #`'#,b)])
+           (with-check-info (['expanded expanded])
+             (unless (tc-result-equal/test? res1 res2)
+               (fail-check "Expression didn't have expected type."))))))]))
 
 (define tests
   (test-suite
--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/test-utils.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/unit-tests/test-utils.rkt
@@ -20,11 +20,6 @@
   (syntax-case stx ()
     [(_ nm a b)
      (syntax/loc stx (test-check nm type-equal? a b))]))
-(define-binary-check (check-tc-result-equal?* tc-result-equal/test? a b))
-(define-syntax (check-tc-result-equal? stx)
-  (syntax-case stx ()
-    [(_ nm a b)
-     (syntax/loc stx (test-case nm (check-tc-result-equal?* a b)))]))
 
 (define-syntax gen-test-main
   (syntax-parser
