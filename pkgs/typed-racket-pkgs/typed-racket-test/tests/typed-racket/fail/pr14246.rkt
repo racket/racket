34c5b327463db014fb0d3c42922d60e75e4ada63
Asumu Takikawa <asumu@ccs.neu.edu>
2013-12-12 16:12:58 -0500

    Quote the identifier in the declaration error msg
    Please merge to v6.0

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/fail/pr14246.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/fail/pr14246.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred #rx"Type Checker: Declaration for n provided, but n has no definition")
+(exn-pred #rx"Type Checker: Declaration for `n' provided, but `n' has no definition")
 #lang typed/racket
 
 ;; Test for the error message for unbound module bindings
