f970d214e155e32c746f2ad0401faf9e65642f53
Eric Dobson <eric.n.dobson@gmail.com>
2013-12-15 09:35:31 -0800

    Split out do-standard-inits.

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typed-racket.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/typed-racket.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (require
- (for-syntax racket/base racket/lazy-require "env/env-req.rkt")
+ (for-syntax racket/base racket/lazy-require
+             "standard-inits.rkt")
  (for-syntax "utils/timing.rkt") ;; only for timing/debugging
  ;; the below requires are needed since they provide identifiers
  ;; that may appear in the residual program
@@ -13,38 +14,9 @@
 (provide (rename-out [module-begin #%module-begin]
                      [top-interaction #%top-interaction])
          with-type
-         (for-syntax do-standard-inits))
+         (for-syntax do-standard-inits)
+         )
 
-(begin-for-syntax
-  (lazy-require
-    [typed-racket/base-env/base-env ((init init-base-env))]
-    [typed-racket/base-env/base-env-numeric ((init init-base-env-numeric))]
-    [typed-racket/base-env/base-structs (initialize-structs)]
-    [typed-racket/base-env/base-env-indexing (initialize-indexing)]
-    [typed-racket/base-env/base-special-env (initialize-special)]
-    [typed-racket/base-env/base-contracted (initialize-contracted)]
-    [(submod typed-racket/base-env/base-types initialize) (initialize-type-names)]))
-
-(define-for-syntax initialized #f)
-(define-for-syntax (do-standard-inits)
-  (unless initialized
-    (do-time "Starting initialization")
-    (initialize-structs)
-    (do-time "Finshed base-structs")
-    (initialize-indexing)
-    (do-time "Finshed base-env-indexing")
-    (init-base-env)
-    (do-time "Finshed base-env")
-    (init-base-env-numeric)
-    (do-time "Finshed base-env-numeric")
-    (initialize-special)
-    (do-time "Finished base-special-env")
-    (initialize-contracted)
-    (do-time "Finished base-contracted")
-    (initialize-type-names)
-    (do-time "Finished base-types")
-    (set! initialized #t))
-  (do-requires))
 
 (define-syntax-rule (drivers [name sym] ...)
   (begin
