13cab9ca7a4e0f58bf036c4c199805eeb77f09d5
Robby Findler <robby@racket-lang.org>
2013-12-31 15:47:21 -0600

    disable planet tests except run-all.rkt
    since run-all.rkt runs them all and this way we can
    make sure they run sequentially

--- pkgs/planet-pkgs/planet-test/info.rkt
+++ pkgs/planet-pkgs/planet-test/info.rkt
@@ -12,3 +12,13 @@
                      "rackunit-lib"
                      "scheme-lib"
                      "scribble-lib"))
+
+(define test-omit-paths
+  '("tests/planet/cmdline-tool.rkt"
+    "tests/planet/docs-build.rkt"
+    "tests/planet/lang.rkt"
+    "tests/planet/submod.rkt"
+    "tests/planet/test-docs-complete.rkt"
+    "tests/planet/thread-safe-resolver.rkt"
+    "tests/planet/version.rkt"
+    "tests/planet/examples"))
