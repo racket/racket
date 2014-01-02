f0b230a2e08685154a3c12ccc221f750ace2d26e
Eric Dobson <eric.n.dobson@gmail.com>
2014-01-01 19:54:06 -0800

    Added generic mechanism for easy to write terminal contracts.

--- pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/static-contracts/combinators/lengths.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-lib/typed-racket/static-contracts/combinators/lengths.rkt
@@ -6,47 +6,30 @@
 
 (require
   "../structures.rkt"
+  "../terminal.rkt"
   "simple.rkt"
   racket/match
+  (except-in racket/contract recursive-contract)
   (for-template racket/base))
 
 (provide
-  list-length/sc
-  vector-length/sc
-  empty-list/sc
-  empty-vector/sc)
+  (contract-out
+    [rename list-length/sc* list-length/sc (natural-number/c . -> . static-contract?)]
+    [vector-length/sc (natural-number/c . -> . static-contract?)]
+    [empty-list/sc static-contract?]
+    [empty-vector/sc static-contract?]))
 
+(define-terminal-sc list-length/sc (n) #:flat
+   #`(λ (l) (and (list? l) (= #,n (length l)))))
 
-(define (length-contract-write-proc v port mode)
-  (match-define (length-contract name length stx) v)
-  (define-values (open close)
-    (if (equal? mode 0)
-        (values "(" ")")
-        (values "#<" ">")))
-  (display open port)
-  (fprintf port "~a/sc" name)
-  (display " " port)
-  (write length port)
-  (display close port))
+(define-terminal-sc vector-length/sc (n) #:flat
+   #`(λ (l) (and (vector? l) (= #,n (vector-length l)))))
 
-
-(struct length-contract static-contract (name length syntax)
-        #:methods gen:sc
-         [(define (sc-map v f) v)
-          (define (sc->contract v f) (length-contract-syntax v))
-          (define (sc->constraints v f) 'flat)]
-        #:methods gen:terminal-sc
-         [(define (terminal-sc-kind v) 'flat)]
-        #:methods gen:custom-write [(define write-proc length-contract-write-proc)])
-
-(define (list-length/sc n)
-  (if (equal? 0 n)
+(define (list-length/sc* n)
+  (if (zero? n)
       empty-list/sc
-      (length-contract 'length n
-                       #`(λ (l) (and (list? l) (= #,n (length l)))))))
-(define empty-list/sc (flat/sc #'null?))
+      empty-vector/sc))
 
-(define (vector-length/sc n)
-  (length-contract 'vector-length n
-                   #`(λ (l) (and (vector? l) (= #,n (vector-length l))))))
+
+(define empty-list/sc (flat/sc #'null?))
 (define empty-vector/sc (vector-length/sc 0))
