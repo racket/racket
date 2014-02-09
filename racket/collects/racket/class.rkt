4d12021dbfd03f755665f9d33909ffda72adc65e
Robby Findler <robby@racket-lang.org>
2014-02-08 20:40:53 -0600

    disable the new class/c implementation for now
    the new implementation doesn't provide eq? guarantees
    as often as the old one does, so hold off on this until
    we can sort that out. Thanks to drdr for pointing out
    the problem:
    
    http://drdr.racket-lang.org/28156/pkgs/drracket-pkgs/drracket-test/tests/drracket/teaching-lang-save-file.rkt

--- racket/collects/racket/class.rkt
+++ racket/collects/racket/class.rkt
@@ -6,8 +6,7 @@
 ;; All of the implementation is actually in private/class-internal.rkt,
 ;;  which provides extra (private) functionality to contract.rkt.
 (require "private/class-internal.rkt"
-         (except-in "private/class-c-old.rkt" class/c)
-         (rename-in "private/class-c-new.rkt" [class/c2 class/c]))
+         "private/class-c-old.rkt")
   
 (provide-public-names)
 (provide generic?)
