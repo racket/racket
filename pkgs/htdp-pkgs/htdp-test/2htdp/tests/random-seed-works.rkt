5475dcaaf994d87a25ccf4c3c7916de05958f148
Matthias Felleisen <matthias@ccs.neu.edu>
2014-02-25 12:22:44 -0500

    a test for PR 14369

---
+++ pkgs/htdp-pkgs/htdp-test/2htdp/tests/random-seed-works.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require 2htdp/universe 2htdp/image rackunit)
+
+(define (main)
+  (random-seed 1324)
+  (big-bang
+   '()
+   #;
+   (on-tick (λ (l) (cons (random 100) l)) 1/100 30)
+   ;; it fails mostly with just time but not always, strange 
+   
+   (to-draw (λ (l) 
+              (text (if (> (length l) 3) "ok" (~a "press a again: " (- 2 (length l)))) 222 *color)))
+   (on-key (λ (l ke) 
+             (if (and (key=? "a" ke) (<= (length l) 3)) (cons (random 100) l) l)))
+   (stop-when (λ (l) (>= (length l) 2)))))
+(define *color 'blue)
+
+(check-equal? (main) (begin (set! *color 'red) (main)))
