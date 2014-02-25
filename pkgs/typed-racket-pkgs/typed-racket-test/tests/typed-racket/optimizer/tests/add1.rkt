40e3c50db7d5873592bc109dc0a0771e473d2107
Eric Dobson <eric.n.dobson@gmail.com>
2014-02-25 00:30:52 -0800

    Add missed optimization tests for add1/sub1.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/add1.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/add1.rkt
@@ -1,12 +1,16 @@
 #;#;
 #<<END
+TR missed opt: add1.rkt 2:0 (add1 (ann 5 Fixnum)) -- out of fixnum range
+TR missed opt: add1.rkt 4:0 (sub1 (ann 3 Fixnum)) -- out of fixnum range
 TR opt: add1.rkt 1:5 (add1 5) -- fixnum add1
-TR opt: add1.rkt 2:5 (sub1 3) -- fixnum sub1
-TR opt: add1.rkt 3:5 (add1 2.3) -- float add1
-TR opt: add1.rkt 4:5 (sub1 2.25) -- float sub1
+TR opt: add1.rkt 3:5 (sub1 3) -- fixnum sub1
+TR opt: add1.rkt 5:5 (add1 2.3) -- float add1
+TR opt: add1.rkt 6:5 (sub1 2.25) -- float sub1
 END
 #<<END
 6
+6
+2
 2
 3.3
 1.25
@@ -15,6 +19,8 @@ END
 #lang typed/racket #:optimize
 #reader tests/typed-racket/optimizer/reset-port
 (ann (add1 5) Fixnum)
+(add1 (ann 5 Fixnum))
 (ann (sub1 3) Fixnum)
+(sub1 (ann 3 Fixnum))
 (ann (add1 2.3) Float)
 (ann (sub1 2.25) Float)
