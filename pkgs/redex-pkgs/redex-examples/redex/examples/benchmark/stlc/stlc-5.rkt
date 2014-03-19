0d14e14b21bb110d5b13dd3a81836361a976fb0e
Jay McCarthy <jay@racket-lang.org>
2014-03-19 15:06:33 -0600

    stlc 5

--- pkgs/redex-pkgs/redex-examples/redex/examples/benchmark/stlc/stlc-5.rkt
+++ pkgs/redex-pkgs/redex-examples/redex/examples/benchmark/stlc/stlc-5.rkt
@@ -267,3 +267,7 @@
       (begin0
         (generate-term stlc M #:i-th index)
         (set! index (add1 index))))))
+
+(define small-counter-example
+  (term (tl ((cons 1) nil))))
+(test-equal (check small-counter-example) #f)
