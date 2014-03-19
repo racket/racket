f7efcd1057051c241467f0dcf00d041242135228
Jay McCarthy <jay@racket-lang.org>
2014-03-19 15:56:42 -0600

    polystlc 6

--- pkgs/redex-pkgs/redex-examples/redex/examples/benchmark/poly-stlc/poly-stlc-6.rkt
+++ pkgs/redex-pkgs/redex-examples/redex/examples/benchmark/poly-stlc/poly-stlc-6.rkt
@@ -292,3 +292,6 @@
         (generate-term poly-stlc M #:i-th index)
         (set! index (add1 index))))))
 
+(define small-counter-example
+  (term ([hd @ int] (([cons @ int] 1) [nil @ int]))))
+(test-equal (check small-counter-example) #f)
