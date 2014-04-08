74f82bbd1d1f0b9a8f265473f4ff5fe888b1d177
Matthew Flatt <mflatt@racket-lang.org>
2014-04-08 10:22:15 -0600

    racket/undefined: remove `undefined?` and `check-not-undefined`
    The intent is to provide a minimal bridge between the current Racket
    and one where `(letrec ([x x]) x)` no longer works.
    
    Merge to v6.0.1
    (cherry picked from commit 83a573ccd8b216d9600435692dfa4da851a55d91)

--- racket/collects/racket/undefined.rkt
+++ racket/collects/racket/undefined.rkt
@@ -1,21 +1,8 @@
 #lang racket/base
-(require '#%kernel)
 
-(provide check-not-undefined
-         undefined
-         undefined?)
+(provide undefined)
 
 ;; In a future version of Racket, this `letrec` pattern
 ;; will not work, but the `racket/undefined` library will
 ;; still export an `undefined`:
 (define undefined (letrec ([x x]) x))
-
-(define (undefined? v) (eq? v undefined))
-(define (check-not-undefined v s)
-  (unless (symbol? s) (raise-argument-error 'check-not-undefined "symbol?" 1 v s))
-  (if (eq? v undefined)
-      (raise (make-exn:fail:contract:variable
-              (format "~a: variable used before its definition" s)
-              (current-continuation-marks)
-              s))
-      v))
