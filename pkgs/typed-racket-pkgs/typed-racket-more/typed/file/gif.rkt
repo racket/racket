8c1206c06303b5ee3635bec2c04c6b8b594fd2ea
Asumu Takikawa <asumu@ccs.neu.edu>
2014-04-08 22:45:47 -0400

    Fix types in typed/file/gif
    Thanks to WarGrey Gyoudmon Ju for reporting the bug
    
    Please merge to v6.0.1

--- pkgs/typed-racket-pkgs/typed-racket-more/typed/file/gif.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-more/typed/file/gif.rkt
@@ -4,13 +4,15 @@
 
 (require/opaque-type GIF-Stream gif-stream? file/gif)
 
-(require/typed/provide file/gif
-  [gif-start ( Output-Port Number Number Number (U #f (Listof (Vectorof Number))) -> Void )]
-  [gif-add-image ( GIF-Stream Number Number Number Number Boolean (U #f Number) String -> Void )]
-  [gif-add-control ( GIF-Stream Symbol Boolean Number (U #f Number) -> Void)]
-  [gif-add-loop-control ( GIF-Stream Number -> Void )]
-  [gif-add-comment ( GIF-Stream String -> Void )]
-  [gif-end ( GIF-Stream -> Void )]
-  [quantize ( String -> (values String (Listof (Vectorof Number)) (U #f (Vectorof Number))))])
+(define-type GIF-Colormap (Listof (Vector Integer Integer Integer)))
 
-(provide gif-stream? GIF-Stream)
+(require/typed/provide file/gif
+  [gif-start ( Output-Port Integer Integer Integer (U #f GIF-Colormap) -> GIF-Stream )]
+  [gif-add-image ( GIF-Stream Integer Integer Integer Integer Any (U #f GIF-Colormap) Bytes -> Void )]
+  [gif-add-control ( GIF-Stream (U 'any 'keep 'restore-bg 'restore-prev) Any Integer (U #f Integer) -> Void)]
+  [gif-add-loop-control ( GIF-Stream Integer -> Void )]
+  [gif-add-comment ( GIF-Stream Bytes -> Void )]
+  [gif-end ( GIF-Stream -> Void )]
+  [quantize ( Bytes -> (values Bytes GIF-Colormap (U #f Integer)))])
+
+(provide gif-stream? GIF-Stream GIF-Colormap)
