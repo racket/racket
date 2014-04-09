7b172ac6d7737f019845da8b27eb9a9076ad2549
Matthew Flatt <mflatt@racket-lang.org>
2014-04-09 08:15:31 -0600

    fix `typed/test-engine`

--- pkgs/htdp-pkgs/htdp-lib/typed/test-engine/type-env-ext.rkt
+++ pkgs/htdp-pkgs/htdp-lib/typed/test-engine/type-env-ext.rkt
@@ -18,62 +18,62 @@
     (-> -Void)]
    ;; insert-test
    [(syntax-parse (local-expand #'(ce:check-expect 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-values-expected _ _ _ _))))))
        #'insert-test])
     (Univ (-> Univ) . -> . -Void)]
    ;; builder
    [(syntax-parse (local-expand #'(ce:check-expect 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ (nvv _ _ builder _)))
+         (let* ((_ _) (_ (nvv _ _ builder _)))
            _))
        #'builder])
     (-> Univ)]
    ;; check-values-expected
    [(syntax-parse (local-expand #'(ce:check-expect 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-values-expected _ _ _ _))))))
        #'check-values-expected])
     ((-> Univ) Univ Univ Univ . -> . -Void)]
    ;; check-values-within
    [(syntax-parse (local-expand #'(ce:check-within 1 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-values-within _ _ _ _ _))))))
        #'check-values-within])
     ((-> Univ) Univ -Real Univ Univ . -> . -Void)]
    ;; check-values-error
    [(syntax-parse (local-expand #'(ce:check-error 1 "foo") 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-values-error _ _ _ _))))))
        #'check-values-error])
     ((-> Univ) -String Univ Univ . -> . -Void)]
    ;; check-range-values-expected
    [(syntax-parse (local-expand #'(ce:check-range 1 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-range-values-expected _ _ _ _ _))))))
        #'check-range-values-expected])
     ((-> -Real) -Real -Real Univ Univ . -> . -Void)]
    ;; check-member-of-values-expected
    [(syntax-parse (local-expand #'(ce:check-member-of 1 1) 'module #f)
-      #:literals (let when define-values)
+      #:literals (let* when define-values)
       [(define-values _
-         (let ((_ _))
+         (let* ((_ _) (_ _))
            (when _
              (insert-test _ (lambda () (check-member-of-values-expected _ _ _ _ _))))))
        #'check-member-of-values-expected])
