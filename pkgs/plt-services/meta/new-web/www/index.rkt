e806035b00e253cce52e125ef479b9bf5c42d2f8
Vincent St-Amour <stamourv@racket-lang.org>
2014-04-11 16:22:44 -0400

    Fix RacketCon date on web page.

--- pkgs/plt-services/meta/new-web/www/index.rkt
+++ pkgs/plt-services/meta/new-web/www/index.rkt
@@ -371,7 +371,7 @@ applications from @a[class: "introlink" href: (docs-path "web-server/")]{web ser
   @h2[style: "font-size: 144%; margin-bottom: 6pt;"]{News}
   @p[style: "font-size: 80%;"]{Racket version 6.0 is out!}
   @p[style: "font-size: 80%;"]{@rcon[2014]{RacketCon 2014} will take place
-  in St.@|nbsp|Louis on September 19.}}}
+  in St.@|nbsp|Louis on September 20.}}}
 
 
 @columns[12 #:row? #t #:center? #t style: "text-align:justify;font-size: 120%; margin-top: 20pt; "]{
