dafad720dbe7806497e5f5efbc9b78312c5b9187
Robby Findler <robby@racket-lang.org>
2014-04-19 17:34:42 -0500

    restore the fullscreen button

--- pkgs/drracket-pkgs/drracket/drracket/private/unit.rkt
+++ pkgs/drracket-pkgs/drracket/drracket/private/unit.rkt
@@ -4443,7 +4443,7 @@
         
       (super-new
        [filename filename]
-       [style '(toolbar-button)]
+       [style '(toolbar-button fullscreen-button)]
        [size-preferences-key 'drracket:window-size]
        [position-preferences-key 'drracket:window-position])
       
