47abc2f00b928088893afdeac0bc6f77478d9f1c
Vincent St-Amour <stamourv@racket-lang.org>
2014-04-23 12:08:35 -0400

    Fix extflonum tests for line numbers.

--- pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/extflonums.rkt
+++ pkgs/typed-racket-pkgs/typed-racket-test/tests/typed-racket/optimizer/tests/extflonums.rkt
@@ -1,35 +1,35 @@
 #;#;
 #<<END
-TR opt: extflonums.rkt 70:0 (extfl+ 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 71:0 (extfl- 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 72:0 (extfl* 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 73:0 (extfl/ 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 74:0 (extfl= 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 75:0 (extfl< 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 76:0 (extfl> 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 77:0 (extfl<= 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 78:0 (extfl>= 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 79:0 (extflmin 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 80:0 (extflmax 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 81:0 (extflexpt 1.0t0 1.0t0) -- binary extflonum
-TR opt: extflonums.rkt 83:0 (extflabs 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 84:0 (extflround 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 85:0 (extflfloor 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 86:0 (extflceiling 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 87:0 (extfltruncate 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 88:0 (extflsin 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 89:0 (extflcos 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 90:0 (extfltan 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 91:0 (extflasin 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 92:0 (extflacos 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 93:0 (extflatan 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 94:0 (extfllog 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 95:0 (extflexp 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 96:0 (extflsqrt 1.0t0) -- unary extflonum
-TR opt: extflonums.rkt 98:0 (->extfl (bitwise-and 1 2)) -- fixnum to extflonum conversion
-TR opt: extflonums.rkt 98:9 (bitwise-and 1 2) -- binary fixnum
-TR opt: extflonums.rkt 99:0 (real->extfl (bitwise-and 1 2)) -- fixnum to extflonum conversion
-TR opt: extflonums.rkt 99:13 (bitwise-and 1 2) -- binary fixnum
+TR opt: extflonums.rkt 10:0 (extfl> 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 11:0 (extfl<= 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 12:0 (extfl>= 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 13:0 (extflmin 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 14:0 (extflmax 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 15:0 (extflexpt 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 17:0 (extflabs 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 18:0 (extflround 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 19:0 (extflfloor 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 20:0 (extflceiling 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 21:0 (extfltruncate 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 22:0 (extflsin 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 23:0 (extflcos 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 24:0 (extfltan 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 25:0 (extflasin 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 26:0 (extflacos 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 27:0 (extflatan 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 28:0 (extfllog 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 29:0 (extflexp 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 30:0 (extflsqrt 1.0t0) -- unary extflonum
+TR opt: extflonums.rkt 32:0 (->extfl (bitwise-and 1 2)) -- fixnum to extflonum conversion
+TR opt: extflonums.rkt 32:9 (bitwise-and 1 2) -- binary fixnum
+TR opt: extflonums.rkt 33:0 (real->extfl (bitwise-and 1 2)) -- fixnum to extflonum conversion
+TR opt: extflonums.rkt 33:13 (bitwise-and 1 2) -- binary fixnum
+TR opt: extflonums.rkt 4:0 (extfl+ 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 5:0 (extfl- 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 6:0 (extfl* 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 7:0 (extfl/ 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 8:0 (extfl= 1.0t0 1.0t0) -- binary extflonum
+TR opt: extflonums.rkt 9:0 (extfl< 1.0t0 1.0t0) -- binary extflonum
 END
 #<<END
 2.0t0
@@ -64,6 +64,7 @@ END
 END
 
 #lang typed/racket
+#reader tests/typed-racket/optimizer/reset-port
 
 (require racket/extflonum)
 
