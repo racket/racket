racket/racket

commit 5ea4c2ab6834259fddf9d5010b6dd1e243e4d354

fix `sync` on place with no writers
A `sync` on a place whose initial channel is known to have no writers
could trigger a place result instead of blocking on the place.

racket/src/racket/src/place.c
@@ -3487,10 +3487,7 @@ static int place_channel_ready(Scheme_Object *so, Scheme_Schedule_Info *sinfo) {
 
     return 1;
   }
-
-  if (no_writers)
-    return 1; /* wake up to discover that we can give up */
-
+  
   return 0;
 }
 

pkgs/racket-pkgs/racket-test/tests/racket/place-no-writer.rkt
@@ -1,0 +1,20 @@
+#lang racket/base
+(require racket/place)
+
+;; Check that syncing on a place (as opposed to place channel)
+;; known to have no writers will fail to sync (as opposed to
+;; returning the place itself, for example, which was the bug
+;; that triggered this test).
+
+(define (main)
+  (for ([i 10])
+    (displayln i)
+    (define p (place pch (void)))
+    (place-wait p)
+    (collect-garbage)
+    (define v (sync/timeout 0 p))
+    (when v
+      (error "failed: " v))))
+
+(module+ main (main))
+(module+ test (main))

commit 04a60d713b0fbf8682a7eae471707278029388eb

fix `sync` on inaccessible place channel
Commit 5ea4c2ab68 broke GCing of a thread that is blocked
via `sync` (as opposed to `place-channel-get`) on a place
channel whose write end is inaccessible.

racket/src/racket/src/place.c
@@ -3547,7 +3547,14 @@ static int place_channel_ready(Scheme_Object *so, Scheme_Schedule_Info *sinfo) {
 
     return 1;
   }
-  
+
+  if (no_writers) {
+    /* block on a semaphore that is not accessible, which may allow the thread
+       to be GCed */
+    scheme_set_sync_target(sinfo, scheme_make_sema(0), scheme_void, NULL, 0, 0, NULL);
+    return 0;
+  }
+
   return 0;
 }
 

pkgs/racket-pkgs/racket-test/tests/racket/place-channel.rkt
@@ -421,27 +421,30 @@
   ;; can be GCed if the other end of the channel is
   ;; unreachable --- where a place's channels should
   ;; all count as "unreachable" when the place ends
-  (displayln "checking place-channel and thread GC interaction")
-  (let ([N 40])
-    (define weaks (make-weak-hash))
-    (for ([i (in-range N)])
-      (define s (make-semaphore))
-      (hash-set!
-       weaks
-       (thread (lambda ()
-                 (define-values (i o) (place-channel))
-                 (define p (place ch (place-channel-get ch)))
-                 (place-channel-put p o)
-                 (place-wait p)
-                 (semaphore-post s)
-                 (sync i)))
-       #t)
-      (sync s))
-    (for ([i 3])
-      (sync (system-idle-evt))
-      (collect-garbage))
-    (unless ((hash-count weaks) . < . (/ N 2))
-      (error "thread-gc test failed")))
+  (define (check-thread sync-ch)
+    (displayln "checking place-channel and thread GC interaction")
+    (let ([N 40])
+      (define weaks (make-weak-hash))
+      (for ([i (in-range N)])
+        (define s (make-semaphore))
+        (hash-set!
+         weaks
+         (thread (lambda ()
+                   (define-values (i o) (place-channel))
+                   (define p (place ch (place-channel-get ch)))
+                   (place-channel-put p o)
+                   (place-wait p)
+                   (semaphore-post s)
+                   (sync-ch i)))
+         #t)
+        (sync s))
+      (for ([i 3])
+        (sync (system-idle-evt))
+        (collect-garbage))
+      (unless ((hash-count weaks) . < . (/ N 2))
+        (error "thread-gc test failed"))))
+  (check-thread place-channel-get)
+  (check-thread sync)
 
 )
   