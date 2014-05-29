f6a43b6b5d9adc60a4468c0038aaa89d69369d3b
Matthew Flatt <mflatt@racket-lang.org>
2014-05-29 13:35:18 +0100

    update RacketCon page to add the group-rate reservation link
    Plus various adjustments to the format.

--- pkgs/plt-services/meta/new-web/rcon/all.rkt
+++ pkgs/plt-services/meta/new-web/rcon/all.rkt
@@ -12,24 +12,43 @@
 
 (register-identity con-site)
 
+(define group-rate-url
+  "http://doubletree.hilton.com/en/dt/groups/personalized/S/STLUSDT-RAC-20140919/index.jhtml?WT.mc_id=POG")
+
+(define hotel @a[href: "http://www.stlunionstationhotel.com/"]{Union Station DoubleTree})
+
 (define index
   @page[#:site con-site
         #:link-title "RacketCon" #:title "RacketCon"
         #:extra-headers style-header]{
-   @columns[8 #:center? #t #:row? #t]{
-    @h2{RacketCon}
-    @p{@b{(fourth RacketCon)} will be held in St. Louis on September 20, 2014,
-       the day after @a[href: "https://thestrangeloop.com/"]{Strange Loop}.
-       Stay turned for more information!}
-    @p{@b{@a[href: "https://www.eventbrite.com/e/racketcon-2014-tickets-11408046775"]{Registration}}
-       is open. Tickets are 30$.}
-    @p{@b{@a[href: "http://www.fogus.me/"]{Michael Fogus}} will be giving the keynote.}
-    @p*{@b{RacketCon} is a public meeting for everyone interested in Racket:
-        developers, contributors, programmers, educators, and by-standers.  It
-        is an opportunity for all members of the community to come together to
-        share plans, ideas, and enthusiasm.  RacketCon will enable the entire
-        Racket community to mingle: to update each other, to exchange ideas, to
-        collaborate, and to help shape the future of Racket.}
-    @ul{@li{@2013:index}
-        @li{@2012:index}
-        @li{@2011:index}}}})
+   @columns[12 #:center? #t #:row? #t]{
+     @h2{RacketCon: 20 September 2014}
+
+     @columns[6 #:center? #f #:row? #f]{
+       @p{@b{(fourth RacketCon)} will be held in St. Louis on
+          the day after @a[href: "https://thestrangeloop.com/"]{Strange Loop}.
+          Stay turned for more information!}
+       @p{@b{@a[href: "https://www.eventbrite.com/e/racketcon-2014-tickets-11408046775"]{Registration}}
+          is open. Tickets are $30.}
+       @p{@b{@a[href: "http://www.fogus.me/"]{Michael Fogus}} will be giving the keynote.}
+
+       @p{RacketCon will be held at the @|hotel| (which is one of the Strange Loop venues).
+          A @a[href: group-rate-url]{group rate} is available for RacketCon attendees.}}
+    
+    @columns[1 #:center? #f #:row? #f]{ }
+
+    @columns[5 #:center? #f #:row? #f]{
+
+      @p*{@b{RacketCon} is a public meeting for everyone interested in Racket:
+          developers, contributors, programmers, educators, and by-standers.  It
+          is an opportunity for all members of the community to come together to
+          share plans, ideas, and enthusiasm.  RacketCon will enable the entire
+          Racket community to mingle: to update each other, to exchange ideas, to
+          collaborate, and to help shape the future of Racket.}
+
+      @p{@nbsp}
+
+      @p{Previous years:}
+      @ul{@li{@2013:index}
+          @li{@2012:index}
+          @li{@2011:index}}}}})
