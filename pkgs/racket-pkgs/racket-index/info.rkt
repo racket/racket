5058c958a6af22bdb8c6dad6319983d9b19a2a1e
Matthew Flatt <mflatt@racket-lang.org>
2014-06-04 09:03:07 +0100

    scribblings/main: keep doc modules in binary mode
    Even in binary mode, the "main" documents need to be re-rendered
    when new packages are installed.

--- pkgs/racket-pkgs/racket-index/info.rkt
+++ pkgs/racket-pkgs/racket-index/info.rkt
@@ -10,3 +10,17 @@
 (define pkg-desc "Racket Documentation driver")
 
 (define pkg-authors '(eli jay matthias mflatt robby ryanc samth))
+
+;; We need to be able to re-render this documentation even in
+;; binary mode, since that's how we list new documentation:
+(define binary-keep-files '("scribblings"
+                            "scribblings/main/compiled/acks_scrbl.zo"
+                            "scribblings/main/compiled/license_scrbl.zo"
+                            "scribblings/main/compiled/local-redirect_scrbl.zo"
+                            "scribblings/main/compiled/release_scrbl.zo"
+                            "scribblings/main/compiled/search_scrbl.zo"
+                            "scribblings/main/compiled/start_scrbl.zo"
+                            "scribblings/main/user/compiled/local-redirect_scrbl.zo"
+                            "scribblings/main/user/compiled/release_scrbl.zo"
+                            "scribblings/main/user/compiled/search_scrbl.zo"
+                            "scribblings/main/user/compiled/start_scrbl.zo"))
