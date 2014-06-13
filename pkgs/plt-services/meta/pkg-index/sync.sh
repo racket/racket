0c68b4548878629311b7ab60d595c40a5d58bf75
Jay McCarthy <jay@racket-lang.org>
2014-06-13 15:51:50 -0600

    Move backup, again

--- pkgs/plt-services/meta/pkg-index/sync.sh
+++ pkgs/plt-services/meta/pkg-index/sync.sh
@@ -3,8 +3,8 @@
 # planet-compat
 rsync -a --progress -h --delete --exclude root --exclude static-gen --exclude static-gen.gz --exclude compiled --exclude doc ../../meta/pkg-index/ plt-etc:local/new-plt/pkgs/plt-services/meta/pkg-index/
 
-rsync -a --progress -h --delete plt-etc:local/galaxy-roots/planet-compat/ ~/Dev/local/pkg.rlo.planet-compat/root/
-rsync -a --progress -h --delete plt-etc:local/galaxy-roots/official/ ~/Dev/local/pkg.rlo.official/root/
+rsync -a --progress -h --delete plt-etc:local/galaxy-roots/planet-compat/ ~/Dev/local/pkg.rlo/planet-compat/root/
+rsync -a --progress -h --delete plt-etc:local/galaxy-roots/official/ ~/Dev/local/pkg.rlo/official/root/
 
 rsync -a --progress -h --delete plt-etc:local/new-plt/pkgs/plt-services/meta/pkg-index//official/static-gen/ official/static-gen 
 rsync -a --progress -h --delete plt-etc:local/new-plt/pkgs/plt-services/meta/pkg-index//official/static-gen.gz/ official/static-gen.gz
