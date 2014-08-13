c359f7ac29476cc2c8a7a4c1077f4730149a9bc7
Matthew Flatt <mflatt@racket-lang.org>
2014-08-13 06:53:16 +0100

    build less when a pre-built `racket` is supplied
    Adjust dependency tracking and makefile rules to that when
    `--enable-racket=...` is provided to `configure`, intermediate
    CGC objects are not compiled.
    
    The new approach uses dependency tracking that was already supported
    by xform, previously used only for Windows.

---
+++ racket/src/racket/gc2/check-sdep.rkt
@@ -0,0 +1,43 @@
+(module xform '#%kernel
+  (#%require '#%min-stx
+             '#%utils
+             '#%paramz)
+
+  (define-values (loop)
+    (lambda (paths)
+      (if (null? paths)
+          (void)
+          (let-values ([(path) (build-path "xsrc" (car paths))])
+            (cond
+             [(regexp-match? #rx"[.][ch]$" path)
+              (define-values (ts) (file-or-directory-modify-seconds path))
+              (define-values (sdep) (path-replace-suffix path ".sdep"))
+              (call/ec
+               (lambda (esc)
+                 (with-continuation-mark
+                     exception-handler-key
+                     (lambda (exn)
+                       (if (exn:fail? exn)
+                           (begin
+                             (printf "~a\n removing ~a\n"
+                                     (exn-message exn)
+                                     path)
+                             (delete-file path)
+                             (esc))
+                           exn))
+                   (let-values ()
+                     (define-values (dloop)
+                       (lambda (paths)
+                         (if (null? paths)
+                             (void)
+                             (let-values ()
+                               (define-values (ts2) (file-or-directory-modify-seconds (bytes->path (car paths))))
+                               (if (ts2 . > . ts)
+                                   (error 'changed-dependency "~a" (car paths))
+                                   (dloop (cdr paths)))))))
+                     (dloop (call-with-input-file sdep read))))))])
+            (loop (cdr paths))))))
+
+  (if (directory-exists? "xsrc")
+      (loop (directory-list "xsrc"))
+      (void)))
