a96b45be9c2f9debf55ad109cfd6d0f21a1603ec
Matthias Felleisen <matthias@ccs.neu.edu>
2014-09-01 21:22:35 +0200

    wrapped in new testing macro

--- pkgs/htdp-pkgs/htdp-test/2htdp/tests/bitmap-as-image-in-universe.rkt
+++ pkgs/htdp-pkgs/htdp-test/2htdp/tests/bitmap-as-image-in-universe.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
-(require pict (only-in 2htdp/universe big-bang on-tick to-draw) rackunit)
+(require pict (only-in 2htdp/universe big-bang on-tick to-draw) "test-aux.rkt")
 
-(check-equal?
- 103 
- (big-bang 100
-           [on-tick add1 1/28 3]
-           [to-draw (lambda (w) (pict->bitmap (circle w)))]))
+(testing
+  (check-equal?
+    103 
+    (big-bang 100
+              [on-tick add1 1/28 3]
+	      [to-draw (lambda (w) (pict->bitmap (circle w)))])))
