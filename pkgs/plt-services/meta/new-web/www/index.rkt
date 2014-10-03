1d51798ccbfd633b1f954dc3c62de0f8946cea16
Robby Findler <robby@racket-lang.org>
2014-10-02 20:14:07 -0500

    remove equality to make the program easier to read
    (in consultation with Jay)

--- pkgs/plt-services/meta/new-web/www/index.rkt
+++ pkgs/plt-services/meta/new-web/www/index.rkt
@@ -251,7 +251,7 @@
      @code{#lang datalog
            ancestor(A, B) :- parent(A, B).
            ancestor(A, B) :-
-             parent(A, C), D = C, ancestor(D, B).
+             parent(A, C), ancestor(C, B).
            parent(john, douglas).
            parent(bob, john).
            ancestor(A, B)?}
