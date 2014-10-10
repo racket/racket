4607396bd749f2eec21c5b4731a88e1ceeefe165
Matthew Flatt <mflatt@racket-lang.org>
2014-10-10 11:30:28 -0600

    Always convert string<->paths with UTF-8 on Windows
    Also, document representation information on paths. In particular,
    explain that Unix and Mac OS X paths are natively byte strings, while
    Windows paths are natively UTF-16 code-unit sequences. The byte-string
    representation of a Windows path is a UTF-8-like encoding of the UTF-16
    code-unit sequence, which is why it makes no sense to convert it using
    the current locale's encoding.

--- racket/src/racket/src/file.c
+++ racket/src/racket/src/file.c
@@ -813,7 +813,11 @@ static Scheme_Object *append_path(Scheme_Object *a, Scheme_Object *b)
 
 Scheme_Object *scheme_char_string_to_path(Scheme_Object *p)
 {
+#ifdef DOS_FILE_SYSTEM
+  p = scheme_char_string_to_byte_string(p);
+#else
   p = scheme_char_string_to_byte_string_locale(p);
+#endif
   p->type = SCHEME_PLATFORM_PATH_KIND;
   return p;
 }
@@ -889,7 +893,11 @@ Scheme_Object *scheme_path_to_char_string(Scheme_Object *p)
 {
   Scheme_Object *s;
 
+#ifdef DOS_FILE_SYSTEM
+  s = scheme_byte_string_to_char_string(p);
+#else
   s = scheme_byte_string_to_char_string_locale(p);
+#endif
 
   if (!SCHEME_CHAR_STRLEN_VAL(s))
     return scheme_make_utf8_string("?");
