0b461f98bd235f776c2feb5f9904a6882473198a
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-12-04 17:23:48 -0500

    A file for this already existed.

--- pkgs/racket-pkgs/racket-test/tests/racket/run-quiet.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
-(require racket/runtime-path)
-(define-runtime-path quiet "quiet.rktl")
-(current-namespace (make-base-namespace))
-(eval '(require racket (for-syntax racket)))
-(load "quiet.rktl")
