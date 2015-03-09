59777ca17ac5aa59b18c1f49cff63c32f3af14dd
Matthew Flatt <mflatt@racket-lang.org>
2015-03-09 13:33:19 -0600

    increase time thread's stack size
    On 64-bit FreeBSD 10.1, 4k is too small.

--- racket/src/racket/src/port.c
+++ racket/src/racket/src/port.c
@@ -11047,7 +11047,7 @@ static void start_green_thread_timer(intptr_t usec)
   itimerdata->jit_stack_boundary_ptr = &scheme_jit_stack_boundary;
   pthread_mutex_init(&itimerdata->mutex, NULL);
   pthread_cond_init(&itimerdata->cond, NULL);
-  tmp = mz_proc_thread_create_w_stacksize(green_thread_timer, itimerdata, 4096);
+  tmp = mz_proc_thread_create_w_stacksize(green_thread_timer, itimerdata, 16384);
   itimerdata->thread = tmp;
   itimerdata->itimer = 1;
 }
