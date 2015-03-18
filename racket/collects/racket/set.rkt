585f29cf0dacc57d5a30ebf444d47cf156f5104c
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-03-18 15:00:10 -0400

    Reduce contract dependencies from `racket/set`.

--- racket/collects/racket/set.rkt
+++ racket/collects/racket/set.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require racket/contract
+(require racket/contract/base
+         racket/contract/combinator
          racket/private/set
          racket/private/set-types
          racket/generic
