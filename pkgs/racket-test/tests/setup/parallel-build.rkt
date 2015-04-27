4e3a7c420a6a515df5d2f167f8d10cc5c9d71340
Eric Dobson <eric.n.dobson@gmail.com>
2015-04-26 22:38:13 -0700

    Make parallel-build send the whole filename to the worker.
    Closes PR 15000.

--- pkgs/racket-test/tests/setup/parallel-build.rkt
+++ pkgs/racket-test/tests/setup/parallel-build.rkt
@@ -10,4 +10,5 @@
 
 (define temporary-directory (make-temporary-file "parallel-build~a" 'directory))
 (check-false (parallel-compile-files (list temporary-directory)))
+(check-false (parallel-compile-files (list (build-path temporary-directory "new-directory" ".."))))
 (delete-directory/files temporary-directory)
