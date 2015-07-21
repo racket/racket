d1af0ec2483e8f11fbfd34f59df53f6ff12e6058
Rui Paulo <rpaulo@felyko.com>
2015-07-21 16:06:12 -0700

    sconfig: add FreeBSD/arm.

--- racket/src/racket/sconfig.h
+++ racket/src/racket/sconfig.h
@@ -466,6 +466,8 @@
 # elif defined(__sparc64__)
 #  define SCHEME_PLATFORM_LIBRARY_SUBPATH "sparc64-freebsd"
 #  define FLUSH_SPARC_REGISTER_WINDOWS
+# elif defined(__arm__)
+#  define SCHEME_PLATFORM_LIBRARY_SUBPATH "arm-freebsd"
 # else
 #  error Unported platform.
 # endif
