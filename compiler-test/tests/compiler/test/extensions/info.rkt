ae3c87c10617b98019395993c41191014f3f416d
Matthew Flatt <mflatt@racket-lang.org>
2015-09-07 12:02:02 -0600

    fix "extensions" test to work when files are compiled

--- compiler-test/tests/compiler/test/extensions/info.rkt
+++ compiler-test/tests/compiler/test/extensions/info.rkt
@@ -1,3 +1,3 @@
 #lang info
-(define test-omit-paths '(#rx".*omit.*"))
-(define test-include-paths '(#rx".*include.*"))
+(define test-omit-paths '(#rx".*omit.*[.](rkt|racket-file)$"))
+(define test-include-paths '(#rx".*include.*[.](rkt|racket-file)$"))
