530e613c63ee31e18621ea5da35e6c60267dc550
Matthew Butterick <mb@mbtype.com>
2016-01-08 13:50:17 -0800

    update copyright from 2015 to 2016
    (cherry picked from commit 0fb11e61e6cf4327cf31d7c30ce5a34d11bf60cd)

--- racket/src/racket/src/regexp.c
+++ racket/src/racket/src/regexp.c
@@ -1,7 +1,7 @@
 /*
  * @(#)regexp.c 1.3 of 18 April 87
  * Revised for PLT Racket, 1995-2001
- * Copyright (c) 2004-2015 PLT Design Inc.
+ * Copyright (c) 2004-2016 PLT Design Inc.
  *
  * Copyright (c) 1986 by University of Toronto.
  * Written by Henry Spencer.  Not derived from licensed software.
