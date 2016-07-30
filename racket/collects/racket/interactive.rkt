4c61013ef2244c9a873ec03b676b7964f5f45511
Leif Andersen <leif@leifandersen.net>
2016-07-29 23:17:26 -0400

    Default repl should remain using ">" for prompt
    Although xrepl uses ->, the default prompt should be `>`, to
    maintain compatability with old shell scripts.

--- racket/collects/racket/interactive.rkt
+++ racket/collects/racket/interactive.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 (when (collection-file-path "main.rkt" "xrepl"
                             #:fail (lambda _ #f))
-  (dynamic-require 'xrepl #f))
+  (dynamic-require 'xrepl #f)
+  (define toplevel-prefix (dynamic-require 'xrepl/xrepl 'toplevel-prefix))
+  (toplevel-prefix ""))
 
 (let ([init-file (cleanse-path (find-system-path 'init-file))])
   (when (file-exists? init-file)
