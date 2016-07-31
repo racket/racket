6fe55ec3071a99706450c7f20dcf083f7d079bb3
Ryan Culpepper <ryanc@racket-lang.org>
2016-07-31 12:37:52 -0400

    syntax/parse: trim txlifts helper module

--- racket/collects/syntax/parse/private/txlift.rkt
+++ racket/collects/syntax/parse/private/txlift.rkt
@@ -1,14 +1,13 @@
 #lang racket/base
 (require (for-template racket/base))
 (provide txlift
-         get-txlifts
          get-txlifts-as-definitions
-         call/txlifts
          with-txlifts
-         with-txlifts/defs)
+         call/txlifts)
 
 ;; Like lifting definitions, but within a single transformer.
 
+;; current-liftbox : Parameter of [#f or (Listof (list Id Stx))]
 (define current-liftbox (make-parameter #f))
 
 (define (call/txlifts proc)
@@ -44,10 +43,3 @@
      (let ([v (proc)])
        (with-syntax ([((var rhs) ...) (get-txlifts)])
          #`(let* ([var rhs] ...) #,v))))))
-
-(define (with-txlifts/defs proc)
-  (call/txlifts
-   (lambda ()
-     (let ([v (proc)])
-       (with-syntax ([(def ...) (get-txlifts-as-definitions)])
-         #`(begin def ... #,v))))))
