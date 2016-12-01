racket/racket

commit 0133954c84f8fed5c1fa244d3bacecaa37d703ba

avoid a made-up OS error in `rename-file-or-directory`
On Unix and OS X, the check to avoid replacing an existing
file or directory is made by Racket, rather than the OS,
so don't claim a system error if the operation fails for
that reason.

Also, update the docs to clarify that the check is not
atomic with the move.

Closes issue #1158

racket/src/racket/src/file.c
@@ -4291,9 +4291,17 @@ static Scheme_Object *rename_file(int argc, Scheme_Object **argv)
 # define MOVE_ERRNO_FORMAT "%E"
 # else
   if (!exists_ok && (scheme_file_exists(dest) || scheme_directory_exists(dest))) {
-    exists_ok = -1;
-    errno = EEXIST;
-    goto failed;
+    /* We use a specialized error message here, because it's not
+       a system error (e.g., setting `errno` to `EEXIST` would
+       be a lie). */
+    scheme_raise_exn((exists_ok < 0) ? MZEXN_FAIL_FILESYSTEM_EXISTS : MZEXN_FAIL_FILESYSTEM, 
+                     "rename-file-or-directory: cannot rename file or directory;\n"
+                     " the destination path already exists\n"
+                     "  source path: %q\n"
+                     "  dest path: %q",
+                     filename_for_error(argv[0]),
+                     filename_for_error(argv[1]));
+    return NULL;
   }
   
   while (1) {
@@ -4305,9 +4313,6 @@ static Scheme_Object *rename_file(int argc, Scheme_Object **argv)
 # define MOVE_ERRNO_FORMAT "%e"
 # endif
 
-#ifndef DOS_FILE_SYSTEM
-failed:
-#endif
   scheme_raise_exn((exists_ok < 0) ? MZEXN_FAIL_FILESYSTEM_EXISTS : MZEXN_FAIL_FILESYSTEM, 
 		   "rename-file-or-directory: cannot rename file or directory\n"
                    "  source path: %q\n"

pkgs/racket-doc/scribblings/reference/filesystem.scrbl
@@ -300,7 +300,8 @@ renamed successfully, the @exnraise[exn:fail:filesystem].
 This procedure can be used to move a file/directory to a different
 directory (on the same filesystem) as well as rename a file/directory within
 a directory. Unless @racket[exists-ok?]  is provided as a true value,
-@racket[new] cannot refer to an existing file or directory. Even if
+@racket[new] cannot refer to an existing file or directory, but the
+check is not atomic with the rename operation on Unix and Mac OS X. Even if
 @racket[exists-ok?] is true, @racket[new] cannot refer to an existing
 file when @racket[old] is a directory, and vice versa.
 

commit 200fbe9b959563c78f1e6af3197ade6b3181d8fc

fix `rename-file-or-directory` for raising `exn:failsystem:fail:exists`
Broken by 0133954c84, which avoided a made-up `EEXIST` but left in
place a no-loner-appropriate test for raising `exn:failsystem:fail:exists`.

racket/src/racket/src/file.c
@@ -4300,7 +4300,7 @@ static Scheme_Object *rename_file(int argc, Scheme_Object **argv)
     /* We use a specialized error message here, because it's not
        a system error (e.g., setting `errno` to `EEXIST` would
        be a lie). */
-    scheme_raise_exn((exists_ok < 0) ? MZEXN_FAIL_FILESYSTEM_EXISTS : MZEXN_FAIL_FILESYSTEM, 
+    scheme_raise_exn(MZEXN_FAIL_FILESYSTEM_EXISTS,
                      "rename-file-or-directory: cannot rename file or directory;\n"
                      " the destination path already exists\n"
                      "  source path: %q\n"

pkgs/racket-test-core/tests/racket/path.rktl
@@ -64,6 +64,7 @@
 (define delete-file/tf (lambda (x) ((make-/tf delete-file exn:fail:filesystem?) x)))
 (define delete-directory/tf (lambda (x) ((make-/tf delete-directory exn:fail:filesystem?) x)))
 (define rename-file-or-directory/tf (lambda (x y) ((make-/tf rename-file-or-directory exn:fail:filesystem?) x y)))
+(define rename-file-or-directory/exists/tf (lambda (x y) ((make-/tf rename-file-or-directory exn:fail:filesystem:exists?) x y)))
 (define make-directory/tf (lambda (x) ((make-/tf make-directory exn:fail:filesystem?) x)))
 (define copy-file/tf (lambda (x y) ((make-/tf copy-file exn:fail:filesystem?) x y)))
 
@@ -191,8 +192,8 @@
 (close-output-port (open-output-file "tmp5"))
 (test #t file-exists? "tmp5")
 (test #t file-exists? "tmp5x")
-(test #f rename-file-or-directory/tf "tmp5" "tmp5x")
-(test #f rename-file-or-directory/tf "tmp5" "down")
+(test #f rename-file-or-directory/exists/tf "tmp5" "tmp5x")
+(test #f rename-file-or-directory/exists/tf "tmp5" "down")
 (delete-file "tmp5")
 (test #f file-exists? "tmp5")
 (test #t rename-file-or-directory/tf (build-path "down" "tmp8") (build-path "down" "tmp8x"))
@@ -201,7 +202,7 @@
 (test #f rename-file-or-directory/tf (build-path deepdir "tmp7") (build-path deepdir "tmp7x"))
 
 (test #t make-directory/tf "downx")
-(test #f rename-file-or-directory/tf "down" "downx")
+(test #f rename-file-or-directory/exists/tf "down" "downx")
 (test #t delete-directory/tf "downx")
 
 (test #t rename-file-or-directory/tf "down" "downx")