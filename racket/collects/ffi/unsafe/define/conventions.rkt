2f6c42f3c1a14a1bb3c8780b84f3b23ea7229526
Leif Andersen <leif@leifandersen.net>
2017-06-02 10:26:57 -0400

    Add a #:make-c-id flag to define-ffi-definer
    This make-c-id allows an author to specify a convention for how
    to connect and identifier defined with define-ffi-definer and
    the actual symbol in the file.
    
    * Adds docs.
    * Adds tests.
    * Adds history.

---
+++ racket/collects/ffi/unsafe/define/conventions.rkt
@@ -0,0 +1,17 @@
+#lang racket/base
+
+(provide convention:hyphen->underscore
+         convention:hyphen->camelcase)
+(require (for-syntax racket/base
+                     racket/syntax
+                     racket/string
+                     syntax/parse))
+
+(define-syntax (convention:hyphen->underscore id)
+  (format-id id (string-replace (symbol->string (syntax-e id)) "-" "_")))
+
+(define-syntax (convention:hyphen->camelcase id)
+  (define str (symbol->string (syntax-e id)))
+  (format-id id
+             (apply string-append
+                    (map string-titlecase (string-split str "-")))))
