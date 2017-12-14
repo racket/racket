0c8711a650c3648c647706cc9ffff92c7d217368
Ryan Culpepper <ryanc@racket-lang.org>
2017-12-14 12:04:38 +0100

    syntax/parse: use lazy-require-syntax

--- racket/collects/syntax/parse/private/sc.rkt
+++ racket/collects/syntax/parse/private/sc.rkt
@@ -1,6 +1,5 @@
 #lang racket/base
-(require (for-syntax racket/base
-                     racket/lazy-require)
+(require racket/lazy-require
          "keywords.rkt")
 
 ;; keep and keep as abs. path -- lazy-loaded macros produce references to this
@@ -8,25 +7,20 @@
 ;; (so for consistency etc, require absolutely from all modules)
 (require syntax/parse/private/residual)
 
-(begin-for-syntax
- (lazy-require
-  ;; load macro transformers lazily via identifier
-  ;; This module path must also be absolute (not sure why,
-  ;; but it definitely breaks on relative module path).
-  [syntax/parse/private/parse-aux
-   (id:define-syntax-class
-    id:define-splicing-syntax-class
-    id:define-integrable-syntax-class
-    id:syntax-parse
-    id:syntax-parser
-    id:define/syntax-parse
-    id:syntax-parser/template
-    id:parser/rhs
-    id:define-eh-alternative-set)]))
-;; FIXME: workaround for phase>0 bug in racket/runtime-path (and thus lazy-require)
-;; Without this, dependencies don't get collected.
-(require racket/runtime-path (for-meta 2 '#%kernel))
-(define-runtime-module-path-index _unused_ 'syntax/parse/private/parse-aux)
+(lazy-require-syntax
+ ;; load macro transformers lazily via identifier
+ ;; This module path must also be absolute (not sure why,
+ ;; but it definitely breaks on relative module path).
+ [syntax/parse/private/parse
+  (define-syntax-class
+   define-splicing-syntax-class
+   define-integrable-syntax-class
+   syntax-parse
+   syntax-parser
+   define/syntax-parse
+   syntax-parser/template
+   parser/rhs
+   define-eh-alternative-set)])
 
 (provide define-syntax-class
          define-splicing-syntax-class
@@ -45,26 +39,3 @@
          syntax-parser/template
          parser/rhs
          define-eh-alternative-set)
-
-(define-syntaxes (define-syntax-class
-                  define-splicing-syntax-class
-                  define-integrable-syntax-class
-                  syntax-parse
-                  syntax-parser
-                  define/syntax-parse
-                  syntax-parser/template
-                  parser/rhs
-                  define-eh-alternative-set)
-  (let ([tx (lambda (get-id)
-              (lambda (stx)
-                ((syntax-local-value (get-id)) stx)))])
-    (values 
-     (tx id:define-syntax-class)
-     (tx id:define-splicing-syntax-class)
-     (tx id:define-integrable-syntax-class)
-     (tx id:syntax-parse)
-     (tx id:syntax-parser)
-     (tx id:define/syntax-parse)
-     (tx id:syntax-parser/template)
-     (tx id:parser/rhs)
-     (tx id:define-eh-alternative-set))))
