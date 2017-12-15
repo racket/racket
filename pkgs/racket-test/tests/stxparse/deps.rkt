95e6244192e8ebbd48765091dbd06de5174e7462
Ryan Culpepper <ryanc@racket-lang.org>
2017-12-15 15:41:55 +0100

    syntax/parse: add debugging code for deps test case

--- pkgs/racket-test/tests/stxparse/deps.rkt
+++ pkgs/racket-test/tests/stxparse/deps.rkt
@@ -8,7 +8,8 @@
     [(mod rel stx load?)
      (when load?
        (define res (omnr mod rel stx #f))
-       (hash-set! table (resolved-module-path-name res) #t))
+       (let ([key (resolved-module-path-name res)])
+         (hash-set! table key (cons rel (hash-ref table key null)))))
      (omnr mod rel stx load?)]
     [args
      (apply omnr args)]))
@@ -19,6 +20,8 @@
 (define-check (check-no-dep mod)
   (define res ((current-module-name-resolver) mod #f #f #f))
   (define name (resolved-module-path-name res))
+  (define (not-loaded? name) (not (hash-ref loaded name #f)))
+  (check-pred not-loaded? name)
   (check-false (hash-ref loaded name #f)))
 
 (parameterize ((current-module-name-resolver
@@ -27,6 +30,12 @@
                  loaded))
                (current-namespace ns))
   (namespace-require 'syntax/parse/pre)
+
+  (when #f
+    (printf "loaded dependencies:\n")
+    (let ([mods (filter path? (hash-keys loaded))])
+      (for ([mod (sort mods path<?)])
+        (printf " ~s\n" mod))))
 
   (test-case "no dep on racket/contract/base"
     (check-no-dep 'racket/contract/base))
