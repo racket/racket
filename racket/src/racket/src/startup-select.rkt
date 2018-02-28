4eacbbd29612819fd98496ace47d9d960ff666df
Matthew Flatt <mflatt@racket-lang.org>
2018-02-28 06:56:16 -0700

    change makefile to be more helpful for "seletc-startup.rkt" failure

--- racket/src/racket/src/startup-select.rkt
+++ racket/src/racket/src/startup-select.rkt
@@ -1,6 +1,6 @@
 (module startup-select '#%kernel
   (if (eval-jit-enabled)
-      (display "cstartup_bytecode")
-      (display "cstartup_c"))
+      (display "bytecode")
+      (display "c"))
   (newline))
 
