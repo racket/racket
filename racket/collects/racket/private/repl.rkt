dba5255e9016e99d1c1fbf56fe2690c82edf3ec0
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2018-05-01 11:38:15 -0400

    Implement `read-eval-print-loop` using `#%kernel`.
    Makes the repl faster to load when using a very small language
    such as `racket/kernel/init`

---
+++ racket/collects/racket/private/repl.rkt
@@ -0,0 +1,33 @@
+(module repl '#%kernel
+
+  (#%provide read-eval-print-loop)
+
+  (define-values (read-eval-print-loop)
+    (lambda ()
+      (letrec-values ([(repl-loop)
+                       (lambda ()
+                         ;; This prompt catches all error escapes, including from read and print.
+                         (call-with-continuation-prompt
+                          (lambda ()
+                            (let-values ([(v) ((current-prompt-read))])
+                              (if (eof-object? v)
+                                  (void)
+                                  (begin
+                                    (call-with-values
+                                     (lambda () 
+                                       ;; This prompt catches escapes during evaluation.
+                                       ;; Unlike the outer prompt, the handler prints
+                                       ;; the results.
+                                       (call-with-continuation-prompt
+                                        (lambda ()
+                                          (let-values ([(w) (cons '#%top-interaction v)])
+                                            ((current-eval) (if (syntax? v)
+                                                                (namespace-syntax-introduce 
+                                                                 (datum->syntax #f w v))
+                                                                w))))))
+                                     (lambda results (for-each (current-print) results)))
+                                    ;; Abort to loop. (Calling `repl-loop` directly would not be a tail call.)
+                                    (abort-current-continuation (default-continuation-prompt-tag))))))
+                          (default-continuation-prompt-tag)
+                          (lambda args (repl-loop))))])
+        (repl-loop)))))
