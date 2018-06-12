380dc42427235a73d349f0af852d32ba9a8c7bed
Matthew Flatt <mflatt@racket-lang.org>
2018-06-12 12:58:56 -0600

    expander: fix interaction of `expand`+`eval` across namespaces
    This repair was motivated by tests in the "rex" package. The
    new test added here failed before by finding 'new both times,
    but in the "rex" case, the mixup led to the same variable
    being imported and exported at the linklet level.

--- racket/src/expander/compile/module-use.rkt
+++ racket/src/expander/compile/module-use.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
+(require "../common/module-path.rkt")
 
 ;; A `module-use` record is just a part of module path index plus
 ;; phase, since that combination is commonly needed
@@ -6,6 +7,24 @@
 (provide (struct-out module-use))
 
 (struct module-use (module phase)
-        ;; transparent for hashing; note that module path indices will
-        ;; be hashed as `equal?`, which makes sense within a module
-        #:transparent)
+  #:property prop:equal+hash
+  (list (lambda (a b eql?)
+          (define a-mod (module-use-module a))
+          (define b-mod (module-use-module b))
+          (and (eql? a-mod b-mod)
+               (eql? (module-use-phase a)
+                     (module-use-phase b))
+               ;; Unusual, but possible with top-level evaluation: can have
+               ;; different "self" MPIs that refer to different modules
+               (let-values ([(a-path a-base) (module-path-index-split a-mod)]
+                            [(b-path b-base) (module-path-index-split b-mod)])
+                 (or a-path
+                     b-path
+                     (eq? (module-path-index-resolved a-mod)
+                          (module-path-index-resolved b-mod))))))
+        (lambda (a hash-code)
+          (+ (hash-code (module-use-module a))
+             (hash-code (module-use-phase a))))
+        (lambda (a hash-code)
+          (+ (hash-code (module-use-module a))
+             (hash-code (module-use-phase a))))))
