c7c4efca95f4da9d803d89622883c73d9a718daa
myfreeweb <greg@unrelenting.technology>
2018-07-24 19:29:20 +0300

    Add FreeBSD/aarch64 platform

--- racket/src/racket/sconfig.h
+++ racket/src/racket/sconfig.h
@@ -346,6 +346,8 @@
 #  define FLUSH_SPARC_REGISTER_WINDOWS
 # elif defined(__arm__)
 #  define SCHEME_PLATFORM_LIBRARY_SUBPATH "arm-freebsd"
+# elif defined(__aarch64__)
+#  define SCHEME_PLATFORM_LIBRARY_SUBPATH "aarch64-freebsd"
 # elif defined(__powerpc__)
 #  define SCHEME_PLATFORM_LIBRARY_SUBPATH "ppc-freebsd"
 #  define MZ_USE_JIT_PPC
