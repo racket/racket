2b9c0c4689fd8eb0b8af675d7e231bd62097bb13
Matthew Flatt <mflatt@racket-lang.org>
2018-10-10 06:25:09 -0600

    avoid using `directory-exists?` as part of a contract
    Although a `directory-exists?` check is useful for providing better
    error messages, it's fundentally a race condition, since an external
    process can always remove a directory between the check and a use of
    the directory. Because of that limitation of `directory-exists?`, we
    normally avoid making it part of a contract. This commit adjust
    937aa3cdb1 to follow that convention while preserving the helpful
    check and documentation improvements.

--- pkgs/racket-test/tests/pkg/tests-api.rkt
+++ pkgs/racket-test/tests/pkg/tests-api.rkt
@@ -14,35 +14,37 @@
    (define pkg-dest-path (build-path tmp-dir "pkg-strip"))
    (make-directory pkg-dest-path)
 
+   (define rx:does-not-exist #rx"directory does not exist")
+
    ;; Giving path to nonexistent directory should raise a contract exception.
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (check-strip-compatible 'source "pkg-strip" "does not exist" error)))
+   (check-exn rx:does-not-exist (lambda () (check-strip-compatible 'source "pkg-strip" "does not exist" error)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (check-strip-compatible 'binary "pkg-strip" "does not exist" error)))
+   (check-exn rx:does-not-exist (lambda () (check-strip-compatible 'binary "pkg-strip" "does not exist" error)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (check-strip-compatible 'binary-lib "pkg-strip" "does not exist" error)))
+   (check-exn rx:does-not-exist (lambda () (check-strip-compatible 'binary-lib "pkg-strip" "does not exist" error)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (check-strip-compatible 'built "pkg-strip" "does not exist" error)))
+   (check-exn rx:does-not-exist (lambda () (check-strip-compatible 'built "pkg-strip" "does not exist" error)))
 
    ;; Path to existing directory should succeed.
    (check-not-exn (lambda () (check-strip-compatible 'source "pkg-strip" pkg-path error)))
 
    ;; Giving path to nonexistent src directory should raise a contract exception.
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'source 5 pkg-dest-path)))
+   (check-exn exn:fail:contract? (lambda () (generate-stripped-directory 'source 5 pkg-dest-path)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'binary "does not exist" pkg-dest-path)))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'binary "does not exist" pkg-dest-path)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'binary-lib "does not exist" pkg-dest-path)))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'binary-lib "does not exist" pkg-dest-path)))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'built "does not exist" pkg-dest-path)))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'built "does not exist" pkg-dest-path)))
 
    ;; Giving path to nonexistent dest directory should raise a contract exception.
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'source pkg-path "does not exist")))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'source pkg-path "does not exist")))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'binary pkg-path "does not exist")))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'binary pkg-path "does not exist")))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'binary-lib pkg-path "does not exist")))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'binary-lib pkg-path "does not exist")))
 
-   (check-exn #rx"expected: \\(and/c path-string\\? directory-exists\\?\\)" (lambda () (generate-stripped-directory 'built pkg-path "does not exist")))
+   (check-exn rx:does-not-exist (lambda () (generate-stripped-directory 'built pkg-path "does not exist")))
 
    ;; Paths to existing src and dest directories should succeed.
    (check-not-exn (lambda () (generate-stripped-directory 'source pkg-path pkg-dest-path)))
