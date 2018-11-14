c0600a0715020d6fbfc777dd8109eba79d414538
Ryan Culpepper <ryanc@racket-lang.org>
2018-11-14 12:42:36 +0100

    change unsafe-fd->evt to support 'check-{read,write}, 'remove
    The interface is based on unsafe-{file-descriptor,socket}->semaphore.

--- racket/collects/ffi/unsafe/port.rkt
+++ racket/collects/ffi/unsafe/port.rkt
@@ -11,18 +11,22 @@
 
 (module fd-evt racket/base
   (require (only-in '#%unsafe
+                    unsafe-start-atomic
+                    unsafe-end-atomic
                     unsafe-poller
                     unsafe-poll-fd
                     unsafe-poll-ctx-fd-wakeup))
   (provide (protect-out unsafe-fd->evt))
 
-  (struct fd-evt (sfd mode)
+  (struct fd-evt (sfd mode [closed? #:mutable])
     #:property prop:evt
     (unsafe-poller
      (lambda (self wakeups)
        (define sfd (fd-evt-sfd self))
        (define mode (fd-evt-mode self))
        (cond
+         [(fd-evt-closed? self)
+          (values (list self) #f)]
          [(unsafe-poll-fd sfd mode)
           (values (list self) #f)]
          [else
@@ -30,19 +34,42 @@
             (unsafe-poll-ctx-fd-wakeup wakeups sfd mode))
           (values #f self)]))))
 
+  ;; sfd=>{read,write}-evt : (Hasheqv Nat => fd-evt)
+  (define sfd=>read-evt  (make-hasheqv))
+  (define sfd=>write-evt (make-hasheqv))
+
   ;; Differences between unsafe-fd->evt and unsafe-{file-descriptor,socket}->semaphore:
   ;; - treats fd as socket (cf unsafe-poll-fd, unsafe-poll-ctx-fd-wakeup)
   ;; - level-triggered, not edge-triggered
-  ;; - no table, no 'remove, no trigger-on-remove
   ;; - no cooperation with ports created by unsafe-{file-descriptor,socket}->port
-
-  ;; TODO: add table, 'remove (or just close operation?); avoids race condition ??
 
   (define (unsafe-fd->evt sfd mode)
     (unless (exact-integer? sfd)
       (raise-argument-error 'unsafe-fd->evt "handle-integer?" 0 sfd mode))
-    (unless (memq mode '(read write))
-      (raise-argument-error 'unsafe-fd->evt "(or/c 'read 'write)" 1 sfd mode))
-    (fd-evt sfd mode)))
+    (unsafe-start-atomic)
+    (begin0
+        (case mode
+          [(read)  (hash-ref! sfd=>read-evt  sfd (lambda () (fd-evt sfd mode #f)))]
+          [(write) (hash-ref! sfd=>write-evt sfd (lambda () (fd-evt sfd mode #f)))]
+          [(check-read)  (hash-ref sfd=>read-evt  sfd #f)]
+          [(check-write) (hash-ref sfd=>write-evt sfd #f)]
+          [(remove)
+           (define (remove-and-close sfd=>evt)
+             (define evt (hash-ref sfd=>evt sfd #f))
+             (when evt
+               (hash-remove! sfd=>evt sfd)
+               (set-fd-evt-closed?! evt #t)))
+           (remove-and-close sfd=>read-evt)
+           (remove-and-close sfd=>write-evt)
+           #f]
+          [(internal-debug)
+           `((read  ,(hash-keys sfd=>read-evt))
+             (write ,(hash-keys sfd=>write-evt)))]
+          [else
+           (unsafe-end-atomic)
+           (raise-argument-error 'unsafe-fd->evt
+                                 "(or/c 'read 'write 'check-read 'check-write 'remove)"
+                                 1 sfd mode)])
+      (unsafe-end-atomic))))
 
 (require (submod "." fd-evt))
