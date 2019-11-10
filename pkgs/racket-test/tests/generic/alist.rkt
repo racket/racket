0ae41ead419e9527d1894ef759645065a1bb5623
Sam Tobin-Hochstadt <samth@indiana.edu>
2019-11-09 22:05:53 -0500

    Revert "Fix gen:dict methods for alists with duplicate keys (#2846)"
    This breaks existing packages; see https://github.com/greghendershott/aws/issues/64
    for an example.
    
    Unfortunately, we probably have to live with this limitation of the
    interface because of existing code.
    
    This reverts commit 966399dec6782ab22641162abf6a528597e352a7.

--- pkgs/racket-test/tests/generic/alist.rkt
+++ pkgs/racket-test/tests/generic/alist.rkt
@@ -16,30 +16,15 @@
    (define (dict-count dict)
      (length (remove-duplicates (alist-v dict) #:key car)))])
 
+
 (module+ test
   (require rackunit)
 
   (define d1 '((1 . a) (2 . b)))
-  (define d2
-    '([a . 1]
-      [a . 2]
-      [a . 3]))
 
   (check-true (dict? d1))
   (check-eq? (dict-ref d1 1) 'a)
   (check-equal? (dict-count (dict-remove d1 2)) 1)
   (check-false (dict-mutable? d1))
   (check-true (dict-can-remove-keys? d1))
-  (check-true (dict-can-functional-set? d1))
-
-  (check-eq? (dict-ref d2 'a) 1)
-  (check-equal? (dict-set d2 'a 4) '([a . 4] [a . 2] [a . 3]))
-  (check-equal? (dict-remove d2 'a) '())
-  (check-eq? (dict-count d2) 1)
-  (check-equal? (dict-keys d2) '(a))
-  (check-equal? (dict-values d2) '(1))
-  (check-equal? (dict->list d2) '([a . 1]))
-  (check-equal?
-    (for/list ([{k v} (in-dict d2)])
-      (cons k v))
-    '([a . 1])))
+  (check-true (dict-can-functional-set? d1)))
