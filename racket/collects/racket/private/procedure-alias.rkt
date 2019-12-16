f7c39512ab9b63a8b60ef52bce97ec9174dad1ec
Sam Tobin-Hochstadt <samth@indiana.edu>
2019-12-16 10:31:23 -0500

    Modules with tests for unsafe mode can be cross-phase persistent.
    Also, mark several more modules as cross-phase persistent.

--- racket/collects/racket/private/procedure-alias.rkt
+++ racket/collects/racket/private/procedure-alias.rkt
@@ -1,19 +1,11 @@
 (module procedure-alias '#%kernel
-  (#%require "define.rkt"
-             "small-scheme.rkt"
-             "more-scheme.rkt"
-             "kw-prop-key.rkt"
-             (for-syntax '#%kernel
-                         "stx.rkt"
-                         "small-scheme.rkt"
-                         "stxcase-scheme.rkt"
-                         "name.rkt"
-                         "norm-define.rkt"
-                         "qqstx.rkt"
-                         "sort.rkt"))
+  (#%declare #:cross-phase-persistent)
+  (#%require "kw-prop-key.rkt")
   
   (#%provide syntax-procedure-alias-property alias-of)
-  (define (syntax-procedure-alias-property stx)
-    (unless (syntax? stx)
-      (raise-argument-error 'syntax-procedure-alias "syntax?" stx))
-    (syntax-property stx alias-of)))
+  (define-values (syntax-procedure-alias-property)
+    (lambda (stx)
+      (if (syntax? stx)
+          (void)
+          (raise-argument-error 'syntax-procedure-alias "syntax?" stx))
+      (syntax-property stx alias-of))))
