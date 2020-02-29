07f9b843a683ea1f8c91c5101a051e16b622e05c
Ryan Culpepper <ryanc@racket-lang.org>
2020-02-29 11:52:44 +0100

    fix syntax/parse docs: add pretty-printing to evaluator

--- pkgs/racket-doc/syntax/scribblings/parse/parse-common.rkt
+++ pkgs/racket-doc/syntax/scribblings/parse/parse-common.rkt
@@ -47,6 +47,7 @@
                                  (thunk))))])
          (make-evaluator 'racket/base
                          #:requires (let ([mods '(racket/promise
+                                                  racket/pretty
                                                   syntax/parse
                                                   syntax/parse/debug
                                                   syntax/parse/experimental/splicing
@@ -58,7 +59,10 @@
                                                   syntax/transformer)])
                                       `((for-syntax racket/base ,@mods)
                                         ,@mods)))))))
-  (when short? (the-eval '(error-print-source-location #f)))
+  (call-in-sandbox-context the-eval
+    (lambda ()
+      (current-print (dynamic-require 'racket/pretty 'pretty-print-handler))
+      (when short? (error-print-source-location #f))))
   the-eval)
 
 ;; ----
