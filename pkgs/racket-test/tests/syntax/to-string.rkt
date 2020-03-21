d0709e406d920133f6cfc3c12c73d61ee57b7ecd
lkh01 <luka.hadzi@pm.me>
2020-03-21 07:11:14 +0100

    add some tests for `syntax->string`

--- pkgs/racket-test/tests/syntax/to-string.rkt
+++ pkgs/racket-test/tests/syntax/to-string.rkt
@@ -4,6 +4,22 @@
 
 (check-equal? (syntax->string #'((a . 
                                       b))) "(a . \n     b)")
-(check-equal? (syntax->string #'((a b c d))) "(a b c d)")
+(check-equal? (syntax->string #'(( a b  c d))) "( a b  c d)")
 (check-equal? (syntax->string #'(a 'b #(a b c) c)) "a 'b #(a b c) c")
 (check-equal? (syntax->string #'((a b  _   d))) "(a b  _   d)")
+
+(check-equal? (syntax->string #'((  a  b  ))) "(  a  b  )")
+; we can't find where . is, default to earliest position
+(check-equal? (syntax->string #'((  a  b  .  c  ))) "(  a  b .   c  )") 
+
+;; quote tests
+(check-equal? (syntax->string #'('a)) "'a")
+(check-equal? (syntax->string #'('  a)) "'  a")
+(check-equal? (syntax->string #'((quote a))) "(quote a)")
+(check-equal? (syntax->string #'((  quote  a  ))) "(  quote  a  )")
+(check-equal? (syntax->string #'((quote a b))) "(quote a b)")
+(check-equal? (syntax->string #'((  quote  a  b  ))) "(  quote  a  b  )")
+(check-equal? (syntax->string #'((quote . a))) "(quote . a)")
+(check-equal? (syntax->string #'((  quote  .  a  ))) "(  quote .   a  )")
+(check-equal? (syntax->string #'((quote a b . c))) "(quote a b . c)")
+(check-equal? (syntax->string #'((  quote  a  b  .  c  ))) "(  quote  a  b .   c  )")
