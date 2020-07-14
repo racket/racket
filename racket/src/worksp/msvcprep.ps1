3e08f509444d1a8a4f272410de2894b306ace7ff
Bogdan Popa <bogdan@defn.io>
2020-07-14 15:52:18 +0300

    win32: add Power Shell script to set env vars
    The added PS script runs msvcprep.bat in a sub shell then reads the environment
    variables line by line and sets them in the current shell.

---
+++ racket/src/worksp/msvcprep.ps1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+    A variant of msvcprep.bat for PowerShell users.
+
+.DESCRIPTION
+    Runs Visual Studio's vcvarsall.bat script to set the necessary
+    environment variables for a given target platform.
+
+.EXAMPLE
+    Set variables for x86.
+    PS C:\>racket\src\worksp\msvcprep.ps1 x86
+
+.EXAMPLE
+    Set variables for x86_64
+    PS C:\>racket\src\worksp\msvcprep.ps1 x86_amd64
+
+.NOTES
+    Adapted from this StackOverflow answer:
+    * https://stackoverflow.com/a/2124759/144981
+#>
+
+$command = "echo off & " + $PSScriptRoot + "\msvcprep.bat " + $Args[0] + " & set"
+cmd /c $command |
+  ForEach {
+      if ($_ -match "=") {
+          $v = $_.split("=", 2)
+          Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
+      }
+  }
+Write-Host "Visual Studio variables set." -ForegroundColor Yellow
