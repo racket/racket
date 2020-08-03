9a0b0b543eb07f1ad0ce10a594e3eda4c5e0cc40
Paulo Matos <pmatos@linki.tools>
2020-08-03 16:23:14 +0200

    Move the script to adjust tool name to the scripts folder

---
+++ .github/scripts/adjust-sarif-tool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MODE="$1"
+shopt -s globstar
+
+for file in **/*.sarif; do
+    echo "Processing $file"
+    jq "setpath([\"runs\",0,\"tool\",\"driver\",\"name\"]; \"clang-${MODE}\")" $file | sponge $file
+done
+EOF
