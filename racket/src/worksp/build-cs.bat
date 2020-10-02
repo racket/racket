cb50bab726b0344794aa814871a36f29b633a096
Matthew Flatt <mflatt@racket-lang.org>
2020-10-02 12:21:34 -0600

    unbreak Windows CS build

--- racket/src/worksp/build-cs.bat
+++ racket/src/worksp/build-cs.bat
@@ -4,7 +4,6 @@ call build.bat
 ..\..\RacketBC.exe -O "info@compiler/cm" -l- setup --boot ../setup-go.rkt ../build/compiled ^
                   ignored ../build/ignored.d ^
                   csbuild.rkt ^
-                  --racketcs-suffix "" --pull ^
-                  -- --depth 1
+                  --racketcs-suffix "" --pull
 
 ..\..\Racket.exe -N "raco" -l- setup
