f9be9a6a5a93eb833b47202eea78e26a34143952
bdeket <bedeke@hotmail.com>
2020-10-29 09:28:21 +0100

    more accurate computation

--- racket/collects/racket/math.rkt
+++ racket/collects/racket/math.rkt
@@ -62,12 +62,7 @@
                     ;; Taylor expansion near 0 to avoid cancellation
                     ;~ z+z^3/3!+z^5/5!+...
                     (define z^2 (* z z))
-                    (for/fold ([s z]
-                               [z^i (* z z^2)]
-                               #:result s)
-                              ([a (in-list '(6. 120. 5040. 362880.))]);3! 5! 7! ...
-                      (values (+ s (/ z^i a))
-                              (* z^i z^2)))]
+                    (+ z (* z z^2 (+ #i1/6 (* z^2 (+ #i1/120 (* z^2 (+ #i1/5040 (* z^2 #i1/362880))))))))]
                    [else        (/ (- (exp z) (exp (- z))) 2)]))]
           [else (/ (- (exp z) (exp (- z))) 2)]))
 
