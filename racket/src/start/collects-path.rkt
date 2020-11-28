1bf4086daec476f1ede384e028554c81acfc9512
Matthew Flatt <mflatt@racket-lang.org>
2020-11-28 16:34:08 -0700

    makefile: strip any code signature in "collects-path.rkt"
    The latest Mac OS tools automatically sign code when linking, and
    "collect-path.rkt" break that signature by changing the executable.
    Avoid that problem by removing the signature, first. (Leave it to
    distribution tools to install a new signature.)

--- racket/src/start/collects-path.rkt
+++ racket/src/start/collects-path.rkt
@@ -1,28 +1,41 @@
+#lang racket/base
+(require racket/cmdline
+         racket/system)
 
 ;; This module is executed by the install process to update
 ;;  the embedded path to "collects" and "lib" in an executable.
 
-;; written in #%kernel because it's loaded with -c (ie, no compiled files)
-(module collects-path '#%kernel  
-  (define-values (fix-one)
-    (lambda (label pos)
-      (let-values ([(dest) (vector-ref (current-command-line-arguments) 0)]
-                   [(path) (vector-ref (current-command-line-arguments) pos)])
-        (let-values ([(i o) (open-input-output-file dest 'update)])
-          (let-values ([(m) (regexp-match-positions label i)]
-                       [(path) (if (string? path)
-                                   (string->path path)
-                                   path)])        
-            (if m
-                (void)
-                (error 'set-collects-path
-                       "cannot find collection-path label in executable file"))
-            (file-position o (cdar m))
-            (write-bytes (path->bytes path) o)
-            (write-byte 0 o)
-            (write-byte 0 o)
-            (close-input-port i)
-            (close-output-port o))))))
+(command-line
+ #:args (dest dir-path config-path)
 
-  (fix-one #rx#"coLLECTs dIRECTORy:" 1)
-  (fix-one #rx#"coNFIg dIRECTORy:" 2))  
+ ;; For a Mac OS executable, first strip any signature that the
+ ;; compiler may have added
+ (when (call-with-input-file*
+        dest
+        (lambda (i)
+          (define bstr (read-bytes 4 i))
+          (and (= 4 (bytes-length bstr))
+               (member (integer-bytes->integer bstr #f)
+                       '(#xFeedFace #xFeedFacf)))))
+   (define codesign (find-executable-path "codesign"))
+   (when codesign
+     (system* codesign "--remove-signature" dest)))
+
+ (define (fix-one label path-in)
+   (define-values (i o) (open-input-output-file dest #:exists 'update))
+   (define m (regexp-match-positions label i))
+   (define path (if (string? path-in)
+                    (string->path path-in)
+                    path-in))
+   (unless m
+     (error 'set-collects-path
+            "cannot find collection-path label in executable file"))
+   (file-position o (cdar m))
+   (write-bytes (path->bytes path) o)
+   (write-byte 0 o)
+   (write-byte 0 o)
+   (close-input-port i)
+   (close-output-port o))
+ 
+ (fix-one #rx#"coLLECTs dIRECTORy:" dir-path)
+ (fix-one #rx#"coNFIg dIRECTORy:" config-path))
