4b7b1872dc2f17afa4fc9ca98d2c6c86bddceb88
Matthew Flatt <mflatt@racket-lang.org>
2020-12-30 03:44:14 -0700

    version bump for `seconds->date` change

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change only when
 ;; "racket_version.h" changes:
-(define version "7.9.0.19")
+(define version "7.9.0.20")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
