racket/racket

commit d96273bc0a58c0c848956932d2b1fdfb32554b08

object-name: return #f for some built-in kinds of values
This change is mostly for making CS consistent with BC, but in the
case of `(object-name #'x)`, it also makes BC consistent with BC
before the macro-expander rewrite.

Closes racket/pconvert#9

racket/src/bc/src/struct.c
@@ -3431,7 +3431,7 @@ intptr_t scheme_get_or_check_structure_shape(Scheme_Object *e, Scheme_Object *ex
                 | ((st->authentic && (!expected || (v & STRUCT_PROC_SHAPE_AUTHENTIC)))
                    ? STRUCT_PROC_SHAPE_AUTHENTIC
                    : 0)
-                | ((st->nonfail_constructor
+                | (((st->more_flags & STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR)
                     && (!expected || (v & STRUCT_PROC_SHAPE_NONFAIL_CONSTR)))
                    ? STRUCT_PROC_SHAPE_NONFAIL_CONSTR
                    : 0)
@@ -3448,7 +3448,7 @@ intptr_t scheme_get_or_check_structure_shape(Scheme_Object *e, Scheme_Object *ex
       st = (Scheme_Struct_Type *)SCHEME_PRIM_CLOSURE_ELS(e)[0];
       want_v = ((st->num_islots << STRUCT_PROC_SHAPE_SHIFT)
                 | STRUCT_PROC_SHAPE_CONSTR
-                | ((st->nonfail_constructor
+                | (((st->more_flags & STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR)
                     && (!expected || (v & STRUCT_PROC_SHAPE_NONFAIL_CONSTR)))
                    ? STRUCT_PROC_SHAPE_NONFAIL_CONSTR
                    : 0));
@@ -4752,7 +4752,7 @@ Scheme_Struct_Type *scheme_make_prefab_struct_type_raw(Scheme_Object *base,
   struct_type->num_islots = num_fields + (parent_type ? parent_type->num_islots : 0);
   struct_type->name_pos = depth;
   struct_type->authentic = 0;
-  struct_type->nonfail_constructor = 1;
+  struct_type->more_flags = STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR;
   struct_type->inspector = scheme_false;
   struct_type->uninit_val = uninit_val;
   struct_type->props = NULL;
@@ -4819,7 +4819,12 @@ static Scheme_Object *_make_struct_type(Scheme_Object *name,
   }
 
   struct_type->name = name;
-  struct_type->nonfail_constructor = (parent_type ? parent_type->nonfail_constructor : 1);
+  struct_type->more_flags = ((parent_type
+                              ? (parent_type->more_flags & STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR)
+                              : STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR)
+                             | ((scheme_starting_up && inspector)
+                                ? STRUCT_TYPE_FLAG_SYSTEM_OPAQUE
+                                : 0));
 
   struct_type->num_slots = num_fields + num_uninit_fields + (parent_type ? parent_type->num_slots : 0);
   struct_type->num_islots = num_fields + (parent_type ? parent_type->num_islots : 0);
@@ -5083,7 +5088,7 @@ static Scheme_Object *_make_struct_type(Scheme_Object *name,
     }
     
     struct_type->guard = guard;
-    struct_type->nonfail_constructor = 0;
+    struct_type->more_flags = 0;
   } else if (chaperone_undefined) {
     struct_type->guard = scheme_undefined;
   }
@@ -5091,7 +5096,7 @@ static Scheme_Object *_make_struct_type(Scheme_Object *name,
   if (parent && SCHEME_NP_CHAPERONEP(parent)) {
     guard = add_struct_type_chaperone_guards(parent, struct_type->guard);
     struct_type->guard = guard;
-    struct_type->nonfail_constructor = 0;
+    struct_type->more_flags = 0;
   }
 
   if (checked_proc)
@@ -5162,7 +5167,7 @@ Scheme_Object *scheme_make_struct_type_from_string(const char *name,
                         guard);
 
   if (scheme_starting_up)
-    /* Force allocation for a strcuture type that may be in the master GC: */
+    /* Force allocation for a struc<ture type that may be in the master GC: */
     scheme_force_struct_type_info((Scheme_Struct_Type *)r);
 
   return r;

racket/src/cs/rumble/struct.ss
@@ -173,6 +173,8 @@
             (and parent
                  (inspector-superior? sup-insp parent))))))
 
+;; result can be 'prefab, #f, an inspector, or `none`, where
+;; `none` is the result for opaque "system" records
 (define (inspector-ref rtd)
   (getprop (record-type-uid rtd) 'inspector none))
 
@@ -572,7 +574,7 @@
    [(rtd name init-count auto-count parent-rtd props insp proc-spec immutables guard constructor-name install-props!)
     (let ([install-props!
            (or install-props!
-               (check-make-struct-type-arguments 'make-struct-type name parent-rtd init-count auto-count
+               (check-make-struct-type-arguments 'make-struct-type (if (pair? name) (car name) name) parent-rtd init-count auto-count
                                                  props insp proc-spec immutables guard constructor-name))])
       (unless (eq? insp 'prefab) ; everything for prefab must be covered in `prefab-key+count->rtd`
         (let* ([parent-rtd* (strip-impersonator parent-rtd)]
@@ -603,7 +605,8 @@
           ;; Finish checking and install new property values:
           (install-props! rtd parent-rtd* all-immutables)
           ;; Record inspector
-          (inspector-set! rtd insp)
+          (unless (pair? name) ; pair implies a system structure type
+            (inspector-set! rtd insp))
           ;; Register guard
           (register-guards! rtd parent-rtd guard 'at-start))))]))
 

pkgs/racket-test-core/tests/racket/macro.rktl
@@ -762,8 +762,8 @@
         #:trim-error #rx"^[^\n]*\n[^\n]*")
   
   (define (in-defctx s) `(let () ,s))
-  (test '("(#(struct:liberal-define-context))\n10\n" "") go '(definition-context expression) in-defctx 'prop:procedure)
-  (test '("(#(struct:liberal-define-context))\n10\n" "") go '(definition-context) in-defctx 'prop:procedure)
+  (test '("(#<liberal-define-context>)\n10\n" "") go '(definition-context expression) in-defctx 'prop:procedure)
+  (test '("(#<liberal-define-context>)\n10\n" "") go '(definition-context) in-defctx 'prop:procedure)
   (test '("expression\n10\n" "") go '(expression) in-defctx 'prop:procedure)
   (test '("" "m: not allowed in context\n  expansion context: definition-context") go '() in-defctx 'prop:procedure
         #:trim-error #rx"^[^\n]*\n[^\n]*")

pkgs/racket-test-core/tests/racket/name.rktl
@@ -214,4 +214,10 @@
 (test 'date*? object-name date*?)
 (test 'date-second object-name date-second)
 
+;; Check that `object-name` produces #f for some primitive value types
+;; that might happen to be implemented with structures at some level
+(test #f object-name (make-thread-group))
+(test #f object-name (current-directory))
+(test #f object-name (quote-syntax x))
+
 (report-errs)

racket/src/bc/src/fun.c
@@ -2715,7 +2715,8 @@ Scheme_Object *scheme_object_name(Scheme_Object *a)
   }
 
   if (SCHEME_STRUCTP(a)) {
-    return SCHEME_STRUCT_NAME_SYM(a);
+    if (!(SCHEME_STRUCT_TYPE(a)->more_flags & STRUCT_TYPE_FLAG_SYSTEM_OPAQUE))
+      return SCHEME_STRUCT_NAME_SYM(a);
   } else if (SCHEME_PROCP(a)) {
     const char *s;
     int len;

racket/src/bc/src/optimize.c
@@ -1470,7 +1470,7 @@ static int ok_constant_super_value(void *data, Scheme_Object *v, int mode)
       Scheme_Struct_Type *st = (Scheme_Struct_Type *)v;
       if (st->num_slots == st->num_islots) {
         if (_nonfail_constr)
-          *_nonfail_constr = st->nonfail_constructor;
+          *_nonfail_constr = (st->more_flags & STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR);
         if (_prefab)
           *_prefab = !!st->prefab_key;
         return st->num_slots + 1;

racket/src/bc/src/schpriv.h
@@ -1096,7 +1096,8 @@ typedef struct Scheme_Struct_Type {
   mzshort num_islots; /* initialized + parent-initialized */
   mzshort name_pos;
   char authentic; /* 1 => chaperones/impersonators disallowed */
-  char nonfail_constructor; /* 1 => constructor never fails */
+  char more_flags; /* STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR => constructor never fails
+                      STRUCT_TYPE_FLAG_SYSTEM_OPAQUE => #f for `object-name`, for example */
 
   Scheme_Object *name;
 
@@ -1126,6 +1127,10 @@ typedef struct Scheme_Struct_Type {
 
 #define STRUCT_TYPE_ALL_IMMUTABLE 0x1
 #define STRUCT_TYPE_CHECKED_PROC  0x2
+
+/* for `more_flags` field */
+#define STRUCT_TYPE_FLAG_NONFAIL_CONSTRUCTOR 0x1
+#define STRUCT_TYPE_FLAG_SYSTEM_OPAQUE       0x2
 
 typedef struct Scheme_Structure
 {

racket/src/bc/src/startup.inc
@@ -43778,7 +43778,7 @@ static const char *startup_source =
 " 0"
 " #f"
 "(list(cons 1/prop:liberal-define-context #t))"
-" #f"
+"(current-inspector)"
 " #f"
 " '()"
 " #f"

commit 4c6bb055b09a17343878fcbbb08e5d293d92d415

cs: fix transparent internal structure types
Commit d96273bc0a broke internal "system" structures in the expander
that need to be transparent.

racket/src/bc/src/struct.c
@@ -5167,7 +5167,7 @@ Scheme_Object *scheme_make_struct_type_from_string(const char *name,
                         guard);
 
   if (scheme_starting_up)
-    /* Force allocation for a struc<ture type that may be in the master GC: */
+    /* Force allocation for a structure type that may be in the master GC: */
     scheme_force_struct_type_info((Scheme_Struct_Type *)r);
 
   return r;

racket/src/cs/rumble/struct.ss
@@ -605,7 +605,8 @@
           ;; Finish checking and install new property values:
           (install-props! rtd parent-rtd* all-immutables)
           ;; Record inspector
-          (unless (pair? name) ; pair implies a system structure type
+          (unless (and (pair? name) ; pair implies a system structure type
+                       insp)
             (inspector-set! rtd insp))
           ;; Register guard
           (register-guards! rtd parent-rtd guard 'at-start))))]))