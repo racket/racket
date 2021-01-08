6e907f5cc91ab1c9b965288c5bfca9d6653855db
Bogdan Popa <bogdan@defn.io>
2021-01-08 13:07:11 +0200

    cs: fixup WRITE_XOR_EXECUTE_CODE define for iPhoneSimulator

--- racket/src/ChezScheme/c/version.h
+++ racket/src/ChezScheme/c/version.h
@@ -327,11 +327,10 @@ typedef int tputsputcchar;
 /* for both iPhone and iPhoneSimulator */
 #if defined(TARGET_OS_IPHONE)
 # define SYSTEM(s) ((void)s, -1)
+# define WRITE_XOR_EXECUTE_CODE
 #endif
 #if defined(__arm64__)
-# if defined(TARGET_OS_IPHONE)
-#  define WRITE_XOR_EXECUTE_CODE
-# else
+# if !defined(TARGET_OS_IPHONE)
 #  define S_MAP_CODE MAP_JIT
 #  define S_ENABLE_CODE_WRITE(on) pthread_jit_write_protect_np(!(on))
 # endif
