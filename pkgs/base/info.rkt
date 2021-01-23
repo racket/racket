442d7d2d00041f3641ba56d2650f32ebe672a9e9
Matthew Flatt <mflatt@racket-lang.org>
2021-01-23 10:11:21 -0700

    Chez Scheme: repair for reify-cc register saving on arm32
    This repair is simpler approach than the original, and it makes the
    manual register allocation a little less fragile.

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change only when
 ;; "racket_version.h" changes:
-(define version "8.0.0.2")
+(define version "8.0.0.3")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
