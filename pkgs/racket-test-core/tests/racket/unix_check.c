8a914d9338e83165bbf48515fd42d8e916a9ec89
Matthew Flatt <mflatt@racket-lang.org>
2021-02-11 19:28:19 -0700

    further repair to unix-signal test

--- pkgs/racket-test-core/tests/racket/unix_check.c
+++ pkgs/racket-test-core/tests/racket/unix_check.c
@@ -12,9 +12,13 @@ int main()
 
   /* SIGPROF tends to be near the end of the range of signal IDs */
   for (i = 1; i < SIGPROF; i++) {
-    sigaction(i, NULL, &sa);
-    if (sa.sa_handler != SIG_DFL)
-      return 1;
+    if (sigaction(i, NULL, &sa) == 0) {
+      if (sa.sa_handler != SIG_DFL)
+        return 1;
+    } else {
+      /* sigaction sometimes isn't allowed at all on SIGKILL,
+         for example, so ignore that failure */
+    }
   }
 
   sigemptyset(&set);
