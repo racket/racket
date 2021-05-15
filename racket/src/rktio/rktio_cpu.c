82789a760b5393876b0392cc9c6dc85ed664922d
Matthew Flatt <mflatt@racket-lang.org>
2021-05-15 09:47:42 -0600

    solaris: make `processor-count` work
    Related to #1572

--- racket/src/rktio/rktio_cpu.c
+++ racket/src/rktio/rktio_cpu.c
@@ -4,8 +4,9 @@
 #if defined(OS_X) && defined(__POWERPC__)
 # include <sys/param.h>
 # include <sys/sysctl.h>
-#elif defined(__linux__) || defined(__QNX__) || defined(OS_X) || defined(__FreeBSD__) || defined(__OpenBSD__)
+#elif defined(__linux__) || defined(__QNX__) || defined(OS_X) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(sun)
 # include <unistd.h>
+# define USE_SYSCONF_NPROCESSORS_ONLN
 #elif defined(RKTIO_SYSTEM_WINDOWS)
 # include <windows.h>
 #endif
@@ -19,7 +20,7 @@ void rktio_init_cpu(rktio_t *rktio)
  
   if (sysctlbyname("hw.ncpu", &processor_count, &size, NULL, 0))
     processor_count = 2;
-#elif defined(__linux__) || defined(__QNX__) || defined(OS_X) || defined(__FreeBSD__) || defined(__OpenBSD__)
+#elif defined(USE_SYSCONF_NPROCESSORS_ONLN)
   processor_count = sysconf(_SC_NPROCESSORS_ONLN);
 #elif defined(RKTIO_SYSTEM_WINDOWS)
   SYSTEM_INFO sysinfo;
