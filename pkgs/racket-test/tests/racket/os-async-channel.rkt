0b8ea67bb3e36a38822cc449b65e4babb579a46d
Matthew Flatt <mflatt@racket-lang.org>
2021-05-22 17:30:28 -0600

    Chez Scheme: use an ordered finalizer for mutexes and condition variables
    That way, a mutex or conditional variable can be referenced by
    something else that has a finalizer and that might use the mutex or
    condition variable.
    
    Closes #3842

--- pkgs/racket-test/tests/racket/os-async-channel.rkt
+++ pkgs/racket-test/tests/racket/os-async-channel.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
-(require ffi/unsafe/os-thread
+(require ffi/unsafe
+         ffi/unsafe/os-thread
          ffi/unsafe/os-async-channel)
 
 (when (os-thread-enabled?)
@@ -37,5 +38,25 @@
          (printf "Done in ~a gets\n" steps)]
         [else
          (os-async-channel-put ch 0)
-         (loop new-counter (add1 steps))]))))
+         (loop new-counter (add1 steps))])))
 
+  ;; ----------------------------------------
+
+  ;; make sure an os-async-channel can be used in a finalizer
+  
+  (define finalized? #f)
+  
+  (let ()  ;; if `begin`, no problem
+    (define c (make-os-async-channel))
+    (register-finalizer (list c)
+                        (lambda (cs)
+                          (os-async-channel-put (car cs) #f)
+                          (set! finalized? #t))))
+
+  (for ([i 100])
+    (unless finalized?
+      (sync (system-idle-evt))
+      (collect-garbage)))
+
+  (unless finalized?
+    (error "finalizer never called")))
