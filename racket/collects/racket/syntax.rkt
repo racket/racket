54aa1114d3cf24734bf3a236b9b99806864a5d57
Mauer-Oats <maueroats@users.noreply.github.com>
2021-06-20 03:11:35 -0500

    format-id: verify first input is syntax (#3881)
    * format-id: verify first input is syntax
    
    * format-id: full args output in error message
    
    * format-id: correct error message
    
    Co-authored-by: Mauer-Oats <maueroats@gmail.com>

--- racket/collects/racket/syntax.rkt
+++ racket/collects/racket/syntax.rkt
@@ -113,6 +113,8 @@
                    #:subs? [subs? #f]
                    #:subs-intro [subs-intro (default-intro)]
                    fmt . args)
+  (unless (or (syntax? lctx) (eq? lctx #f))
+    (apply raise-argument-error 'format-id "(or/c syntax? #f)" 0 lctx fmt args))
   (check-restricted-format-string 'format-id fmt)
   (define arg-strs (map (lambda (a) (->string a 'format-id)) args))
   (define str (apply format fmt arg-strs))
