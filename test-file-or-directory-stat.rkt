9eecc22f3e7a36a2faa857426d8a02c4d3bf8aef
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2021-09-11 21:45:25 +0200

    Add first test for `file-or-directory-stat`
    For now, I implement the tests with Rackunit. Integrate the tests into
    `pkgs/racket-test-core/tests/racket/file.rktl` later.

---
+++ test-file-or-directory-stat.rkt
@@ -0,0 +1,24 @@
+#lang errortrace racket/base
+
+(module+ test
+  (require
+    racket/file
+    rackunit
+    rackunit/text-ui)
+
+  (run-tests
+    (test-suite "Reading stat info"
+      (test-case "Writing temporary file and reading stat"
+        (define start-time-milliseconds (current-inexact-milliseconds))
+        (define temp-file-path (make-temporary-file))
+        (define TEST-STRING "stat test")
+        (display-to-file TEST-STRING temp-file-path #:exists 'truncate)
+        (define stat-result (file-or-directory-stat temp-file-path))
+        ; TODO: Make sure the file is removed even if `file-or-directory-stat`
+        ; raises an exception.
+        (delete-file temp-file-path)
+        (check-equal? (hash-ref stat-result 'size) (string-length TEST-STRING))
+        (check-pred positive? (hash-ref stat-result 'inode))
+        (check-pred positive? (hash-ref stat-result 'device-id)))
+  ))
+)
