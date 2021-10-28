3a8a7102abff334ee4e054c3597bebba32bda307
Matthew Flatt <mflatt@racket-lang.org>
2021-10-28 12:41:15 -0600

    makefile: avoid `codesign` on Mac OS in favor of Racket implementation
    It seems that using `codesign` to remove a signature might sometimes
    leave the file in a state that `strip` does not like. If true, that
    behavior might be related to build-tool changes around Monterey. In
    any case, Racket's version needs to work on executables derived from
    the `racket` excutable and launcher, so let's use it always.

---
+++ racket/src/mac/codesign.rkt
@@ -0,0 +1,22 @@
+#lang racket/base
+(require racket/cmdline
+         compiler/private/mach-o)
+
+(define remove? #f)
+
+(define file
+  (command-line
+   #:once-each
+   [("--remove-signature") "Remove signature"
+                           (set! remove? #t)]
+   [("-s") identity "Add ad hoc-signature"
+           (unless (equal? identity "-")
+             (error "identity must be `-`"))]
+   [("--entitlements") plist-file "With entitlements"
+                       (void)]
+   #:args (file)
+   file))
+
+(if remove?
+    (remove-signature file)
+    (add-ad-hoc-signature file))
