093c5571bd7b6d3635b3520580da0cb6f6170d17
John Clements <clements@racket-lang.org>
2022-02-07 13:27:49 -0800

    Update version number for the v8.4 release

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change only when
 ;; "racket_version.h" changes:
-(define version "8.3.900")
+(define version "8.4")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
