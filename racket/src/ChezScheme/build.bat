1ef05006f5685444ad9fe121afd365b9451e8982
Matthew Flatt <mflatt@racket-lang.org>
2022-03-16 19:41:31 -0600

    Windows DLL and /MD vs /MT

--- racket/src/ChezScheme/build.bat
+++ racket/src/ChezScheme/build.bat
@@ -3,21 +3,36 @@ setlocal
 
 set M=%1
 set WORKAREA=%M%
+set LINKAS=dll
+set RUNTIMEAS=static
+set SRCDIR=%~dp0
 
 if "%WORKAREA%"=="" goto needargument
 
+:argloop
+shift
+set ARG=%1
+if defined ARG (
+  if "%ARG%"=="/exe" set LINKAS=exe && goto argloop
+  if "%ARG%"=="/MD" set RUNTIMEAS=dll && goto argloop
+  if "%ARG%"=="/MT" set RUNTIMEAS=static && goto argloop
+  echo Unrecognized argument %ARG%
+  exit /B 1
+)  
+
 if not exist %WORKAREA% mkdir %WORKAREA%
 
-echo srcdir=%~dp0 > %WORKAREA%\Mf-config
+echo srcdir=%SRCDIR% > %WORKAREA%\Mf-config
 echo m=%M% >> %WORKAREA%\Mf-config
-echo forWindows=y >> %WORKAREA%\Mf-config
+echo linkAs=%LINKAS% >> %WORKAREA%\Mf-config
+echo runtimeAs=%RUNTIMEAS% >> %WORKAREA%\Mf-config
 
 echo workarea=%WORKAREA% > Makefile
 echo !include %WORKAREA%\Mf-config >> Makefile
-type "%~dp0\makefiles\Makefile.nt" >> Makefile
+type "%SRCDIR%\makefiles\Makefile.nt" >> Makefile
 
-xcopy /-i /y "%~dp0\makefiles\buildmain.zuo" main.zuo > NUL
-xcopy /-i /y "%~dp0\makefiles\workmain.zuo" %WORKAREA%\main.zuo > NUL
+xcopy /-i /y "%SRCDIR%\makefiles\buildmain.zuo" main.zuo > NUL
+xcopy /-i /y "%SRCDIR%\makefiles\workmain.zuo" %WORKAREA%\main.zuo > NUL
 
 echo Configured for %M%
 
