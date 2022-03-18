bf01818b41de8d9f5aa3943bb4eb921f664cd245
Matthew Flatt <mflatt@racket-lang.org>
2022-03-17 19:23:39 -0600

    Windows DLL variants and resources

--- racket/src/ChezScheme/build.bat
+++ racket/src/ChezScheme/build.bat
@@ -4,8 +4,9 @@ setlocal
 set M=%1
 set WORKAREA=%M%
 set LINKAS=dll
-set RUNTIMEAS=static
+set RUNTIMEAS=dll
 set SRCDIR=%~dp0
+set MAKETARGET=all-dlls
 
 if "%WORKAREA%"=="" goto needargument
 
@@ -16,9 +17,22 @@ if defined ARG (
   if "%ARG%"=="/exe" set LINKAS=exe && goto argloop
   if "%ARG%"=="/MD" set RUNTIMEAS=dll && goto argloop
   if "%ARG%"=="/MT" set RUNTIMEAS=static && goto argloop
+  if "%ARG%"=="/only" set MAKETARGET=build && goto argloop
+  if "%ARG%"=="/none" set MAKETARGET=none && goto argloop
   echo Unrecognized argument %ARG%
   exit /B 1
-)  
+)
+
+if "%M%"=="i3nt" set VSCONFIG=x86
+if "%M%"=="ti3nt" set VSCONFIG=x86
+if "%M%"=="a6nt" set VSCONFIG=amd64
+if "%M%"=="ta6nt" set VSCONFIG=amd86
+if "%M%"=="arm64nt" set VSCONFIG=x64_arm64
+if "%M%"=="tarm64nt" set VSCONFIG=x64_arm64
+if "%VSCONFIG%"=="" (
+  echo Unrecognized machine type %M%
+  exit /B 1
+)
 
 if not exist %WORKAREA% mkdir %WORKAREA%
 
@@ -35,6 +49,16 @@ xcopy /-i /y "%SRCDIR%\makefiles\buildmain.zuo" main.zuo > NUL
 xcopy /-i /y "%SRCDIR%\makefiles\workmain.zuo" %WORKAREA%\main.zuo > NUL
 
 echo Configured for %M%
+
+if %MAKETARGET%==none goto donebuilding
+
+for %%X in (cl.exe) do (set FOUND=%%~$PATH:X)
+if not defined FOUND (
+  echo Configuring VS for %VSCONFIG%
+  call "%SRCDIR%/c/vs.bat" %VSCONFIG%
+ )
+
+nmake /nologo %MAKETARGET%
 
 goto donebuilding
 
