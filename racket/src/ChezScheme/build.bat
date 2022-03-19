d719450ac457be3da41724f0790d932bcd92bf9c
Matthew Flatt <mflatt@racket-lang.org>
2022-03-18 20:36:15 -0600

    Windows repairs

--- racket/src/ChezScheme/build.bat
+++ racket/src/ChezScheme/build.bat
@@ -19,16 +19,17 @@ if defined ARG (
   if "%ARG%"=="/MD" set RUNTIMEAS=dll && goto argloop
   if "%ARG%"=="/MT" set RUNTIMEAS=static && goto argloop
   if "%ARG%"=="/only" set MAKETARGET=build && goto argloop
+  if "%ARG%"=="/kernel" set MAKETARGET=kernel && goto argloop
   if "%ARG%"=="/none" set MAKETARGET=none && goto argloop
   echo Unrecognized argument %ARG%
   exit /B 1
 )
 
-if "%M%"=="pb" set VSCONFIG=x86
+if "%M%"=="pb" set VSCONFIG=x86_amd64
 if "%M%"=="i3nt" set VSCONFIG=x86
 if "%M%"=="ti3nt" set VSCONFIG=x86
-if "%M%"=="a6nt" set VSCONFIG=amd64
-if "%M%"=="ta6nt" set VSCONFIG=amd86
+if "%M%"=="a6nt" set VSCONFIG=x86_amd64
+if "%M%"=="ta6nt" set VSCONFIG=x86_amd64
 if "%M%"=="arm64nt" set VSCONFIG=x64_arm64
 if "%M%"=="tarm64nt" set VSCONFIG=x64_arm64
 if "%VSCONFIG%"=="" (
@@ -47,8 +48,8 @@ echo workarea=%WORKAREA% > Makefile
 echo !include %WORKAREA%\Mf-config >> Makefile
 type "%SRCDIR%\makefiles\Makefile.nt" >> Makefile
 
-xcopy /-i /y "%SRCDIR%\makefiles\buildmain.zuo" main.zuo > NUL
-xcopy /-i /y "%SRCDIR%\makefiles\workmain.zuo" %WORKAREA%\main.zuo > NUL
+copy /y "%SRCDIR%\makefiles\buildmain.zuo" main.zuo > NUL
+copy /y "%SRCDIR%\makefiles\workmain.zuo" %WORKAREA%\main.zuo > NUL
 
 echo Configured for %M%
 
