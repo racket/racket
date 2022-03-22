5b9ba2e45cd5123e45edc1209bb8d40da5e0956d
Matthew Flatt <mflatt@racket-lang.org>
2022-03-22 09:14:08 -0600

    link librktio.so build to io and cs demo targets

--- racket/src/rktio/make64.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo ============================================
-echo Assumes that the "librktio" project is built
-echo ============================================
-cl /Zi /I..\worksp\librktio demo.c ..\worksp\librktio\x64\Release\librktio.lib ws2_32.lib user32.lib shell32.lib
