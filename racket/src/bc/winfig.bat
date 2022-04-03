403e0813e3ca8cd303a333d211d7fc744d5ee0e7
Matthew Flatt <mflatt@racket-lang.org>
2022-04-03 11:48:47 -0600

    remove replaced projects and makefiles

--- racket/src/bc/winfig.bat
+++ racket/src/bc/winfig.bat
@@ -6,7 +6,7 @@ set SRCDIR=%~dp0
 copy /y "%SRCDIR%\buildmain.zuo" main.zuo > NUL
 echo srcdir=%SRCDIR% > Mf-config
 echo PREFLAGS=/DWIN32 >> Mf-config
-echo CFLAGS=/Ox /Zi >> Mf-config
+echo CFLAGS=/Ox /Zi /GS- >> Mf-config
 
 cl.exe /nologo /Fe: winfig.exe "%SRCDIR%\..\win\winfig.c"
 winfig.exe >> Mf-config
--- racket/src/worksp/checkvs9.c
+++
@@ -1,8 +0,0 @@
-
-int main() {
-#if _MSC_VER < 1600
-  return 1;
-#else
-  return 0;
-#endif
-}
