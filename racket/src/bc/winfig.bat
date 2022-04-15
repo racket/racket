fb71df7e5a6f5da14c57f4ff0883aa3452f5b6a4
Matthew Flatt <mflatt@racket-lang.org>
2022-04-15 15:42:59 -0600

    fix PREFLAGS` vs. `CPPFLAGS` for Windows

--- racket/src/bc/winfig.bat
+++ racket/src/bc/winfig.bat
@@ -5,7 +5,7 @@ set SRCDIR=%~dp0
 
 copy /y "%SRCDIR%\buildmain.zuo" main.zuo > NUL
 echo srcdir=%SRCDIR% > Mf-config
-echo PREFLAGS=/DWIN32 >> Mf-config
+echo CPPFLAGS=/DWIN32 >> Mf-config
 echo CFLAGS=/Ox /GS- >> Mf-config
 
 cl.exe /nologo /Fe: winfig.exe "%SRCDIR%\..\worksp\winfig.c"
