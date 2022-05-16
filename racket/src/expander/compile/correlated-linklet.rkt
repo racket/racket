b9dfd94d47be7279dbdf31c85075f4b9a9e2f993
AlexKnauth <alexander@knauth.org>
2022-05-16 19:07:00 -0400

    use hash-map/copy
    instead of enumerating variants of hash tables

--- racket/src/expander/compile/correlated-linklet.rkt
+++ racket/src/expander/compile/correlated-linklet.rkt
@@ -90,16 +90,9 @@
             (hash-set (or ht '#hasheq()) k p)
             ht)))]
     [(hash? v)
-     (cond
-       [(hash-eq? v)
-        (for/hasheq ([(key value) (in-hash v)])
-          (values (->faslable key) (->faslable value)))]
-       [(hash-eqv? v)
-        (for/hasheqv ([(key value) (in-hash v)])
-          (values (->faslable key) (->faslable value)))]
-       [else
-        (for/hash ([(key value) (in-hash v)])
-          (values (->faslable key) (->faslable value)))])]
+     (hash-map/copy v
+                    (lambda (key value)
+                      (values (->faslable key) (->faslable value))))]
     [(correlated-linklet? v)
      (faslable-correlated-linklet (->faslable (correlated-linklet-expr v))
                                   (->faslable (correlated-linklet-name v)))]
@@ -133,16 +126,9 @@
            (correlated-property c k p))
          c)]
     [(hash? v)
-     (cond
-       [(hash-eq? v)
-        (for/hasheq ([(key value) (in-hash v)])
-          (values (faslable-> key) (faslable-> value)))]
-       [(hash-eqv? v)
-        (for/hasheqv ([(key value) (in-hash v)])
-          (values (faslable-> key) (faslable-> value)))]
-       [else
-        (for/hash ([(key value) (in-hash v)])
-          (values (faslable-> key) (faslable-> value)))])]
+     (hash-map/copy v
+                    (lambda (key value)
+                      (values (faslable-> key) (faslable-> value))))]
     [(faslable-correlated-linklet? v)
      (make-correlated-linklet (faslable-> (faslable-correlated-linklet-expr v))
                               (faslable-> (faslable-correlated-linklet-name v)))]
