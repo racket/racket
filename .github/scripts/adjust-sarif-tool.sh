7bcf938f1de07a72c892fc6fe06eb1183502470c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-05-18 09:17:48 -0700

    ci: strip path for correct reporting

--- .github/scripts/adjust-sarif-tool.sh
+++ .github/scripts/adjust-sarif-tool.sh
@@ -4,5 +4,6 @@ shopt -s globstar
 
 for file in **/*.sarif; do
     echo "Processing $file"
+    sed -i -e 's|"uri": "file:///__w/racket/racket/|"uri": "|g' $file
     jq "setpath([\"runs\",0,\"tool\",\"driver\",\"name\"]; \"clang-${MODE}\")" $file | sponge $file
 done
