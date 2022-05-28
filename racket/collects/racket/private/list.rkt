f8824575fbf1dae51077cf79c6a6dc3e3f29431e
NoahStoryM <noahstorym@gmail.com>
2022-05-28 11:01:39 +0800

    Update.

--- racket/collects/racket/private/list.rkt
+++ racket/collects/racket/private/list.rkt
@@ -437,15 +437,15 @@
                (let loop ([f f0] [fs fs0] [i 0] [rfuns '()])
                  (unless (procedure? f)
                    (apply raise-argument-error 'name "procedure?" i f0 fs0))
-                 (if (pair? fs)
-                     (begin (can-compose name i f f0 fs0)
-                            (loop (car fs) (cdr fs) (add1 i) (cons f rfuns)))
-                     (cond
-                       [(andmap id? rfuns) f]
-                       [(id? f) (pipeline (car rfuns) (cdr rfuns))]
-                       [else
-                        (let ([rfuns (remove* (list values) rfuns)])
-                          (simple-compose (pipeline (car rfuns) (cdr rfuns)) f))])))]))))
+                 (cond
+                   [(pair? fs)
+                    (can-compose name i f f0 fs0)
+                    (loop (car fs) (cdr fs) (add1 i) (cons f rfuns))]
+                   [(andmap id? rfuns) f]
+                   [(id? f) (pipeline (car rfuns) (cdr rfuns))]
+                   [else
+                    (let ([rfuns (remove* (list values) rfuns)])
+                      (simple-compose (pipeline (car rfuns) (cdr rfuns)) f))]))]))))
       (mk compose1 app1 can-compose1 pipeline1
           (lambda (f g) (mk-simple-compose app1 f g)))
       (mk compose  app* can-compose* pipeline*
