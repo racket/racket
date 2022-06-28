8fd848105b11eed6aea32d948697601277825471
Philip McGrath <philip@philipmcgrath.com>
2022-06-28 18:27:51 -0400

    unify license text
    Generate LICENSE.txt and racket/src/LICENSE.txt from
    pkgs/racket-index/scribblings/main/license.scrbl.
    
    Needs https://github.com/racket/scribble/pull/334
    Needs https://github.com/racket/scribble/pull/337

--- pkgs/racket-index/info.rkt
+++ pkgs/racket-index/info.rkt
@@ -3,7 +3,7 @@
 (define collection 'multi)
 
 (define deps '("base"
-               "scribble-lib"))
+               ["scribble-lib" #:version "1.46"]))
 (define build-deps '("scheme-lib"
                      "at-exp-lib"))
 
