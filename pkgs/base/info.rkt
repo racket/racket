6155b17ab8034d43eed76d68abcc416f8bd82da7
Matthew Flatt <mflatt@racket-lang.org>
2022-09-10 04:51:28 +0800

    Chez Scheme: add "reboot" bootstrap
    A "reboot" bootstrap is like "rktboot", but based on an existing Chez
    Scheme instead of an existing Racket. Like "rktboot", "reboot"
    implements enough of Chez Scheme to run on a relatively unrelated Chez
    Scheme implementation, as opposed to the normal "cross boot" process
    that needs a Chez Scheme with the same record representation,
    intermediate structures for expanded code, etc.

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.6.0.11")
+(define version "8.6.0.12")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
