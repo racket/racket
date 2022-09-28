e2bc6bf5d443fb51805e8e7f3c569ea10895b454
Matthew Flatt <mflatt@racket-lang.org>
2022-09-28 09:37:50 +0800

    Chez Scheme: add local unboxing for `flmin` and `flmax`
    Also, while we're changing the version number, reorder machine
    constants to be more consistent.

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.6.0.13")
+(define version "8.6.0.14")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
