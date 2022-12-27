95ea8ea08b2ec6b7dad2a1719aea29f7d1f9194f
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-12-27 22:27:09 +0700

    match: fix hash-table pattern and its doc, add more optimizations
    https://github.com/racket/racket/commit/8377fa842fb3b29650b0fd19b6ef13a555b843e4
    introduces an optimization for hash-table patterns whose keys are
    all literals. Unfortunately, it incorrectly allows partial matching,
    such as:
    
    (match (hash 1 2 3 4)
      [(hash-table (1 x)) 'matched])
    
    This issue was noticed and there's an attempt to fix it in
    https://github.com/racket/racket/commit/393582492203608a579a062b3fcece0ad4e18e88
    Yet, it only fixes a very specific case, and the problem in general remains.
    
    This PR fixes the issue properly, by requiring matching against the full
    hash table. However, this buggy behavior in fact is useful in many
    situations, so the PR also adds more optimization opportunity for this case:
    
    (match (hash 1 2 3 4)
      [(hash-table (1 x) _ ...) 'matched])
    
    Lastly, the PR documents the id pattern for matching against a key-value
    pair with the list representation. The feature was intentionally
    supported but undocumented since 2008
    (https://github.com/racket/racket/commit/ae4acf1d517beeb7673edb12cff77add32ebf89c).

--- pkgs/racket-test/tests/match/match-tests.rkt
+++ pkgs/racket-test/tests/match/match-tests.rkt
@@ -51,7 +51,173 @@
                        [_ "non-empty"])
                      "empty"))
      ))
-  
+
+  (define hash-table-no-rep-tests
+    (test-suite "hash-table patterns (no ..k)"
+      (test-case "literal keys"
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table (1 a) (3 b))
+                         (list a b)])
+                      '(2 4))
+
+        (check-equal? (match (hash 'a 'b 'c 'd)
+                        [(hash-table ('c x) ('a y))
+                         (list x y)])
+                      '(d b))
+
+        (check-true (match (hash)
+                      [(hash-table) #t]))
+
+        (check-equal? (match (hash "a" "b" "c" "d" "e" "f")
+                        [(hash-table ("e" x) ("a" "b") ("c" y))
+                         (list x y)])
+                      '("f" "d")))
+
+      (test-case "literal keys predicate"
+        (check-equal? (match (hash 'a 'b 'c 'd)
+                        [(hash-table ('a x)) 1]
+                        [(hash-table ('a x) ('c y)) 2])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table) 1]
+                        [(hash-table (1 a)) 2]
+                        [(hash-table (2 a)) 3]
+                        [(hash-table (1 a) (3 b) (5 c)) 4]
+                        [(hash-table (1 a) (2 b)) 5]
+                        [(hash-table (1 10) (3 b)) 6]
+                        [(hash-table (1 a) (3 b)) 7]
+                        [(hash-table (1 a) (3 4)) 8])
+                      7)
+
+        ;; Duplicate keys
+        (check-equal? (match (hash "a" "b" "c" "d")
+                        [(hash-table ("a" x) ("a" y))
+                         (list x y)]
+                        [(hash-table _ _) 42])
+                      42))
+
+      (test-case "non literal keys"
+        (check-equal? (match (hash (list 1 2) 'b (list 3 4) 'd)
+                        [(hash-table ((list 1 2) x) ((list 3 4) y)) (list x y)])
+                      '(b d))
+
+        (check-equal? (match (hash (list 1 2) 'b (list 3 4) 'd)
+                        [(hash-table (c d) (a 'b)) (list a c d)])
+                      '((1 2) (3 4) d))
+
+        (check-equal? (match (hash (list 1 2) 'b (list 3 4) 'd)
+                        [(hash-table p (a 'b)) (list a p)])
+                      '((1 2) ((3 4) d)))
+
+        (check-equal? (match (hash (list 1 2) 'x (list 3 4) 'x)
+                        [(hash-table _ q) (cadr q)])
+                      'x))
+
+      (test-case "non literal keys predicate"
+        (check-equal? (match (hash (list 1 2) 'b (list 3 4) 'd)
+                        [(hash-table) 1]
+                        [(hash-table a) 2]
+                        [(hash-table a b c) 3]
+                        [(hash-table a b) 4]
+                        [(hash-table (p 'd) _) p])
+                      4)
+
+        (check-equal? (match (hash (list 1 2) 'b (list 3 4) 'd)
+                        [(hash-table) 1]
+                        [(hash-table a) 2]
+                        [(hash-table a b c) 3]
+                        [(hash-table (p 'd) _) p]
+                        [(hash-table a b) 4])
+                      (list 3 4)))))
+
+  (define hash-table-rep-tests
+    (test-suite "hash-table patterns (with ..k)"
+      (test-case "literal keys"
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (3 b) (5 c) (1 a) _ ...)
+                         (list a b c)])
+                      '(2 4 6))
+
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (3 a) (5 b) _ ...)
+                         (list a b)])
+                      '(4 6))
+
+        ;; Duplicate keys are fine
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (3 a) (3 b) _ ...)
+                         (list a b)])
+                      '(4 4))
+
+        (check-true (match (hash 1 2 3 4 5 6)
+                      [(hash-table _ ...) #t]))
+
+        (check-true (match (hash 1 2 3 4 5 6)
+                      [(hash-table (1 2) _ ...) #t])))
+
+      (test-case "literal keys predicate"
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (3 42) (5 b) _ ...) #f]
+                        [(hash-table (5 b) _ ...) b])
+                      6)
+
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (1 _) (3 _) (5 _) (7 _) _ ...) #f]
+                        [(hash-table (2 _) _ ...) #f]
+                        [(hash-table (5 b) _ ...) b])
+                      6))
+
+      (test-case "non literal keys"
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (3 42) (5 b) _ ...) #f]
+                        [(hash-table (5 b) _ ...) b])
+                      6)
+
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (a 2) _ ...) a])
+                      1)
+
+        (check-true (match (hash 1 2 3 4)
+                      [(hash-table p _ _ ...) #t]))
+
+
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table (1 x) _ ...) x])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table (1 x) _ ..0) x])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table (1 x) _ ..1) x])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4)
+                        [(hash-table (1 x) p ...) p])
+                      (list (list 3 4)))
+
+        (check-equal? (match (hash 1 2)
+                        [(hash-table (1 x) p ...) p])
+                      '()))
+
+      (test-case "non literal keys predicate"
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table _ ..4) 1]
+                        [(hash-table _ ..3) 2])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table a b c d _ ...) 1]
+                        [(hash-table a b c _ ...) 2])
+                      2)
+
+        (check-equal? (match (hash 1 2 3 4 5 6)
+                        [(hash-table (_ 10) b c _ ...) 1]
+                        [(hash-table a b c _ ...) 2])
+                      2))))
+
   (define nonlinear-tests
     (test-suite 
      "Non-linear patterns"
@@ -135,5 +301,7 @@
                      doc-tests
                      simple-tests
                      nonlinear-tests
-                     match-expander-tests))
+                     match-expander-tests
+                     hash-table-no-rep-tests
+                     hash-table-rep-tests))
   )
