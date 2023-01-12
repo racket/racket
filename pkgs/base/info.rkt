de853f845b0a90cd3fee32ea11c0bf68d829fd6c
Bob Burger <bburger@indigobio.com>
2023-01-12 16:03:06 -0500

    Unicode 15.0 support
    [Adapted from a Chez Scheme change to include a Racket BC update and
     a version bump.]

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.8.0.7")
+(define version "8.8.0.8")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
