48918e7efc3003d8005c5053088b5f827174345e
John Clements <clements@racket-lang.org>
2023-01-11 21:30:23 -0800

    Alpha version number for the v8.8 release

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.7.0.11")
+(define version "8.7.900")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
