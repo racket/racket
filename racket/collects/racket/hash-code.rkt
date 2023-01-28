1ade9528506463b9aec10fe9869c1b804b6495f3
AlexKnauth <alexander@knauth.org>
2023-01-27 23:36:46 -0500

    Add hash-code operations
    hash-code-combine, hash-code-combine-unordered, and `*` star versions

---
+++ racket/collects/racket/hash-code.rkt
@@ -0,0 +1,93 @@
+#lang racket/base
+
+(provide hash-code-combine
+         hash-code-combine*
+         hash-code-combine-unordered
+         hash-code-combine-unordered*)
+
+(require racket/fixnum)
+
+;; fixnum shift-left logical with wraparound
+(define fxsll/wraparound fxlshift/wraparound)
+
+;; fixnum logical exclusive or
+(define fxlogxor fxxor)
+
+;; fixnum shift-right logical
+;; shifting negative fixnums can produce large positive fixnums
+(define (fxsrl a b)
+  (cond
+    [(and (fixnum? a) (negative? a) (positive? b))
+     (fx-/wraparound (fxrshift (bitwise-and a (most-positive-fixnum)) b)
+                     (fxrshift (most-negative-fixnum) b))]
+    ;                          ^ could be (bitwise-and a (most-negative-fixnum))
+    ; but when `a` is a negative fixnum, they're equivalent
+    ; because `a` and `(most-negative-fixnum)` have the same negative-sign-bits,
+    ; and everywhere else `a` might have 1s, `(most-negative-fixnum)` has 0s
+    [else (fxrshift a b)]))
+
+;; Adapted from racket/src/cs/rumble/hash-code.ss
+;; which is adapted from ChezScheme/s/newhash.ss
+
+;; maps non-fixnum integers to positive fixnums
+(define (->fx v)
+  (cond
+    [(fixnum? v) v]
+    [(exact-integer? v) (bitwise-and v (most-positive-fixnum))]
+    [else (raise-argument-error '->fx "exact-integer?" v)]))
+
+;; Adapted from `mix-hash-code` in racket/src/cs/rumble/hash-code.ss
+;; which is adapted from an `update` function in ChezScheme/s/newhash.ss
+;; and adapted from One-at-a-Time Hash in "A Hash Function for Hash Table Lookup"
+;; Bob Jenkins's expansion of his September 1997 Dr Dobbs article on Hash Functions
+;; http://www.burtleburtle.net/bob/hash/doobs.html#one
+(define (fxmix-hash-code hc)
+  (let ([hc2 (fx+/wraparound hc (fxsll/wraparound (fx+/wraparound hc 1) 10))])
+    (fxlogxor hc2 (fxsrl hc2 6))))
+
+;; NOTE: this `hash-code-combine` function is NOT the same as the
+;; `hash-code-combine` from racket/src/cs/rumble/hash-code.ss
+;; (combine) = 0
+;; (combine A) = (mix A)
+;; (combine A B) = (unordered (mix A) (mix (unordered (mix A) B)))
+;; Example reshuffles to avoid collision:
+;; (unordered (combine A B) C) != (unordered (combine A C) B)
+;; (combine A (mix B)) != (combine B (mix A))
+(define hash-code-combine
+  (case-lambda
+    [() 0]
+    [(a) (fxmix-hash-code (->fx a))]
+    [(a b)
+     (let ([mxa (fxmix-hash-code (->fx a))])
+       (fx+/wraparound mxa (fxmix-hash-code (fx+/wraparound mxa (->fx b)))))]
+    [hcs (hash-codes-combine hcs)]))
+
+(define hash-code-combine*
+  (case-lambda
+    [(l) (hash-codes-combine l)]
+    [(f . r) (hash-codes-combine (apply list* f r))]))
+
+(define (hash-codes-combine hcs)
+  (for/fold ([acc 0]) ([hc (in-list hcs)])
+    (fx+/wraparound acc (fxmix-hash-code (fx+/wraparound acc (->fx hc))))))
+
+;; hash-code-combine-unordered is commutative and associative
+;; (unordered) = 0
+;; (unordered A) = A
+;; (unordered A B) = (unordered B A)
+;; (unordered A B C) = (unordered A (unordered B C)) = (unordered (unordered A B) C)
+(define hash-code-combine-unordered
+  (case-lambda
+    [() 0]
+    [(a) (->fx a)]
+    [(a b) (fx+/wraparound (->fx a) (->fx b))]
+    [hcs (hash-codes-combine-unordered hcs)]))
+
+(define hash-code-combine-unordered*
+  (case-lambda
+    [(l) (hash-codes-combine-unordered l)]
+    [(f . r) (hash-codes-combine-unordered (apply list* f r))]))
+
+(define (hash-codes-combine-unordered hcs)
+  (for/fold ([acc 0]) ([hc (in-list hcs)])
+    (fx+/wraparound acc (->fx hc))))
