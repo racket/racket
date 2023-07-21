e5309fb616dd464ca65018576e20f0f197fec6fd
shhyou <shu-hung.you@eecs.northwestern.edu>
2023-07-21 14:17:48 -0500

    Bump base version (due to dd5696e7)

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.10.0.1")
+(define version "8.10.0.2")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
