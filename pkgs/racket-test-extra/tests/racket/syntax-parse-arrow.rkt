91be1ba8d8db3bb19af71b47066b3b27a5c35c56
shhyou <shu-hung.you@eecs.northwestern.edu>
2023-09-24 19:25:30 -0500

    Fixup of b98f4332

--- pkgs/racket-test-extra/tests/racket/syntax-parse-arrow.rkt
+++ pkgs/racket-test-extra/tests/racket/syntax-parse-arrow.rkt
@@ -3,15 +3,20 @@
 (require syntax/parse
          drracket/check-syntax)
 
-(provide stx check-syntax-annotations)
+(provide code stx check-syntax-annotations)
+
+(define code
+  (string-append
+   "(module anonymous racket/base\n"
+   "    (require syntax/parse)\n"
+   "    (lambda (stx)\n"
+   "      (syntax-parse stx\n"
+   "        [e #'e]\n"
+   "        [e #'e])))\n"))
 
 (define stx
-  #'(module anonymous racket/base
-      (require syntax/parse)
-      (lambda (stx)
-        (syntax-parse stx
-          [e #'e]
-          [e #'e]))))
+  (read-syntax "<racket-test-extra>/tests/racket/dummy-file.rkt"
+               (open-input-string code)))
 
 (define check-syntax-annotations
   (show-content stx))
@@ -21,10 +26,10 @@
   ;; check that #'e points to e at L13
   (check-match check-syntax-annotations
                `(,_ ...
-                 #(syncheck:add-arrow/name-dup/pxpy 246 247 ,_ ,_ 250 251 ,_ ,_ ,_ ...)
+                 #(syncheck:add-arrow/name-dup/pxpy 108 109 ,_ ,_ 112 113 ,_ ,_ ,_ ...)
                  ,_ ...))
   ;; check that #'e points to e at L14
   (check-match check-syntax-annotations
                `(,_ ...
-                 #(syncheck:add-arrow/name-dup/pxpy 264 265 ,_ ,_ 268 269 ,_ ,_ ,_ ...)
+                 #(syncheck:add-arrow/name-dup/pxpy 124 125 ,_ ,_ 128 129 ,_ ,_ ,_ ...)
                  ,_ ...)))
