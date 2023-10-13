e436248c29140f6c01e4e5a2781fbed5a2f4c7f0
Matthew Flatt <mflatt@racket-lang.org>
2023-10-13 04:29:18 -0600

    bump version to reflect updated Chez Scheme

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.11.0.2")
+(define version "8.11.0.3")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
