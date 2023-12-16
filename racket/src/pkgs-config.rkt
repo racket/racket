c5c9bb607a13d3cb322dece3415235ab845e2aa7
Philip McGrath <philip@philipmcgrath.com>
2023-12-16 16:38:45 -0500

    adjust build stamp for development and snapshots
    This commit follows up on b613f64 by adjusting the default
    `build-stamp` configuration that Racket's build system generates for
    `distro-build` snapshots and for development builds driven by the
    top-level Makefile.
    
    The generated stamps now incorporate formatting and information
    similar to that which DrRacket previously added to its banner. Moving
    this work to the build system makes the enhanced stamps available for
    all uses of `(get-build-stamp)` and `(banner)`, and the build system
    should be the most reliable way to record information about the build.
    
    Related to https://github.com/racket/racket/pull/4847
    Related to https://github.com/racket/drracket/commit/841d5b3

--- racket/src/pkgs-config.rkt
+++ racket/src/pkgs-config.rkt
@@ -1,13 +1,42 @@
 #lang racket/base
 (require racket/cmdline
          racket/format
-         racket/path
+         racket/pretty
+         racket/port
+         racket/string
+         racket/date
+         racket/system
          racket/file)
 
-;; Adjust the configuration to consult a catalog that is
-;; expected to map some packages to directory links.
-
-;; Used by the top-level Makefile in the main Racket repository.
+(module+ main
+  (command-line
+   #:usage-help
+   "Used by the top-level Makefile in the main Racket repository."
+   "If no <option> is provided, does nothing."
+   #:once-any
+   [("--pkgs-catalog")
+    default-src-catalog src-catalog
+    ("Configure a development build to consult first a local catalog,"
+     "which is expected to map packages in this repository to directory links,"
+     "next <src-catalog>, and then <default-src-catalog> (if different) before"
+     "falling back to the built-in default catalog."
+     "If a config.rktd file is created, also set its installation-name,"
+     "build-stamp, default-scope, interactive-file, and"
+     "gui-interactive-file to values suitable for development from Git."
+     "If a config.rktd file already exists, check its catalog configuration"
+     "and potentially update its build stamp as with `--maybe-update-stamp`.")
+    (pkgs-catalog default-src-catalog src-catalog)]
+   [("--maybe-update-stamp")
+    ("Update the build stamp if and only if a config.rktd file already"
+     "exists and indicates that its build stamp was previously generated"
+     "by this script.")
+    (maybe-update-stamp)]
+   [("--display-auto-stamp")
+    ("Display to standard output an automatic build stamp (in the same style"
+     "as above) followed by a newline. Used by `distro-build`.")
+    (displayln (auto-stamp))]
+   #:args ()
+   (void)))
 
 (define config-dir-path (build-path "racket" "etc"))
 (define config-file-path (build-path config-dir-path "config.rktd"))
@@ -15,24 +44,21 @@
 (define catalog-relative-path (build-path 'up "share" "pkgs-catalog"))
 (define catalog-relative-path-str (path->string catalog-relative-path))
 
-(define-values (default-src-catalog src-catalog)
-  (command-line
-   #:args
-   (default-src-catalog src-catalog)
-   (values default-src-catalog src-catalog)))
+(define (write-config config)
+  (call-with-atomic-output-file config-file-path
+    (λ (out _tmp)
+      (pretty-write config out))))
 
-(define src-catalog-is-default?
-  (equal? src-catalog default-src-catalog))
-
-(when (file-exists? config-file-path)
-  (call-with-input-file*
-   config-file-path
-   (lambda (i)
-     (define r (read i))
-     (define l (hash-ref r 'catalogs #f))
+(define (pkgs-catalog default-src-catalog src-catalog)
+  (define src-catalog-is-default?
+    (equal? src-catalog default-src-catalog))
+  (cond
+    [(file-exists? config-file-path)
+     (define config (file->value config-file-path))
+     (define l (hash-ref config 'catalogs #f))
      (define starts-as-expected?
        (and (list? l)
-            ((length l) . >= . 1)
+            (pair? l)
             (equal? (car l) catalog-relative-path-str)))
      (define has-src-catalog?
        (or (and src-catalog-is-default?
@@ -55,29 +81,95 @@
                   "")
               (if (not starts-as-expected?)
                   catalog-relative-path-str
-                  src-catalog))))))
+                  src-catalog)))
+     (update-stamp-if-auto config)]
+    [else
+     (printf "Writing ~a\n" config-file-path)
+     (make-directory* config-dir-path)
+     (write-config (hash 'catalogs
+                         (cons catalog-relative-path-str
+                               (if src-catalog-is-default?
+                                   '(#f)
+                                   (list src-catalog #f)))
+                         'installation-name
+                         "development"
+                         'default-scope
+                         "installation"
+                         'automatic-development-build-stamp?
+                         #t
+                         'build-stamp
+                         (auto-stamp)
+                         'interactive-file
+                         'racket/interactive
+                         'gui-interactive-file
+                         'racket/gui/interactive))]))
 
-(unless (file-exists? config-file-path)
-  (printf "Writing ~a\n" config-file-path)
-  (let-values ([(base name dir?) (split-path config-file-path)])
-    (when (path? base) (make-directory* base)))
-  (call-with-output-file*
-   config-file-path
-   (lambda (o)
-     (write (hash 'catalogs
-                  (cons catalog-relative-path-str
-                        (append
-                         (if src-catalog-is-default?
-                             '()
-                             (list src-catalog))
-                         (list #f)))
-                  'installation-name
-                  "development"
-                  'default-scope
-                  "installation"
-                  'interactive-file
-                  'racket/interactive
-                  'gui-interactive-file
-                  'racket/gui/interactive)
-            o)
-     (newline o))))
+(define (maybe-update-stamp)
+  (when (file-exists? config-file-path)
+    (update-stamp-if-auto (file->value config-file-path))))
+
+(define (update-stamp-if-auto config)
+  (when (hash-ref config 'automatic-development-build-stamp? #f)
+    (printf "Updating build-stamp in ~a\n" config-file-path)
+    (write-config (hash-set config 'build-stamp (auto-stamp)))))
+
+(define (auto-stamp)
+  (cond
+    [(format-current-commit)
+     => (λ (desc)
+          (~a (today) "-" desc))]
+    [else
+     (today)]))
+
+(define (today)
+  ;; Use Racket instead of:
+  ;;  - `date +%Y-%m-%d` to handle SOURCE_DATA_EPOCH and to avoid
+  ;;     portability complications.
+  ;;  - `git log -1 --pretty=%cs` because the date for snapshot
+  ;;     builds should be updated even on days with no commits here.
+  ;;     Users can still choose the commit date instead via e.g.
+  ;;     `SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)`.
+  (define env
+    (getenv "SOURCE_DATE_EPOCH"))
+  (define date
+    (if (and (non-empty-string? env)
+             (not (regexp-match? #px"\\D" env)))
+        (seconds->date (string->number env) #f)
+        (current-date)))
+  (parameterize ([date-display-format 'iso-8601])
+    (date->string date)))
+
+(define (format-current-commit)
+  (let/ec return
+    (define git-cmd
+      (or (find-executable-path "git")
+          (find-executable-path "git.exe")
+          (and (eq? 'macosx (system-type))
+               (find-executable-path "/opt/local/bin/git"))))
+    (unless (and git-cmd
+                 (parameterize ([current-output-port (open-output-nowhere)]
+                                [current-input-port (open-input-string "")]
+                                ;; failure here just means we are not in a git tree,
+                                ;; so discard error messages
+                                [current-error-port (open-output-nowhere)])
+                   (with-handlers ([exn:fail? (λ (e) #f)])
+                     (system* git-cmd "rev-parse" "--git-dir"))))
+      (return #f))
+    (define (git . args)
+      ;; failure here means something is likely wrong, so pass through stderr,
+      ;; but don't break the build
+      (define o (open-output-string))
+      (or (and (parameterize ([current-output-port o]
+                              [current-input-port (open-input-string "")])
+                 (apply system* git-cmd args))
+               (let ([s (string-trim (get-output-string o))])
+                 (and (non-empty-string? s) s)))
+          (return #f)))
+    (~a (git "log" "-1" "--abbrev=10" "--pretty=format:%h")
+        (or (for/or ([state '("broken" "dirty")]
+                     [mark '("-broken" "*")])
+              (define desc
+                (git "describe" "--always" "--long" (~a "--" state)))
+              (and (string-suffix? desc state)
+                   mark))
+            ""))))
