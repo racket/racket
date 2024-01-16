racket/racket

commit 3b5b0a7e5c4a954802d41daf3c35535062a7d00e

repair `for/fold` binding and `#:break`
Besides fixing `#:break` when an accumulator variable is shadowed by a
`for/fold` clause or local definition, this commit adjusts the way
that accumulator variables are bound so that they work more sanely
with shadowing.

An unusual aspect of `for/fold` is that the lifetimes of variables do
not line with with textual nesting, but scope does --- or, at least,
does after this commit. For example, in

 (for*/fold ([a ....])
            ([i ....]
             [j ....])
    ....)

you would expect that `a`, `i`, and `j` are bound for everything to
the right of the binding. There's a new `i` and `j` for each
iteration, but each `i` lives as long as all the `j`s` inside. But the
liftime of `a` is inside the liftimes of `i` and `j`, insead of
outside.

Before this commit, `a` wasn't exactly bound this way, and shadowing
throught a `for`-clause binding acted weirdly: the value of the
shadowed variable taken from the accumulator on the first iteration of
a loop and the shadowing binding for subsequent loops. This weirdness
interacted with the implementation of `#:break`, so that it didn't
work right.

The new implementation binds each accumulator variable as a macro that
refers to a suitable binding for the current iteration, essentially
like using a syntax parameter (but implemented for this limited
context in a more direct way).

A remaining weirdness is that an accumulator variable is not bound of
the right-hande size of clauses for the ones that determine an
outermost iteration. Unfortunately, change that breaks too much code,
and in a way that can be particularly difficult to track down. Add
`#:when? #t` as the first clause to avoid that binding weirdness.

pkgs/racket-test-core/tests/racket/for.rktl
@@ -296,6 +296,30 @@
            (number->string i))
          c)))
 
+;; make sure `#:break` is not confused by shadowing
+(test 0 'break-0 (for*/fold ([x 0]) ([x '(1)]) #:break #true (add1 x)))
+(test 0 'break-0 (for*/fold ([x 0]) ([x '(1)] [y '(3)]) #:break #true (add1 x)))
+(test 0 'break-0 (for*/fold ([x 0]) ([x '(1)] #:break #true) (add1 x)))
+(test 0 'break-0 (for*/fold ([x 0]) ([x '(1)] [y '(3)] #:break #true) (add1 x)))
+(test 0 'break-0 (for*/fold ([x 0]) ([x '(1)] #:break #true [y '(3)]) (add1 x)))
+
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)]) #:break #false (add1 x)))
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] [y '(3)]) #:break false (add1 x)))
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] #:break #false) (add1 x)))
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] [y '(3)] #:break #false) (add1 x)))
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] #:break #false [y '(3)]) (add1 x)))
+
+(test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] [y (in-value 3)]) #:break false (add1 x)))
+(test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] [y (in-value 3)] #:break #false) (add1 x)))
+(test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] #:break #false [y (in-value 3)]) (add1 x)))
+
+;; make sure `#:final` is not treated like `#:break`
+(test 2 'final-0 (for*/fold ([x 0]) ([x '(1)]) #:final #true (add1 x)))
+(test 2 'final-0 (for*/fold ([x 0]) ([x '(1)] [y '(3)]) #:final #true (add1 x)))
+(test 2 'final-0 (for*/fold ([x 0]) ([x '(1)] #:final #true) (add1 x)))
+(test 2 'final-0 (for*/fold ([x 0]) ([x '(1)] [y '(3)] #:final #true) (add1 x)))
+(test 2 'final-0 (for*/fold ([x 0]) ([x '(1)] #:final #true [y '(3)]) (add1 x)))
+
 ;; Basic sanity checks.
 (test '#(1 2 3 4) 'for/vector (for/vector ((i (in-range 4))) (+ i 1)))
 (test '#(1 2 3 4) 'for/vector-fast (for/vector #:length 4 ((i (in-range 4))) (+ i 1)))
@@ -497,6 +521,21 @@
                                            ([x (values (in-value 2))])
                                    x))
 
+;; Check against pre-8.11.1.3 weird effect of shadowing fold variables
+(let ([accum null])
+  (test '("x" 10 10)
+        'weird-shadow
+        (cons (for*/fold ([x 0]) ([x '(10)] [y '(1 2)]) (set! accum (cons x accum)) "x")
+              accum)))
+(let ([accum null])
+  (test '("x" 10 10)
+        'weird-shadow
+        (cons
+         (for*/fold ([x 0]) ([x '(10)] [y '(1 2)] #:break #false) (set! accum (cons x accum)) "x")
+         accum)))
+
+;; Check against pre-8.11.1.3 weird ordering of fold variable's initial value,
+;; but for continued weird absence of fold varibales in the initial clause
 (let ([x 'out]
       [prints '()])
   (for/fold ([x (begin
@@ -507,7 +546,7 @@
                            (list 1 2 3)))])
     (set! prints (cons x prints))
     x)
-  (test '(3 2 1 (top out) (rhs out)) values prints))
+  (test '(3 2 1 (rhs out) (top out)) values prints))
 
 ;; check ranges on `in-vector', especially as a value
 (test '() 'in-empty-vector (let ([v (in-vector '#())]) (for/list ([e v]) e)))

pkgs/racket-doc/scribblings/reference/for.scrbl
@@ -414,23 +414,18 @@ terminates, if a @racket[result-expr] is provided then the result of the
 ]
 
 The binding and evaluation order of @racket[accum-id]s and
-@racket[init-expr]s do not completely follow the textual,
-left-to-right order relative to the @racket[for-clause]s. Instead, the
-sequence expressions in @racket[for-clause]s that determine the
-outermost iteration are evaluated first, then the @racket[init-expr]s
-are evaluated and the @racket[accum-id]s are bound, and finally the
-outermost iteration's identifiers are bound. One consequence is that
-the @racket[accum-id]s are not bound in @racket[for-clause]s for the
-outermost initialization. At the same time, when a @racket[accum-id]
-is used as a @racket[for-clause] binding for the outermost iteration,
-the @racket[for-clause] binding shadows the @racket[accum-id] binding
-in the loop body (which is what you would expect syntactically).
-A fresh variable for each @racket[accum-id] (at a
-fresh location) is bound in each nested iteration that is created by a
-later group for @racket[for-clause]s (after a @racket[#:when] or
-@racket[#:unless], for example).
+@racket[init-expr]s follow the textual, left-to-right order relative
+to the @racket[for-clause]s, except that (for historical reasons)
+@racket[accum-id]s are not available in the @racket[for-clause]s for
+the outermost iteration. The lifetimes of variables are not quite the
+same as the lexical nesting, however: the variable referenced by a
+@racket[accum-id] has a fresh location in each iteration.
 
-@history[#:changed "6.11.0.1" @elem{Added the @racket[#:result] form.}]
+@history[#:changed "6.11.0.1" @elem{Added the @racket[#:result] form.}
+         #:changed "8.11.1.3" @elem{Changed evaluation order to match textual left-to-right order,
+                                    including evaluating @racket[init-expr]s before the first
+                                    @racket[for-clause]'s right-hand side and fixing shadowing of
+                                    @racket[accum-id].}]
 }
 
 @(define for/foldr-eval ((make-eval-factory '(racket/promise racket/sequence racket/stream))))

commit 26477e0e8fba1cedb17722a483d79dcb19030e5b

fix `for/fold` binding in the presence of `#:result`
This bug was introduced in 3b5b0a7.  The `fold-var`s incorrectly
scoped over the whole `for/fold` expansion, while the "remaining
weirdness" behavior was that outermost iteration clauses should not
see the `fold-var`s.

Also revert the `for/foldr` change that appears to be incorrect.
Unlike `for/fold`, `for/foldr` does not need to use this approach,
because `fold-var`s are only visible to the body.

Related to #4898.

racket/collects/racket/private/for.rkt
@@ -1546,26 +1546,26 @@
             ...)
         next-k)))
 
-  (define-for-syntax ((make-inner-recur/foldr/strict int-vars) stx)
+  (define-for-syntax ((make-inner-recur/foldr/strict fold-vars) stx)
     (syntax-case stx ()
       [(_ () [expr ...] next-k)
-       #`(let-values ([#,(map syntax-local-introduce int-vars) next-k])
+       #`(let-values ([#,(map syntax-local-introduce fold-vars) next-k])
            expr ...)]))
 
-  (define-for-syntax ((make-inner-recur/foldr/lazy int-vars delayed-id delayer-id) stx)
+  (define-for-syntax ((make-inner-recur/foldr/lazy fold-vars delayed-id delayer-id) stx)
     (syntax-case stx ()
       [(_ () [expr ...] next-k)
-       (with-syntax ([(int-var ...) (map syntax-local-introduce int-vars)]
+       (with-syntax ([(fold-var ...) (map syntax-local-introduce fold-vars)]
                      [delayed-id (syntax-local-introduce delayed-id)]
                      [delayer-id delayer-id])
          #`(let*-values
                ([(delayed-id) (delayer-id next-k)]
                 #,@(cond
-                     [(= (length int-vars) 1)
-                      #`([(int-var ...) delayed-id])]
+                     [(= (length fold-vars) 1)
+                      #`([(fold-var ...) delayed-id])]
                      [(delayer? (syntax-local-value #'delayer-id (lambda () #f)))
-                      #`([(int-var) (delayer-id (let-values ([(int-var ...) (force delayed-id)])
-                                                   int-var))]
+                      #`([(fold-var) (delayer-id (let-values ([(fold-var ...) (force delayed-id)])
+                                                   fold-var))]
                          ...)]
                      [else #'()]))
              expr ...))]))
@@ -1854,41 +1854,40 @@
                            [delayed-id delayed-id]
                            [delayer-id delayer-id])
                (check-identifier-bindings #'orig-stx #`(fold-var ... delayed-id) "accumulator" (void))
-               (with-syntax ([(int-var ...)
-                              (map (lambda (fold-var)
-                                     (datum->syntax fold-var
-                                                    (string->uninterned-symbol
-                                                     (symbol->string (syntax-e fold-var)))
-                                                    fold-var
-                                                    fold-var))
-                                   (syntax->list #'(fold-var ...)))])
-                 (cond
-                   [right?
-                    (define loop-stx
-                      (quasisyntax/loc #'orig-stx
-                        (for/foldX/derived [orig-stx inner-recur/foldr #,for*? #f ()]
-                          () ([fold-var (make-fold-var 'int-var)] ...)
-                          (done-k-proc)
-                          (done-k-proc)
-                          #f
-                          . rest)))
+               (cond
+                 [right?
+                  (define loop-stx
                     (quasisyntax/loc #'orig-stx
-                      (let ([done-k-proc (lambda () (#%expression (values* fold-init ...)))])
-                        (define-syntax inner-recur/foldr
-                          #,(if delay?
-                                #'(make-inner-recur/foldr/lazy
-                                   (list (quote-syntax int-var) ...)
-                                   (quote-syntax delayed-id)
-                                   (quote-syntax delayer-id))
-                                #'(make-inner-recur/foldr/strict
-                                   (list (quote-syntax int-var) ...))))
-                        #,(if result-expr
-                              ;; Make sure `fold-var`s in `result-expr` are also delayed, if relevant
-                              (let ([result-expr #`(letrec-syntax ([fold-var (make-fold-var 'int-var)] ...)
-                                                     #,result-expr)])
-                                #`(inner-recur/foldr () [#,result-expr] #,loop-stx))
-                              loop-stx)))]
-                   [else
+                      (for/foldX/derived [orig-stx inner-recur/foldr #,for*? #f ()]
+                        ()
+                        ()
+                        (done-k-proc)
+                        (done-k-proc)
+                        #f
+                        . rest)))
+                  (quasisyntax/loc #'orig-stx
+                    (let ([done-k-proc (lambda () (values* fold-init ...))])
+                      (define-syntax inner-recur/foldr
+                        #,(if delay?
+                              #'(make-inner-recur/foldr/lazy
+                                 (list (quote-syntax fold-var) ...)
+                                 (quote-syntax delayed-id)
+                                 (quote-syntax delayer-id))
+                              #'(make-inner-recur/foldr/strict
+                                 (list (quote-syntax fold-var) ...))))
+                      #,(if result-expr
+                            ;; Make sure `fold-var`s in `result-expr` are also delayed, if relevant
+                            #`(inner-recur/foldr () [#,result-expr] #,loop-stx)
+                            loop-stx)))]
+                 [else
+                  (with-syntax ([(int-var ...)
+                                 (map (lambda (fold-var)
+                                        (datum->syntax fold-var
+                                                       (string->uninterned-symbol
+                                                        (symbol->string (syntax-e fold-var)))
+                                                       fold-var
+                                                       fold-var))
+                                      (syntax->list #'(fold-var ...)))])
                     (quasisyntax/loc #'orig-stx
                       (let ([int-var (let ([fold-var fold-init])
                                        fold-var)]
@@ -1902,10 +1901,9 @@
                                      . rest))])
                             (if result-expr
                                 (quasisyntax/loc #'orig-stx
-                                  (letrec-syntax ([fold-var (make-fold-var 'int-var)] ...)
-                                    (let-values ([(fold-var ...) #,loop-stx])
-                                      #,result-expr)))
-                                loop-stx))))]))))]
+                                  (let-values ([(fold-var ...) #,loop-stx])
+                                    #,result-expr))
+                                loop-stx)))))])))]
           [(_ orig-stx . rst)
            (raise-syntax-error #f "bad syntax" #'orig-stx)]))
 

pkgs/racket-test-core/tests/racket/for.rktl
@@ -304,12 +304,12 @@
 (test 0 'break-0 (for*/fold ([x 0]) ([x '(1)] #:break #true [y '(3)]) (add1 x)))
 
 (test 2 'break-2 (for*/fold ([x 0]) ([x '(1)]) #:break #false (add1 x)))
-(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] [y '(3)]) #:break false (add1 x)))
+(test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] [y '(3)]) #:break #false (add1 x)))
 (test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] #:break #false) (add1 x)))
 (test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] [y '(3)] #:break #false) (add1 x)))
 (test 2 'break-2 (for*/fold ([x 0]) ([x '(1)] #:break #false [y '(3)]) (add1 x)))
 
-(test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] [y (in-value 3)]) #:break false (add1 x)))
+(test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] [y (in-value 3)]) #:break #false (add1 x)))
 (test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] [y (in-value 3)] #:break #false) (add1 x)))
 (test 2 'break-v2 (for*/fold ([x 0]) ([x '(1)] #:break #false [y (in-value 3)]) (add1 x)))
 
@@ -1519,6 +1519,111 @@
         [_ #f])))
 
   (for ([i (in-digits 12)]) i))
+
+;; ----------------------------------------
+;; Check more fold variables in outermost iteration clauses
+
+(test '(3 2 1)
+      'for/fold-var-in-outermost
+      (let ([a '(1 2 3)])
+        (for/fold ([a '()])
+                  ([x (in-list a)])
+          (cons x a))))
+
+(test '(1 2 3)
+      'for/fold-var-in-outermost/result
+      (let ([a '(1 2 3)])
+        (for/fold ([a '()]
+                   #:result (reverse a))
+                  ([x (in-list a)])
+          (cons x a))))
+
+(test '(1 2 3)
+      'for/foldr-var-in-outermost
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()])
+                   ([x (in-list a)])
+          (cons x a))))
+
+(test '(3 2 1)
+      'for/foldr-var-in-outermost/result
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()]
+                    #:result (reverse a))
+                   ([x (in-list a)])
+          (cons x a))))
+
+(test '(1 2 3)
+      'for/foldr-var-in-outermost/delay
+      (let ([a '(1 2 3)])
+        (force
+         (for/foldr ([a '()]
+                     #:delay)
+                    ([x (in-list a)])
+           (cons x (force a))))))
+
+(test '(3 2 1)
+      'for/foldr-var-in-outermost/delay/result
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()]
+                    #:delay
+                    #:result (reverse (force a)))
+                   ([x (in-list a)])
+          (cons x (force a)))))
+
+(test '()
+      'for/fold-var-not-in-outermost
+      (let ([a '(1 2 3)])
+        (for/fold ([a '()])
+                  (#:when #t
+                   [x (in-list a)])
+          (cons x a))))
+
+(test '()
+      'for/fold-var-not-in-outermost/result
+      (let ([a '(1 2 3)])
+        (for/fold ([a '()]
+                   #:result (reverse a))
+                  (#:when #t
+                   [x (in-list a)])
+          (cons x a))))
+
+(test '(1 2 3)
+      'for/foldr-var-not-in-outermost
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()])
+                   (#:when #t
+                    [x (in-list a)])
+          (cons x a))))
+
+(test '(3 2 1)
+      'for/foldr-var-not-in-outermost/result
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()]
+                    #:result (reverse a))
+                   (#:when #t
+                    [x (in-list a)])
+          (cons x a))))
+
+(test '(1 2 3)
+      'for/foldr-var-not-in-outermost/delay
+      (let ([a '(1 2 3)])
+        (force
+         (for/foldr ([a '()]
+                     #:delay)
+                    (#:when #t
+                     [x (in-list a)])
+           (cons x (force a))))))
+
+(test '(3 2 1)
+      'for/foldr-var-not-in-outermost/delay/result
+      (let ([a '(1 2 3)])
+        (for/foldr ([a '()]
+                    #:delay
+                    #:result (reverse (force a)))
+                   (#:when #t
+                    [x (in-list a)])
+          (cons x (force a)))))
 
 ;; ----------------------------------------
 