d6e4a6ad47cb64b179bf9e1d5272a1746593ec19
John Clements <clements@racket-lang.org>
2024-04-13 06:11:33 +0000

    Post-release version for the v8.13 release

--- pkgs/base/info.rkt
+++ pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.12.0.14")
+(define version "8.13.0.1")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))
