93064b3ac61f1f7c693c2c77d3e940e19925fd10
Jörgen Brandt <joergen@cuneiform-lang.org>
2024-11-27 22:51:08 +0100

    cover the case where (empty-tag-shorthand) is 'always or 'never

--- pkgs/racket-test/tests/xml/test-xexpr.rkt
+++ pkgs/racket-test/tests/xml/test-xexpr.rkt
@@ -16,5 +16,41 @@
      (call-with-output-string
       (lambda (out)
         (write-xexpr xe out)))
-     "<doc><short/><alsoShort/></doc>")))
+     "<doc><short/><alsoShort/></doc>"))
+
+  (parameterize ([empty-tag-shorthand '(short)])
+    (check-equal?
+     (call-with-output-string
+      (lambda (out)
+        (write-xexpr xe out)))
+     "<doc><short/><alsoShort></alsoShort></doc>"))
+
+  (parameterize ([empty-tag-shorthand '(alsoShort)])
+    (check-equal?
+     (call-with-output-string
+      (lambda (out)
+        (write-xexpr xe out)))
+     "<doc><short></short><alsoShort/></doc>"))
+
+  (parameterize ([empty-tag-shorthand '()])
+    (check-equal?
+     (call-with-output-string
+      (lambda (out)
+        (write-xexpr xe out)))
+     "<doc><short></short><alsoShort></alsoShort></doc>"))
+
+  (parameterize ([empty-tag-shorthand 'always])
+    (check-equal?
+     (call-with-output-string
+      (lambda (out)
+        (write-xexpr xe out)))
+     "<doc><short/><alsoShort/></doc>"))
+
+  (parameterize ([empty-tag-shorthand 'never])
+    (check-equal?
+     (call-with-output-string
+      (lambda (out)
+        (write-xexpr xe out)))
+     "<doc><short></short><alsoShort></alsoShort></doc>")))
+
     
