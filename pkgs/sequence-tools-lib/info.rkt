b9f1ea6809055e0016ec97410e5e9e1e1c45fc1d
jocyber <jordaneharman@protonmail.com>
2025-03-01 19:38:54 -0600

    Add sequence-tools package
    Introduces a new package that will contain additional sequence bindings (including lists)
    to improve the functional aspect of the language.

---
+++ pkgs/sequence-tools-lib/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+(define pkg-authors '(jocyber))
+(define version "1.0")
+(define license '(Apache-2.0 OR MIT))
+(define pkg-desc "Implementation of sequence-tools")
+
+(define deps '("base"))
+(define build-deps '("rackunit-lib"))
+
