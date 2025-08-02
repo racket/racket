racket/racket

commit 8c586f93f62c2ceaff01b7017f773abe70fa12a2

FFI: revise pointer implementation
Use the new `ftype-scheme-object-pointer`, `ftype-any-ref`,
`ftype-any-set!`, etc., facilities from Chez Scheme. This change makes
`ptr-ref` and `pre-set!` significantly faster, and it reduces the
overhead of foreign-function calls.

pkgs/racket-test-core/tests/racket/foreign-test.rktl
@@ -741,11 +741,9 @@
   (define p (cast (ptr-add (malloc 10) 5) _pointer _thing-pointer))
   (test #t cpointer-gcable? p)
   (define q (cast p _thing-pointer _stuff-pointer))
-  (test (cast p _pointer _intptr)
-        cast q _pointer _intptr)
+  (test #t ptr-equal? p q)
   (collect-garbage)
-  (test (cast p _thing-pointer _intptr)
-        cast q _stuff-pointer _intptr))
+  (test #t ptr-equal? p q))
 
 ;; For casts where the BC output might share with the input, so
 ;; an offset pointer needs to be 'atomic-interior

racket/src/cs/primitive/foreign.ss
@@ -1,25 +1,25 @@
 
 (define-primitive-table foreign-table
   [_bool (known-ctype 'boolean)]
-  [_bytes (known-ctype 'uptr)]
+  [_bytes (known-ctype 'ftype-pointer)]
   [_double (known-ctype 'double)]
   [_double* (known-ctype 'double)]
   [_fixint (known-ctype 'integer-32)]
   [_fixnum (known-ctype 'fixnum)]
   [_float (known-ctype 'float)]
-  [_fpointer (known-ctype 'void*)]
-  [_gcpointer (known-ctype 'void*)]
+  [_fpointer (known-ctype 'ftype-pointer)]
+  [_gcpointer (known-ctype 'ftype-scheme-object-pointer)]
   [_int16 (known-ctype 'integer-16)]
   [_int32 (known-ctype 'integer-32)]
   [_int64 (known-ctype 'integer-64)]
   [_int8 (known-ctype 'integer-8)]
   [_longdouble (known-ctype 'double)]
-  [_path (known-ctype 'uptr)]
-  [_pointer (known-ctype 'void*)]
+  [_path (known-ctype 'ftype-pointer)]
+  [_pointer (known-ctype 'ftype-pointer)]
   [_scheme (known-ctype 'scheme-object)]
   [_stdbool (known-ctype 'integer-8)]
-  [_string/ucs-4 (known-ctype 'uptr)]
-  [_string/utf-16 (known-ctype 'uptr)]
+  [_string/ucs-4 (known-ctype 'ftype-pointer)]
+  [_string/utf-16 (known-ctype 'ftype-pointer)]
   [_symbol (known-ctype 'string)]
   [_ufixint (known-ctype 'unsigned-32)]
   [_ufixnum (known-ctype 'fixnum)]

Makefile
@@ -112,7 +112,7 @@ CFLAGS_FOR_BUILD =
 
 # This branch name must be changed each time the pb boot files are
 # updated:
-PB_BRANCH = v10.2.0-pre-release.1-1
+PB_BRANCH = v10.2.0-pre-release.2-1
 PB_REPO = https://github.com/racket/pb
 
 # Set to empty for Git before v1.7.10:

main.zuo
@@ -133,10 +133,11 @@
        (shell/wait "git checkout -q" branch
                    (hash 'dir pb-dir))]
       [(eq? step 'build)
-       (define scheme (let ([s (lookup 'SCHEME)])
-                        (if (equal? s "")
-                            (find-executable-path "scheme")
-                            s)))
+       (define scheme (and (not (lookup 'RKTBOOT #f))
+                           (let ([s (lookup 'SCHEME)])
+                             (if (equal? s "")
+                                 (find-executable-path "scheme")
+                                 s))))
        (cond
          [scheme
           (define reboot (dynamic-require "racket/src/ChezScheme/s/reboot.zuo" 'reboot))

pkgs/base/info.rkt
@@ -14,7 +14,7 @@
 
 ;; In the Racket source repo, this version should change exactly when
 ;; "racket_version.h" changes:
-(define version "8.15.0.10")
+(define version "8.15.0.11")
 
 (define deps `("racket-lib"
                ["racket" #:version ,version]))

racket/collects/ffi/unsafe.rkt
@@ -1530,7 +1530,6 @@
         ;; done converting:
         (void/reference-sink p)
         v)))
-  
   (cond
    [(and (cpointer? p)
          (cpointer-gcable? p))

racket/src/cs/rumble/memory.ss
@@ -592,9 +592,12 @@
 ;; accommodates a limitation of the traditional Racket implementation
 (define (run-one-collect-callback v save sel)
   (let ([protocol (#%vector-ref v 0)]
-        [proc (cpointer-address (#%vector-ref v 1))]
+        [proc (ftype-pointer-address (cptr->fptr 'collect-callback (#%vector-ref v 1)))]
         [ptr (lambda (i)
-               (cpointer*-address (#%vector-ref v (fx+ 2 i))))]
+               (let ([n (#%vector-ref v (fx+ 2 i))])
+                 (if (integer? n)
+                     n
+                     (ftype-pointer-address (cptr->fptr 'collect-callback n)))))]
         [val (lambda (i)
                (#%vector-ref v (fx+ 2 i)))])
     (case protocol

commit e7295180aa5cfeafcc6b1b9082382422abbeb3d9

CS ffi: repair some immobile cell operations
An immobile cell's addresss might flow into a place that claims not to
be GCable and then back to `free-immobile-cell`, which did not handle
those pointers correctly. This became broken with
8c586f93f62c2ceaff01b7017f773abe70fa12a2.

The JPEG encoder ran into this problem, but the main effect is an
extremely slow leak.

Also, clean up locking of struct results from foreign calls, which are
not necessarily allocated as GCable.

racket/src/cs/rumble/foreign.ss
@@ -1281,10 +1281,12 @@
     (foreign-free (ftype-pointer-address p))))
 
 (define/who (lock-cpointer p)
-  (lock-object (ftype-scheme-object-pointer-object p)))
+  (when (ftype-scheme-object-pointer? p)
+    (lock-object (ftype-scheme-object-pointer-object p))))
 
 (define/who (unlock-cpointer p)
-  (unlock-object (ftype-scheme-object-pointer-object p)))
+  (when (ftype-scheme-object-pointer? p)
+    (unlock-object (ftype-scheme-object-pointer-object p))))
 
 (define-record-type (cpointer/cell make-cpointer/cell cpointer/cell?)
   (parent cpointer)
@@ -1299,12 +1301,23 @@
      (eq-hashtable-set! immobile-cells vec #t))
     (fptr->cptr (make-ftype-scheme-object-pointer vec))))
 
+(define (cptr->immobile-cell-object who b)
+  (let ([f (cptr->fptr who b)])
+    (cond
+      [(ftype-scheme-object-pointer? f) (ftype-scheme-object-pointer-object f)]
+      [else
+       ;; an immobile cell pointer might go through a path that doesn't claim the
+       ;; pointer as GCable, since that's one of the main roles of an immobile
+       ;; cell; in that case, we need to cast
+       (reference-address->object (ftype-pointer-address f))])))
+
 (define/who (free-immobile-cell b)
-  (with-global-lock
-   (eq-hashtable-delete! immobile-cells (ftype-scheme-object-pointer-object (cptr->fptr who b)))))
+  (let ([obj (cptr->immobile-cell-object who b)])
+    (with-global-lock
+     (eq-hashtable-delete! immobile-cells obj))))
 
 (define/who (immobile-cell-ref b)
-  (bytevector-reference-ref  (ftype-scheme-object-pointer-object (cptr->fptr who b)) 0))
+  (bytevector-reference-ref (cptr->immobile-cell-object who b) 0))
 
 (define (immobile-cell->address b)
   (ftype-pointer-address (cpointer-fptr b)))

pkgs/racket-test-core/tests/racket/foreign-test.rktl
@@ -783,6 +783,14 @@
                                 _bytes
                                 _pointer))))
 
+;; test immobile cells
+(for ([free-via-non-gcable? (in-list '(#f #t))])
+  (let ([b (malloc-immobile-cell 'hello)])
+    (test 'hello ptr-ref b _racket)
+    (define b/non-gcable (cast (cast b _pointer _intptr) _intptr _pointer))
+    (test 'hello ptr-ref b/non-gcable _racket)
+    (free-immobile-cell (if free-via-non-gcable? b/non-gcable b))))
+
 ;; test 'interior allocation mode
 (let ()
   ;; Example by Ron Garcia