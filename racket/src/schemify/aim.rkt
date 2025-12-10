25a4d551f0b114e98570b8872eebb84ffdcc0542
Jens Axel Søgaard <jensaxel@soegaard.net>
2025-12-10 16:12:35 +0100

    Include 'system in keyword list
    Leaving `system` out of the keyword list happens to work, since `system` is handled in the last clause. My guess is that `system` wasn't left out on purpose though.

--- racket/src/schemify/aim.rkt
+++ racket/src/schemify/aim.rkt
@@ -3,7 +3,7 @@
 
 ;; macro statically ensures that the second argument is a valid target
 (define-syntax aim?
-  (syntax-rules (cify interp)
+  (syntax-rules (cify interp system)
     [(_ e 'cify) (eq? e 'cify)]
     [(_ e 'interp) (eq? e 'interp)]
     [(_ e 'system) (eq? e 'system)]))
