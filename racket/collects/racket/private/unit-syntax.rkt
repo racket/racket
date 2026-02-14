dc14be217501e98d5c18d05286ca389c562e76e8
Jade Sailor <jgitpublic.45358@sai9.net>
2026-02-14 10:37:40 -0500

    Add comments to (most) `private` modules used by main-distribution packages
    This patch is part of an effort to clarify what in `racket/private`
    is intended to be used (or is used) by other first-party packages.
    The overall plan is:
    
    (1) Change first-party packages to use public APIs when possible.
    
    (2) For any packages which have racket/private dependencies which are
    part of racket/base's implementation, make a new
    `racket/private/for-thatpackage` module which provides only the appropriate
    chunks.
    
    (3) For packages which have dependencies on racket/private which are not
    part of racket/base's implementation, add a comment to the appropriate
    file noting that it's used by the other package.
    
    
    This patch implements part (3), adding comments, except for
    racket/collects/racket/private/class-c-old.rkt, which currently has pending
    edits on a long-lived branch; comments can be added to it in a later commit.
    (See https://github.com/racket/racket/pull/5425#issuecomment-3902063247)

--- racket/collects/racket/private/unit-syntax.rkt
+++ racket/collects/racket/private/unit-syntax.rkt
@@ -1,7 +1,14 @@
 #lang racket/base
 
-;; This module exists for backwards compatibility.
-;; It is used by mzlib/a-signature from compability-lib.
+;;----------------------------------------------------------------------
+;; Internals needed by the `compatability` package; used to implement
+;; `mzlib/a-signature`
+;;
+;; Note that this module only semi-private, in the sense that `compatability-lib`
+;; lives in another repository. This means any backwards-compatable API
+;; changes need coordination. Try to avoid adding new exports to this file;
+;; instead, consider whether a public API is appropriate.
+
 (require racket/syntax
          "unit/exptime/util.rkt")
 (provide (rename-out [current-syntax-context error-syntax])
