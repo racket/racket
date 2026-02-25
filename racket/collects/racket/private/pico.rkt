6947517296f72d1ae8cf6e7f0c85ad242199923e
Jade Sailor <jgitpublic.45358@sai9.net>
2026-02-25 10:04:08 -0500

    racket/private/pico: skeleton module
    New module which will serve as a convenient place to put a couple
    functions which can easily be written in kernel, and which can be
    combined in a single cross-phase-persistent module for performance reasons.

---
+++ racket/collects/racket/private/pico.rkt
@@ -0,0 +1,15 @@
+(module pico '#%kernel
+
+  (#%declare #:cross-phase-persistent)
+
+  ; ======================================================================
+  ; Various functions which are straightforward to write in kernel,
+  ; merged into a single cross-phase-persistent module for load time reasons.
+  ; ======================================================================
+  ;
+
+
+  
+  ;
+  ; --------------------------------------------------
+  )
