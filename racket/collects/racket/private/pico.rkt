13db717da3e6b3d59324d6125fd0ce727bfe3cf5
Jade Sailor <jgitpublic.45358@sai9.net>
2026-02-25 11:46:53 -0500

    Merge member.rkt into pico.rkt
    Move `member` and `memw` from member.rkt into pico.rkt, with no changes.
    Delete the now-empty `member.rkt` and adjust requires from other files
    appropriately.
    
    Testing: grep the repo for `member.rkt`; grep a package catalog archive for
    `racket/private/member`; code review

--- racket/collects/racket/private/pico.rkt
+++ racket/collects/racket/private/pico.rkt
@@ -2,14 +2,60 @@
 
   (#%declare #:cross-phase-persistent)
 
+  (#%provide member memw)
+
   ; ======================================================================
   ; Various functions which are straightforward to write in kernel,
   ; merged into a single cross-phase-persistent module for load time reasons.
   ; ======================================================================
   ;
 
+  ; --------------------------------------------------
+  ; member and friends
 
-  
+  (define-values (bad-list)
+    (λ (who orig-l)
+      (raise-arguments-error who "not a proper list"
+                             "in" orig-l)))
+
+  (define-values (member-impl)
+    (lambda (who v orig-l eql?)
+      (define-values (loop)
+        (lambda (ls turtle)
+          (if (null? ls)
+              #f
+              (if (not (pair? ls))
+                  (bad-list who orig-l)
+                  (if (eql? v (car ls))
+                      ls
+                      (let-values ([(ls) (cdr ls)])
+                        (if (null? ls)
+                            #f
+                            (if (if (not (pair? ls)) #t (eq? ls turtle))
+                                (bad-list who orig-l)
+                                (if (eql? v (car ls))
+                                    ls
+                                    (loop (cdr ls) (cdr turtle)))))))))))
+      (loop orig-l orig-l)))
+
+  (define-values (member)
+    (case-lambda
+        [(v ls)
+         (member-impl 'member v ls equal?)]
+        [(v ls eql?)
+         (if (if (procedure? eql?)
+                 (procedure-arity-includes? eql? 2)
+                 #f)
+             (void)
+             (raise-argument-error 'member
+                                   "(procedure-arity-includes/c 2)"
+                                   eql?))
+         (member-impl 'member v ls eql?)]))
+
+  (define-values (memw)
+    (lambda (v ls)
+      (member-impl 'memw v ls equal-always?)))
+
   ;
   ; --------------------------------------------------
   )
