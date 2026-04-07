0bedfe40f807a85fe28e262d6188364387ae475c
Jade Sailor <jgitpublic.45358@sai9.net>
2026-04-07 13:43:03 -0400

    racket/base: kernel syntax impl. of define-values-for-syntax
    Replace the `define-values-for-syntax` implementation with one using only
    kernel syntax, eliminating many requires from `racket/private/define.rkt`.
    
    This is part of a larger effort to centralize and modernize some core syntax
    constructs; see https://github.com/racket/racket/pull/5406 for more on that.

--- racket/collects/racket/private/define.rkt
+++ racket/collects/racket/private/define.rkt
@@ -3,9 +3,7 @@
 ;; #%define : define and define-syntax
 
 (module define '#%kernel
-  (#%require (for-syntax '#%kernel 
-                         "letstx-scheme.rkt" "stxcase-scheme.rkt" "stx.rkt" "qqstx.rkt"
-                         "norm-define.rkt")
+  (#%require (for-syntax '#%kernel "stx.rkt")
              "define-et-al.rkt")
 
   (#%provide define 
@@ -15,13 +13,25 @@
 
   (define-syntaxes (define-values-for-syntax)
     (lambda (stx)
-      (syntax-case stx ()
-        [(_ (id ...) expr)
-         (begin
-           (for-each (lambda (x)
-                       (unless (identifier? x)
-                         (raise-syntax-error #f "not an identifier" x stx)))
-                     (syntax->list #'(id ...)))
-           #'(begin-for-syntax
-               (define-values (id ...) expr)))])))
-  )
+      (raise-syntax-error-if (identifier? stx) "bad syntax" stx)
+      (define-values (lst) (syntax->list stx))
+      (raise-syntax-error-unless lst "bad syntax (illegal use of `.')" stx)
+      (raise-syntax-error-if (null? (cdr lst)) "bad syntax (missing names to define)" stx)
+      (define-values (ids) (syntax->list (cadr lst)))
+      (raise-syntax-error-unless ids
+                                 "bad syntax (expected a list of identifiers)"
+                                 stx
+                                 (cadr lst))
+      (for-each (lambda (id)
+                  (raise-syntax-error-unless (identifier? id) "not an identifier" stx id))
+                ids)
+      ; TODO(jsailor): April 2026-ish: uncomment when raise-if-duplicate-identifiers is committed
+      ;(raise-if-duplicate-identifiers "duplicate identifier" stx ids)
+      (raise-syntax-error-if (null? (cddr lst)) "bad syntax (missing expression after identifiers)" stx)
+      (raise-syntax-error-if (pair? (cdddr lst)) "bad syntax (multiple expressions after identifiers)" stx)
+      (datum->syntax #f
+                     (list (quote-syntax begin-for-syntax)
+                           (datum->syntax #f
+                                          (list* (quote-syntax define-values) (cdr lst))
+                                          stx))
+                     stx))))
